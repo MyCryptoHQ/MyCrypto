37e8c0817e9bc05da39b463db0996060ae88da49
Daniel Ternyak <dternyak@gmail.com>
2017-04-14 01:24:16 -0500

    add config reducers to RootReducer/CombineReducer

--- common/reducers/index.js
+++ common/reducers/index.js
@@ -3,14 +3,17 @@ import * as inbox from './inbox'
 import * as auth from './auth'
 import * as loginCR from './loginCR'
 import * as dashboard from './dashboard'
+import * as config from './config'
 
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux'
+
 export default combineReducers({
     ...layout,
     ...inbox,
     ...auth,
     ...dashboard,
     ...loginCR,
+    ...config,
     routing: routerReducer
 })
