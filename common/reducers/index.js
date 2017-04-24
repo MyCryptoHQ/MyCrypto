270c4911c8b8e9824b57d286368dd83360a64c29
Daniel Ternyak <dternyak@gmail.com>
2017-04-24 18:59:57 -0500

    Remove unneeded reducer imports.

--- common/reducers/index.js
+++ common/reducers/index.js
@@ -1,22 +1,10 @@
-import * as layout from './layout';
-import * as inbox from './inbox'
-import * as auth from './auth'
-import * as loginCR from './loginCR'
-import * as dashboard from './dashboard'
 import * as generateWallet from './generateWallet'
 import * as config from './config'
-
 import { reducer as formReducer } from 'redux-form'
-
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux'
 
 export default combineReducers({
-    ...layout,
-    ...inbox,
-    ...auth,
-    ...dashboard,
-    ...loginCR,
     ...generateWallet,
     ...config,
     form: formReducer,
