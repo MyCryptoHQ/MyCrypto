eb658ec9ff59561f550f9a60bde78b85907a23dc
Daniel Ternyak <dternyak@gmail.com>
2017-04-24 18:58:48 -0500

    Refactor/remove commented out codes.

--- common/routing/index.jsx
+++ common/routing/index.jsx
@@ -1,19 +1,12 @@
-import React from 'react';
-import {Route, Redirect, browserHistory, IndexRoute} from 'react-router';
-import {useBasename} from 'history'
-import {App, Tabs} from 'containers';
+import React from "react";
+import {browserHistory, Redirect, Route} from "react-router";
+import {useBasename} from "history";
+import {App, Tabs} from "containers";
 
 export const history = getHistory()
 
-/**
- * Returns application routing with protected by AuthCheck func routes
- * @param {Function} AuthCheck checks is user logged in
- */
-export const Routing = (AuthCheck) => (
+export const Routing = () => (
     <Route name="App" path='' component={App}>
-        {/*<IndexRoute name="Login" component={Login}/>*/}
-        {/*<Route name="Login" path="/auth" component={Login}/>*/}
-        {/*<Route name="Inbox" path="/inbox"  component={Inbox}/>*/}
         <Route name="Tabs" path="/" component={Tabs}/>
         <Redirect from="/*" to="/"/>
     </Route>
