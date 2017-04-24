67c4b826f5f46730ade6b46649ab0e9fbb4ffe86
Daniel Ternyak <dternyak@gmail.com>
2017-04-24 18:59:28 -0500

    Add Flow types.

--- common/actions/config.js
+++ common/actions/config.js
@@ -1,24 +1,22 @@
+// @flow
 export const CONFIG_LANGUAGE_CHANGE = 'CONFIG_LANGUAGE_CHANGE';
 export const CONFIG_LANGUAGE_DROPDOWN_TOGGLE = 'CONFIG_LANGUAGE_DROPDOWN_TOGGLE';
 export const CONFIG_NODE_CHANGE = 'CONFIG_NODE_CHANGE';
 export const CONFIG_NODE_DROPDOWN_TOGGLE = 'CONFIG_NODE_DROPDOWN_TOGGLE';
 
 
-export const CHANGE_LANGUAGE = (index) => ({
+export const CHANGE_LANGUAGE = (index: number) => Object({
     type: CONFIG_LANGUAGE_CHANGE, index: index
 })
 
-
-export const TOGGLE_LANGUAGE_DROPDOWN = () => ({
+export const TOGGLE_LANGUAGE_DROPDOWN = () => Object({
     type: CONFIG_LANGUAGE_DROPDOWN_TOGGLE
 })
 
-
-export const CHANGE_NODE = (index) => ({
+export const CHANGE_NODE = (index: number) => Object({
     type: CONFIG_NODE_CHANGE, index: index
 })
 
-
-export const TOGGLE_NODE_DROPDOWN = () => ({
+export const TOGGLE_NODE_DROPDOWN = () => Object({
     type: CONFIG_NODE_DROPDOWN_TOGGLE
 })
