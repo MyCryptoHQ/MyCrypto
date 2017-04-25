fa18b408cd27780a8eaaad2f74469fbd6e57acdb
Daniel Ternyak <dternyak@gmail.com>
2017-04-24 19:03:08 -0500

    Remove authCheck dead code.

--- common/components/Root/index.jsx
+++ common/components/Root/index.jsx
@@ -10,24 +10,13 @@ export default class Root extends Component {
         routes: PropTypes.func
     };
 
-    authCheck(nextState, replace, callback) {
-        let {store} = this.props
-        let {loggedIn} = store.getState().auth
-        let authPath = '/auth'
-        if (!loggedIn) {
-            replace(authPath)
-        }
-
-        callback()
-    }
-
     render() {
         const {store, history, routes} = this.props;
         // key={Math.random()} = hack for HMR from https://github.com/webpack/webpack-dev-server/issues/395
         return (
             <Provider store={store} key={Math.random()}>
                 <Router history={history} key={Math.random()}>
-                    {routes(::this.authCheck)}
+                    {routes()}
                 </Router>
             </Provider>
         );
