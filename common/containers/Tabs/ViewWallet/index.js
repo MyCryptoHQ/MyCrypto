e23409cbac64b69828ab0f64f4c4f8106a52ed45
Daniel Ternyak <dternyak@gmail.com>
2017-04-26 23:02:03 -0500

    better styling on ViewWallet view

--- common/containers/Tabs/ViewWallet/index.js
+++ common/containers/Tabs/ViewWallet/index.js
@@ -7,9 +7,11 @@ export default class ViewWallet extends Component {
 
     render() {
         return (
-            <div style={{marginTop: '50px'}}>
-                <h1 >View Wallet</h1>
-            </div>
+            <section className="container" style={{minHeight: '50%'}}>
+                <div className="tab-content">
+                    <h1>View Wallet</h1>
+                </div>
+            </section>
         )
     }
 }
