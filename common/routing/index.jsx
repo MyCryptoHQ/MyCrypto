f8e9906b371961349bc243ead03719bad4db52aa
Daniel Kaspo <danielkaspo@gmail.com>
2017-05-31 19:12:41 -0400

    Added and marked up wallet decrypt component.
    Still needs lots of work at this point, but all the visuals are done,
    it's been renamed to what Taylor said it should be called, and all the
    Aria labels have been properly updated.

--- common/routing/index.jsx
+++ common/routing/index.jsx
@@ -3,7 +3,7 @@ import {browserHistory, Redirect, Route} from 'react-router';
 import {useBasename} from 'history';
 import {App} from 'containers';
 import GenerateWallet from 'containers/Tabs/GenerateWallet'
-import ViewWallet from 'containers/Tabs/ViewWallet'
+import WalletDecrypt from 'containers/Tabs/WalletDecrypt'
 import Help from 'containers/Tabs/Help'
 import Swap from 'containers/Tabs/Swap'
 
@@ -12,7 +12,7 @@ export const history = getHistory()
 export const Routing = () => (
     <Route name="App" path='' component={App}>
         <Route name="GenerateWallet" path="/" component={GenerateWallet}/>
-        <Route name="ViewWallet" path="/view-wallet" component={ViewWallet}/>
+        <Route name="WalletDecrypt" path="/view-wallet" component={WalletDecrypt}/>
         <Route name="Help" path="/help" component={Help}/>
         <Route name="Swap" path="/swap" component={Swap}/>
 
