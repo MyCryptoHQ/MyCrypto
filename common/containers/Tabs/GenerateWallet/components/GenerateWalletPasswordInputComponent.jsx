ff81133b5e128045628877324d5de9143086cba6
Daniel Ternyak <dternyak@gmail.com>
2017-06-20 01:14:23 -0500

    reformat

--- common/containers/Tabs/GenerateWallet/components/GenerateWalletPasswordInputComponent.jsx
+++ common/containers/Tabs/GenerateWallet/components/GenerateWalletPasswordInputComponent.jsx
@@ -1,40 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-
 export default class GenerateWalletPasswordInputComponent extends Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    static propTypes = {
-        generateWalletShowPassword: PropTypes.func,
-        showPassword: PropTypes.bool,
-        input: PropTypes.object,
-        meta: PropTypes.object
-    };
+  static propTypes = {
+    generateWalletShowPassword: PropTypes.func,
+    showPassword: PropTypes.bool,
+    input: PropTypes.object,
+    meta: PropTypes.object
+  };
 
-
-    render() {
-        return (
-            <div>
-                <div>
-                    <div className="input-group" style={{width: '100%'}}>
-                        <input {...this.props.input}
-                               name="password"
-                               className={this.props.meta.error ? 'form-control is-invalid' : 'form-control'}
-                               type={this.props.showPassword ? 'text' : 'password'}
-                               placeholder="Do NOT forget to save this!"
-                               aria-label="Enter a strong password (at least 9 characters)"/>
-                        <span
-                            onClick={() => this.props.generateWalletShowPassword()}
-                            aria-label="make password visible"
-                            role="button"
-                            className="input-group-addon eye"/>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div>
+        <div>
+          <div className="input-group" style={{ width: '100%' }}>
+            <input
+              {...this.props.input}
+              name="password"
+              className={
+                this.props.meta.error
+                  ? 'form-control is-invalid'
+                  : 'form-control'
+              }
+              type={this.props.showPassword ? 'text' : 'password'}
+              placeholder="Do NOT forget to save this!"
+              aria-label="Enter a strong password (at least 9 characters)"
+            />
+            <span
+              onClick={() => this.props.generateWalletShowPassword()}
+              aria-label="make password visible"
+              role="button"
+              className="input-group-addon eye"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
