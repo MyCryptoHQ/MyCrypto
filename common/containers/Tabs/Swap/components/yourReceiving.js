28415cd0e8bbdd5c4584e7e04ce5c1f48cd3b79d
Daniel Ternyak <dternyak@gmail.com>
2017-06-24 01:09:44 -0500

    Consume new receivingAddressSwap action creator and receivingAddress state, validate receiving address input using Validator class.

--- common/containers/Tabs/Swap/components/yourReceiving.js
+++ common/containers/Tabs/Swap/components/yourReceiving.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { DONATION_ADDRESSES_MAP } from 'config/data';
+import Validator from 'libs/validator';
 
 export default class YourReceiving extends Component {
   constructor(props) {
     super(props);
+    this.validator = new Validator();
+    this.state = {
+      validAddress: false
+    };
   }
 
   static propTypes = {
-    destinationKind: PropTypes.string.isRequired
+    destinationKind: PropTypes.string.isRequired,
+    receivingAddressSwap: PropTypes.func.isRequired,
+    receivingAddress: PropTypes.string
+  };
+
+  onChangeReceivingAddress = event => {
+    const value = event.target.value;
+    this.props.receivingAddressSwap(value);
+    let validAddress;
+    // TODO - find better pattern here once currencies move beyond BTC, ETH, REP
+    if (this.props.destinationKind === 'BTC') {
+      validAddress = this.validator.isValidBTCAddress(value);
+    } else {
+      validAddress = this.validator.isValidETHAddress(value);
+    }
+    this.setState({ validAddress });
   };
 
   render() {
-    const { destinationKind } = this.props;
+    const { destinationKind, receivingAddress } = this.props;
+    const { validAddress } = this.state;
     return (
       <article className="swap-start">
         <section className="swap-address block">
@@ -23,16 +44,20 @@ export default class YourReceiving extends Component {
                 <strong> ({destinationKind})</strong>
               </label>
               <input
-                className="form-control"
+                className={`form-control ${validAddress
+                  ? 'is-valid'
+                  : 'is-invalid'}`}
                 type="text"
+                value={receivingAddress}
+                onChange={this.onChangeReceivingAddress}
                 placeholder={DONATION_ADDRESSES_MAP[destinationKind]}
               />
             </div>
           </section>
           <section className="row text-center">
-            <a className="btn btn-primary btn-lg">
+            <button disabled={!validAddress} className="btn btn-primary btn-lg">
               <span>Start Swap</span>
-            </a>
+            </button>
           </section>
         </section>
       </article>
