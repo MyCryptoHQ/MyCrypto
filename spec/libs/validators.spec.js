6fce9029c5dd3dbc1d071f959cf82ac4a228df00
crptm <crptm@users.noreply.github.com>
2017-06-27 03:38:45 +0400

    fix spec

---
+++ spec/libs/validators.spec.js
@@ -0,0 +1,18 @@
+import { isValidBTCAddress, isValidETHAddress } from '../../common/libs/validators';
+import { DONATION_ADDRESSES_MAP } from '../../common/config/data';
+
+describe('Validator', () => {
+    it('should validate correct BTC address as true', () => {
+        expect(isValidBTCAddress(DONATION_ADDRESSES_MAP.BTC)).toBeTruthy();
+    });
+    it('should validate incorrect BTC address as false', () => {
+        expect(isValidBTCAddress('nonsense' + DONATION_ADDRESSES_MAP.BTC + 'nonsense')).toBeFalsy();
+    });
+
+    it('should validate correct ETH address as true', () => {
+        expect(isValidETHAddress(DONATION_ADDRESSES_MAP.ETH)).toBeTruthy();
+    });
+    it('should validate incorrect ETH address as false', () => {
+        expect(isValidETHAddress('nonsense' + DONATION_ADDRESSES_MAP.ETH + 'nonsense')).toBeFalsy();
+    });
+});
