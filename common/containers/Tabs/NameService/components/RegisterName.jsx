303af01c719d8a356c9df9af20c63b2c005a5c3c
Will O'Beirne <wbobeirne@gmail.com>
2017-07-17 20:56:22 -0400

    Check in work so far, putting on ice for now.

--- common/containers/Tabs/NameService/components/RegisterName.jsx
+++ common/containers/Tabs/NameService/components/RegisterName.jsx
@@ -3,13 +3,36 @@ import PropTypes from 'prop-types';
 import './RegisterName.scss';
 
 export default class RegisterName extends React.Component {
+  state = {
+    desiredName: ''
+  };
+
+  _handleSubmit = ev => {
+    ev.preventDefault();
+  };
+
+  _handleInput = ev => {
+    this.setState({
+      desiredName: ev.target.value.toLowerCase()
+    });
+  };
+
   render() {
+    const { desiredName } = this.state;
+
     return (
       <div className="RegisterName">
-        <form className="RegisterName-nameform">
-          <div className="RegisterName-nameform-inputs input-group">
-            <input className="form-control" placeholder="myetherwallet" />
-            <span className="input-group-addon">.eth</span>
+        <form className="RegisterName-nameform" onSubmit={this._handleSubmit}>
+          <div className="RegisterName-nameform-inputs">
+            <div className="input-group">
+              <input
+                className="form-control"
+                placeholder="myetherwallet"
+                value={desiredName}
+                onChange={this._handleInput}
+              />
+              <span className="input-group-addon">.eth</span>
+            </div>
           </div>
 
           <button className="RegisterName-nameform-submit btn btn-primary">
