c8a8b4aa022b8c11ae80f68cc6fb209c6f09537b
danny <skubakdj@gmail.com>
2017-07-18 22:41:13 -0400

    add styling

--- common/components/Header/components/gasPriceDropdown.jsx
+++ common/components/Header/components/gasPriceDropdown.jsx
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import './GasPriceDropdown.scss';
 
 export default class gasPriceDropdown extends Component {
   constructor(props) {
@@ -19,52 +20,70 @@ export default class gasPriceDropdown extends Component {
     changeGasPrice: PropTypes.func
   };
 
-  /* TODO: actually style this widget :) */
-
   render() {
+    // col-xs-4 applies padding not observed in MEWv3, this overrides
+    const paddingReset = { paddingLeft: '0px', paddingRight: '0px' };
+
     return (
-
-
-    <span className="dropdown" style={{color: 'black'}}>
-      <a
-        aria-haspopup="true"
-        aria-label="adjust gas price"
-        className="dropdown-toggle btn btn-white"
-        onClick={this.toggleExpanded}
-      >
-        <span>Gas Price</span>: {this.props.gasPriceGwei} Gwei
-        <i className="caret"></i>
-      </a>
-      {this.state.expanded &&
-        <ul className="dropdown-menu">
-          <div className="header--gas">
-            <span>Gas Price</span>: {this.props.gasPriceGwei} Gwei
-            <input
-              type="range"
-              min={this.props.gasPriceMinGwei}
-              max={this.props.gasPriceMaxGwei}
-              onChange={this.updateGasPrice}
-            />
-            <p className="small col-xs-4 text-left">Not So Fast</p>
-            <p className="small col-xs-4 text-center">Fast</p>
-            <p className="small col-xs-4 text-right">Fast AF</p>
-            <p className="small" style={{'whiteSpace':'normal', 'fontWeight': '300', 'margin':'2rem 0 0'}}>
-              Gas Price is the amount you pay per unit of gas. TX fee = gas price * gas limit & is paid to miners for including your TX in a block. Higher the gas price = faster transaction, but more expensive. Default is 21 GWEI.
-            </p>
-            <p>
-              <a
-                href="https://myetherwallet.groovehq.com/knowledge_base/topics/what-is-gas"
-                target="_blank"
-                style={{color: 'black'}}
+      <span className="dropdown">
+        <a
+          aria-haspopup="true"
+          aria-label="adjust gas price"
+          className="dropdown-toggle"
+          onClick={this.toggleExpanded}
+        >
+          <span>Gas Price</span>: {this.props.gasPriceGwei} Gwei
+          <i className="caret" />
+        </a>
+        {this.state.expanded &&
+          <ul
+            className="dropdown-menu"
+            style={{ padding: '.5rem', minWidth: '300px' }}
+          >
+            <div className="GasPrice-header">
+              <span>Gas Price</span>: {this.props.gasPriceGwei} Gwei
+              <input
+                type="range"
+                value={this.props.gasPriceGwei}
+                min={this.props.gasPriceMinGwei}
+                max={this.props.gasPriceMaxGwei}
+                onChange={this.updateGasPrice}
+              />
+              <p style={paddingReset} className="small col-xs-4 text-left">
+                Not So Fast
+              </p>
+              <p style={paddingReset} className="small col-xs-4 text-center">
+                Fast
+              </p>
+              <p style={paddingReset} className="small col-xs-4 text-right">
+                Fast AF
+              </p>
+              <p
+                className="small"
+                style={{
+                  whiteSpace: 'normal',
+                  fontWeight: '300',
+                  margin: '2rem 0 0'
+                }}
               >
-                Read more
-              </a>
-            </p>
-          </div>
-        </ul>}
-    </span>
-
-    )
+                Gas Price is the amount you pay per unit of gas.{' '}
+                <code>TX fee = gas price * gas limit</code> & is paid to miners
+                for including your TX in a block. Higher the gas price = faster
+                transaction, but more expensive. Default is <code>21 GWEI</code>.
+              </p>
+              <p>
+                {/* TODO: maybe not hardcode a link? :) */}
+                <a
+                  href="https://myetherwallet.groovehq.com/knowledge_base/topics/what-is-gas"
+                  target="_blank"
+                >
+                  Read more
+                </a>
+              </p>
+            </div>
+          </ul>}
+      </span>
+    );
   }
 
   toggleExpanded = () => {
@@ -75,7 +94,7 @@ export default class gasPriceDropdown extends Component {
     });
   };
 
-  updateGasPrice = (e) => {
+  updateGasPrice = (e: SyntheticInputEvent) => {
     this.props.changeGasPrice(e.currentTarget.valueAsNumber);
-  }
+  };
 }
