809d97374c74c1c2a7d59406aee0c36f5ad917d2
Will O'Beirne <wbobeirne@gmail.com>
2017-07-20 15:05:38 -0400

    Move favicon stuff to static folder. Expand manifest.json. Link it all up in HTML.

--- common/index.jsx
+++ common/index.jsx
@@ -1,3 +1,9 @@
+// Application styles
+import 'assets/styles/etherwallet-master.less';
+
+// Static icons
+import 'assets/images/icons/icon16.png';
+
 import React from 'react';
 import { render } from 'react-dom';
 import { syncHistoryWithStore } from 'react-router-redux';
@@ -5,8 +11,6 @@ import { syncHistoryWithStore } from 'react-router-redux';
 import { Root } from 'components';
 import { Routing, history } from './routing';
 import { store } from './store';
-// application styles
-import 'assets/styles/etherwallet-master.less';
 
 const renderRoot = Root => {
   let syncedHistory = syncHistoryWithStore(history, store);
