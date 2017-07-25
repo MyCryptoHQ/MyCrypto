06a0203a4c6991210745e12b8ab55353b5069c27
Will O'Beirne <wbobeirne@gmail.com>
2017-07-25 12:42:42 -0400

    Remove unnecessary class.

--- common/components/ui/Identicon.jsx
+++ common/components/ui/Identicon.jsx
@@ -17,7 +17,6 @@ export default function Identicon(props: Props) {
     : '';
   return (
     <div
-      className="Identicon"
       style={{ position: 'relative', width: size, height: size }}
       title="Address Indenticon"
     >
