42a3662f620bc3440195e8d0801438663048bea4
Daniel Ternyak <dternyak@gmail.com>
2017-07-28 12:52:31 -0500

    soften diff

--- common/actions/swap.js
+++ common/actions/swap.js
@@ -1,16 +1,31 @@
 // @flow
-import * as swapTypes from './swapTypes';
+import {
+  OriginKindSwapAction,
+  DestinationKindSwapAction,
+  OriginAmountSwapAction,
+  DestinationAmountSwapAction,
+  BityRatesSwapAction,
+  DestinationAddressSwapAction,
+  OrderCreateSucceededSwapAction,
+  OrderCreateRequestedSwapAction,
+  OrderStatusSucceededSwapAction,
+  ChangeStepSwapAction,
+  Pairs,
+  RestartSwapAction,
+  LoadBityRatesSwapAction,
+  StopLoadBityRatesSwapAction,
+  BityOrderResponse,
+  BityOrderPostResponse
+} from './swapTypes';
 
-export function changeStepSwap(value: number): swapTypes.ChangeStepSwapAction {
+export function changeStepSwap(value: number): ChangeStepSwapAction {
   return {
     type: 'SWAP_STEP',
     value
   };
 }
 
-export const originKindSwap = (
-  value: string
-): swapTypes.OriginKindSwapAction => {
+export const originKindSwap = (value: string): OriginKindSwapAction => {
   return {
     type: 'SWAP_ORIGIN_KIND',
     value
@@ -19,16 +34,14 @@ export const originKindSwap = (
 
 export const destinationKindSwap = (
   value: string
-): swapTypes.DestinationKindSwapAction => {
+): DestinationKindSwapAction => {
   return {
     type: 'SWAP_DESTINATION_KIND',
     value
   };
 };
 
-export const originAmountSwap = (
-  value: ?number
-): swapTypes.OriginAmountSwapAction => {
+export const originAmountSwap = (value: ?number): OriginAmountSwapAction => {
   return {
     type: 'SWAP_ORIGIN_AMOUNT',
     value
@@ -37,16 +50,14 @@ export const originAmountSwap = (
 
 export const destinationAmountSwap = (
   value: ?number
-): swapTypes.DestinationAmountSwapAction => {
+): DestinationAmountSwapAction => {
   return {
     type: 'SWAP_DESTINATION_AMOUNT',
     value
   };
 };
 
-export const updateBityRatesSwap = (
-  value: swapTypes.Pairs
-): swapTypes.BityRatesSwapAction => {
+export const updateBityRatesSwap = (value: Pairs): BityRatesSwapAction => {
   return {
     type: 'SWAP_UPDATE_BITY_RATES',
     value
@@ -55,34 +66,34 @@ export const updateBityRatesSwap = (
 
 export const destinationAddressSwap = (
   value: ?string
-): swapTypes.DestinationAddressSwapAction => {
+): DestinationAddressSwapAction => {
   return {
     type: 'SWAP_DESTINATION_ADDRESS',
     value
   };
 };
 
-export const restartSwap = (): swapTypes.RestartSwapAction => {
+export const restartSwap = (): RestartSwapAction => {
   return {
     type: 'SWAP_RESTART'
   };
 };
 
-export const loadBityRatesSwap = (): swapTypes.LoadBityRatesSwapAction => {
+export const loadBityRatesSwap = (): LoadBityRatesSwapAction => {
   return {
     type: 'SWAP_LOAD_BITY_RATES'
   };
 };
 
-export const stopLoadBityRatesSwap = (): swapTypes.StopLoadBityRatesSwapAction => {
+export const stopLoadBityRatesSwap = (): StopLoadBityRatesSwapAction => {
   return {
     type: 'SWAP_STOP_LOAD_BITY_RATES'
   };
 };
 
 export function orderCreateSucceededSwap(
-  payload: swapTypes.BityOrderPostResponse
-): swapTypes.OrderCreateSucceededSwapAction {
+  payload: BityOrderPostResponse
+): OrderCreateSucceededSwapAction {
   return {
     type: 'SWAP_ORDER_CREATE_SUCCEEDED',
     payload
@@ -94,7 +105,7 @@ export function orderCreateRequestedSwap(
   destinationAddress: string,
   pair: string,
   mode: number = 0
-): swapTypes.OrderCreateRequestedSwapAction {
+): OrderCreateRequestedSwapAction {
   return {
     type: 'SWAP_ORDER_CREATE_REQUESTED',
     payload: {
@@ -113,8 +124,8 @@ export function orderTimeTickSwap() {
 }
 
 export function orderStatusSucceededSwap(
-  payload: swapTypes.BityOrderResponse
-): swapTypes.OrderStatusSucceededSwapAction {
+  payload: BityOrderResponse
+): OrderStatusSucceededSwapAction {
   return {
     type: 'SWAP_BITY_ORDER_STATUS_SUCCEEDED',
     payload
