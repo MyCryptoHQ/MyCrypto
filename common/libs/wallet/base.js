3ef2b51a686d13fbf6723cd9994f894a41dc51fe
skubakdj <skubakdj@gmail.com>
2017-08-07 23:25:23 -0400

    Add Promises & Signing to Wallet Classes  (#105)
    * add validation for raw tx
    
    * add node module, add signing lib
    
    * add tx & message signing, promise everything
    
    * remove unnecessary returns
    
    * move isValidRawTx to for loop
    
    * add & use new type RawTx
    
    * implement cleaner promises, reject instead of throw

--- common/libs/wallet/base.js
+++ common/libs/wallet/base.js
@@ -1,11 +1,15 @@
 // @flow
 
 export default class BaseWallet {
-  getAddress(): string {
-    throw 'Implement me';
+  getAddress(): Promise<any> {
+    return Promise.reject('Implement me');
   }
 
-  getNakedAddress(): string {
-    return this.getAddress().replace('0x', '').toLowerCase();
+  getNakedAddress(): Promise<any> {
+    return new Promise(resolve => {
+      this.getAddress.then(address => {
+        resolve(address.replace('0x', '').toLowerCase());
+      });
+    });
   }
 }
