affc5d9879a1d5f3cd291135acc49d4aea5baa0d
Isaac Kang <isaacjkang@gmail.com>
2017-08-09 13:44:37 -0400

    convert ViewOnly to stateless func

--- common/components/WalletDecrypt/ViewOnly.jsx
+++ common/components/WalletDecrypt/ViewOnly.jsx
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import translate from 'translations';
+import PropTypes from 'prop-types';
 
-export default class ViewOnlyDecrypt extends Component {
-  render() {
-    return (
-      <section className="col-md-4 col-sm-6">
-        <div id="selectedUploadKey">
-          <h4>{translate('ADD_Radio_2_alt')}</h4>
+const ViewOnlyDecrypt = ({ value, onChange, onUnlock }) =>
+  <section className="col-md-4 col-sm-6">
+    <div id="selectedUploadKey">
+      <h4>
+        {translate('ADD_Radio_2_alt')}
+      </h4>
 
-          <div className="form-group">
-            <input type="file" id="fselector" />
+      <div className="form-group">
+        <input type="file" id="fselector" />
 
-            <a
-              className="btn-file marg-v-sm"
-              id="aria1"
-              tabIndex="0"
-              role="button"
-            >
-              {translate('ADD_Radio_2_short')}
-            </a>
-          </div>
-        </div>
-      </section>
-    );
-  }
-}
+        <a className="btn-file marg-v-sm" id="aria1" tabIndex="0" role="button">
+          {translate('ADD_Radio_2_short')}
+        </a>
+      </div>
+    </div>
+  </section>;
+
+ViewOnlyDecrypt.propTypes = {
+  value: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onUnlock: PropTypes.func.isRequired
+};
+
+export default ViewOnlyDecrypt;
