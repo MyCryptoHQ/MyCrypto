d181883f5f6c26fe01bb0e5084961021b0285dc0
Isaac Kang <isaacjkang@gmail.com>
2017-08-14 18:36:01 -0400

    add flow type to props. update to stateless, functional.

--- common/components/WalletDecrypt/ViewOnly.jsx
+++ common/components/WalletDecrypt/ViewOnly.jsx
@@ -1,21 +1,19 @@
+// @flow
 import React from 'react';
 import translate from 'translations';
-import PropTypes from 'prop-types';
 
 export type AddressValue = {
   address: string
 };
 
-const ViewOnlyDecrypt = ({ value, onChange, onUnlock }) => {
-  const { address } = value;
+export type ViewOnlyProps = {
+  value: AddressValue,
+  onChange: (value: AddressValue) => void,
+  onUnlock: () => void
+};
 
-  const onKeyDown = (e: SyntheticKeyboardEvent) => {
-    if (e.keyCode === 13) {
-      e.preventDefault();
-      e.stopPropagation();
-      onUnlock();
-    }
-  };
+const ViewOnlyDecrypt = ({ value, onChange, onUnlock }: ViewOnlyProps) => {
+  const { address } = value;
 
   const onAddressChange = (e: SyntheticInputEvent) => {
     // need to validate here
@@ -25,13 +23,20 @@ const ViewOnlyDecrypt = ({ value, onChange, onUnlock }) => {
     });
   };
 
+  const onKeyDown = (e: SyntheticKeyboardEvent) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      e.stopPropagation();
+      onUnlock();
+    }
+  };
+
   return (
     <section className="col-md-4 col-sm-6">
       <div id="selectedTypeKey">
         <h4>
           {translate('ADD_Label_5')}
         </h4>
-
         <div className="form-group">
           <textarea
             id="address-label"
@@ -46,12 +51,6 @@ const ViewOnlyDecrypt = ({ value, onChange, onUnlock }) => {
       </div>
     </section>
   );
-};
-
-ViewOnlyDecrypt.propTypes = {
-  value: PropTypes.object.isRequired,
-  onChange: PropTypes.func.isRequired,
-  onUnlock: PropTypes.func.isRequired
 };
 
 export default ViewOnlyDecrypt;
