d49f45fa306c8d72228def8fb509c8e6d29dae66
danny <skubakdj@gmail.com>
2017-08-14 13:18:41 -0400

    add v3 references

--- common/libs/decrypt.js
+++ common/libs/decrypt.js
@@ -2,6 +2,7 @@
 
 import { createHash, createDecipheriv } from 'crypto';
 
+//adapted from https://github.com/kvhnuke/etherwallet/blob/de536ffebb4f2d1af892a32697e89d1a0d906b01/app/scripts/myetherwallet.js#L230
 export function decryptPrivKey(encprivkey: string, password: string): Buffer {
   let cipher = encprivkey.slice(0, 128);
   cipher = decodeCryptojsSalt(cipher);
@@ -15,6 +16,7 @@ export function decryptPrivKey(encprivkey: string, password: string): Buffer {
   return new Buffer(privKey.toString(), 'hex');
 }
 
+//adapted from https://github.com/kvhnuke/etherwallet/blob/de536ffebb4f2d1af892a32697e89d1a0d906b01/app/scripts/myetherwallet.js#L284
 export function decodeCryptojsSalt(input: string): Object {
   let ciphertext = new Buffer(input, 'base64');
   if (ciphertext.slice(0, 8).toString() === 'Salted__') {
@@ -29,6 +31,7 @@ export function decodeCryptojsSalt(input: string): Object {
   }
 }
 
+//adapted from https://github.com/kvhnuke/etherwallet/blob/de536ffebb4f2d1af892a32697e89d1a0d906b01/app/scripts/myetherwallet.js#L297
 export function evp_kdf(data: Buffer, salt: Buffer, opts: Object) {
   // A single EVP iteration, returns `D_i`, where block equlas to `D_(i-1)`
 
