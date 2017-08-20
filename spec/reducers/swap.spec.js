ce1aed25ee0b7a543ae73e81549db14291bbd1a7
Daniel Ternyak <dternyak@gmail.com>
2017-08-20 09:36:19 +0200

    add additional swap reducer tests

--- spec/reducers/swap.spec.js
+++ spec/reducers/swap.spec.js
@@ -14,6 +14,7 @@ describe('swap reducer', () => {
     });
   });
 
+  // TODO
   // it('should handle SWAP_ORIGIN_KIND', () => {
   //   const originKind = 'ETH';
   //   expect(swap(undefined, swapActions.originKindSwap(originKind))).toEqual({
@@ -21,4 +22,66 @@ describe('swap reducer', () => {
   //     originKind
   //   });
   // });
+
+  it('should handle SWAP_ORIGIN_AMOUNT', () => {
+    const originAmount = 2;
+    expect(
+      swap(undefined, swapActions.originAmountSwap(originAmount))
+    ).toEqual({
+      ...INITIAL_STATE,
+      originAmount
+    });
+  });
+
+  it('should handle SWAP_DESTINATION_AMOUNT', () => {
+    const destinationAmount = 2;
+    expect(
+      swap(undefined, swapActions.destinationAmountSwap(destinationAmount))
+    ).toEqual({
+      ...INITIAL_STATE,
+      destinationAmount
+    });
+  });
+
+  it('should handle SWAP_DESTINATION_ADDRESS', () => {
+    // TODO - should reducer throw if address is not in correct format?
+    const destinationAddress = '341a0sdf83';
+    expect(
+      swap(undefined, swapActions.destinationAddressSwap(destinationAddress))
+    ).toEqual({
+      ...INITIAL_STATE,
+      destinationAddress
+    });
+  });
+
+  it('should handle SWAP_RESTART', () => {
+    // TODO - should reducer throw if address is not in correct format?
+    const bityRates = {
+      BTCETH: 0.01,
+      ETHREP: 10
+    };
+    expect(
+      swap(
+        {
+          ...INITIAL_STATE,
+          bityRates,
+          originAmount: 1
+        },
+        swapActions.restartSwap()
+      )
+    ).toEqual({
+      ...INITIAL_STATE,
+      bityRates
+    });
+  });
+
+  it('should handle SWAP_ORDER_TIME', () => {
+    const secondsRemaining = 300;
+    expect(
+      swap(undefined, swapActions.orderTimeSwap(secondsRemaining))
+    ).toEqual({
+      ...INITIAL_STATE,
+      secondsRemaining
+    });
+  });
 });
