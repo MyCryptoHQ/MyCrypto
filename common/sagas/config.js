ad17ae676a6d6b45090d724d1163e197be7fab66
Daniel Ternyak <dternyak@gmail.com>
2017-08-28 11:23:19 -0500

    Change reload to 250 ms from 1500 ms

--- common/sagas/config.js
+++ common/sagas/config.js
@@ -3,7 +3,7 @@ import { takeEvery } from 'redux-saga/effects';
 // @HACK For now we reload the app when doing a language swap to force non-connected
 // data to reload. Also the use of timeout to avoid using additional actions for now.
 function* handleLanguageChange() {
-  yield setTimeout(() => location.reload(), 1500);
+  yield setTimeout(() => location.reload(), 250);
 }
 
 export default function* handleConfigChanges() {
