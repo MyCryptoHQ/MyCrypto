8ffe8e387d40e2ca4ac9db507326ea4c17bc89e4
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-08-28 09:42:21 -0600

    First pass at handling API failure.

--- common/sagas/rates.js
+++ common/sagas/rates.js
@@ -2,18 +2,28 @@
 import { put, call } from 'redux-saga/effects';
 import type { Effect } from 'redux-saga/effects';
 import { setRates } from 'actions/rates';
+import { showNotification } from 'actions/notifications';
 
 const symbols = ['USD', 'EUR', 'GBP', 'BTC', 'CHF', 'REP'];
 
-function fetchRates(symbols) {
-  return fetch(
+const fetchRates = symbols =>
+  fetch(
     `https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=${symbols.join(
       ','
     )}`
-  ).then(r => r.json());
-}
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Could not fetch rate data.');
+  });
 
 export default function* ratesSaga(): Generator<Effect, void, any> {
-  const rates = yield call(fetchRates, symbols);
-  yield put(setRates(rates));
+  try {
+    const rates = yield call(fetchRates, symbols);
+    yield put(setRates(rates));
+  } catch (error) {
+    const action = yield showNotification('danger', error);
+    yield put(action);
+  }
 }
