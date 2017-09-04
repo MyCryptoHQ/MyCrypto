d05525e68232045723ade029f71bea9cb34cf745
HenryNguyen5 <ngu.henry@hotmail.com>
2017-09-03 20:04:27 -0400

    ENS - Tab + General Infomation Panel (#147)

--- common/components/ui/index.js
+++ common/components/ui/index.js
@@ -5,3 +5,4 @@ export { default as Identicon } from './Identicon';
 export { default as Modal } from './Modal';
 export { default as UnlockHeader } from './UnlockHeader';
 export { default as QRCode } from './QRCode';
+export { default as NewTabLink } from './NewTabLink.jsx';
---
+++ common/containers/Tabs/ENS/index.jsx
@@ -0,0 +1,7 @@
+// @flow
+import { connect } from 'react-redux';
+import ENS from './components/ENS';
+
+const mapStateToProps = state => ({});
+
+export default connect(mapStateToProps)(ENS);
