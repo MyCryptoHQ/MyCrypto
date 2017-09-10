729ea3297efae7012fba6a7563d1bb534972d211
henrynguyen5 <ngu.henry@hotmail.com>
2017-09-10 14:26:37 -0400

    IWallet change

--- common/containers/Tabs/SendTransaction/index.jsx
+++ common/containers/Tabs/SendTransaction/index.jsx
@@ -41,7 +41,7 @@ import type { BroadcastTxRequestedAction } from 'actions/wallet';
 import { showNotification } from 'actions/notifications';
 import type { ShowNotificationAction } from 'actions/notifications';
 // LIBS
-import BaseWallet from 'libs/wallet/base';
+import type { IWallet } from 'libs/wallet/IWallet';
 import { isValidETHAddress } from 'libs/validators';
 import type { RPCNode } from 'libs/nodes';
 import type {
@@ -93,7 +93,7 @@ type Props = {
       [string]: string
     }
   },
-  wallet: BaseWallet,
+  wallet: IWallet,
   balance: Ether,
   node: NodeConfig,
   nodeLib: RPCNode,
