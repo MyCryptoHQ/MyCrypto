78db7174d901abbadb8ac1a111798f1bca8e8d7e
henrynguyen5 <ngu.henry@hotmail.com>
2017-09-12 12:23:15 -0400

    Clear ENS resolvedDomain when network request errors

--- common/reducers/ens/domainSelector.js
+++ common/reducers/ens/domainSelector.js
@@ -1,21 +1,30 @@
 // @flow
-import type { EnsAction, ResolveDomainSuccess } from 'actions/ens';
+import type {
+  EnsAction,
+  ResolveDomainSuccess,
+  ResolveDomainFailed
+} from 'actions/ens';
 import type { State } from './types';
 
 const INITIAL_STATE: State = {};
 
-const currentDomainName = (
+const setCurrentDomainName = (
   state: State,
   action: ResolveDomainSuccess
 ): State => {
   const { domain: domainName } = action.payload;
   return { currentDomain: domainName };
 };
+const clearCurrentDomainName = (): State => {
+  return { currentDomain: null };
+};
 
 export default (state: State = INITIAL_STATE, action: EnsAction): State => {
   switch (action.type) {
     case 'ENS_RESOLVE_DOMAIN_SUCCESS':
-      return currentDomainName(state, action);
+      return setCurrentDomainName(state, action);
+    case 'ENS_RESOLVE_DOMAIN_FAILED':
+      return clearCurrentDomainName();
     default:
       return state;
   }
