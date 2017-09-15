a612e47e043da3312c74c8f5394bf115039f1fc0
Daniel Ternyak <dternyak@gmail.com>
2017-09-14 17:55:13 -0700

    create PreFooter components and styles

---
+++ common/components/Footer/PreFooter.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './PreFooter.scss';
+
+const PreFooter = () => {
+  return (
+    <section className="pre-footer">
+      <div className="container">
+        <p>
+          MyEtherWallet.com does not hold your keys for you. We cannot access
+          accounts, recover keys, reset passwords, nor reverse transactions.
+          Protect your keys &amp; always check that you are on correct URL.
+          <a href="#"> You are responsible for your security.</a>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default PreFooter;
