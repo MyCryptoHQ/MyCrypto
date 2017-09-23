8ea5b89424dea10b9ff43c562a8230934c4d4afd
Will O'Beirne <wbobeirne@gmail.com>
2017-09-23 16:32:03 -0400

    Fix some errors, ignore some silly rules.

--- common/typescript/ethereumjs-tx.d.ts
+++ common/typescript/ethereumjs-tx.d.ts
@@ -2,8 +2,9 @@ declare module 'ethereumjs-tx' {
   import { BigNumber } from 'bignumber.js';
   import BN = require('bn.js');
   import { Buffer } from 'buffer';
-  /**
-   *   type fieldName =
+
+  /*
+  type fieldName =
     | 'nonce'
     | 'gasPrice'
     | 'gasLimit'
@@ -21,8 +22,8 @@ declare module 'ethereumjs-tx' {
     allowZero?: boolean;
     default?: Buffer;
   };
+  */
 
-   */
   type DataParamType = any; //TS complaining about big numbers heres
   interface Data {
     chainId: number;
@@ -44,72 +45,72 @@ declare module 'ethereumjs-tx' {
      * If the tx's `to` is to the creation address
      * @return {Boolean}
      */
-    toCreationAddress(): string;
+    public toCreationAddress(): string;
 
     /**
      * Computes a sha3-256 hash of the serialized tx
      * @param {Boolean} [includeSignature=true] whether or not to inculde the signature
      * @return {Buffer}
      */
-    hash(bool: boolean): Buffer;
+    public hash(bool: boolean): Buffer;
 
     /**
      * returns the public key of the sender
      * @return {Buffer}
      */
-    getChainId(): Buffer;
+    public getChainId(): Buffer;
 
     /**
      * returns the sender's address
      * @return {Buffer}
      */
-    getSenderAddress(): Buffer;
+    public getSenderAddress(): Buffer;
 
     /**
      * returns the public key of the sender
      * @return {Buffer}
      */
-    getSenderPublicKey(): Buffer;
+    public getSenderPublicKey(): Buffer;
 
     /**
      * Determines if the signature is valid
      * @return {Boolean}
      */
-    verifySignature(): boolean;
+    public verifySignature(): boolean;
 
     /**
      * sign a transaction with a given a private key
      * @param {Buffer} privateKey
      */
-    sign(privateKey: Buffer);
+    public sign(privateKey: Buffer);
 
     /**
      * The amount of gas paid for the data in this tx
      * @return {BN}
      */
-    getDataFee(): BN;
+    public getDataFee(): BN;
 
     /**
      * the minimum amount of gas the tx must have (DataFee + TxFee + Creation Fee)
      * @return {BN}
      */
-    getBaseFee(): BN;
+    public getBaseFee(): BN;
 
     /**
-   * the up front amount that an account must have for this transaction to be valid
-   * @return {BN}
-   */
-    getUpfrontCost(): BN;
+     * the up front amount that an account must have for this transaction to be valid
+     * @return {BN}
+     */
+    public getUpfrontCost(): BN;
 
     /**
-   * validates the signature and checks to see if it has enough gas
-   * @param {Boolean} [stringError=false] whether to return a string with a dscription of why the validation failed or return a Bloolean
-   * @return {Boolean|String}
-   */
-    validate(stringError: boolean): boolean | string;
+     * validates the signature and checks to see if it has enough gas
+     * @param {Boolean} [stringError=false] whether to return a string with a dscription of why the validation failed or return a Bloolean
+     * @return {Boolean|String}
+     */
+    public validate(stringError: boolean): boolean | string;
 
-    toJSON(): any;
+    public toJSON(): any;
 
-    serialize(): Buffer;
+    public serialize(): Buffer;
   }
 }
