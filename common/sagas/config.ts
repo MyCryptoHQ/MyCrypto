3072e50357ef411e5a3cee5f00fe7c2980cfe8e5
Daniel Ternyak <dternyak@gmail.com>
2017-09-28 17:44:49 -0700

    Create sagas for polling offline status

--- common/sagas/config.ts
+++ common/sagas/config.ts
@@ -1,5 +1,39 @@
-import { SagaIterator } from 'redux-saga';
-import { takeEvery } from 'redux-saga/effects';
+import { toggleOfflineConfig, TToggleOfflineConfig } from 'actions/config';
+import { delay, SagaIterator } from 'redux-saga';
+import {
+  call,
+  cancel,
+  fork,
+  put,
+  take,
+  takeLatest,
+  takeEvery,
+  select
+} from 'redux-saga/effects';
+import { AppState } from 'reducers';
+import { State as ConfigState } from 'reducers/config';
+import { TypeKeys } from 'actions/config/constants';
+
+export const getConfig = (state: AppState): ConfigState => state.config;
+
+export function* pollOfflineStatus(action?: any): SagaIterator {
+  while (true) {
+    const offline = !navigator.onLine;
+    const config = yield select(getConfig);
+    const offlineState = config.offline;
+    if (offline !== offlineState) {
+      yield put(toggleOfflineConfig());
+    }
+    yield call(delay, 250);
+  }
+}
+
+// Fork our recurring API call, watch for the need to cancel.
+function* handlePollOfflineStatus(): SagaIterator {
+  const pollOfflineStatusTask = yield fork(pollOfflineStatus);
+  yield take('CONFIG_STOP_POLL_OFFLINE_STATE');
+  yield cancel(pollOfflineStatusTask);
+}
 
 // @HACK For now we reload the app when doing a language swap to force non-connected
 // data to reload. Also the use of timeout to avoid using additional actions for now.
@@ -8,6 +42,10 @@ function* reload(): SagaIterator {
 }
 
 export default function* handleConfigChanges(): SagaIterator {
-  yield takeEvery('CONFIG_NODE_CHANGE', reload);
-  yield takeEvery('CONFIG_LANGUAGE_CHANGE', reload);
+  yield takeLatest(
+    TypeKeys.CONFIG_POLL_OFFLINE_STATUS,
+    handlePollOfflineStatus
+  );
+  yield takeEvery(TypeKeys.CONFIG_NODE_CHANGE, reload);
+  yield takeEvery(TypeKeys.CONFIG_LANGUAGE_CHANGE, reload);
 }
