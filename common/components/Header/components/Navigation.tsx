MyCryptoHQ/MyCrypto

commit ad83b5a18135014e23d3596826b46cd399e65702

Update React-Router(-DOM) to 4.x and Simplify Routing (#241)

common/components/Header/components/NavigationLink.tsx
@@ -1,6 +1,6 @@
 import classnames from 'classnames';
 import React from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import translate, { translateRaw } from 'translations';
 import './NavigationLink.scss';
 
@@ -25,24 +25,25 @@ export default class NavigationLink extends React.Component<Props, {}> {
     });
     const linkLabel = `nav item: ${translateRaw(link.name)}`;
 
-    const linkEl = link.external ? (
-      <a
-        className={linkClasses}
-        href={link.to}
-        aria-label={linkLabel}
-        target="_blank"
-      >
-        {translate(link.name)}
-      </a>
-    ) : (
-      <Link
-        className={linkClasses}
-        to={(link as any).to}
-        aria-label={linkLabel}
-      >
-        {translate(link.name)}
-      </Link>
-    );
+    const linkEl =
+      link.external || !link.to ? (
+        <a
+          className={linkClasses}
+          href={link.to}
+          aria-label={linkLabel}
+          target="_blank"
+        >
+          {translate(link.name)}
+        </a>
+      ) : (
+        <Link
+          className={linkClasses}
+          to={(link as any).to}
+          aria-label={linkLabel}
+        >
+          {translate(link.name)}
+        </Link>
+      );
 
     return <li className="NavigationLink">{linkEl}</li>;
   }

common/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { TChangeGasPrice, TChangeLanguage, TChangeNode } from 'actions/config';
 import logo from 'assets/images/logo-myetherwallet.svg';
 import { Dropdown } from 'components/ui';
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import {
   ANNOUNCEMENT_MESSAGE,
   ANNOUNCEMENT_TYPE,

common/containers/Tabs/ENS/components/ENS.tsx
@@ -2,27 +2,32 @@ import React from 'react';
 import { GeneralInfoPanel } from './GeneralInfoPanel';
 import Title from './Title';
 import UnfinishedBanner from './UnfinishedBanner';
+import App from 'containers/App';
 
 interface ContainerTabPaneActiveProps {
   children: React.ReactElement<any> | React.ReactElement<any>[];
 }
 
-const ContainerTabPaneActive = ({ children }: ContainerTabPaneActiveProps) =>
-  <section className="container">
-    <div className="tab-content">
-      <main className="tab-pane active">
-        <section role="main" className="row">
-          {children}
-        </section>
-      </main>
-    </div>
-  </section>;
+const ContainerTabPaneActive = ({ children }: ContainerTabPaneActiveProps) => (
+  <App>
+    <section className="container">
+      <div className="tab-content">
+        <main className="tab-pane active">
+          <section role="main" className="row">
+            {children}
+          </section>
+        </main>
+      </div>
+    </section>
+  </App>
+);
 
-const ENS = () =>
+const ENS = () => (
   <ContainerTabPaneActive>
     <UnfinishedBanner />
     <Title />
     <GeneralInfoPanel />
-  </ContainerTabPaneActive>;
+  </ContainerTabPaneActive>
+);
 
 export default ENS;

common/containers/Tabs/GenerateWallet/index.tsx
@@ -13,6 +13,7 @@ import { AppState } from 'reducers';
 import DownloadWallet from './components/DownloadWallet';
 import EnterPassword from './components/EnterPassword';
 import PaperWallet from './components/PaperWallet';
+import App from 'containers/App';
 
 interface Props {
   // Redux state
@@ -71,7 +72,11 @@ class GenerateWallet extends Component<Props, {}> {
         content = <h1>Uh oh. Not sure how you got here.</h1>;
     }
 
-    return <section className="Tab-content">{content}</section>;
+    return (
+      <App>
+        <section className="Tab-content">{content}</section>
+      </App>
+    );
   }
 }
 

common/containers/Tabs/Help/index.tsx
@@ -1,46 +1,47 @@
 import React from 'react';
-
 import translate from 'translations';
+import App from 'containers/App';
 
-const Help = () =>
-  <section className="container" style={{ minHeight: '50%' }}>
-    <div className="tab-content">
-      <article className="tab-pane help active">
-        <h1>
-          {translate('NAV_Help')}
-        </h1>
-        <article className="collapse-container">
-          <div>
-            <ul>
-              <li>
-                <h3>
-                  <a
-                    href="https://www.reddit.com/r/ethereum/comments/47nkoi/psa_check_your_ethaddressorg_wallets_and_any/d0eo45o"
-                    target="_blank"
-                  >
-                    <span className="text-danger">
-                      {translate('HELP_Warning')}
-                    </span>
-                  </a>
-                </h3>
-              </li>
-              <li>
-                <h3>
-                  This page is deprecated. Please check out our more up-to-date
-                  and searchable{' '}
-                  <a
-                    href="https://myetherwallet.groovehq.com/help_center"
-                    target="_blank"
-                  >
-                    Knowledge Base.{' '}
-                  </a>
-                </h3>
-              </li>
-            </ul>
-          </div>
+const Help = () => (
+  <App>
+    <section className="container" style={{ minHeight: '50%' }}>
+      <div className="tab-content">
+        <article className="tab-pane help active">
+          <h1>{translate('NAV_Help')}</h1>
+          <article className="collapse-container">
+            <div>
+              <ul>
+                <li>
+                  <h3>
+                    <a
+                      href="https://www.reddit.com/r/ethereum/comments/47nkoi/psa_check_your_ethaddressorg_wallets_and_any/d0eo45o"
+                      target="_blank"
+                    >
+                      <span className="text-danger">
+                        {translate('HELP_Warning')}
+                      </span>
+                    </a>
+                  </h3>
+                </li>
+                <li>
+                  <h3>
+                    This page is deprecated. Please check out our more
+                    up-to-date and searchable{' '}
+                    <a
+                      href="https://myetherwallet.groovehq.com/help_center"
+                      target="_blank"
+                    >
+                      Knowledge Base.{' '}
+                    </a>
+                  </h3>
+                </li>
+              </ul>
+            </div>
+          </article>
         </article>
-      </article>
-    </div>
-  </section>;
+      </div>
+    </section>
+  </App>
+);
 
 export default Help;

common/containers/Tabs/SendTransaction/index.tsx
@@ -5,6 +5,7 @@ import { BalanceSidebar } from 'components';
 // COMPONENTS
 import { UnlockHeader } from 'components/ui';
 import { donationAddressMap, NetworkConfig, NodeConfig } from 'config/data';
+import App from 'containers/App';
 // CONFIG
 import { TransactionWithoutGas } from 'libs/messages';
 import { RPCNode } from 'libs/nodes';
@@ -176,115 +177,117 @@ export class SendTransaction extends React.Component<Props, State> {
     const customMessage = customMessages.find(m => m.to === to);
 
     return (
-      <section className="Tab-content">
-        <UnlockHeader title={'NAV_SendEther'} />
+      <App>
+        <section className="Tab-content">
+          <UnlockHeader title={'NAV_SendEther'} />
 
-        <div className="row">
-          {/* Send Form */}
-          {unlocked && (
-            <main className="col-sm-8">
-              <div className="Tab-content-pane">
-                {hasQueryString && (
-                  <div className="alert alert-info">
-                    <p>{translate('WARN_Send_Link')}</p>
-                  </div>
-                )}
-
-                <AddressField
-                  placeholder={donationAddressMap.ETH}
-                  value={this.state.to}
-                  onChange={readOnly ? null : this.onAddressChange}
-                />
-                <AmountField
-                  value={value}
-                  unit={unit}
-                  tokens={this.props.tokenBalances
-                    .filter(token => !token.balance.eq(0))
-                    .map(token => token.symbol)
-                    .sort()}
-                  onChange={readOnly ? void 0 : this.onAmountChange}
-                />
-                <GasField
-                  value={gasLimit}
-                  onChange={readOnly ? void 0 : this.onGasChange}
-                />
-                {unit === 'ether' && (
-                  <DataField
-                    value={data}
-                    onChange={readOnly ? void 0 : this.onDataChange}
-                  />
-                )}
-                <CustomMessage message={customMessage} />
-
-                <div className="row form-group">
-                  <div className="col-xs-12 clearfix">
-                    <button
-                      disabled={this.state.generateDisabled}
-                      className="btn btn-info btn-block"
-                      onClick={this.generateTxFromState}
-                    >
-                      {translate('SEND_generate')}
-                    </button>
-                  </div>
-                </div>
-
-                {transaction && (
-                  <div>
-                    <div className="row form-group">
-                      <div className="col-sm-6">
-                        <label>{translate('SEND_raw')}</label>
-                        <textarea
-                          className="form-control"
-                          value={transaction.rawTx}
-                          rows={4}
-                          readOnly={true}
-                        />
-                      </div>
-                      <div className="col-sm-6">
-                        <label>{translate('SEND_signed')}</label>
-                        <textarea
-                          className="form-control"
-                          value={transaction.signedTx}
-                          rows={4}
-                          readOnly={true}
-                        />
-                      </div>
+          <div className="row">
+            {/* Send Form */}
+            {unlocked && (
+              <main className="col-sm-8">
+                <div className="Tab-content-pane">
+                  {hasQueryString && (
+                    <div className="alert alert-info">
+                      <p>{translate('WARN_Send_Link')}</p>
                     </div>
+                  )}
 
-                    <div className="form-group">
+                  <AddressField
+                    placeholder={donationAddressMap.ETH}
+                    value={this.state.to}
+                    onChange={readOnly ? null : this.onAddressChange}
+                  />
+                  <AmountField
+                    value={value}
+                    unit={unit}
+                    tokens={this.props.tokenBalances
+                      .filter(token => !token.balance.eq(0))
+                      .map(token => token.symbol)
+                      .sort()}
+                    onChange={readOnly ? void 0 : this.onAmountChange}
+                  />
+                  <GasField
+                    value={gasLimit}
+                    onChange={readOnly ? void 0 : this.onGasChange}
+                  />
+                  {unit === 'ether' && (
+                    <DataField
+                      value={data}
+                      onChange={readOnly ? void 0 : this.onDataChange}
+                    />
+                  )}
+                  <CustomMessage message={customMessage} />
+
+                  <div className="row form-group">
+                    <div className="col-xs-12 clearfix">
                       <button
-                        className="btn btn-primary btn-block col-sm-11"
-                        disabled={!this.state.transaction}
-                        onClick={this.openTxModal}
+                        disabled={this.state.generateDisabled}
+                        className="btn btn-info btn-block"
+                        onClick={this.generateTxFromState}
                       >
-                        {translate('SEND_trans')}
+                        {translate('SEND_generate')}
                       </button>
                     </div>
                   </div>
-                )}
-              </div>
-            </main>
-          )}
 
-          {/* Sidebar */}
-          {unlocked && (
-            <section className="col-sm-4">
-              <BalanceSidebar />
-            </section>
-          )}
-        </div>
+                  {transaction && (
+                    <div>
+                      <div className="row form-group">
+                        <div className="col-sm-6">
+                          <label>{translate('SEND_raw')}</label>
+                          <textarea
+                            className="form-control"
+                            value={transaction.rawTx}
+                            rows={4}
+                            readOnly={true}
+                          />
+                        </div>
+                        <div className="col-sm-6">
+                          <label>{translate('SEND_signed')}</label>
+                          <textarea
+                            className="form-control"
+                            value={transaction.signedTx}
+                            rows={4}
+                            readOnly={true}
+                          />
+                        </div>
+                      </div>
 
-        {transaction &&
-          showTxConfirm && (
-            <ConfirmationModal
-              wallet={this.props.wallet}
-              node={this.props.node}
-              signedTx={transaction.signedTx}
-              onClose={this.hideConfirmTx}
-              onConfirm={this.confirmTx}
-            />
-          )}
-      </section>
+                      <div className="form-group">
+                        <button
+                          className="btn btn-primary btn-block col-sm-11"
+                          disabled={!this.state.transaction}
+                          onClick={this.openTxModal}
+                        >
+                          {translate('SEND_trans')}
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </main>
+            )}
+
+            {/* Sidebar */}
+            {unlocked && (
+              <section className="col-sm-4">
+                <BalanceSidebar />
+              </section>
+            )}
+          </div>
+
+          {transaction &&
+            showTxConfirm && (
+              <ConfirmationModal
+                wallet={this.props.wallet}
+                node={this.props.node}
+                signedTx={transaction.signedTx}
+                onClose={this.hideConfirmTx}
+                onConfirm={this.confirmTx}
+              />
+            )}
+        </section>
+      </App>
     );
   }
 

common/containers/Tabs/Swap/index.tsx
@@ -40,6 +40,7 @@ import CurrentRates from './components/CurrentRates';
 import PartThree from './components/PartThree';
 import ReceivingAddress from './components/ReceivingAddress';
 import SwapInfoHeader from './components/SwapInfoHeader';
+import App from 'containers/App';
 
 interface ReduxStateProps {
   originAmount: number | null;
@@ -187,18 +188,20 @@ class Swap extends Component<ReduxActionProps & ReduxStateProps, {}> {
     };
 
     return (
-      <section className="Tab-content swap-tab">
-        {step === 1 && <CurrentRates {...CurrentRatesProps} />}
-        {(step === 2 || step === 3) && (
-            <SwapInfoHeader {...SwapInfoHeaderProps} />
-          )}
+      <App>
+        <section className="Tab-content swap-tab">
+          {step === 1 && <CurrentRates {...CurrentRatesProps} />}
+          {(step === 2 || step === 3) && (
+              <SwapInfoHeader {...SwapInfoHeaderProps} />
+            )}
 
-        <main className="Tab-content-pane">
-          {step === 1 && <CurrencySwap {...CurrencySwapProps} />}
-          {step === 2 && <ReceivingAddress {...ReceivingAddressProps} />}
-          {step === 3 && <PartThree {...PartThreeProps} />}
-        </main>
-      </section>
+          <main className="Tab-content-pane">
+            {step === 1 && <CurrencySwap {...CurrencySwapProps} />}
+            {step === 2 && <ReceivingAddress {...ReceivingAddressProps} />}
+            {step === 3 && <PartThree {...PartThreeProps} />}
+          </main>
+        </section>
+      </App>
     );
   }
 }

common/containers/Tabs/ViewWallet/index.tsx
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import translate from 'translations';
+import App from 'containers/App';
 
 export default class ViewWallet extends Component {
   public render() {
     return (
-      <section className="container">
-        <div className="tab-content">
-          <article className="tab-pane active">
-            <article className="collapse-container">
-              <div>
-                <h1>View Wallet Info</h1>
-              </div>
-              <div>
-                <p>
-                  {translate('VIEWWALLET_Subtitle')}
-                </p>
-              </div>
+      <App>
+        <section className="container">
+          <div className="tab-content">
+            <article className="tab-pane active">
+              <article className="collapse-container">
+                <div>
+                  <h1>View Wallet Info</h1>
+                </div>
+                <div>
+                  <p>{translate('VIEWWALLET_Subtitle')}</p>
+                </div>
+              </article>
             </article>
-          </article>
-        </div>
-      </section>
+          </div>
+        </section>
+      </App>
     );
   }
 }

commit 6f7e3c27e2f984f28021e0c4fd03b1c948a9c7a0

Fix regressions introduced in #241 (#247)
* Fix location query

* Remove unnecessary polyfill

* Fix tslint error & update history types

* Parse search str using Node Query String

* Revert "Parse search str using Node Query String"

This reverts commit 0a482dabf29c3fbcbfd3112b974d6b98c14dca2e.

* fix formatting

* Add query-string

* Rename App container to TabSection & pass it location prop

* Add react-router withRouter HOC to NavigationLink

* various adjustments/finalizations

* Add location to props interface

common/components/Header/components/Navigation.tsx
@@ -36,7 +36,6 @@ const tabs = [
 ];
 
 interface Props {
-  location: object;
   color?: string;
 }
 
@@ -62,7 +61,7 @@ export default class Navigation extends Component<Props, State> {
    */
 
   public render() {
-    const { location, color } = this.props;
+    const { color } = this.props;
     const borderStyle: BorderStyle = {};
 
     if (color) {
@@ -80,7 +79,6 @@ export default class Navigation extends Component<Props, State> {
           <a
             aria-hidden="true"
             className="Navigation-arrow Navigation-arrow--left"
-            // onClick={() => this.scrollLeft()}
           >
             &#171;
           </a>
@@ -89,13 +87,7 @@ export default class Navigation extends Component<Props, State> {
         <div className="Navigation-scroll container">
           <ul className="Navigation-links">
             {tabs.map(link => {
-              return (
-                <NavigationLink
-                  key={link.name}
-                  link={link}
-                  location={location}
-                />
-              );
+              return <NavigationLink key={link.name} link={link} />;
             })}
           </ul>
         </div>
@@ -104,7 +96,6 @@ export default class Navigation extends Component<Props, State> {
           <a
             aria-hidden="true"
             className="Navigation-arrow Navigation-arrow-right"
-            // onClick={() => this.scrollRight()}
           >
             &#187;
           </a>

common/components/Header/components/NavigationLink.tsx
@@ -1,6 +1,6 @@
 import classnames from 'classnames';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import translate, { translateRaw } from 'translations';
 import './NavigationLink.scss';
 
@@ -10,12 +10,19 @@ interface Props {
     to?: string;
     external?: boolean;
   };
-  location: any;
 }
 
-export default class NavigationLink extends React.Component<Props, {}> {
+interface InjectedLocation extends Props {
+  location: { pathname: string };
+}
+
+class NavigationLink extends React.Component<Props, {}> {
+  get injected() {
+    return this.props as InjectedLocation;
+  }
   public render() {
-    const { link, location } = this.props;
+    const { link } = this.props;
+    const { location } = this.injected;
     const linkClasses = classnames({
       'NavigationLink-link': true,
       'is-disabled': !link.to,
@@ -48,3 +55,6 @@ export default class NavigationLink extends React.Component<Props, {}> {
     return <li className="NavigationLink">{linkEl}</li>;
   }
 }
+
+// withRouter is a HOC which provides NavigationLink with a react-router location prop
+export default withRouter(NavigationLink);

common/components/Header/index.tsx
@@ -17,7 +17,6 @@ import { getKeyByValue } from 'utils/helpers';
 import './index.scss';
 
 interface Props {
-  location: {};
   languageSelection: string;
   nodeSelection: string;
   gasPriceGwei: number;
@@ -124,10 +123,7 @@ export default class Header extends Component<Props, {}> {
           </section>
         </section>
 
-        <Navigation
-          location={this.props.location}
-          color={selectedNetwork.color}
-        />
+        <Navigation color={selectedNetwork.color} />
       </div>
     );
   }

common/containers/TabSection/NotificationRow.tsx
@@ -19,12 +19,8 @@ export default class NotificationRow extends Component<Props, {}> {
 
     return (
       <div className={notifClass} role="alert" aria-live="assertive">
-        <span className="sr-only">
-          {level}
-        </span>
-        <div className="Notification-message">
-          {msg}
-        </div>
+        <span className="sr-only">{level}</span>
+        <div className="Notification-message">{msg}</div>
         <button
           className="Notification-close"
           aria-label="dismiss"

common/containers/TabSection/Notifications.tsx
@@ -19,13 +19,13 @@ export class Notifications extends React.Component<Props, {}> {
     }
     return (
       <div className="Notifications">
-        {this.props.notifications.map((n, i) =>
+        {this.props.notifications.map((n, i) => (
           <NotificationRow
             key={`${n.level}-${i}`}
             notification={n}
             onClose={this.props.closeNotification}
           />
-        )}
+        ))}
       </div>
     );
   }

common/containers/TabSection/index.tsx
@@ -14,9 +14,6 @@ import Notifications from './Notifications';
 interface Props {
   // FIXME
   children: any;
-  location: any;
-  router: any;
-  isMobile: boolean;
 
   languageSelection: string;
   nodeSelection: string;
@@ -27,7 +24,7 @@ interface Props {
   changeNode: TChangeNode;
   changeGasPrice: TChangeGasPrice;
 }
-class App extends Component<Props, {}> {
+class TabSection extends Component<Props, {}> {
   public render() {
     const {
       children,
@@ -42,7 +39,6 @@ class App extends Component<Props, {}> {
     } = this.props;
 
     const headerProps = {
-      location,
       languageSelection,
       nodeSelection,
       gasPriceGwei,
@@ -78,4 +74,4 @@ export default connect(mapStateToProps, {
   changeGasPrice: dChangeGasPrice,
   changeLanguage: dChangeLanguage,
   changeNode: dChangeNode
-})(App);
+})(TabSection);

common/containers/Tabs/ENS/components/ENS.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { GeneralInfoPanel } from './GeneralInfoPanel';
 import Title from './Title';
 import UnfinishedBanner from './UnfinishedBanner';
-import App from 'containers/App';
+import TabSection from 'containers/TabSection';
 
 interface ContainerTabPaneActiveProps {
   children: React.ReactElement<any> | React.ReactElement<any>[];
 }
 
 const ContainerTabPaneActive = ({ children }: ContainerTabPaneActiveProps) => (
-  <App>
+  <TabSection>
     <section className="container">
       <div className="tab-content">
         <main className="tab-pane active">
@@ -19,7 +19,7 @@ const ContainerTabPaneActive = ({ children }: ContainerTabPaneActiveProps) => (
         </main>
       </div>
     </section>
-  </App>
+  </TabSection>
 );
 
 const ENS = () => (

common/containers/Tabs/GenerateWallet/index.tsx
@@ -13,7 +13,7 @@ import { AppState } from 'reducers';
 import DownloadWallet from './components/DownloadWallet';
 import EnterPassword from './components/EnterPassword';
 import PaperWallet from './components/PaperWallet';
-import App from 'containers/App';
+import TabSection from 'containers/TabSection';
 
 interface Props {
   // Redux state
@@ -73,9 +73,9 @@ class GenerateWallet extends Component<Props, {}> {
     }
 
     return (
-      <App>
+      <TabSection>
         <section className="Tab-content">{content}</section>
-      </App>
+      </TabSection>
     );
   }
 }

common/containers/Tabs/Help/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import translate from 'translations';
-import App from 'containers/App';
+import TabSection from 'containers/TabSection';
 
 const Help = () => (
-  <App>
+  <TabSection>
     <section className="container" style={{ minHeight: '50%' }}>
       <div className="tab-content">
         <article className="tab-pane help active">
@@ -41,7 +41,7 @@ const Help = () => (
         </article>
       </div>
     </section>
-  </App>
+  </TabSection>
 );
 
 export default Help;

common/containers/Tabs/SendTransaction/index.tsx
@@ -5,7 +5,7 @@ import { BalanceSidebar } from 'components';
 // COMPONENTS
 import { UnlockHeader } from 'components/ui';
 import { donationAddressMap, NetworkConfig, NodeConfig } from 'config/data';
-import App from 'containers/App';
+import TabSection from 'containers/TabSection';
 // CONFIG
 import { TransactionWithoutGas } from 'libs/messages';
 import { RPCNode } from 'libs/nodes';
@@ -51,6 +51,7 @@ import {
   DataField,
   GasField
 } from './components';
+import queryString from 'query-string';
 // MISC
 import customMessages from './messages';
 
@@ -80,11 +81,6 @@ interface State {
 }
 
 interface Props {
-  location: {
-    query: {
-      [key: string]: string;
-    };
-  };
   wallet: IWallet;
   balance: Ether;
   node: NodeConfig;
@@ -96,6 +92,7 @@ interface Props {
   transactions: BroadcastTransactionStatus[];
   showNotification: TShowNotification;
   broadcastTx: TBroadcastTx;
+  location: { search: string };
 }
 
 const initialState: State = {
@@ -177,7 +174,7 @@ export class SendTransaction extends React.Component<Props, State> {
     const customMessage = customMessages.find(m => m.to === to);
 
     return (
-      <App>
+      <TabSection>
         <section className="Tab-content">
           <UnlockHeader title={'NAV_SendEther'} />
 
@@ -287,12 +284,15 @@ export class SendTransaction extends React.Component<Props, State> {
               />
             )}
         </section>
-      </App>
+      </TabSection>
     );
   }
 
   public parseQuery() {
-    const query = this.props.location.query;
+    const searchStr = this.props.location.search;
+    console.log(searchStr);
+    const query = queryString.parse(searchStr);
+    console.log(query);
     const to = getParam(query, 'to');
     const data = getParam(query, 'data');
     // FIXME validate token against presets

common/containers/Tabs/Swap/index.tsx
@@ -40,7 +40,7 @@ import CurrentRates from './components/CurrentRates';
 import PartThree from './components/PartThree';
 import ReceivingAddress from './components/ReceivingAddress';
 import SwapInfoHeader from './components/SwapInfoHeader';
-import App from 'containers/App';
+import TabSection from 'containers/TabSection';
 
 interface ReduxStateProps {
   originAmount: number | null;
@@ -188,7 +188,7 @@ class Swap extends Component<ReduxActionProps & ReduxStateProps, {}> {
     };
 
     return (
-      <App>
+      <TabSection>
         <section className="Tab-content swap-tab">
           {step === 1 && <CurrentRates {...CurrentRatesProps} />}
           {(step === 2 || step === 3) && (
@@ -201,7 +201,7 @@ class Swap extends Component<ReduxActionProps & ReduxStateProps, {}> {
             {step === 3 && <PartThree {...PartThreeProps} />}
           </main>
         </section>
-      </App>
+      </TabSection>
     );
   }
 }

common/containers/Tabs/ViewWallet/index.tsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import translate from 'translations';
-import App from 'containers/App';
+import TabSection from 'containers/TabSection';
 
 export default class ViewWallet extends Component {
   public render() {
     return (
-      <App>
+      <TabSection>
         <section className="container">
           <div className="tab-content">
             <article className="tab-pane active">
@@ -20,7 +20,7 @@ export default class ViewWallet extends Component {
             </article>
           </div>
         </section>
-      </App>
+      </TabSection>
     );
   }
 }

common/containers/index.ts
@@ -1,2 +1,2 @@
-import App from './App';
-export default App;
+import TabSection from './TabSection';
+export default TabSection;