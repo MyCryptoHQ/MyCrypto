aefb155b82d54aa2102b6248d5aa9817010b0045
James Prado <james-prado@users.noreply.github.com>
2017-10-04 00:30:10 -0400

    Remove console logs (#262)

--- common/containers/Tabs/SendTransaction/index.tsx
+++ common/containers/Tabs/SendTransaction/index.tsx
@@ -290,9 +290,7 @@ export class SendTransaction extends React.Component<Props, State> {
 
   public parseQuery() {
     const searchStr = this.props.location.search;
-    console.log(searchStr);
     const query = queryString.parse(searchStr);
-    console.log(query);
     const to = getParam(query, 'to');
     const data = getParam(query, 'data');
     // FIXME validate token against presets
