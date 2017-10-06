992c52cc7ca5963a7357835a1c7fee61203ec4d4
Daniel Ternyak <dternyak@gmail.com>
2017-10-06 15:36:46 -0700

    - allow size to be specified in offline toggle

--- common/components/BalanceSidebar/OfflineToggle.tsx
+++ common/components/BalanceSidebar/OfflineToggle.tsx
@@ -6,22 +6,25 @@ import {
 import OfflineSymbol from 'components/ui/OfflineSymbol';
 import { connect } from 'react-redux';
 
+type sizeType = 'small' | 'medium' | 'large';
+
 interface OfflineToggleProps {
   offline: boolean;
   forceOffline: boolean;
   forceOfflineConfig: TForceOfflineConfig;
+  size?: sizeType;
 }
 
 class OfflineToggle extends React.Component<OfflineToggleProps, {}> {
   public render() {
-    const { forceOfflineConfig, offline, forceOffline } = this.props;
+    const { forceOfflineConfig, offline, forceOffline, size } = this.props;
 
     return (
       <div>
         {!offline ? (
           <div className="row text-center">
             <div className="col-md-3">
-              <OfflineSymbol offline={offline || forceOffline} size="small" />
+              <OfflineSymbol offline={offline || forceOffline} size={size} />
             </div>
             <div className="col-md-6">
               <button className="btn-xs btn-info" onClick={forceOfflineConfig}>
