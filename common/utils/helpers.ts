4b615dfad429bec3746464be3cf88874ce3593a9
Daniel Ternyak <dternyak@gmail.com>
2017-10-06 15:40:47 -0700

    move getParam into helper util

--- common/utils/helpers.ts
+++ common/utils/helpers.ts
@@ -1,3 +1,12 @@
 export function getKeyByValue(object, value) {
   return Object.keys(object).find(key => object[key] === value);
 }
+
+export function getParam(query: { [key: string]: string }, key: string) {
+  const keys = Object.keys(query);
+  const index = keys.findIndex(k => k.toLowerCase() === key.toLowerCase());
+  if (index === -1) {
+    return null;
+  }
+  return query[keys[index]];
+}
