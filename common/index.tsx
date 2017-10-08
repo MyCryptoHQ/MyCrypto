c7fce4035408ebd30b415b27516703bd0819ee2a
Will O'Beirne <wbobeirne@gmail.com>
2017-10-08 12:02:21 -0700

    Move all checks directly to HTML to avoid issues.

--- common/index.tsx
+++ common/index.tsx
@@ -6,27 +6,14 @@ import { render } from 'react-dom';
 import { Root } from './components';
 import createHistory from 'history/createBrowserHistory';
 import { configuredStore } from './store';
-import checkBadBrowser from './utils/checkBadBrowser';
 import 'sass/styles.scss';
 
-// Check if they have a bad browser before rendering root.
-if (checkBadBrowser()) {
-  const el = document.getElementsByClassName('BadBrowser')[0];
-  el.classList.add('is-open');
-  // Dumb check for known mobile OS's. Not important to catch all, just
-  // displays more appropriate information.
-  if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-    el.classList.add('is-mobile');
-  }
-}
-else {
-  const history = createHistory();
+const history = createHistory();
 
-  render(
-    <Root store={configuredStore} history={history} />,
-    document.getElementById('app')
-  );
-}
+render(
+  <Root store={configuredStore} history={history} />,
+  document.getElementById('app')
+);
 
 if (module.hot) {
   module.hot.accept('reducers', () =>
