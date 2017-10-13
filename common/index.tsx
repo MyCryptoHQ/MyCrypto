4fb542e8bf80ef31be750d38271a13d55699de44
Daniel Ternyak <dternyak@gmail.com>
2017-10-12 21:28:13 -0700

    support Root HMR

--- common/index.tsx
+++ common/index.tsx
@@ -3,20 +3,23 @@ import 'assets/styles/etherwallet-master.less';
 import 'font-awesome/scss/font-awesome.scss';
 import React from 'react';
 import { render } from 'react-dom';
-import { Root } from './components';
+import Root from './Root';
 import createHistory from 'history/createBrowserHistory';
 import { configuredStore } from './store';
 import 'sass/styles.scss';
 
 const history = createHistory();
 
-render(
-  <Root store={configuredStore} history={history} />,
-  document.getElementById('app')
-);
+const appEl = document.getElementById('app');
+
+render(<Root store={configuredStore} history={history} />, appEl);
 
 if (module.hot) {
   module.hot.accept('reducers', () =>
     configuredStore.replaceReducer(require('reducers'))
   );
+
+  module.hot.accept('./Root', () => {
+    render(<Root store={configuredStore} history={history} />, appEl);
+  });
 }
