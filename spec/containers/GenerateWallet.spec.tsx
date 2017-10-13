232e00b1874c0e3cbb567b05896ce3d9652788e9
james-prado <jamesprado7@gmail.com>
2017-10-13 14:19:26 -0400

    Issue with jest & es5 imports

---
+++ spec/containers/GenerateWallet.spec.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Enzyme, { shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import GenerateWallet from '../../common/containers/Tabs/GenerateWallet';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const Test = () => <p>qwerty</p>;
+
+it('render snapshot', () => {
+  const wrapper = shallow(<GenerateWallet />);
+  expect(wrapper).toMatchSnapshot();
+});
