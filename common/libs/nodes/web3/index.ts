05a0b5879b8f62fc20a2d4630b8c8fafc1c5c947
danny <skubakdj@gmail.com>
2017-10-18 13:32:12 -0400

    add web3 node support

---
+++ common/libs/nodes/web3/index.ts
@@ -0,0 +1,126 @@
+import Big, { BigNumber } from 'bignumber.js';
+import { Token } from 'config/data';
+import { TransactionWithoutGas } from 'libs/messages';
+import { Wei } from 'libs/units';
+import { INode } from '../INode';
+import ERC20 from 'libs/erc20';
+
+export default class Web3Node implements INode {
+  private web3: any;
+
+  constructor(web3: any) {
+    this.web3 = web3;
+  }
+
+  public getBalance(address: string): Promise<Wei> {
+    return new Promise((resolve, reject) => {
+      this.web3.eth.getBalance(address, (err, res) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(new Wei(res.toString()));
+      });
+    });
+  }
+
+  public estimateGas(transaction: TransactionWithoutGas): Promise<BigNumber> {
+    return new Promise((resolve, reject) =>
+      this.web3.eth.estimateGas(
+        {
+          to: transaction.to,
+          data: transaction.data
+        },
+        (err, res) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(new Big(res.toString()));
+        }
+      )
+    );
+  }
+
+  public getTokenBalance(address: string, token: Token): Promise<BigNumber> {
+    return new Promise((resolve, reject) => {
+      this.web3.eth.call(
+        {
+          to: token.address,
+          data: ERC20.balanceOf(address)
+        },
+        'pending',
+        (err, res) => {
+          if (err) {
+            // TODO - Error handling
+            return resolve(new Big(0));
+          }
+
+          resolve(new Big(res.toString()).div(new Big(10).pow(token.decimal)));
+        }
+      );
+    });
+  }
+
+  public getTokenBalances(
+    address: string,
+    tokens: Token[]
+  ): Promise<BigNumber[]> {
+    return new Promise((resolve, reject) => {
+      const batch = this.web3.createBatch();
+      const totalCount = tokens.length;
+      const returnArr = new Array<BigNumber>(totalCount);
+      let finishCount = 0;
+
+      tokens.forEach((token, index) =>
+        batch.add(
+          this.web3.eth.call.request(
+            {
+              to: token.address,
+              data: ERC20.balanceOf(address)
+            },
+            'pending',
+            (err, res) => finish(token, index, err, res)
+          )
+        )
+      );
+      batch.execute();
+
+      function finish(token, index, err, res) {
+        if (err) {
+          // TODO - Error handling
+          returnArr[index] = new Big(0);
+        } else {
+          returnArr[index] = new Big(res.toString()).div(
+            new Big(10).pow(token.decimal)
+          );
+        }
+
+        finishCount++;
+        if (finishCount === totalCount) {
+          resolve(returnArr);
+        }
+      }
+    });
+  }
+
+  public getTransactionCount(address: string): Promise<string> {
+    return new Promise((resolve, reject) =>
+      this.web3.eth.getTransactionCount(address, 'pending', (err, txCount) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(txCount);
+      })
+    );
+  }
+
+  public sendRawTx(signedTx: string): Promise<string> {
+    return new Promise((resolve, reject) =>
+      this.web3.eth.sendRawTransaction(signedTx, (err, txHash) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(txHash);
+      })
+    );
+  }
+}
