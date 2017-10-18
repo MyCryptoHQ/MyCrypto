25faea9900fecfccea3df76ccc361725e75d2c59
danny <skubakdj@gmail.com>
2017-10-18 15:10:57 -0400

    correct spacing display issue in Web3 component

--- common/components/WalletDecrypt/Web3.tsx
+++ common/components/WalletDecrypt/Web3.tsx
@@ -11,21 +11,25 @@ export default class Web3Decrypt extends Component<Props> {
   public render() {
     return (
       <section className="Web3Decrypt col-md-4 col-sm-6">
-        <button
-          className="Web3Decrypt btn btn-primary btn-lg"
-          onClick={this.props.onUnlock}
-        >
-          {translate('ADD_MetaMask')}
-        </button>
+        <div>
+          <button
+            className="Web3Decrypt btn btn-primary btn-lg"
+            onClick={this.props.onUnlock}
+          >
+            {translate('ADD_MetaMask')}
+          </button>
+        </div>
 
-        <a
-          className="Web3Decrypt-install btn btn-sm btn-default"
-          href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en"
-          target="_blank"
-          rel="noopener"
-        >
-          {translate('Download MetaMask')}
-        </a>
+        <div>
+          <a
+            className="Web3Decrypt-install btn btn-sm btn-default"
+            href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en"
+            target="_blank"
+            rel="noopener"
+          >
+            {translate('Download MetaMask')}
+          </a>
+        </div>
       </section>
     );
   }
