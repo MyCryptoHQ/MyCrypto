fbfe305a6fae1f13b93c667c1c64194441a53df9
danny <skubakdj@gmail.com>
2017-10-18 13:37:37 -0400

    add web3 wallet & web3 wallet ui selection

---
+++ common/libs/wallet/web3.ts
@@ -0,0 +1,68 @@
+import { IWallet } from './IWallet';
+import { ExtendedRawTransaction, RawTransaction } from 'libs/transaction';
+import { networkIdToName } from 'libs/values';
+
+export default class Web3Wallet implements IWallet {
+  private web3: any;
+  private address: string;
+  private network: string;
+
+  constructor(web3: any, address: string, network: string) {
+    this.web3 = web3;
+    this.address = address;
+    this.network = network;
+  }
+
+  public getAddress(): Promise<string> {
+    return Promise.resolve(this.address);
+  }
+
+  public signRawTransaction(rawTx: RawTransaction): Promise<string> {
+    return Promise.reject(
+      new Error('Web3 wallets cannot sign raw transactions.')
+    );
+  }
+
+  public sendTransaction(transaction: ExtendedRawTransaction): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const { from, to, value, gasLimit, gasPrice, data, nonce } = transaction;
+
+      const web3Tx = {
+        from,
+        to,
+        value,
+        gas: gasLimit,
+        gasPrice,
+        data,
+        nonce
+      };
+
+      // perform sanity check to ensure network hasn't changed
+      this.web3.version.getNetwork((err1, networkId) => {
+        const networkName = networkIdToName(networkId);
+
+        if (err1) {
+          return reject(err1);
+        }
+
+        if (this.network !== networkName) {
+          return reject(
+            new Error(
+              `Expected MetaMask / Mist network to be ${this
+                .network}, but got ${networkName}. ` +
+                `Please change the network or restart MyEtherWallet.`
+            )
+          );
+        }
+
+        // execute transaction
+        this.web3.eth.sendTransaction(web3Tx, (err2, txHash) => {
+          if (err2) {
+            return reject(err2);
+          }
+          resolve(txHash);
+        });
+      });
+    });
+  }
+}
