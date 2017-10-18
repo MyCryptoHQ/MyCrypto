5e904fc18e0eee9d567ba3fd91427db906c3f65d
Daniel Ternyak <dternyak@gmail.com>
2017-10-18 14:48:34 -0700

    - add sanitizeHex and padLeftEven functions from V3

--- common/libs/values.ts
+++ common/libs/values.ts
@@ -14,3 +14,15 @@ export function valueToHex(value: Ether): string {
   // Finally, hex it up!
   return `0x${wei.toString(16)}`;
 }
+
+export function padLeftEven(hex) {
+  return hex.length % 2 !== 0 ? '0' + hex : hex;
+}
+
+export function sanitizeHex(hex) {
+  hex = hex.substring(0, 2) === '0x' ? hex.substring(2) : hex;
+  if (hex === '') {
+    return '';
+  }
+  return '0x' + padLeftEven(hex);
+}
