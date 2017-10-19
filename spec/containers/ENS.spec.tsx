5e99be5bc4f38376c65faa63ab87b114e3399fe7
james-prado <jamesprado7@gmail.com>
2017-10-19 15:05:51 -0400

    Add basic snapshot tests for each pages

--- spec/containers/ENS.spec.tsx
+++ spec/containers/ENS.spec.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import Enzyme, { shallow, mount, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import GenerateWallet from 'containers/Tabs/GenerateWallet';
+import ENS from 'containers/Tabs/ENS';
 import shallowWithStore from '../utils/shallowWithStore';
 import { createMockStore } from 'redux-test-utils';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 it('render snapshot', () => {
-  const testState = {
-    form: {
-      walletPasswordForm: {}
-    },
-    generateWallet: {
-      activeStep: {},
-      password: {},
-      wallet: {}
-    }
-  };
+  const testState = {};
   const store = createMockStore(testState);
-  const component = shallowWithStore(<GenerateWallet />, store);
+  const component = shallowWithStore(<ENS />, store);
 
   expect(component).toMatchSnapshot();
 });
