70fe6cd61f0b58bb842eb00af63cc786d2a6e52c
Daniel Ternyak <dternyak@gmail.com>
2017-10-23 13:41:35 -0700

    Address PR comments

--- common/libs/values.ts
+++ common/libs/values.ts
@@ -15,14 +15,15 @@ export function valueToHex(value: Ether): string {
   return `0x${wei.toString(16)}`;
 }
 
-export function padLeftEven(hex) {
-  return hex.length % 2 !== 0 ? '0' + hex : hex;
+export function padLeftEven(hex: string) {
+  return hex.length % 2 !== 0 ? `0${hex}` : hex;
 }
 
-export function sanitizeHex(hex) {
+// TODO: refactor to not mutate argument
+export function sanitizeHex(hex: string) {
   hex = hex.substring(0, 2) === '0x' ? hex.substring(2) : hex;
   if (hex === '') {
     return '';
   }
-  return '0x' + padLeftEven(hex);
+  return `0x${padLeftEven(hex)}`;
 }
