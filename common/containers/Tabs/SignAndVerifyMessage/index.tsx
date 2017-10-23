f8c27622f28afbdfbb7a6b1a0a6eead593f03665
danny <skubakdj@gmail.com>
2017-10-23 16:08:16 -0400

    correct var to start on sign tab

--- common/containers/Tabs/SignAndVerifyMessage/index.tsx
+++ common/containers/Tabs/SignAndVerifyMessage/index.tsx
@@ -11,7 +11,7 @@ interface State {
 
 export default class SignAndVerifyMessage extends Component<{}, State> {
   public state: State = {
-    activeTab: 'interact'
+    activeTab: 'sign'
   };
 
   public changeTab = activeTab => () => this.setState({ activeTab });
