0b1b06e0b50212bdefe2dd3d6773eb68385d39c2
HenryNguyen5 <ngu.henry@hotmail.com>
2017-11-02 15:37:45 -0400

    Change import type

--- common/containers/Tabs/ENS/components/ENS.tsx
+++ common/containers/Tabs/ENS/components/ENS.tsx
@@ -3,7 +3,7 @@ import Title from './Title';
 import { GeneralInfoPanel } from './GeneralInfoPanel';
 import UnfinishedBanner from './UnfinishedBanner';
 import NameInput from './NameInput';
-import NameResolve from './NameResolve';
+import { NameResolve } from './NameResolve';
 import { AppState } from 'reducers';
 import TabSection from 'containers/TabSection';
 
