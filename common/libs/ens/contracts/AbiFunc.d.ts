dd8c4b00657b5d9594396f580fe7b927c2ff6b6a
HenryNguyen5 <ngu.henry@hotmail.com>
2017-11-02 15:35:50 -0400

    Fix Abi typedef

--- common/libs/ens/contracts/AbiFunc.d.ts
+++ common/libs/ens/contracts/AbiFunc.d.ts
@@ -1,16 +1,31 @@
 import { RawTransaction } from 'libs/transaction';
+import { IUserSendParams } from 'libs/contracts/ABIFunction';
+import BN from 'bn.js';
+
 interface SendReturn {
   signedTx: string;
   rawTx: RawTransaction;
 }
+
+interface ISend<T> extends IUserSendParams {
+  input: T;
+}
+
+interface ISendParamless {
+  to?: string;
+  gasLimit: BN;
+  value: string;
+}
+
 export interface ABIFunc<T, K = void> {
-  send(x: T): Promise<SendReturn>;
+  send(x: ISend<T>): Promise<SendReturn>;
   call(x: T): Promise<K>;
   encodeOutput(x: T): string;
   decodeOutput(argStr: string): K;
 }
+
 export interface ABIFuncParamless<T = void> {
-  send(): Promise<SendReturn>;
+  send(x: ISendParamless): Promise<SendReturn>;
   call(): Promise<T>;
   encodeOutput(): string;
   decodeOutput(argStr: string): T;
