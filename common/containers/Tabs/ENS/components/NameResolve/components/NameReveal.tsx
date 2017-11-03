716baeb78748eb1a1a9dea423b09ca6cee4e92d8
HenryNguyen5 <ngu.henry@hotmail.com>
2017-11-03 19:34:45 -0400

    Add unit display for highest bidder

--- common/containers/Tabs/ENS/components/NameResolve/components/NameReveal.tsx
+++ common/containers/Tabs/ENS/components/NameResolve/components/NameReveal.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { IRevealDomainRequest } from 'libs/ens';
 import { EnsTime } from './CountDown';
+import { UnitDisplay } from 'components/ui';
+import { Wei } from 'libs/units';
 
 export const NameReveal: React.SFC<IRevealDomainRequest> = props => {
   return (
@@ -9,7 +11,17 @@ export const NameReveal: React.SFC<IRevealDomainRequest> = props => {
         <p>
           It's time to reveal the bids for <strong>{props.name}.eth.</strong>{' '}
         </p>
-        <p>Current Highest bid is </p>
+        <p>
+          Current Highest bid is {' '}
+          <strong>
+            <UnitDisplay
+              value={Wei(props.highestBid)}
+              unit="ether"
+              symbol="ETH"
+              long={true}
+            />
+          </strong>
+        </p>
       </h1>
       <EnsTime text="Auction closes on" time={+props.registrationDate * 1000} />
     </section>
