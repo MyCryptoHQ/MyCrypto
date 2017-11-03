d715d68f0f067b02a20ebc8d3aa44b0ae9c017f9
HenryNguyen5 <ngu.henry@hotmail.com>
2017-11-03 19:19:55 -0400

    Implement reveal search functionality

--- common/containers/Tabs/ENS/components/NameResolve/components/NameAuction.tsx
+++ common/containers/Tabs/ENS/components/NameResolve/components/NameAuction.tsx
@@ -1,60 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { IBaseDomainRequest } from 'libs/ens';
+import { EnsTime } from './CountDown';
 import moment from 'moment';
-
-class CountDown extends Component<
-  {
-    initialTime: number;
-  },
-  {
-    currentTime: number;
-  }
-> {
-  public state = { currentTime: 0 };
-  constructor() {
-    super();
-    this.startCountDown();
-  }
-
-  public render() {
-    const { currentTime } = this.state;
-    return <p>{this.humanizeTime(currentTime)}</p>;
-  }
-
-  private humanizeTime = (time: number) => {
-    let timeRemaining = time;
-    const floorTime = unit => Math.floor(timeRemaining / unit);
-    const pad = (num: number) => num.toString().padStart(2, '0');
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    const days = floorTime(day);
-    timeRemaining -= days * day;
-    const hours = floorTime(hour);
-    timeRemaining -= hours * hour;
-    const minutes = floorTime(minute);
-    timeRemaining -= minutes * minute;
-    const seconds = floorTime(second);
-
-    return `${pad(days)} Days ${pad(hours)} Hours ${pad(minutes)} Minutes ${pad(
-      seconds
-    )} Seconds `;
-  };
-
-  private startCountDown = () => {
-    const intervalId = window.setInterval(() => {
-      const nextTime = +moment(this.props.initialTime).diff(+moment(), 'ms');
-
-      if (nextTime < 0) {
-        return clearInterval(intervalId);
-      }
-
-      this.setState({ currentTime: nextTime });
-    }, 1000);
-  };
-}
 
 const getDeadlines = (registrationDate: string) => {
   // Get the time to reveal bids, and the time when the action closes
@@ -63,19 +10,6 @@ const getDeadlines = (registrationDate: string) => {
   const revealBidTime = +time.subtract(2, 'days');
   return { auctionCloseTime, revealBidTime };
 };
-
-interface ITime {
-  text: string;
-  time: number;
-}
-
-const EnsTime: React.SFC<ITime> = ({ text, time }) => (
-  <div className="sm-6 col-xs-12 order-info">
-    <p>{text}</p>
-    <h4>{moment(time).toString()}</h4>
-    <CountDown initialTime={time} />
-  </div>
-);
 
 export const NameAuction: React.SFC<IBaseDomainRequest> = ({
   registrationDate,
--- common/containers/Tabs/ENS/components/NameResolve/components/NameReveal.tsx
+++ common/containers/Tabs/ENS/components/NameResolve/components/NameReveal.tsx
@@ -1,4 +1,17 @@
 import React from 'react';
 import { IRevealDomainRequest } from 'libs/ens';
+import { EnsTime } from './CountDown';
 
-export const NameReveal: React.SFC<IRevealDomainRequest> = props => null;
+export const NameReveal: React.SFC<IRevealDomainRequest> = props => {
+  return (
+    <section className="row text-center">
+      <h1>
+        <p>
+          It's time to reveal the bids for <strong>{props.name}.eth.</strong>{' '}
+        </p>
+        <p>Current Highest bid is </p>
+      </h1>
+      <EnsTime text="Auction closes on" time={+props.registrationDate * 1000} />
+    </section>
+  );
+};
