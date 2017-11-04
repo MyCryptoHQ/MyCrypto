18348a9626f1d36e39979f5e0fa6dfd9f89cb3ad
HenryNguyen5 <ngu.henry@hotmail.com>
2017-11-03 20:18:28 -0400

    Fill out forbidden/NYA modes

--- common/containers/Tabs/ENS/components/NameResolve/components/NameForbidden.tsx
+++ common/containers/Tabs/ENS/components/NameResolve/components/NameForbidden.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import { IBaseDomainRequest } from 'libs/ens';
 
-export const NameForbidden: React.SFC<IBaseDomainRequest> = props => null;
+export const NameForbidden: React.SFC<IBaseDomainRequest> = props => (
+  <h1>The name {props.name}.eth is forbidden</h1>
+);
