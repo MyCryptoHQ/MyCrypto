857f8386bcdfd26daf82ac4c80c92b4028c2a368
HenryNguyen5 <ngu.henry@hotmail.com>
2017-11-13 17:20:08 -0500

    Add gas query and token query render cbs

---
+++ common/components/renderCbs/TokenQuery.tsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Query } from './Query';
+import { connect } from 'react-redux';
+import { AppState } from 'reducers';
+import { getTokens, MergedToken } from 'selectors/wallet';
+
+interface Props {
+  tokens: MergedToken[];
+  withQuery({ token }: { token: MergedToken | null | undefined });
+}
+
+class TokenQueryClass extends Component<Props, {}> {
+  public render() {
+    return (
+      <Query
+        params={['unit']}
+        withQuery={({ unit }) => this.props.withQuery(this.paramGetter(unit))}
+      />
+    );
+  }
+  private paramGetter = (unit: string | null) =>
+    unit
+      ? { token: this.props.tokens.find(t => t.symbol === unit) }
+      : { token: null };
+}
+
+export const TokenQuery = connect((state: AppState) => ({
+  tokens: getTokens(state)
+}))(TokenQueryClass);
