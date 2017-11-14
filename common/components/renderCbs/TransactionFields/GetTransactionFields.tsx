83d28818e0226c9f1d96720d36d7f22d0a43bb5e
HenryNguyen5 <ngu.henry@hotmail.com>
2017-11-14 18:39:31 -0500

    Merge valid prop into componeent

--- common/components/renderCbs/TransactionFields/GetTransactionFields.tsx
+++ common/components/renderCbs/TransactionFields/GetTransactionFields.tsx
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { AppState } from 'reducers';
+import {
+  TypeKeys,
+  TransactionFieldPayloadedAction
+} from 'actions/transactionFields';
 
 interface ReduxProps {
   transactionFields: AppState['transactionFields'];
 }
 
-interface GetterProps {
+interface IValid {
+  valid: boolean;
+}
+
+type MergeValid<T> = { [P in keyof T]: T[P] & IValid };
+
+interface Props {
   withFieldValues(
-    values: AppState['transactionFields']
+    values: MergeValid<AppState['transactionFields']>
   ): React.ReactElement<any> | null;
 }
 
-class GetTransactionFieldsClass extends Component<
-  GetterProps & ReduxProps,
-  {}
-> {
+class GetTransactionFieldsClass extends Component<Props & ReduxProps, {}> {
   public render() {
-    return this.props.withFieldValues(this.props.transactionFields);
+    return this.props.withFieldValues(this.getMergedProps());
   }
+
+  private getMergedProps = (): MergeValid<AppState['transactionFields']> => {
+    const { transactionFields } = this.props;
+
+    return Object.keys(transactionFields).reduce((obj, currField: TypeKeys) => {
+      const prop: TransactionFieldPayloadedAction['payload'] =
+        transactionFields[currField];
+      const valid = !!prop.value;
+      const mergedProp = { ...prop, valid };
+      return { ...obj, [currField]: mergedProp };
+    }, {}) as MergeValid<AppState['transactionFields']>;
+  };
 }
 
 export const GetTransactionFields = connect((state: AppState) => ({
