f3b9009f5531512232d8bbdf746a1301965c0f58
HenryNguyen5 <ngu.henry@hotmail.com>
2017-11-20 13:56:55 -0500

    Handle query string default values in unit drop down

--- common/containers/Tabs/SendTransaction/components/Fields/components/AmountField/UnitDropDown.tsx
+++ common/containers/Tabs/SendTransaction/components/Fields/components/AmountField/UnitDropDown.tsx
@@ -1,18 +1,54 @@
-import React from 'react';
-import { SetDecimalMetaField, SetUnitMetaField } from 'components/renderCbs';
+import React, { Component } from 'react';
+import {
+  SetDecimalMetaField,
+  SetUnitMetaField,
+  TokenQuery
+} from 'components/renderCbs';
 import { ConditionalUnitDropdown } from './components';
+import { MergedToken } from 'selectors/wallet';
+import { SetDecimalMetaAction, SetUnitMetaAction } from 'actions/transaction';
 
 export const UnitDropDown: React.SFC<{}> = () => (
-  <SetDecimalMetaField
-    withDecimalSetter={decimalSetter => (
-      <SetUnitMetaField
-        withUnitSetter={unitSetter => (
-          <ConditionalUnitDropdown
-            onDecimalChange={decimalSetter}
-            onUnitChange={unitSetter}
+  <TokenQuery
+    withQuery={({ token }) => (
+      <SetDecimalMetaField
+        withDecimalSetter={decimalSetter => (
+          <SetUnitMetaField
+            withUnitSetter={unitSetter => (
+              <DefaultUnitDropDown
+                decimalSetter={decimalSetter}
+                unitSetter={unitSetter}
+                token={token}
+              />
+            )}
           />
         )}
       />
     )}
   />
 );
+
+interface Props {
+  token: MergedToken | undefined | null;
+  decimalSetter(payload: SetDecimalMetaAction['payload']);
+  unitSetter(payload: SetUnitMetaAction['payload']);
+}
+
+class DefaultUnitDropDown extends Component<Props, {}> {
+  public componentWillMount() {
+    const { token, decimalSetter, unitSetter } = this.props;
+    if (token) {
+      decimalSetter(token.decimal);
+      unitSetter(token.symbol);
+    }
+  }
+  public render() {
+    const { decimalSetter, unitSetter } = this.props;
+    return (
+      <ConditionalUnitDropdown
+        onDecimalChange={decimalSetter}
+        onUnitChange={unitSetter}
+      />
+    );
+  }
+}
