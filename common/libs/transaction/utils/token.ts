517a4740f0cd72a2e1d9d9a3574a691479586664
HenryNguyen5 <ngu.henry@hotmail.com>
2017-11-20 21:37:06 -0500

    Add helper function for encoding transfer data

--- common/libs/transaction/utils/token.ts
+++ common/libs/transaction/utils/token.ts
@@ -2,7 +2,7 @@ import { bufferToHex, toBuffer } from 'ethereumjs-util';
 import { Wei, TokenValue } from 'libs/units';
 import { ITransaction, ITokenTransaction } from '../typings';
 import ERC20 from 'libs/erc20';
-export { enoughTokens, makeTxObj, validateTokenBalance };
+export { enoughTokens, makeTxObj, validateTokenBalance, encodeTransfer };
 
 const enoughTokens = (t: ITransaction, tokenBalance: TokenValue) => {
   const { _value: value } = ERC20.transfer.decodeInput(bufferToHex(t.data!));
@@ -16,10 +16,11 @@ const validateTokenBalance = (t: ITransaction, tokenBalance: TokenValue) => {
 };
 const makeTxObj = (t: ITokenTransaction): ITransaction => {
   const { tokenValue, ...rest } = t;
-  const data = toBuffer(
-    ERC20.transfer.encodeInput({ _to: t.to, _value: tokenValue })
-  );
+  const data = encodeTransfer(t.to, tokenValue);
   const value = Wei('0');
 
   return { ...rest, data, value };
 };
+
+const encodeTransfer = (to: Buffer, value: TokenValue) =>
+  toBuffer(ERC20.transfer.encodeInput({ _to: to, _value: value }));
