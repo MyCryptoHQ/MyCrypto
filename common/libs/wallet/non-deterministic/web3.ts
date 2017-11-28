7b8390f22f4c4106f384daf7d3bd44f922e11fb7
danny <skubakdj@gmail.com>
2017-11-28 14:38:37 -0500

    remove comments

--- common/libs/wallet/non-deterministic/web3.ts
+++ common/libs/wallet/non-deterministic/web3.ts
@@ -27,28 +27,6 @@ export default class Web3Wallet implements IWallet {
     );
   }
 
-  // public signMessage(msg: string): Promise<string> {
-  //   return new Promise((resolve, reject) => {
-  //     const msgHex = bufferToHex(Buffer.from(msg));
-  //     const options = {
-  //       method: 'personal_sign',
-  //       params: [msgHex, this.address],
-  //       signingAddr: this.address
-  //     };
-
-  //     this.web3.currentProvider.sendAsync(options, (err, data) => {
-  //       if (err) {
-  //         return reject(err);
-  //       }
-
-  //       if (data.error) {
-  //         return reject(data.error);
-  //       }
-  //       resolve(data.result);
-  //     });
-  //   });
-  // }
-
   public async signMessage(msg: string): Promise<string> {
     const msgHex = bufferToHex(Buffer.from(msg));
     const state = configuredStore.getState();
@@ -86,56 +64,6 @@ export default class Web3Wallet implements IWallet {
 
     return nodeLib.sendTransaction(web3Tx);
   }
-
-  // public sendTransaction(transaction: ExtendedRawTransaction): Promise<string> {
-  //   return new Promise((resolve, reject) => {
-  //     const { from, to, value, gasLimit, gasPrice, data, nonce } = transaction;
-
-  //     const web3Tx = {
-  //       from,
-  //       to,
-  //       value,
-  //       gas: gasLimit,
-  //       gasPrice,
-  //       data,
-  //       nonce
-  //     };
-
-  //     // perform sanity check to ensure network hasn't changed
-  //     this.web3.version.getNetwork((err1, networkId) => {
-  //       const networkName = networkIdToName(networkId);
-
-  //       if (err1) {
-  //         return reject(err1);
-  //       }
-
-  //       if (this.network !== networkName) {
-  //         return reject(
-  //           new Error(
-  //             `Expected MetaMask / Mist network to be ${
-  //               this.network
-  //             }, but got ${networkName}. ` +
-  //               `Please change the network or restart MyEtherWallet.`
-  //           )
-  //         );
-  //       }
-
-  //       // execute transaction
-  //       this.web3.eth.sendTransaction(web3Tx, (err2, txHash) => {
-  //         if (err2) {
-  //           return reject(err2);
-  //         }
-  //         resolve(txHash);
-  //       });
-  //     });
-  //   });
-  // }
-
-  // private nodeCheck(nodeLib: Web3Node | INode) {
-  //   if (!this.isWeb3Node(nodeLib)) {
-  //     throw new Error('Expected node type to be Web3.')
-  //   }
-  // }
 
   private async networkCheck(lib: Web3Node) {
     const netId = await lib.getNetworkId();
