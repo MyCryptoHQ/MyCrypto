799ab05d07f0957794a901ad8edd9b7a563afebb
Daniel Ternyak <dternyak@gmail.com>
2017-12-07 18:17:23 -0800

    Convert ErrorScreen Component to SFC

--- common/components/ErrorScreen/index.tsx
+++ common/components/ErrorScreen/index.tsx
@@ -5,26 +5,26 @@ const SUBJECT = 'Error!';
 const DESCRIPTION =
   'I encountered an error while using MyEtherWallet. Here are the steps to re-create the issue: ...';
 
-export default class ErrorScreen extends React.Component<{}, {}> {
-  public render() {
-    return (
-      <div className={`ErrorScreen`}>
-        <div className="ErrorScreen-content">
-          <h2>Oops!</h2>
-          <p>Something went really wrong, so we're showing you this red error page! 😱</p>
-          <p>
-            Please contact{' '}
-            <a
-              target="_blank"
-              rel="noopener"
-              href={`mailto:support@myetherwallet.com?Subject=${SUBJECT}&body=${DESCRIPTION}`}
-            >
-              support@myetherwallet.com
-            </a>{' '}
-            if a refresh doesn't fix it (or click it anyway to open a ticket 😊 ).
-          </p>
-        </div>
+const ErrorScreen: React.SFC<{}> = () => {
+  return (
+    <div className={`ErrorScreen`}>
+      <div className="ErrorScreen-content">
+        <h2>Oops!</h2>
+        <p>Something went really wrong, so we're showing you this red error page! 😱</p>
+        <p>
+          Please contact{' '}
+          <a
+            target="_blank"
+            rel="noopener"
+            href={`mailto:support@myetherwallet.com?Subject=${SUBJECT}&body=${DESCRIPTION}`}
+          >
+            support@myetherwallet.com
+          </a>{' '}
+          if a refresh doesn't fix it (or click it anyway to open a ticket 😊 ).
+        </p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default ErrorScreen;
