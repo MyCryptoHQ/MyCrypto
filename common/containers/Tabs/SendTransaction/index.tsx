cf6557ddc32e870651854052f421a583d36cde93
james-prado <jamesprado7@gmail.com>
2017-12-09 00:48:07 -0500

    Add NavigationPrompts to WalletDecrypt

--- common/containers/Tabs/SendTransaction/index.tsx
+++ common/containers/Tabs/SendTransaction/index.tsx
@@ -13,7 +13,7 @@ import {
   GasField
 } from './components';
 import TransactionSucceeded from 'components/ExtendedNotifications/TransactionSucceeded';
-import NavigationPrompt from './components/NavigationPrompt';
+// import NavigationPrompt from './components/NavigationPrompt';
 // CONFIG
 import { donationAddressMap, NetworkConfig } from 'config/data';
 // LIBS
@@ -32,35 +32,17 @@ import {
 import { UnitKey, Wei, getDecimal, toWei } from 'libs/units';
 import { isValidETHAddress } from 'libs/validators';
 // LIBS
-import {
-  IWallet,
-  Balance,
-  Web3Wallet,
-  LedgerWallet,
-  TrezorWallet
-} from 'libs/wallet';
+import { IWallet, Balance, Web3Wallet, LedgerWallet, TrezorWallet } from 'libs/wallet';
 import pickBy from 'lodash/pickBy';
 import React from 'react';
 // REDUX
 import { connect } from 'react-redux';
 import { AppState } from 'reducers';
 import { showNotification, TShowNotification } from 'actions/notifications';
-import {
-  broadcastTx,
-  TBroadcastTx,
-  resetWallet,
-  TResetWallet
-} from 'actions/wallet';
-import {
-  pollOfflineStatus as dPollOfflineStatus,
-  TPollOfflineStatus
-} from 'actions/config';
+import { broadcastTx, TBroadcastTx, resetWallet, TResetWallet } from 'actions/wallet';
+import { pollOfflineStatus as dPollOfflineStatus, TPollOfflineStatus } from 'actions/config';
 // SELECTORS
-import {
-  getGasPriceGwei,
-  getNetworkConfig,
-  getNodeLib
-} from 'selectors/config';
+import { getGasPriceGwei, getNetworkConfig, getNodeLib } from 'selectors/config';
 import {
   getTokenBalances,
   getTokens,
@@ -267,9 +249,7 @@ export class SendTransaction extends React.Component<Props, State> {
     const { offline, forceOffline, balance } = this.props;
     const customMessage = customMessages.find(m => m.to === to);
     const decimal =
-      unit === 'ether'
-        ? getDecimal('ether')
-        : (this.state.token && this.state.token.decimal) || 0;
+      unit === 'ether' ? getDecimal('ether') : (this.state.token && this.state.token.decimal) || 0;
     const isWeb3Wallet = this.props.wallet instanceof Web3Wallet;
     const isLedgerWallet = this.props.wallet instanceof LedgerWallet;
     const isTrezorWallet = this.props.wallet instanceof TrezorWallet;
@@ -280,20 +260,13 @@ export class SendTransaction extends React.Component<Props, State> {
             title={
               <div>
                 {translate('NAV_SendEther')}
-                {offline || forceOffline ? (
-                  <span style={{ color: 'red' }}> (Offline)</span>
-                ) : null}
+                {offline || forceOffline ? <span style={{ color: 'red' }}> (Offline)</span> : null}
               </div>
             }
             allowReadOnly={true}
           />
-          <NavigationPrompt
-            when={unlocked}
-            onConfirm={this.props.resetWallet}
-          />
           <div className="row">
             {/* Send Form */}
-
             {unlocked &&
               !((offline || forceOffline) && isWeb3Wallet) && (
                 <main className="col-sm-8">
@@ -321,24 +294,14 @@ export class SendTransaction extends React.Component<Props, State> {
                       isReadOnly={readOnly}
                       onUnitChange={this.onUnitChange}
                     />
-                    <GasField
-                      value={gasLimit}
-                      onChange={readOnly ? void 0 : this.onGasChange}
-                    />
+                    <GasField value={gasLimit} onChange={readOnly ? void 0 : this.onGasChange} />
                     {(offline || forceOffline) && (
                       <div>
-                        <NonceField
-                          value={nonce}
-                          onChange={this.onNonceChange}
-                          placeholder={'0'}
-                        />
+                        <NonceField value={nonce} onChange={this.onNonceChange} placeholder={'0'} />
                       </div>
                     )}
                     {unit === 'ether' && (
-                      <DataField
-                        value={data}
-                        onChange={readOnly ? void 0 : this.onDataChange}
-                      />
+                      <DataField value={data} onChange={readOnly ? void 0 : this.onDataChange} />
                     )}
                     <CustomMessage message={customMessage} />
 
@@ -348,9 +311,7 @@ export class SendTransaction extends React.Component<Props, State> {
                           disabled={this.state.generateDisabled}
                           className="btn btn-info btn-block"
                           onClick={
-                            isWeb3Wallet
-                              ? this.generateWeb3TxFromState
-                              : this.generateTxFromState
+                            isWeb3Wallet ? this.generateWeb3TxFromState : this.generateTxFromState
                           }
                         >
                           {isWeb3Wallet
@@ -399,17 +360,12 @@ export class SendTransaction extends React.Component<Props, State> {
                             />
                             {offline && (
                               <p>
-                                To broadcast this transaction, paste the above
-                                into{' '}
+                                To broadcast this transaction, paste the above into{' '}
                                 <a href="https://myetherwallet.com/pushTx">
                                   {' '}
                                   myetherwallet.com/pushTx
                                 </a>{' '}
-                                or{' '}
-                                <a href="https://etherscan.io/pushTx">
-                                  {' '}
-                                  etherscan.io/pushTx
-                                </a>
+                                or <a href="https://etherscan.io/pushTx"> etherscan.io/pushTx</a>
                               </p>
                             )}
                           </div>
@@ -439,9 +395,7 @@ export class SendTransaction extends React.Component<Props, State> {
                 <main className="col-sm-8">
                   <div className="Tab-content-pane">
                     <h4>Sorry...</h4>
-                    <p>
-                      MetaMask / Mist wallets are not available in offline mode.
-                    </p>
+                    <p>MetaMask / Mist wallets are not available in offline mode.</p>
                   </div>
                 </main>
               )}
@@ -586,23 +540,14 @@ export class SendTransaction extends React.Component<Props, State> {
     this.setState({ gasLimit: value, gasChanged: true });
   };
 
-  public handleEverythingAmountChange = (
-    value: string,
-    unit: string
-  ): string => {
+  public handleEverythingAmountChange = (value: string, unit: string): string => {
     if (unit === 'ether') {
       const { balance, gasPrice } = this.props;
       const { gasLimit } = this.state;
       const bigGasLimit = Wei(gasLimit);
-      value = getBalanceMinusGasCosts(
-        bigGasLimit,
-        gasPrice,
-        balance.wei
-      ).toString();
+      value = getBalanceMinusGasCosts(bigGasLimit, gasPrice, balance.wei).toString();
     } else {
-      const tokenBalance = this.props.tokenBalances.find(
-        tBalance => tBalance.symbol === unit
-      );
+      const tokenBalance = this.props.tokenBalances.find(tBalance => tBalance.symbol === unit);
       if (!tokenBalance) {
         throw new Error(`${unit}: not found in token balances;`);
       }
@@ -680,10 +625,7 @@ export class SendTransaction extends React.Component<Props, State> {
       if (network.blockExplorer !== undefined) {
         this.props.showNotification(
           'success',
-          <TransactionSucceeded
-            txHash={txHash}
-            blockExplorer={network.blockExplorer}
-          />,
+          <TransactionSucceeded txHash={txHash} blockExplorer={network.blockExplorer} />,
           0
         );
       }
--- common/containers/Tabs/SignAndVerifyMessage/components/SignMessage/index.tsx
+++ common/containers/Tabs/SignAndVerifyMessage/components/SignMessage/index.tsx
@@ -6,12 +6,13 @@ import translate from 'translations';
 import { showNotification, TShowNotification } from 'actions/notifications';
 import { ISignedMessage } from 'libs/signing';
 import { IFullWallet } from 'libs/wallet';
-import FullWalletOnly from 'components/renderCbs/FullWalletOnly';
+import { AppState } from 'reducers';
 import SignButton from './SignButton';
 import './index.scss';
 
 interface Props {
   showNotification: TShowNotification;
+  wallet: IFullWallet;
 }
 
 interface State {
@@ -31,8 +32,9 @@ export class SignMessage extends Component<Props, State> {
   public state: State = initialState;
 
   public render() {
+    const { wallet } = this.props;
     const { message, signedMessage } = this.state;
-
+    const unlocked = !!wallet && !wallet.isReadOnly;
     const messageBoxClass = classnames([
       'SignMessage-inputBox',
       'form-control',
@@ -53,10 +55,15 @@ export class SignMessage extends Component<Props, State> {
             <div className="SignMessage-help">{translate('MSG_info2')}</div>
           </div>
 
-          <FullWalletOnly
-            withFullWallet={this.renderSignButton}
-            withoutFullWallet={this.renderUnlock}
-          />
+          {unlocked && (
+            <SignButton
+              wallet={wallet}
+              message={this.state.message}
+              showNotification={this.props.showNotification}
+              onSignMessage={this.onSignMessage}
+            />
+          )}
+          <WalletDecrypt hidden={unlocked} />
 
           {!!signedMessage && (
             <div>
@@ -84,21 +91,10 @@ export class SignMessage extends Component<Props, State> {
   private onSignMessage = (signedMessage: ISignedMessage) => {
     this.setState({ signedMessage });
   };
-
-  private renderSignButton = (fullWallet: IFullWallet) => {
-    return (
-      <SignButton
-        wallet={fullWallet}
-        message={this.state.message}
-        showNotification={this.props.showNotification}
-        onSignMessage={this.onSignMessage}
-      />
-    );
-  };
-
-  private renderUnlock() {
-    return <WalletDecrypt />;
-  }
 }
 
-export default connect(null, { showNotification })(SignMessage);
+const mapStateToProps = (state: AppState) => ({
+  wallet: state.wallet.inst
+});
+
+export default connect(mapStateToProps, { showNotification })(SignMessage);
