2c07703cd965539504b878e290d6f47987b80a71
danny <skubakdj@gmail.com>
2017-12-14 15:37:06 -0500

    add specs for sendEverything and reset sagas

---
+++ spec/sagas/transaction/reset.spec.ts
@@ -0,0 +1,17 @@
+import { SagaIterator } from 'redux-saga';
+import { TypeKeys } from 'actions/wallet';
+import { takeEvery, put } from 'redux-saga/effects';
+import { reset as resetActionCreator } from 'actions/transaction';
+import { resetTransactionState } from 'sagas/transaction/reset';
+
+describe('resetTransactionState*', () => {
+  const gen = resetTransactionState();
+
+  it('sould put resetActionCreator', () => {
+    expect(gen.next().value).toEqual(put(resetActionCreator()));
+  });
+
+  it('should be done', () => {
+    expect(gen.next().done).toEqual(true);
+  });
+});
