b1d27cb97ad9e95eade3dbc7cb05810e08063068
henrynguyen5 <ngu.henry@hotmail.com>
2017-12-14 14:56:35 -0500

    Add post-signing verification against fields, clean up gas price

--- common/selectors/transaction/transaction.ts
+++ common/selectors/transaction/transaction.ts
@@ -1,13 +1,26 @@
 import { AppState } from 'reducers';
 import { getCurrentTo, getCurrentValue } from './current';
 import { getFields } from './fields';
-import { makeTransaction } from 'libs/transaction';
+import { makeTransaction, IHexStrTransaction } from 'libs/transaction';
 import EthTx from 'ethereumjs-tx';
 import { getUnit } from 'selectors/transaction/meta';
 import { reduceToValues, isFullTx } from 'selectors/transaction/helpers';
-import { getGasPrice, getGasLimit, getDataExists } from 'selectors/transaction';
+import {
+  getGasPrice,
+  getGasLimit,
+  getDataExists,
+  getSerializedTransaction
+} from 'selectors/transaction';
 import { Wei } from 'libs/units';
-export { getTransaction, getTransactionState, getGasCost, nonValueTransaction };
+import { getTransactionFields } from 'libs/transaction/utils/ether';
+import { getNetworkConfig } from 'selectors/config';
+export {
+  getTransaction,
+  getTransactionState,
+  getGasCost,
+  nonValueTransaction,
+  serializedAndTransactionFieldsMatch
+};
 
 const getTransactionState = (state: AppState) => state.transaction;
 
@@ -44,4 +57,30 @@ const getGasCost = (state: AppState) => {
   const cost = gasLimit.value.mul(gasPrice.value);
 
   return cost;
+};
+
+const serializedAndTransactionFieldsMatch = (state: AppState, isLocallySigned: boolean) => {
+  const serialzedTransaction = getSerializedTransaction(state);
+  const { transaction, isFullTransaction } = getTransaction(state);
+  if (!isFullTransaction || !serialzedTransaction) {
+    return false;
+  }
+  const t1 = getTransactionFields(transaction);
+  // inject chainId into t1 as it wont have it from the fields
+  const networkConfig = getNetworkConfig(state);
+  if (!networkConfig) {
+    return false;
+  }
+  const { chainId } = networkConfig;
+  t1.chainId = chainId;
+
+  const t2 = getTransactionFields(makeTransaction(serialzedTransaction));
+  const checkValidity = (tx: IHexStrTransaction) =>
+    Object.keys(tx).reduce((match, currField) => match && t1[currField] === t2[currField], true);
+  //reduce both ways to make sure both are exact same
+  const transactionsMatch = checkValidity(t1) && checkValidity(t2);
+  // if its signed then verify the signature too
+  return transactionsMatch && isLocallySigned
+    ? makeTransaction(serialzedTransaction).verifySignature()
+    : true;
 };
