9f821c19e8e1b95f61d92f056200a5620e8df0d6
HenryNguyen5 <ngu.henry@hotmail.com>
2017-12-16 21:55:35 -0500

    Fix tests

--- spec/pages/GenerateWallet.spec.tsx
+++ spec/pages/GenerateWallet.spec.tsx
@@ -4,8 +4,20 @@ import Adapter from 'enzyme-adapter-react-16';
 import GenerateWallet from 'containers/Tabs/GenerateWallet';
 import shallowWithStore from '../utils/shallowWithStore';
 import { createMockStore } from 'redux-test-utils';
+import { RouteComponentProps } from 'react-router-dom';
+import { createMockRouteComponentProps } from '../utils/mockRouteComponentProps';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+const routeProps: RouteComponentProps<any> = createMockRouteComponentProps({
+  match: { path: '/generate', url: '/generate', isExact: true, params: {} },
+  location: { pathname: '/generate', search: '', hash: '', key: '4gvanv' },
+  history: {
+    length: 34,
+    action: 'PUSH',
+    location: { pathname: '/generate', search: '', hash: '', key: '4gvanv', state: {} }
+  }
+});
 
 it('render snapshot', () => {
   const testState = {
@@ -16,7 +28,7 @@ it('render snapshot', () => {
     }
   };
   const store = createMockStore(testState);
-  const component = shallowWithStore(<GenerateWallet />, store);
+  const component = shallowWithStore(<GenerateWallet {...routeProps} />, store);
 
   expect(component).toMatchSnapshot();
 });
