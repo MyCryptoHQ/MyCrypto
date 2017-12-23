fdb179af219dafc2bb344d516967f91807213010
james-prado <jamesprado7@gmail.com>
2017-12-23 18:00:53 -0500

    Add overflow-y:scroll to table

--- common/containers/Tabs/ENS/components/NameResolve/components/NameOwned.tsx
+++ common/containers/Tabs/ENS/components/NameResolve/components/NameOwned.tsx
@@ -21,37 +21,39 @@ export const NameOwned: React.SFC<IOwnedDomainRequest> = ({
         <strong>{name}.eth</strong> is already owned
       </h1>
     </div>
-    <table className="table table-striped">
-      <tbody>
-        <tr>
-          <td>Name: </td>
-          <MonoTd>
-            <NewTabLink content={`${name}.eth`} href={lookupLink(`${name}.eth`)} />
-          </MonoTd>
-        </tr>
-        <tr>
-          <td>Labelhash ({name}): </td>
-          <MonoTd>{labelHash}</MonoTd>
-        </tr>
-        <tr>
-          <td>Namehash ({name}.eth): </td>
-          <MonoTd>{nameHash}</MonoTd>
-        </tr>
-        <tr>
-          <td>Owner:</td>
-          <MonoTd>{ownerAddress}</MonoTd>
-        </tr>
-        <tr>
-          <td>Highest Bidder (Deed Owner): </td>
-          <MonoTd>
-            <span>{highestBid}</span>
-          </MonoTd>
-        </tr>
-        <tr>
-          <td>Resolved Address: </td>
-          <MonoTd>{resolvedAddress}</MonoTd>
-        </tr>
-      </tbody>
-    </table>
+    <div className="table-wrapper">
+      <table className="table table-striped">
+        <tbody>
+          <tr>
+            <td>Name: </td>
+            <MonoTd>
+              <NewTabLink content={`${name}.eth`} href={lookupLink(`${name}.eth`)} />
+            </MonoTd>
+          </tr>
+          <tr>
+            <td>Labelhash ({name}): </td>
+            <MonoTd>{labelHash}</MonoTd>
+          </tr>
+          <tr>
+            <td>Namehash ({name}.eth): </td>
+            <MonoTd>{nameHash}</MonoTd>
+          </tr>
+          <tr>
+            <td>Owner:</td>
+            <MonoTd>{ownerAddress}</MonoTd>
+          </tr>
+          <tr>
+            <td>Highest Bidder (Deed Owner): </td>
+            <MonoTd>
+              <span>{highestBid}</span>
+            </MonoTd>
+          </tr>
+          <tr>
+            <td>Resolved Address: </td>
+            <MonoTd>{resolvedAddress}</MonoTd>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </section>
 );
