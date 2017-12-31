41e5d5e91949af0ed01226e5e49f59218c36af2c
James Prado <james-prado@users.noreply.github.com>
2017-12-31 01:44:23 -0500

    Update tab isActive Condition
    Add condition to fix help tab appearing as active at `localhost:3000/`

--- common/components/Header/components/NavigationLink.tsx
+++ common/components/Header/components/NavigationLink.tsx
@@ -18,13 +18,14 @@ class NavigationLink extends React.Component<Props, {}> {
     const { link, location, isHomepage } = this.props;
     // isActive if
     // 1) Current path is the same as link
-    // 2) the first path is the same for both links (/account and /account/send)
+    // 2) the first path is the same for both links (/account and /account/send) && is not an external link
     // 3) we're at the root path and this is the "homepage" nav item
     const isActive =
       location.pathname === link.to ||
-      (link.to && location.pathname.split('/')[1] === link.to.split('/')[1]) ||
+      (link.to &&
+        location.pathname.split('/')[1] === link.to.split('/')[1] &&
+        location.pathname.split('/')[0] === link.to.split('/')[0]) ||
       (isHomepage && location.pathname === '/');
-
     const linkClasses = classnames({
       'NavigationLink-link': true,
       'is-disabled': !link.to,
