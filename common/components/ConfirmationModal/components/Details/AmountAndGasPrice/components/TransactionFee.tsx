daa117d4ccda88ed6d89b140f4a08e12b4a08808
james-prado <jamesprado7@gmail.com>
2018-01-17 06:38:46 -0500

    Add transaction fee to tx confirmation modal

---
+++ common/components/ConfirmationModal/components/Details/AmountAndGasPrice/components/TransactionFee.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { getTransactionFields, makeTransaction } from 'libs/transaction';
+import { SerializedTransaction } from 'components/renderCbs';
+import { UnitDisplay } from 'components/ui';
+import { Wei } from 'libs/units';
+import { AppState } from 'reducers';
+import { connect } from 'react-redux';
+import { getNetworkConfig } from 'selectors/config';
+import BN from 'bn.js';
+
+interface Props {
+  rates: AppState['rates']['rates'];
+  network: AppState['config']['network'];
+  isOffline: AppState['config']['offline'];
+}
+
+class TransactionFeeClass extends React.Component<Props> {
+  public render() {
+    const { rates, network, isOffline } = this.props;
+    return (
+      <SerializedTransaction
+        withSerializedTransaction={serializedTransaction => {
+          const transactionInstance = makeTransaction(serializedTransaction);
+          const { gasPrice, gasLimit } = getTransactionFields(transactionInstance);
+          const fee = Wei(gasPrice).mul(Wei(gasLimit));
+          const usdBN = network.isTestnet
+            ? new BN(0)
+            : fee && rates[network.unit] && fee.muln(rates[network.unit].USD);
+
+          return (
+            <React.Fragment>
+              <UnitDisplay unit={'ether'} value={fee} symbol={'ETH'} checkOffline={false} />{' '}
+              {!isOffline && (
+                <span>
+                  ($
+                  <UnitDisplay
+                    value={usdBN}
+                    unit="ether"
+                    displayShortBalance={2}
+                    displayTrailingZeroes={true}
+                    checkOffline={true}
+                  />)
+                </span>
+              )}
+            </React.Fragment>
+          );
+        }}
+      />
+    );
+  }
+}
+
+function mapStateToProps(state: AppState) {
+  return {
+    rates: state.rates.rates,
+    network: getNetworkConfig(state),
+    isOffline: state.config.offline
+  };
+}
+export const TransactionFee = connect(mapStateToProps)(TransactionFeeClass);
