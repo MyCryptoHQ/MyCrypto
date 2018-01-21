395d895cbfb9eede85c55da1bc34a187bf436e81
henrynguyen5 <ngu.henry@hotmail.com>
2018-01-21 16:58:03 -0500

    Add enum to electron events

--- electron-app/main/updater.ts
+++ electron-app/main/updater.ts
@@ -4,32 +4,41 @@ import EVENTS from '../../shared/electronEvents';
 import TEST_RELEASE from './testrelease.json';
 autoUpdater.autoDownload = false;
 
+enum AutoUpdaterEvents {
+  CHECKING_FOR_UPDATE = 'checking-for-update',
+  UPDATE_NOT_AVAILABLE = 'update-not-available',
+  UPDATE_AVAILABLE = 'update-available',
+  DOWNLOAD_PROGRESS = 'download-progress',
+  UPDATE_DOWNLOADED = 'update-downloaded',
+  ERROR = 'error'
+}
+
 export default (app: App, window: BrowserWindow) => {
   // Set to 'true' if you want to test update behavior. Requires a recompile.
   const shouldMockUpdate = true && process.env.NODE_ENV !== 'production';
 
   // Report update status
-  autoUpdater.on('checking-for-update', () => {
+  autoUpdater.on(AutoUpdaterEvents.CHECKING_FOR_UPDATE, () => {
     window.webContents.send(EVENTS.UPDATE.CHECKING_FOR_UPDATE);
   });
 
-  autoUpdater.on('update-not-available', () => {
+  autoUpdater.on(AutoUpdaterEvents.UPDATE_NOT_AVAILABLE, () => {
     window.webContents.send(EVENTS.UPDATE.UPDATE_NOT_AVAILABLE);
   });
 
-  autoUpdater.on('update-available', info => {
+  autoUpdater.on(AutoUpdaterEvents.UPDATE_AVAILABLE, info => {
     window.webContents.send(EVENTS.UPDATE.UPDATE_AVAILABLE, info);
   });
 
-  autoUpdater.on('download-progress', progress => {
+  autoUpdater.on(AutoUpdaterEvents.DOWNLOAD_PROGRESS, progress => {
     window.webContents.send(EVENTS.UPDATE.DOWNLOAD_PROGRESS, progress);
   });
 
-  autoUpdater.on('update-downloaded', () => {
+  autoUpdater.on(AutoUpdaterEvents.UPDATE_DOWNLOADED, () => {
     window.webContents.send(EVENTS.UPDATE.UPDATE_DOWNLOADED);
   });
 
-  autoUpdater.on('error', (err, msg) => {
+  autoUpdater.on(AutoUpdaterEvents.ERROR, (err, msg) => {
     console.error('Update failed with an error');
     console.error(err);
     window.webContents.send(EVENTS.UPDATE.ERROR, msg);
