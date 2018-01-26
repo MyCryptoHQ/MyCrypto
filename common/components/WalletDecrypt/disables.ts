2309d05c06e9885c0696b109aa6d2ce719e5c06c
William O'Beirne <wbobeirne@gmail.com>
2018-01-26 15:08:39 -0500

    Disable more wallets conditionally + explain why (#924)
    * Add more wallet disables, move all into selector.
    
    * Add reasons for disabled wallets.
    
    * Disable read only in lite send.
    
    * Fix view address showing insecure icon.

--- common/components/WalletDecrypt/disables.ts
+++ common/components/WalletDecrypt/disables.ts
@@ -1,15 +1,31 @@
 import { MiscWalletName, SecureWalletName, WalletName } from 'config';
 
-enum WalletMode {
-  READ_ONLY = 'READ_ONLY',
-  UNABLE_TO_SIGN = 'UNABLE_TO_SIGN',
-  ONLINE_ONLY = 'ONLINE_ONLY'
+export interface DisabledWallets {
+  wallets: WalletName[];
+  reasons: {
+    [key: string]: string;
+  };
 }
 
-const walletModes: { [key in WalletMode]: WalletName[] } = {
-  [WalletMode.READ_ONLY]: [MiscWalletName.VIEW_ONLY],
-  [WalletMode.UNABLE_TO_SIGN]: [SecureWalletName.TREZOR, MiscWalletName.VIEW_ONLY],
-  [WalletMode.ONLINE_ONLY]: [SecureWalletName.WEB3, SecureWalletName.TREZOR]
-};
+enum WalletMode {
+  READ_ONLY = 'READ_ONLY',
+  UNABLE_TO_SIGN = 'UNABLE_TO_SIGN'
+}
 
-export default walletModes;
+// Duplicating reasons is kind of tedious, but saves having to run through a
+// bunch of loops to format it differently
+export const DISABLE_WALLETS: { [key in WalletMode]: DisabledWallets } = {
+  [WalletMode.READ_ONLY]: {
+    wallets: [MiscWalletName.VIEW_ONLY],
+    reasons: {
+      [MiscWalletName.VIEW_ONLY]: 'Read only is not allowed'
+    }
+  },
+  [WalletMode.UNABLE_TO_SIGN]: {
+    wallets: [SecureWalletName.TREZOR, MiscWalletName.VIEW_ONLY],
+    reasons: {
+      [SecureWalletName.TREZOR]: 'This wallet can’t sign messages',
+      [MiscWalletName.VIEW_ONLY]: 'This wallet can’t sign messages'
+    }
+  }
+};
