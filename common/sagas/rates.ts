27e18d1fa6454bc8a4cb95e2f8c409e871ed3df6
henrynguyen5 <ngu.henry@hotmail.com>
2018-02-07 18:36:09 -0500

    Add return typing

--- common/sagas/rates.ts
+++ common/sagas/rates.ts
@@ -1,12 +1,12 @@
 import { fetchCCRatesSucceeded, fetchCCRatesFailed, FetchCCRatesRequested } from 'actions/rates';
 import { SagaIterator } from 'redux-saga';
 import { call, put, takeLatest } from 'redux-saga/effects';
-import { fetchRates } from 'api/rates';
+import { fetchRates, CCResponse } from 'api/rates';
 import { TypeKeys } from 'actions/rates/constants';
 
 export function* fetchRatesSaga(action: FetchCCRatesRequested): SagaIterator {
   try {
-    const rates = yield call(fetchRates, action.payload);
+    const rates: CCResponse = yield call(fetchRates, action.payload);
     yield put(fetchCCRatesSucceeded(rates));
   } catch (e) {
     yield put(fetchCCRatesFailed());
