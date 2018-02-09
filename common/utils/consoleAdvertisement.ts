5b14ecc48fa334ddc6dc2ab9fd5a94a1aeba0d9a
Michael H <mhoekema04+github@gmail.com>
2018-02-09 21:43:38 +0100

    Update consoleAdvertisement.ts

--- common/utils/consoleAdvertisement.ts
+++ common/utils/consoleAdvertisement.ts
@@ -11,17 +11,17 @@ export default function consoleAdvertisement() {
   Nhyyyyyyyyyy+.    .+yyyyyyo-     :syyyyyyyyyhdN          _       _         _    _     _ 
  Nhyyyyyyyyyo.        .+yyo-         :yyyyyyyyyyd         | |     (_)       | |  | |   | |
  hyyyyyyyyo.            .-             :yyyyyyyyhm        | | ___  _ _ __   | |  | |___| |
-myyyyyyyy.     -+oo/.        .+ooo:`    `oyyyyyyyh    _   | |/ _ \| | '_ \  | |  | / __| |
-hyyyyyyy+    -syyyyyyo-    .oyyyyyyy/`   /yyyyyyyy   | |__| | (_) | | | | | | |__| \__ \_|
+myyyyyyyy.     -+oo/.        .+ooo:      oyyyyyyyh    _   | |/ _ \| | '_ \  | |  | / __| |
+hyyyyyyy+    -syyyyyyo-    .oyyyyyyy/    /yyyyyyyy   | |__| | (_) | | | | | | |__| \__ \_|
 yyyyyyyyy+::syyyyyyyyyyo::oyyyyyyyyyyy/:+yyyyyyyyy    \____/ \___/|_|_| |_|  \____/|___(_)
 yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
 yyyyyyyyyyyyyyyyysyyyyyyyyyyyyyysyyyyyyyyyyyyyyyyy    https://github.com/MyCrypto/mycrypto.com
-hyyyyyyyyyyyyyys. `:syyyyyyyyy/` `+yyyyyyyyyyyyyyy
-myyyyyyyyyyyyyy/     :syyyyy/`    .yyyyyyyyyyyyyyh
- hyyyyyyyyyyyyyy:      .::-`     -syyyyyyyyyyyyyhd
+hyyyyyyyyyyyyyys.  :syyyyyyyyy/   +yyyyyyyyyyyyyyy
+myyyyyyyyyyyyyy/     :syyyyy/     .yyyyyyyyyyyyyyh
+ hyyyyyyyyyyyyyy:      .::-      -syyyyyyyyyyyyyhd
  Nyyyyyyyyyyyyyyys:            -syyyyyyyyyyyyyyymN
   mhyyyyyyyyyyyyyyys:        -oyyyyyyyyyyyyyyyhdN 
-   Nhyyyyyyyyyyyyyyyys:`   -oyyyyyyyyyyyyyyyyhmN  
+   Nhyyyyyyyyyyyyyyyys:    -oyyyyyyyyyyyyyyyyhmN  
     NdhyyyyyyyyyyyyyyyyysoyyyyyyyyyyyyyyyyyhdN    
       mhhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhhmN     
         mdhyyyyyyyyyyyyyyyyyyyyyyyyyyyyhhNN       
