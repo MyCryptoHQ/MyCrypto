f070a04d32c45a527aa8b9a6dbc12d579328ed0e
Will O'Beirne <wbobeirne@gmail.com>
2018-02-09 17:41:19 -0500

    Fix console advert link, adjust logo and make it easier to edit.

--- common/utils/consoleAdvertisement.ts
+++ common/utils/consoleAdvertisement.ts
@@ -1,32 +1,32 @@
 // Simply importing this file will do the console log conditionally
+// prettier-ignore
 export default function consoleAdvertisement() {
   console.log(
     `%c
-               NNmdhhyyyyyyyyhhddmN               
-            mdhyyyyyyyyyyyyyyyyyyyyyhdN           
-         mdyyyyyyyyyyyyyyyyyyyyyyyyyyyyhdN        
-      Ndhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhhm      
-    Nmhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhhm       
-    dyyyyyyyyyyys++oyyyyyyyyyys+/+syyyyyyyyyyhd   
-  Nhyyyyyyyyyy+.    .+yyyyyyo-     :syyyyyyyyyhdN          _       _         _    _     _ 
- Nhyyyyyyyyyo.        .+yyo-         :yyyyyyyyyyd         | |     (_)       | |  | |   | |
- hyyyyyyyyo.            .-             :yyyyyyyyhm        | | ___  _ _ __   | |  | |___| |
-myyyyyyyy.     -+oo/.        .+ooo:      oyyyyyyyh    _   | |/ _ \| | '_ \  | |  | / __| |
-hyyyyyyy+    -syyyyyyo-    .oyyyyyyy/    /yyyyyyyy   | |__| | (_) | | | | | | |__| \__ \_|
-yyyyyyyyy+::syyyyyyyyyyo::oyyyyyyyyyyy/:+yyyyyyyyy    \____/ \___/|_|_| |_|  \____/|___(_)
-yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
-yyyyyyyyyyyyyyyyysyyyyyyyyyyyyyysyyyyyyyyyyyyyyyyy    https://github.com/MyCrypto/MyCrypto
-hyyyyyyyyyyyyyys.  :syyyyyyyyy/   +yyyyyyyyyyyyyyy
-myyyyyyyyyyyyyy/     :syyyyy/     .yyyyyyyyyyyyyyh
- hyyyyyyyyyyyyyy:      .::-      -syyyyyyyyyyyyyhd
- Nyyyyyyyyyyyyyyys:            -syyyyyyyyyyyyyyymN
-  mhyyyyyyyyyyyyyyys:        -oyyyyyyyyyyyyyyyhdN 
-   Nhyyyyyyyyyyyyyyyys:    -oyyyyyyyyyyyyyyyyhmN  
-    NdhyyyyyyyyyyyyyyyyysoyyyyyyyyyyyyyyyyyhdN    
-      mhhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhhmN     
-        mdhyyyyyyyyyyyyyyyyyyyyyyyyyyyyhhNN       
-          NmdhyyyyyyyyyyyyyyyyyyyyyyhhmN          
-             NNmddhhyyyyyyyyyyhhhddN                                                               
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃                                                                                                ┃
+┃               ,:;;;;;;;;;;;,                                                                   ┃
+┃            ;;;;;;;;;;;;;;;;;;;;;                                                               ┃
+┃         ;;;;;;;;;;;;;;;;;;;;;;;;;;,                                                            ┃
+┃       ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;.                                                          ┃
+┃      ;;;;;;;;;;'';;;;;;;;;'';;;;;;;;;;                                                         ┃
+┃    .;;;;;;;;:      ;;;;;      :;;;;;;;;                                                        ┃
+┃    ;;;;;;;:          '          ;;;;;;;;             _       _         _    _     _            ┃
+┃   ;;;;;;;     ;;;;       ;;;;     ;;;;;;:           | |     (_)       | |  | |   | |           ┃
+┃  ';;;;;;;   ;;;;;;;;   ;;;;;;;;   ;;;;;;;           | | ___  _ _ __   | |  | |___| |           ┃
+┃  :;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;       _   | |/ _ \\| | '_ \\  | |  | / __| |           ┃
+┃  ,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;      | |__| | (_) | | | | | | |__| \\__ \\_|           ┃
+┃   ;;;;;;;;;;;;   ;;;;;;;;;   ;;;;;;;;;;;;       \\____/ \\___/|_|_| |_|  \\____/|___(_)           ┃
+┃   ;;;;;;;;;;;;     ;;;;;     ;;;;;;;;;;;,                                                      ┃
+┃    ;;;;;;;;;;;;:           :;;;;;;;;;;;;        https://github.com/MyCrypto/MyCrypto           ┃
+┃     ;;;;;;;;;;;;;:       :;;;;;;;;;;;;;                                                        ┃
+┃      ;;;;;;;;;;;;;;:   :;;;;;;;;;;;;;;                                                         ┃
+┃       ';;;;;;;;;;;;;;;;;;;;;;;;;;;;;                                                           ┃
+┃         ';;;;;;;;;;;;;;;;;;;;;;;;;                                                             ┃
+┃            ';;;;;;;;;;;;;;;;;;;'                                                               ┃
+┃                 :;;;;;;;;;:                                                                    ┃
+┃                                                                                                ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 `,
     'background: #163151; color: #FFF;'
   );
