40feedf347f4405a0d1419a0270890d5eb9468e6
Will O'Beirne <wbobeirne@gmail.com>
2018-02-19 13:53:09 -0500

    tscheck

--- common/sagas/transaction/reset.ts
+++ common/sagas/transaction/reset.ts
@@ -2,7 +2,6 @@ import { SagaIterator } from 'redux-saga';
 import { TypeKeys } from 'actions/wallet';
 import { takeEvery, put } from 'redux-saga/effects';
 import { reset as resetActionCreator } from 'actions/transaction';
-import { TypeKeys as ConfigTK } from 'actions/config';
 
 export function* resetTransactionState(): SagaIterator {
   yield put(resetActionCreator());
