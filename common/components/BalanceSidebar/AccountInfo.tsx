9b3b69503f2871b80c24de27a02d096a858d9556
Amidou Kante <kamdiou@gmail.com>
2018-02-19 14:18:21 -0800

    can copy clipboard to mouse

--- common/components/BalanceSidebar/AccountInfo.tsx
+++ common/components/BalanceSidebar/AccountInfo.tsx
@@ -9,6 +9,7 @@ import { AppState } from 'reducers';
 import { connect } from 'react-redux';
 import { NetworkConfig } from 'types/network';
 import { TSetAccountBalance, setAccountBalance } from 'actions/wallet';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 interface OwnProps {
   wallet: IWallet;
@@ -36,7 +37,9 @@ class AccountInfo extends React.Component<Props, State> {
   public state = {
     showLongBalance: false,
     address: '',
-    confirmAddr: false
+    confirmAddr: false,
+    copied: false,
+    currentColor: 'black'
   };
 
   public async setAddressFromWallet() {
@@ -69,6 +72,15 @@ class AccountInfo extends React.Component<Props, State> {
     });
   };
 
+  public onCopy = () => {
+    this.setState(state => {
+      return {
+        copied: true,
+        currentColor: 'blue'
+      };
+    });
+  };
+
   public render() {
     const { network, balance, isOffline } = this.props;
     const { address, showLongBalance, confirmAddr } = this.state;
@@ -90,21 +102,24 @@ class AccountInfo extends React.Component<Props, State> {
           </div>
           <div className="AccountInfo-address-wrapper">
             <div className="AccountInfo-address-addr">{address}</div>
-            <div className="copy-clipboard">
-              <svg
-                aria-hidden="true"
-                data-prefix="far"
-                data-icon="copy"
-                role="img"
-                viewBox="0 0 448 512"
-                className="svg-inline--fa fa-copy fa-w-14 fa-2x"
-              >
-                <path
-                  fill="currentColor"
-                  d="M433.941 65.941l-51.882-51.882A48 48 0 0 0 348.118 0H176c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h224c26.51 0 48-21.49 48-48v-48h80c26.51 0 48-21.49 48-48V99.882a48 48 0 0 0-14.059-33.941zM266 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h74v224c0 26.51 21.49 48 48 48h96v42a6 6 0 0 1-6 6zm128-96H182a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h106v88c0 13.255 10.745 24 24 24h88v202a6 6 0 0 1-6 6zm6-256h-64V48h9.632c1.591 0 3.117.632 4.243 1.757l48.368 48.368a6 6 0 0 1 1.757 4.243V112z"
-                />
-              </svg>
-            </div>
+            <CopyToClipboard onCopy={this.onCopy} text={address}>
+              <div className="copy-clipboard" title="copy to clipboard">
+                <svg
+                  aria-hidden="true"
+                  data-prefix="far"
+                  data-icon="copy"
+                  role="img"
+                  viewBox="0 0 448 512"
+                  className="svg-inline--fa fa-copy fa-w-14 fa-2x"
+                >
+                  <path
+                    fill={this.state.currentColor}
+                    d="M433.941 65.941l-51.882-51.882A48 48 0 0 0 348.118 0H176c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h224c26.51 0 48-21.49 48-48v-48h80c26.51 0 48-21.49 48-48V99.882a48 48 0 0 0-14.059-33.941zM266 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h74v224c0 26.51 21.49 48 48 48h96v42a6 6 0 0 1-6 6zm128-96H182a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h106v88c0 13.255 10.745 24 24 24h88v202a6 6 0 0 1-6 6zm6-256h-64V48h9.632c1.591 0 3.117.632 4.243 1.757l48.368 48.368a6 6 0 0 1 1.757 4.243V112z"
+                  />
+                </svg>
+                <p>{this.state.copied ? 'copied!' : null}</p>
+              </div>
+            </CopyToClipboard>
           </div>
         </div>
 
