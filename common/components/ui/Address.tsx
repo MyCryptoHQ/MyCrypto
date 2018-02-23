0f13100c9d3304e98ad8549eae28aa3538624447
Will O'Beirne <wbobeirne@gmail.com>
2018-02-23 17:05:31 -0500

    tscheck

--- common/components/ui/Address.tsx
+++ common/components/ui/Address.tsx
@@ -4,30 +4,21 @@ import NewTabLink from './NewTabLink';
 import { IWallet } from 'libs/wallet';
 import { BlockExplorerConfig } from 'types/network';
 
-// interface BaseProps {
-//   explorer?: BlockExplorerConfig | null;
-// }
-//
-// interface AddressProps extends BaseProps {
-//   address: string;
-//   wallet: undefined;
-// };
-//
-// interface WalletProps extends BaseProps {
-//   wallet: IWallet;
-//   address: undefined;
-// };
-//
-// type Props = AddressProps | WalletProps;
-
-interface BaseProps {
+interface Props {
   explorer?: BlockExplorerConfig | null;
   address?: string;
   wallet?: IWallet;
 }
 
 const Address: React.SFC<Props> = ({ address, wallet, explorer }) => {
-  const addr = toChecksumAddress(wallet ? wallet.getAddressString() : (address as string));
+  let addr = '';
+  if (wallet) {
+    addr = wallet.getAddressString();
+  } else if (address) {
+    addr = address;
+  }
+  addr = toChecksumAddress(addr);
+
   if (explorer) {
     return <NewTabLink href={explorer.addressUrl(addr)}>{addr}</NewTabLink>;
   } else {
