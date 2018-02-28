cf3d6e368821d74bef6b9aaf26fa9b52954be033
Will O'Beirne <wbobeirne@gmail.com>
2018-02-28 14:08:36 -0500

    Add exclusive props, type guard.

--- common/components/ui/Address.tsx
+++ common/components/ui/Address.tsx
@@ -4,23 +4,34 @@ import NewTabLink from './NewTabLink';
 import { IWallet } from 'libs/wallet';
 import { BlockExplorerConfig } from 'types/network';
 
-interface Props {
+interface BaseProps {
   explorer?: BlockExplorerConfig | null;
-  address?: string;
-  wallet?: IWallet;
 }
 
-const Address: React.SFC<Props> = ({ address, wallet, explorer }) => {
+interface AddressProps extends BaseProps {
+  address: string;
+}
+
+interface WalletProps extends BaseProps {
+  wallet: IWallet;
+}
+
+type Props = AddressProps | WalletProps;
+
+const isAddressProps = (props: Props): props is AddressProps =>
+  typeof (props as AddressProps).address === 'string';
+
+const Address: React.SFC<Props> = props => {
   let addr = '';
-  if (wallet) {
-    addr = wallet.getAddressString();
-  } else if (address) {
-    addr = address;
+  if (isAddressProps(props)) {
+    addr = props.address;
+  } else {
+    addr = props.wallet.getAddressString();
   }
   addr = toChecksumAddress(addr);
 
-  if (explorer) {
-    return <NewTabLink href={explorer.addressUrl(addr)}>{addr}</NewTabLink>;
+  if (props.explorer) {
+    return <NewTabLink href={props.explorer.addressUrl(addr)}>{addr}</NewTabLink>;
   } else {
     return <React.Fragment>{addr}</React.Fragment>;
   }
