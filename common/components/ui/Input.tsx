b81f7363bf5385bbdf2686f64978bc35714d6b14
Will O'Beirne <wbobeirne@gmail.com>
2018-03-02 18:01:15 -0500

    Prevent scrolling to adjust number input.

--- common/components/ui/Input.tsx
+++ common/components/ui/Input.tsx
@@ -9,6 +9,7 @@ class Input extends React.Component<HTMLProps<HTMLInputElement>, State> {
   public state: State = {
     hasBlurred: false
   };
+
   public render() {
     return (
       <input
@@ -19,11 +20,19 @@ class Input extends React.Component<HTMLProps<HTMLInputElement>, State> {
             this.props.onBlur(e);
           }
         }}
+        onWheel={this.props.type === 'number' ? this.preventNumberScroll : undefined}
         className={`input-group-input  ${this.props.className} ${
           this.state.hasBlurred ? 'has-blurred' : ''
         } ${!!this.props.value && this.props.value.toString().length > 0 ? 'has-value' : ''}`}
       />
     );
+  }
+
+  // When number inputs are scrolled on while in focus, the number changes.
+  private preventNumberScroll(ev: React.WheelEvent<HTMLInputElement>) {
+    if (document.activeElement === ev.currentTarget) {
+      ev.preventDefault();
+    }
   }
 }
 
