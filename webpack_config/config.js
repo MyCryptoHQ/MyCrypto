9be46bf8aa87086a3c3178f1c81e172667e52931
Danny Skubak <skubakdj@gmail.com>
2018-03-18 00:52:47 -0400

    Update blockie package in webpack config (#1337)

--- webpack_config/config.js
+++ webpack_config/config.js
@@ -43,7 +43,7 @@ module.exports = {
     'bip39',
     'bn.js',
     'classnames',
-    'ethereum-blockies',
+    'ethereum-blockies-base64',
     'ethereumjs-abi',
     'ethereumjs-tx',
     'ethereumjs-util',
