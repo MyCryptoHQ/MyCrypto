e79bdf55efb8525eb83579023ca46111b257aa61
Connor Bryan <connorbryan@Connors-MacBook-Pro.local>
2018-04-12 13:55:14 -0700

    Additional test cases

--- common/utils/shuffle.ts
+++ common/utils/shuffle.ts
@@ -1,9 +1,17 @@
 export default function shuffle(array: any[]) {
-  // Fisher-Yates shuffle
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+  const finalArray = [...array];
+
+  if (finalArray.length === 0 || finalArray.length === 1) {
+    return finalArray;
   }
 
-  return array;
+  while (finalArray.toString() === array.toString()) {
+    // Fisher-Yates shuffle
+    for (let i = finalArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [finalArray[i], finalArray[j]] = [finalArray[j], finalArray[i]];
+    }
+  }
+
+  return finalArray;
 }
