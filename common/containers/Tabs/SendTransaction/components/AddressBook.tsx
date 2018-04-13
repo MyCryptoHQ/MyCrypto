cab367fcb4676e250fdc486bd9f19238958d85fe
Connor Bryan <connorbryan@Connors-MacBook-Pro.local>
2018-04-13 13:24:10 -0700

    Further templating of the AddressBook view

--- common/containers/Tabs/SendTransaction/components/AddressBook.tsx
+++ common/containers/Tabs/SendTransaction/components/AddressBook.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { IWallet } from 'libs/wallet';
+import { AddressField } from 'components';
+import { Input } from 'components/ui';
 
 interface Props {
   wallet: IWallet;
@@ -7,6 +9,28 @@ interface Props {
 
 export default class AddressBook extends React.Component<Props> {
   public render() {
-    return <p>AddressBook</p>;
+    return (
+      <div className="AddressBook">
+        <div className="Tab-content-pane">
+          <div className="row form-group">
+            <div className="col-sm-12">
+              <AddressField />
+            </div>
+            <div className="col-sm-12">
+              <label>Label</label>
+              <Input />
+            </div>
+            <div className="col-xs-12">
+              <button className="btn btn-primary pull-right">
+                <i className="fa fa-plus" /> Add Entry
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="Tab-content-pane">
+          <p>Table</p>
+        </div>
+      </div>
+    );
   }
 }
