df7ed8515027545843222be40af0ba572b923eef
Will O'Beirne <wbobeirne@gmail.com>
2018-04-25 15:21:07 -0400

    Update all links back to old mycrypto to classic.mycrypto, add footer link too.

--- common/config/links.ts
+++ common/config/links.ts
@@ -46,6 +46,10 @@ export const socialMediaLinks: Link[] = [
 
 export const productLinks: Link[] = [
   {
+    link: 'https://classic.mycrypto.com/',
+    text: translateRaw('MYCRYPTO_CLASSIC')
+  },
+  {
     link:
       'https://chrome.google.com/webstore/detail/etheraddresslookup/pdknmigbbbhmllnmgdfalmedcmcefdfn',
     text: translateRaw('ETHER_ADDRESS_LOOKUP')
@@ -60,7 +64,7 @@ export const productLinks: Link[] = [
     text: translateRaw('ETHERSCAMDB')
   },
   {
-    link: 'https://www.mycrypto.com/helpers.html',
+    link: 'https://classic.mycrypto.com/helpers.html',
     text: translateRaw('FOOTER_HELP_AND_DEBUGGING')
   }
 ];
