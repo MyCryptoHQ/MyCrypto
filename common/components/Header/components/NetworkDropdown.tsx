a79855731f9d9253803c9b7b76780755443eeb32
Will O'Beirne <wbobeirne@gmail.com>
2018-04-26 14:56:39 -0400

    Convert node label data into selector function

--- common/components/Header/components/NetworkDropdown.tsx
+++ common/components/Header/components/NetworkDropdown.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { DropdownShell } from 'components/ui';
 import NetworkSelector from 'components/NetworkSelector';
-import { getNodeId, getNodeConfig, getNetworkConfig, getAllNodes } from 'selectors/config';
-import { isAutoNodeConfig } from 'libs/nodes';
-import { NodeConfig } from 'types/node';
-import { NetworkConfig } from 'types/network';
+import { getNodeConfig, getSelectedNodeLabel } from 'selectors/config';
 import { AppState } from 'reducers';
 import './NetworkDropdown.scss';
 
@@ -14,10 +11,8 @@ interface OwnProps {
 }
 
 interface StateProps {
-  nodeSelection: string;
-  node: NodeConfig;
-  network: NetworkConfig;
-  allNodes: { [key: string]: NodeConfig };
+  node: ReturnType<typeof getNodeConfig>;
+  nodeLabel: ReturnType<typeof getSelectedNodeLabel>;
 }
 
 type Props = OwnProps & StateProps;
@@ -26,20 +21,30 @@ class NetworkDropdown extends React.Component<Props> {
   private dropdown: DropdownShell | null;
 
   public render() {
-    const { nodeSelection } = this.props;
+    const { node } = this.props;
 
     return (
       <DropdownShell
         ariaLabel="Dropdown"
         renderLabel={this.renderLabel}
         renderOptions={this.renderOptions}
-        disabled={nodeSelection === 'web3'}
+        disabled={node.id === 'web3'}
         size="smr"
         color="white"
         ref={el => (this.dropdown = el)}
       />
     );
   }
+
+  private renderLabel = () => {
+    const { nodeLabel } = this.props;
+    return (
+      <span>
+        {nodeLabel.network}
+        <small>{nodeLabel.info}</small>
+      </span>
+    );
+  };
 
   private renderOptions = () => {
     return (
@@ -50,36 +55,6 @@ class NetworkDropdown extends React.Component<Props> {
           onSelectNode={this.onSelect}
         />
       </div>
-    );
-  };
-
-  private renderLabel = () => {
-    const { allNodes, network, node } = this.props;
-    let suffix;
-
-    if (node.isCustom) {
-      // Custom nodes have names
-      suffix = node.name;
-    } else if (node.isAuto) {
-      // Auto nodes should show the count of all nodes it uses. If only one,
-      // show the service name of the node.
-      const networkNodes = Object.values(allNodes).filter(
-        n => !isAutoNodeConfig(n) && n.network === node.network
-      );
-
-      if (networkNodes.length > 1) {
-        suffix = `${networkNodes.length} Nodes`;
-      } else {
-        suffix = networkNodes[0].service;
-      }
-    } else {
-      suffix = node.service;
-    }
-
-    return (
-      <span>
-        {network.name} <small>({suffix})</small>
-      </span>
     );
   };
 
@@ -98,8 +73,6 @@ class NetworkDropdown extends React.Component<Props> {
 }
 
 export default connect((state: AppState): StateProps => ({
-  nodeSelection: getNodeId(state),
   node: getNodeConfig(state),
-  network: getNetworkConfig(state),
-  allNodes: getAllNodes(state)
+  nodeLabel: getSelectedNodeLabel(state)
 }))(NetworkDropdown);
