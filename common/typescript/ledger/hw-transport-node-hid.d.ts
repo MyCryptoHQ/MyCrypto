bd72b95e8056f2e9d84ee870a8f0709696ec61b0
HenryNguyen5 <ngu.henry@hotmail.com>
2018-04-27 19:36:44 -0400

    Finish types

--- common/typescript/ledger/hw-transport-node-hid.d.ts
+++ common/typescript/ledger/hw-transport-node-hid.d.ts
@@ -1,11 +1,17 @@
-import LedgerTransport, { Observer, DescriptorEvent } from 'ledgerhq__hw-transport';
+import LedgerTransport, { Observer, DescriptorEvent, Subscription } from 'ledgerhq__hw-transport';
 import { HID, Device } from 'node-hid';
 
-type ObserverEvents = {} | {} | {};
 declare module 'ledgerhq__hw-transport-node-hid' {
   export default class TransportNodeHid extends LedgerTransport<string> {
     /**
-     * Creates an instance of TransportNodeHid.
+     * @description Creates an instance of TransportNodeHid.
+     * @example
+     *
+     * ```ts
+     * import TransportNodeHid from "@ledgerhq/hw-transport-node-u2f";
+     * TransportNodeHid.create().then(transport => ...);
+     * ```
+     *
      * @param {HID} device
      * @param {boolean} [ledgerTransport]
      * @param {number} [timeout]
@@ -30,8 +36,62 @@ declare module 'ledgerhq__hw-transport-node-hid' {
      * @returns {Promise<string[]>}
      * @memberof TransportNodeHid
      */
-    public static list(): Promise<Device['path'][]>;
+    public static list<Descriptor = string>(): Promise<Descriptor[]>;
 
-    public static listen(observer: Observer<DescriptorEvent<string, HID>, Device['path']>);
+    /**
+     *
+     * @description Listen all device events for a given Transport.
+     * The method takes an Observer of DescriptorEvent and returns a Subscription
+     * according to Observable paradigm https://github.com/tc39/proposal-observable
+     * a DescriptorEvent is a { descriptor, type } object.
+     * Type can be "add" or "remove" and descriptor is a value you can pass to open(descriptor).
+     * Each listen() call will first emit all potential device already connected and then will emit events can come over times,
+     * for instance if you plug a USB device after listen().
+     * @static
+     * @template Descriptor
+     * @template Device
+     * @template Err
+     * @param {Observer<DescriptorEvent<Descriptor, Device>, Err>} observer
+     * @returns {Subscription}
+     * @memberof TransportNodeHid
+     */
+    public static listen<Descriptor = string, Device = HID, Err = void>(
+      observer: Observer<DescriptorEvent<Descriptor, Device>, Err>
+    ): Subscription;
+
+    /**
+     *
+     * @description Attempt to create a Transport instance with a descriptor.
+     * @static
+     * @template Descriptor
+     * @param {Descriptor} devicePath
+     * @returns {Promise<TransportNodeHid>}
+     * @memberof TransportNodeHid
+     */
+    public static open<Descriptor = string>(devicePath: Descriptor): Promise<TransportNodeHid>;
+
+    /**
+     *
+     * @description Low level api to communicate with the device.
+     * @param {Buffer} apdu
+     * @returns {Promise<Buffer>}
+     * @memberof TransportNodeHid
+     */
+    public exchange(apdu: Buffer): Promise<Buffer>;
+
+    /**
+     *
+     * @description Does nothing
+     * @memberof TransportNodeHid
+     */
+    public setScrambleKey(): void;
+
+    /**
+     *
+     * @description Close the exchange with the device.
+     * @returns {Promise<void>}
+     * @memberof TransportNodeHid
+     */
+    public close(): Promise<void>;
   }
 }
--- common/typescript/ledger/hw-transport.d.ts
+++ common/typescript/ledger/hw-transport.d.ts
@@ -1,12 +1,93 @@
 declare module 'ledgerhq__hw-transport' {
+  /**
+   * @description all possible status codes.
+   * @see https://github.com/LedgerHQ/blue-app-btc/blob/d8a03d10f77ca5ef8b22a5d062678eef788b824a/include/btchip_apdu_constants.h#L85-L115
+   * @example
+   * import { StatusCodes } from "@ledgerhq/hw-transport";
+   * @export
+   * @enum {number}
+   */
+  export enum StatusCodes {
+    PIN_REMAINING_ATTEMPTS = 0x63c0,
+    INCORRECT_LENGTH = 0x6700,
+    COMMAND_INCOMPATIBLE_FILE_STRUCTURE = 0x6981,
+    SECURITY_STATUS_NOT_SATISFIED = 0x6982,
+    CONDITIONS_OF_USE_NOT_SATISFIED = 0x6985,
+    INCORRECT_DATA = 0x6a80,
+    NOT_ENOUGH_MEMORY_SPACE = 0x6a84,
+    REFERENCED_DATA_NOT_FOUND = 0x6a88,
+    FILE_ALREADY_EXISTS = 0x6a89,
+    INCORRECT_P1_P2 = 0x6b00,
+    INS_NOT_SUPPORTED = 0x6d00,
+    CLA_NOT_SUPPORTED = 0x6e00,
+    TECHNICAL_PROBLEM = 0x6f00,
+    OK = 0x9000,
+    MEMORY_PROBLEM = 0x9240,
+    NO_EF_SELECTED = 0x9400,
+    INVALID_OFFSET = 0x9402,
+    FILE_NOT_FOUND = 0x9404,
+    INCONSISTENT_FILE = 0x9408,
+    ALGORITHM_NOT_SUPPORTED = 0x9484,
+    INVALID_KCV = 0x9485,
+    CODE_NOT_INITIALIZED = 0x9802,
+    ACCESS_CONDITION_NOT_FULFILLED = 0x9804,
+    CONTRADICTION_SECRET_CODE_STATUS = 0x9808,
+    CONTRADICTION_INVALIDATION = 0x9810,
+    CODE_BLOCKED = 0x9840,
+    MAX_VALUE_REACHED = 0x9850,
+    GP_AUTH_FAILED = 0x6300,
+    LICENSING = 0x6f42,
+    HALTED = 0x6faa
+  }
+
+  export enum AltStatusCodes {
+    'Incorrect length' = 0x6700,
+    'Security not satisfied (dongle locked or have invalid access rights)' = 0x6982,
+    'Condition of use not satisfied (denied by the user?)' = 0x6985,
+    'Invalid data received' = 0x6a80,
+    'Invalid parameter received' = 0x6b00,
+    INTERNAL_ERROR = 'Internal error, please report'
+  }
+
   export interface Subscription {
     unsubscribe: () => void;
   }
 
-  export interface Observer<Ev, NextRet, ErrParam, ErrRet, CompleteRet> {
-    next: (event: Ev) => NextRet;
-    error: (e: ErrParam) => ErrRet;
-    complete: () => CompleteRet;
+  export interface ITransportError extends Error {
+    name: 'TransportError';
+    message: string;
+    stack?: string;
+    id: string;
+  }
+
+  /**
+   * TransportError is used for any generic transport errors.
+   * e.g. Error thrown when data received by exchanges are incorrect or if exchanged failed to communicate with the device for various reason.
+   */
+  export class TransportError extends Error {
+    new(message: string, id: string): ITransportError;
+  }
+
+  export interface ITransportStatusError extends Error {
+    name: 'TransportStatusError';
+    message: string;
+    stack?: string;
+    statusCode: number;
+    statusText: keyof typeof StatusCodes | 'UNKNOWN_ERROR';
+  }
+
+  /**
+   * Error thrown when a device returned a non success status.
+   * the error.statusCode is one of the `StatusCodes` exported by this library.
+   */
+  export class TransportStatusError extends Error {
+    new(statusCode: number): ITransportStatusError;
+  }
+
+  export interface Observer<Ev, Err> {
+    next: (event: Ev) => void;
+    error: (e: Err) => void;
+    complete: () => void;
   }
 
   export interface DescriptorEvent<Descriptor, Device> {
@@ -51,30 +132,15 @@ declare module 'ledgerhq__hw-transport' {
      * Each listen() call will first emit all potential device already connected and then will emit events can come over times,
      * for instance if you plug a USB device after listen() or a bluetooth device become discoverable.
      * @static
-     * @template Ev
-     * @template NextRet
-     * @template ErrParam
-     * @template ErrRet
-     * @template CompleteRet
-     * @param {Observer<Ev, NextRet, ErrParam, ErrRet, CompleteRet>} observer
+     * @template Descriptor
+     * @template Device
+     * @template Err
+     * @param {Observer<DescriptorEvent<Descriptor, Device>, Err>} observer
      * @returns {Subscription}
      * @memberof LedgerTransport
      */
-    public static listen<
-      Descriptor,
-      Device = any,
-      NextRet = any,
-      ErrParam = any,
-      ErrRet = any,
-      CompleteRet = any
-    >(
-      observer: Observer<
-        DescriptorEvent<Descriptor, Device>,
-        NextRet,
-        ErrParam,
-        ErrRet,
-        CompleteRet
-      >
+    public static listen<Descriptor, Device = any, Err = any>(
+      observer: Observer<DescriptorEvent<Descriptor, Device>, Err>
     ): Subscription;
 
     /**
