5aa41d9932ff3c4b96c01d5961f98d7474976274
Will O'Beirne <wbobeirne@gmail.com>
2018-05-04 11:56:17 -0400

    Adjust API

--- electron-app/preload/index.ts
+++ electron-app/preload/index.ts
@@ -1,2 +1,2 @@
-import { setupClient } from 'shared/enclave/client/preload';
-setupClient();
+import { registerProtocol } from 'shared/enclave/preload';
+registerProtocol();
--- shared/enclave/preload.ts
+++ shared/enclave/preload.ts
@@ -1,7 +1,7 @@
 import { webFrame } from 'electron';
 import { PROTOCOL_NAME } from 'shared/enclave/utils';
 
-export function setupClient() {
+export function registerProtocol() {
   // Whitelist custom protocol
   webFrame.registerURLSchemeAsPrivileged(PROTOCOL_NAME);
 }
