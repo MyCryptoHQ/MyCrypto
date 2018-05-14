661d862311e526e5e4ad0ee2bdb724f881e2a5d0
HenryNguyen5 <ngu.henry@hotmail.com>
2018-05-14 13:06:04 -0400

    Improve electron versioning (#1777)
    * Add electron version as package json field, unify version export
    
    * Base electron versioning on assets in uploaded binaries
    
    * Update schema, remove dead code
    
    * Remove unused import
    
    * Remove console logs
    
    * correct electron-version
    
    * better styling for versions
    
    * bump electron-version

--- spec/utils/helpers.spec.ts
+++ spec/utils/helpers.spec.ts
@@ -1,4 +1,4 @@
-import { objectContainsObjectKeys, isNewerVersion } from 'utils/helpers';
+import { objectContainsObjectKeys } from 'utils/helpers';
 
 describe('objectContainsObjectKeys', () => {
   it('should return true when object contains all keys of another object', () => {
@@ -30,47 +30,5 @@ describe('objectContainsObjectKeys', () => {
     };
 
     expect(objectContainsObjectKeys(checkingObject, containingObject)).toBeFalsy();
-  });
-});
-
-describe('isNewerVersion', () => {
-  it('Should return true for newer major version', () => {
-    expect(isNewerVersion('1.0.0', '2.0.0')).toBeTruthy();
-  });
-
-  it('Should return true for newer minor version', () => {
-    expect(isNewerVersion('1.0.0', '1.1.0')).toBeTruthy();
-  });
-
-  it('Should return true for newer patch version', () => {
-    expect(isNewerVersion('1.0.0', '1.0.1')).toBeTruthy();
-  });
-
-  it('Should return false for same version', () => {
-    expect(isNewerVersion('1.0.0', '1.0.0')).toBeFalsy();
-  });
-
-  it('Should return false for older version', () => {
-    expect(isNewerVersion('1.0.1', '1.0.0')).toBeFalsy();
-  });
-
-  it('Should return true for newer release candidate', () => {
-    expect(isNewerVersion('1.0.0-RC.0', '1.0.0-RC.1')).toBeTruthy();
-  });
-
-  it('Should return false for same release candidate', () => {
-    expect(isNewerVersion('1.0.0-RC.1', '1.0.0-RC.1')).toBeFalsy();
-  });
-
-  it('Should return false for older release candidate', () => {
-    expect(isNewerVersion('1.0.0-RC.1', '1.0.0-RC.0')).toBeFalsy();
-  });
-
-  it('Should return true for same version, but no release candidate', () => {
-    expect(isNewerVersion('1.0.0-RC.999', '1.0.0')).toBeTruthy();
-  });
-
-  it('Should return true for newer non-release candidate', () => {
-    expect(isNewerVersion('1.0.0-RC.999', '1.0.1')).toBeTruthy();
   });
 });
