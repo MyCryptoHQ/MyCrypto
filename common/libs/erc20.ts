3ca36470a8f35ed08bb57a7df4fdc9234517d729
HenryNguyen5 <ngu.henry@hotmail.com>
2018-05-15 22:12:37 -0400

    Add support for decimal and symbol getters

--- common/libs/erc20.ts
+++ common/libs/erc20.ts
@@ -1,20 +1,51 @@
 import Contract from 'libs/contracts';
+import { ABIFunc, ABIFuncParamless } from 'libs/ens/contracts/AbiFunc';
 
-interface ABIFunc<T, K = void> {
-  encodeInput(x: T): string;
-  decodeInput(argStr: string): T;
-  decodeOutput(argStr: string): K;
-}
+type uint256 = any;
+type uint8 = any;
 
 type address = any;
-type uint256 = any;
 
 interface IErc20 {
+  decimals: ABIFuncParamless<{ decimals: uint8 }>;
+  symbol: ABIFuncParamless<{ symbol: string }>;
+
   balanceOf: ABIFunc<{ _owner: address }, { balance: uint256 }>;
   transfer: ABIFunc<{ _to: address; _value: uint256 }>;
 }
 
 const erc20Abi = [
+  {
+    name: 'decimals',
+    type: 'function',
+
+    constant: true,
+    payable: false,
+    inputs: [],
+
+    outputs: [
+      {
+        name: '',
+        type: 'uint8'
+      }
+    ]
+  },
+  {
+    name: 'symbol',
+    type: 'function',
+    constant: true,
+    payable: false,
+
+    inputs: [],
+
+    outputs: [
+      {
+        name: '',
+        type: 'string'
+      }
+    ]
+  },
+
   {
     name: 'balanceOf',
     type: 'function',
@@ -35,11 +66,13 @@ const erc20Abi = [
       }
     ]
   },
+
   {
     name: 'transfer',
     type: 'function',
     constant: false,
     payable: false,
+
     inputs: [
       {
         name: '_to',
@@ -57,7 +90,32 @@ const erc20Abi = [
         type: 'bool'
       }
     ]
+  },
+  {
+    name: 'Transfer',
+    type: 'event',
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        name: '_from',
+        type: 'address'
+      },
+      {
+        indexed: true,
+        name: '_to',
+        type: 'address'
+      },
+      {
+        indexed: false,
+        name: '_value',
+        type: 'uint256'
+      }
+    ]
   }
 ];
 
-export default (new Contract(erc20Abi) as any) as IErc20;
+export default (new Contract(erc20Abi, {
+  decimals: ['decimals'],
+  symbol: ['symbol']
+}) as any) as IErc20;
