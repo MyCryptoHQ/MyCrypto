b50b375ad224d13edc2adbc24256106605f0c939
HenryNguyen5 <ngu.henry@hotmail.com>
2018-05-18 19:01:05 -0400

    Use common input field between decimal and symbol fields

--- common/components/BalanceSidebar/TokenBalances/AddCustomTokenForm/DecimalField.tsx
+++ common/components/BalanceSidebar/TokenBalances/AddCustomTokenForm/DecimalField.tsx
@@ -1,126 +1,34 @@
 import React from 'react';
-import { Input } from 'components/ui';
-import Spinner from 'components/ui/Spinner';
-import ERC20 from 'libs/erc20';
-import { shepherdProvider } from 'libs/nodes';
 import { isPositiveIntegerOrZero } from 'libs/validators';
 import { translateRaw } from 'translations';
 import { Result } from 'nano-result';
+
+import { FieldInput } from './FieldInput';
 
 interface OwnProps {
   address?: string;
   onChange(decimals: Result<string>): void;
 }
 
-interface State {
-  userInput: string;
-  decimals: Result<string>;
-  autoDecimal: boolean;
-  addressToLoad?: string;
-  loading: boolean;
-}
-
-export class DecimalField extends React.Component<OwnProps, State> {
-  public static getDerivedStateFromProps(
-    nextProps: OwnProps,
-    prevState: State
-  ): Partial<State> | null {
-    if (nextProps.address && nextProps.address !== prevState.addressToLoad) {
-      return { loading: true, autoDecimal: true, addressToLoad: nextProps.address };
-    }
-    return null;
-  }
-
-  public state: State = {
-    userInput: '',
-    autoDecimal: true,
-    decimals: Result.from({ res: '' }),
-    loading: false
-  };
-
-  private currentRequest: Promise<any> | null;
-
-  public componentDidUpdate() {
-    if (this.state.addressToLoad && this.state.loading) {
-      this.attemptToLoadDecimals(this.state.addressToLoad);
-    }
-  }
-
-  public componentWillUnmount() {
-    if (this.currentRequest) {
-      this.currentRequest = null;
-    }
-  }
+export class DecimalField extends React.Component<OwnProps> {
   public render() {
-    const { decimals, autoDecimal, loading, userInput } = this.state;
-
     return (
-      <label className="AddCustom-field form-group">
-        <div className="input-group-header">{translateRaw('TOKEN_DEC')}</div>
-        {loading ? (
-          <Spinner />
-        ) : (
-          <Input
-            isValid={decimals.ok()}
-            className="input-group-input-small"
-            type="text"
-            name="Decimals"
-            readOnly={autoDecimal}
-            value={decimals.ok() ? decimals.unwrap() : userInput}
-            onChange={this.handleFieldChange}
-          />
-        )}
-        {decimals.err() && <div className="AddCustom-field-error">{decimals.err()}</div>}
-      </label>
+      <FieldInput
+        fieldName={translateRaw('TOKEN_DEC')}
+        fieldToFetch={'decimals'}
+        shouldEnableAutoField={req => !(req.toVal().res === '0')}
+        address={this.props.address}
+        userInputValidator={this.isValidUserInput}
+        onChange={this.props.onChange}
+      />
     );
   }
 
-  private handleFieldChange = (args: React.FormEvent<HTMLInputElement>) => {
-    const userInput = args.currentTarget.value;
+  private isValidUserInput = (userInput: string) => {
     const validDecimals = isPositiveIntegerOrZero(Number(userInput));
     const decimals: Result<string> = validDecimals
       ? Result.from({ res: userInput })
       : Result.from({ err: 'Invalid decimal' });
-
-    this.setState({ decimals, userInput });
-    this.props.onChange(decimals);
+    return decimals;
   };
-
-  private attemptToLoadDecimals(address: string) {
-    const req = this.loadDecimals(address);
-
-    // process request
-    this.currentRequest = req
-      // set state on successful request e.g it was not cancelled
-      // and then also set our current request to null
-      .then(({ decimals }) =>
-        this.setState({
-          decimals,
-          loading: false,
-          autoDecimal: decimals.toVal().res === '0' ? false : true
-        })
-      )
-      .catch(e => {
-        console.error(e);
-        // if the component is unmounted, then dont call set state
-        if (!this.currentRequest) {
-          return;
-        }
-
-        // otherwise it was a failed fetch call
-        this.setState({ autoDecimal: false, loading: false });
-      })
-      .then(() => (this.currentRequest = null));
-  }
-
-  private loadDecimals(address: string) {
-    return shepherdProvider
-      .sendCallRequest({ data: ERC20.decimals.encodeInput(), to: address })
-      .then(ERC20.decimals.decodeOutput)
-      .then(({ decimals }) => {
-        const result = Result.from({ res: decimals });
-        this.props.onChange(result);
-        return { decimals: result };
-      });
-  }
 }
--- common/components/BalanceSidebar/TokenBalances/AddCustomTokenForm/SymbolField.tsx
+++ common/components/BalanceSidebar/TokenBalances/AddCustomTokenForm/SymbolField.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { Input } from 'components/ui';
-import Spinner from 'components/ui/Spinner';
-import ERC20 from 'libs/erc20';
-import { shepherdProvider } from 'libs/nodes';
 import { translateRaw } from 'translations';
 import { IGenerateSymbolLookup } from './AddCustomTokenForm';
 import { Result } from 'nano-result';
+import { FieldInput } from './FieldInput';
 
 interface OwnProps {
   address?: string;
@@ -13,117 +10,30 @@ interface OwnProps {
   onChange(symbol: Result<string>): void;
 }
 
-interface State {
-  symbol: Result<string>;
-  autoSymbol: boolean;
-  userInput: string;
-  addressToLoad?: string;
-  loading: boolean;
-}
-
-export class SymbolField extends React.Component<OwnProps, State> {
-  public static getDerivedStateFromProps(
-    nextProps: OwnProps,
-    prevState: State
-  ): Partial<State> | null {
-    if (nextProps.address && nextProps.address !== prevState.addressToLoad) {
-      return { loading: true, autoSymbol: true, addressToLoad: nextProps.address };
-    }
-    return null;
-  }
-
-  public state: State = {
-    userInput: '',
-    autoSymbol: true,
-    symbol: Result.from({ res: '' }),
-    loading: false
-  };
-
-  private currentRequest: Promise<any> | null;
-
-  public componentDidUpdate() {
-    if (this.state.addressToLoad && this.state.loading) {
-      this.attemptToLoadSymbol(this.state.addressToLoad);
-    }
-  }
-
-  public componentWillUnmount() {
-    if (this.currentRequest) {
-      this.currentRequest = null;
-    }
-  }
+export class SymbolField extends React.Component<OwnProps> {
   public render() {
-    const { userInput, symbol, autoSymbol, loading } = this.state;
-
     return (
-      <label className="AddCustom-field form-group">
-        <div className="input-group-header">{translateRaw('TOKEN_SYMBOL')}</div>
-        {loading ? (
-          <Spinner />
-        ) : (
-          <Input
-            isValid={symbol.ok()}
-            className="input-group-input-small"
-            type="text"
-            name="Symbol"
-            readOnly={autoSymbol}
-            value={symbol.ok() ? symbol.unwrap() : userInput}
-            onChange={this.handleFieldChange}
-          />
-        )}
-        {symbol.err() && <div className="AddCustom-field-error">{symbol.err()}</div>}
-      </label>
+      <FieldInput
+        fieldName={translateRaw('TOKEN_SYMBOL')}
+        fieldToFetch={'symbol'}
+        shouldEnableAutoField={req => !req.err()}
+        address={this.props.address}
+        userInputValidator={this.isValidUserInput}
+        fetchedFieldValidator={field =>
+          field
+            ? Result.from({ res: field })
+            : Result.from({ err: 'No Symbol found, please input the token symbol manually' })
+        }
+        onChange={this.props.onChange}
+      />
     );
   }
 
-  private handleFieldChange = (args: React.FormEvent<HTMLInputElement>) => {
-    const userInput = args.currentTarget.value;
+  private isValidUserInput = (userInput: string) => {
     const validSymbol = !this.props.symbolLookup[userInput];
     const symbol: Result<string> = validSymbol
       ? Result.from({ res: userInput })
       : Result.from({ err: 'A token with this symbol already exists' });
-
-    this.setState({ userInput, symbol });
-    this.props.onChange(symbol);
+    return symbol;
   };
-
-  private attemptToLoadSymbol(address: string) {
-    const req = this.loadSymbols(address);
-
-    // process request
-    this.currentRequest = req
-      // set state on successful request e.g it was not cancelled
-      // and then also set our current request to null
-      .then(({ symbol }) =>
-        this.setState({
-          symbol,
-          loading: false,
-          autoSymbol: symbol.err() ? false : true
-        })
-      )
-      .catch(e => {
-        console.error(e);
-        // if the component is unmounted, then dont call set state
-        if (!this.currentRequest) {
-          return;
-        }
-
-        // otherwise it was a failed fetch call
-        this.setState({ autoSymbol: false, loading: false });
-      })
-      .then(() => (this.currentRequest = null));
-  }
-
-  private loadSymbols(address: string) {
-    return shepherdProvider
-      .sendCallRequest({ data: ERC20.symbol.encodeInput(), to: address })
-      .then(ERC20.symbol.decodeOutput)
-      .then(({ symbol }) => {
-        const result: Result<string> = symbol
-          ? Result.from({ res: symbol })
-          : Result.from({ err: 'No Symbol found, please input the token symbol manually' });
-        this.props.onChange(result);
-        return { symbol: result };
-      });
-  }
 }
