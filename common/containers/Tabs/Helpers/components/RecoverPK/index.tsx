9467d1e6dcfe6877b12406dca96219947d5b8f16
Bakhtiar <bakh@zineme.com>
2018-05-21 22:56:06 +0600

    wip show spinner while guessing private key in helpers

--- common/containers/Tabs/Helpers/components/RecoverPK/index.tsx
+++ common/containers/Tabs/Helpers/components/RecoverPK/index.tsx
@@ -1,26 +1,70 @@
 import React from 'react';
 import './index.scss';
-import { Input } from 'components/ui';
+import { Input, Spinner } from 'components/ui';
 import { fromPrivateKey } from 'ethereumjs-wallet';
 import { toBuffer } from 'ethereumjs-util';
 
-const sorryPhrase = 'Sorry not found :(';
+interface Result {
+  pk: string;
+  status: string;
+}
 
 interface State {
   pk: string;
   address: string;
-  actualPK: string;
+  result: Result;
 }
 
 export default class RecoverPK extends React.Component<State> {
   public state = {
     pk: '',
     address: '',
-    actualPK: ''
+    result: {
+      pk: '',
+      status: ''
+    }
   };
 
   public render() {
-    const { pk, address, actualPK } = this.state;
+    const { pk, address, result } = this.state;
+    let content = null;
+
+    if (result.status === 'ok') {
+      content = (
+        <div>
+          <label className="input-group">
+            <div className="input-group-header">Actual Private Key</div>
+            <Input value={result.pk} type="text" isValid={true} name="actualPK" readOnly={true} />
+          </label>
+          <ul className="recover-pk-list">
+            <li style={{ color: 'red' }}>Consider yourself very very lucky.</li>
+            <li>Now you need to create a new secure wallet and move all funds to it.</li>
+            <li>
+              In order to prevent the same thing from happening, please make sure your address you
+              are sending to matches the address on your paper wallet.
+            </li>
+            <li>
+              <a href="https://support.mycrypto.com/getting-started/creating-a-new-wallet-on-mycrypto.html">
+                Read this guide for more information.
+              </a>
+            </li>
+            <li>
+              <a href="https://support.mycrypto.com/getting-started/protecting-yourself-and-your-funds.html">
+                Learn how to protect yourself and your funds.
+              </a>
+            </li>
+          </ul>
+        </div>
+      );
+    }
+
+    if (result.status === 'error') {
+      content = <p className="error-text">Sorry, Private Key not found :(</p>;
+    }
+
+    if (result.status === 'loading') {
+      content = <Spinner size="x3" />;
+    }
 
     return (
       <div className="Tab-content">
@@ -49,43 +93,7 @@ export default class RecoverPK extends React.Component<State> {
               Start the Search!
             </button>
 
-            {actualPK ? (
-              <label className="input-group">
-                <div className="input-group-header">Actual Private Key</div>
-                <Input
-                  value={actualPK}
-                  type="text"
-                  isValid={true}
-                  name="actualPK"
-                  readOnly={true}
-                />
-              </label>
-            ) : (
-              ''
-            )}
-
-            {actualPK && actualPK !== sorryPhrase ? (
-              <ul className="recover-pk-list">
-                <li style={{ color: 'red' }}>Consider yourself very very lucky.</li>
-                <li>Now you need to create a new secure wallet and move all funds to it.</li>
-                <li>
-                  In order to prevent the same thing from happening, please make sure your address
-                  you are sending to matches the address on your paper wallet.
-                </li>
-                <li>
-                  <a href="https://support.mycrypto.com/getting-started/creating-a-new-wallet-on-mycrypto.html">
-                    Read this guide for more information.
-                  </a>
-                </li>
-                <li>
-                  <a href="https://support.mycrypto.com/getting-started/protecting-yourself-and-your-funds.html">
-                    Learn how to protect yourself and your funds.
-                  </a>
-                </li>
-              </ul>
-            ) : (
-              ''
-            )}
+            {content}
           </div>
         </section>
       </div>
@@ -96,13 +104,17 @@ export default class RecoverPK extends React.Component<State> {
     const currentState: any = { ...this.state };
 
     currentState[event.currentTarget.name] = event.currentTarget.value;
-    currentState.actualPK = '';
+    currentState.result = {
+      pk: '',
+      status: ''
+    };
 
     this.setState(currentState);
   };
 
   private searchPK = () => {
-    const targetPublicAddress = this.state.address;
+    const { address, pk, result } = this.state;
+    const targetPublicAddress = address;
     const characters = [
       'a',
       'b',
@@ -122,39 +134,64 @@ export default class RecoverPK extends React.Component<State> {
       '9'
     ];
 
-    let basePrivateKey = this.state.pk;
+    let basePrivateKey = pk;
     basePrivateKey =
-      basePrivateKey.substring(0, 2) == '0x' ? basePrivateKey.substring(2) : basePrivateKey;
+      basePrivateKey.substring(0, 2) === '0x' ? basePrivateKey.substring(2) : basePrivateKey;
 
     let wallet;
-    let actualPK;
-    for (let keyPosition = 0; keyPosition <= basePrivateKey.length; keyPosition++) {
-      for (let character = 0; character < characters.length; character++) {
-        let pkArray = basePrivateKey.split('');
-        pkArray.splice(keyPosition, 0, characters[character]);
-        let privateKeyGuess = pkArray.join('');
 
-        try {
-          wallet = fromPrivateKey(toBuffer('0x' + privateKeyGuess));
-        } catch (error) {
-          wallet = null;
-        }
+    function* guessPK() {
+      for (const idx of Object.keys(basePrivateKey.split(''))) {
+        for (const character of characters) {
+          const pkArray = basePrivateKey.split('');
+          pkArray.splice(Number(idx), 0, character);
+          const privateKeyGuess = pkArray.join('');
 
-        if (wallet) {
-          let publicAddress = wallet.getAddressString();
-
-          if (publicAddress.toLowerCase() == targetPublicAddress.toLowerCase()) {
-            actualPK = privateKeyGuess;
+          try {
+            wallet = fromPrivateKey(toBuffer('0x' + privateKeyGuess));
+          } catch (error) {
+            wallet = null;
           }
+
+          if (wallet) {
+            const publicAddress = wallet.getAddressString();
+
+            if (publicAddress.toLowerCase() === targetPublicAddress.toLowerCase()) {
+              result.status = 'ok';
+              result.pk = privateKeyGuess;
+            }
+          }
+
+          yield result;
         }
+      }
+
+      if (!result.pk) {
+        result.status = 'error';
+      }
+
+      yield result;
+    }
+
+    if (basePrivateKey.length === 63) {
+      result.status = 'loading';
+
+      this.setState({
+        ...this.state,
+        result
+      });
+
+      let newResult = { ...result };
+      const gen = guessPK();
+
+      while (newResult.status === 'loading') {
+        newResult = gen.next().value;
       }
     }
 
-    if (!actualPK) actualPK = sorryPhrase;
-
     this.setState({
       ...this.state,
-      actualPK
+      result
     });
   };
 }
