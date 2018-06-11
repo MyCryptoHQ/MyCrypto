004fa40a305ab15438aaaf468d50383856bce4f4
Danny Skubak <skubakdj@gmail.com>
2018-06-11 16:18:02 -0400

    move transaction reset to componentWillMount

--- common/components/TXMetaDataPanel/TXMetaDataPanel.tsx
+++ common/components/TXMetaDataPanel/TXMetaDataPanel.tsx
@@ -60,7 +60,7 @@ interface State {
 
 class TXMetaDataPanel extends React.Component<Props, State> {
   public static defaultProps: DefaultProps = {
-    initialState: 'simple'
+    initialState: 'advanced'
   };
 
   public state: State = {
@@ -68,9 +68,14 @@ class TXMetaDataPanel extends React.Component<Props, State> {
     sliderState: (this.props as DefaultProps).initialState
   };
 
-  public componentDidMount() {
+  public componentWillMount() {
     if (!this.props.offline) {
       this.props.resetTransactionRequested();
+    }
+  }
+
+  public componentDidMount() {
+    if (!this.props.offline) {
       this.props.fetchCCRates([this.props.network.unit]);
       this.props.getNonceRequested();
     }
