d75cd5ccdb7cc6ec8c23553b5e2fa7337576537c
Danny Skubak <skubakdj@gmail.com>
2018-06-11 17:46:03 -0400

    Bug Fix for #1863 (#1929)
    * use RedirectWithQuery wrapper instead of Redirect
    
    * remove unused var

--- common/Root.tsx
+++ common/Root.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Provider, connect } from 'react-redux';
-import { withRouter, Switch, Redirect, HashRouter, Route, BrowserRouter } from 'react-router-dom';
+import { withRouter, Switch, HashRouter, Route, BrowserRouter } from 'react-router-dom';
 // Components
 import Contracts from 'containers/Tabs/Contracts';
 import ENS from 'containers/Tabs/ENS';
@@ -80,7 +80,6 @@ class RootClass extends Component<Props, State> {
     const routes = (
       <CaptureRouteNotFound>
         <Switch>
-          <Redirect exact={true} from="/" to="/account" />
           <Route path="/account" component={SendTransaction} />
           <Route path="/generate" component={GenerateWallet} />
           <Route path="/swap" component={Swap} />
@@ -90,6 +89,7 @@ class RootClass extends Component<Props, State> {
           <Route path="/tx-status" component={CheckTransaction} exact={true} />
           <Route path="/pushTx" component={BroadcastTx} />
           <Route path="/support-us" component={SupportPage} exact={true} />
+          <RedirectWithQuery exactArg={true} from="/" to="/account" />
           <RouteNotFound />
         </Switch>
       </CaptureRouteNotFound>
