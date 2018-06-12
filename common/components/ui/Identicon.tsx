a791791daf6328fb5e87fdb3a06765d1d7656585
Will O'Beirne <wbobeirne@gmail.com>
2018-06-12 13:39:30 -0400

    Identicon back to SFC

--- common/components/ui/Identicon.tsx
+++ common/components/ui/Identicon.tsx
@@ -16,47 +16,45 @@ interface StateProps {
 
 type Props = OwnProps & StateProps;
 
-class Identicon extends React.Component<Props> {
-  public render() {
-    const size = this.props.size || '4rem';
-    const { address, isValidAddress, className = '' } = this.props;
-    const identiconDataUrl = isValidAddress(address) ? makeBlockie(address) : '';
-    return (
-      // Use inline styles for printable wallets
-      <div
-        className={`Identicon ${className}`}
-        title="Address Identicon"
-        style={{ width: size, height: size, position: 'relative' }}
-        aria-hidden={!identiconDataUrl}
-      >
-        {identiconDataUrl && (
-          <img
-            src={identiconDataUrl}
-            alt="Unique Address Image"
-            style={{
-              height: '100%',
-              width: '100%',
-              padding: '0px',
-              borderRadius: '50%'
-            }}
-          />
-        )}
-        <div
-          className="border"
+const Identicon: React.SFC<Props> = props => {
+  const size = props.size || '4rem';
+  const { address, isValidAddress, className = '' } = props;
+  const identiconDataUrl = isValidAddress(address) ? makeBlockie(address) : '';
+  return (
+    // Use inline styles for printable wallets
+    <div
+      className={`Identicon ${className}`}
+      title="Address Identicon"
+      style={{ width: size, height: size, position: 'relative' }}
+      aria-hidden={!identiconDataUrl}
+    >
+      {identiconDataUrl && (
+        <img
+          src={identiconDataUrl}
+          alt="Unique Address Image"
           style={{
-            position: 'absolute',
             height: '100%',
             width: '100%',
-            top: 0,
-            boxShadow: '0 1px 2px 0 rgba(0, 0, 0, 0.15), inset 0 0 3px 0 rgba(0, 0, 0, 0.15)',
-            borderRadius: '50%',
-            pointerEvents: 'none'
+            padding: '0px',
+            borderRadius: '50%'
           }}
         />
-      </div>
-    );
-  }
-}
+      )}
+      <div
+        className="border"
+        style={{
+          position: 'absolute',
+          height: '100%',
+          width: '100%',
+          top: 0,
+          boxShadow: '0 1px 2px 0 rgba(0, 0, 0, 0.15), inset 0 0 3px 0 rgba(0, 0, 0, 0.15)',
+          borderRadius: '50%',
+          pointerEvents: 'none'
+        }}
+      />
+    </div>
+  );
+};
 
 export default connect((state: AppState): StateProps => ({
   isValidAddress: getIsValidAddressFn(state)
