3e61f788dd3bbfceb0855600266365cbcdaf20ce
Connor Bryan <cchromium@gmail.com>
2018-06-12 01:56:51 -0500

    Found more tests

---
+++ common/features/notifications/sagas.spec.ts
@@ -0,0 +1,34 @@
+import { delay } from 'redux-saga';
+import { call, put } from 'redux-saga/effects';
+
+import { ShowNotificationAction } from './types';
+import { showNotification, closeNotification } from './actions';
+import { handleNotification } from './sagas';
+
+describe('handleNotification*', () => {
+  const level = 'success';
+  const msg = 'msg';
+  const duration = 10;
+  const notificationAction1: ShowNotificationAction = showNotification(level, msg, duration);
+  const notificationAction2: ShowNotificationAction = showNotification(level, msg, 0);
+  const gen1 = handleNotification(notificationAction1);
+  const gen2 = handleNotification(notificationAction2);
+
+  it('should call delay with duration', () => {
+    expect(gen1.next(notificationAction1).value).toEqual(call(delay, duration));
+  });
+
+  it('should return when duration is zero', () => {
+    expect(gen2.next(notificationAction2).done).toEqual(true);
+  });
+
+  it('should put closeNotification', () => {
+    expect(gen1.next(notificationAction1).value).toEqual(
+      put(closeNotification(notificationAction1.payload))
+    );
+  });
+
+  it('should be done', () => {
+    expect(gen1.next().done).toEqual(true);
+  });
+});
