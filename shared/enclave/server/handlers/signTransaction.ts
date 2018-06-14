dd2c62219a348f90b378a222dd896e923f3d7620
Will O'Beirne <wbobeirne@gmail.com>
2018-06-14 12:46:59 -0400

    No default exports

--- shared/enclave/server/handlers/signTransaction.ts
+++ shared/enclave/server/handlers/signTransaction.ts
@@ -1,7 +1,7 @@
 import { getWalletLib } from 'shared/enclave/server/wallets';
 import { SignTransactionParams, SignTransactionResponse } from 'shared/enclave/types';
 
-export default function(params: SignTransactionParams): Promise<SignTransactionResponse> {
+export function signTransaction(params: SignTransactionParams): Promise<SignTransactionResponse> {
   const wallet = getWalletLib(params.walletType);
   return wallet.signTransaction(params.transaction, params.path);
 }
