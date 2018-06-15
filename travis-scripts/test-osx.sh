c296753872f186c06fe5261865a92097f21292dd
HenryNguyen5 <ngu.henry@hotmail.com>
2018-06-15 15:42:05 -0400

    Move test scripts to their own folder, set scripts to fail on any command failure

---
+++ travis-scripts/test-osx.sh
@@ -0,0 +1,5 @@
+set -ev # return value 1 (error) if any command fails, and display each command before its run
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  yarn build:electron
+  ls -la dist/electron-builds
+fi
