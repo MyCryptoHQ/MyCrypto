553fb8c5ba778511ba88a6397d160db9a9a7b551
Connor Bryan <cchromium@gmail.com>
2018-06-16 14:08:43 -0500

    [Fix] Make ENS Value Consistent (#1956) @ skubakdj

--- common/containers/Tabs/ENS/components/NameResolve/components/NameOwned.tsx
+++ common/containers/Tabs/ENS/components/NameResolve/components/NameOwned.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-
-import translate from 'translations';
 import { IOwnedDomainRequest } from 'libs/ens';
+import { fromWei, Wei } from 'libs/units';
 import { NewTabLink, Address } from 'components/ui';
-
+import translate from 'translations';
 const lookupLink = (name: string) => `https://etherscan.io/enslookup?q=${name}`;
 
 type ChildrenProps = any;
@@ -36,7 +35,7 @@ export const NameOwned: React.SFC<IOwnedDomainRequest> = ({
             <MonoTd>{labelHash}</MonoTd>
           </tr>
           <tr>
-            <td>{translate('NAME_OWNED_NAMEHASH', { name })} </td>
+            <td>{translate('NAME_OWNED_NAMEHASH', { name })}: </td>
             <MonoTd>{nameHash}</MonoTd>
           </tr>
           <tr>
@@ -46,9 +45,9 @@ export const NameOwned: React.SFC<IOwnedDomainRequest> = ({
             </MonoTd>
           </tr>
           <tr>
-            <td>{translate('NAME_OWNED_HIGHEST_BIDDER')}</td>
+            <td>{translate('NAME_OWNED_HIGHEST_BID')}:</td>
             <MonoTd>
-              <span>{highestBid}</span>
+              <span>{fromWei(Wei(highestBid), 'ether')} ETH</span>
             </MonoTd>
           </tr>
           <tr>
--- common/features/ens/helpers.ts
+++ common/features/ens/helpers.ts
@@ -105,7 +105,7 @@ export function* resolveDomainRequest(name: string): SagaIterator {
     name,
     ...domainData,
     ...result,
-    labelHash: hash.toString('hex'),
+    labelHash: ethUtil.addHexPrefix(hash.toString('hex')),
     nameHash
   };
   return returnValue;
