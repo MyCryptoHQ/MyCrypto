71fce90e3e6dc85214ac38d4f7467f1e0f7a5b9e
Will O'Beirne <wbobeirne@gmail.com>
2018-06-25 13:34:27 -0400

    Fix PIN not unlocking

--- shared/enclave/server/views/pin.ts
+++ shared/enclave/server/views/pin.ts
@@ -6,7 +6,11 @@ const EVENT = 'enclave:pin';
 export function showPinPrompt(): Promise<string> {
   return new Promise((resolve, reject) => {
     const scriptNonce = Math.floor(Math.random() * 1000000000000);
-    const html = template.replace(/\$scriptNonce/g, scriptNonce.toString());
+    const html = template
+      .replace(/\$scriptNonce/g, scriptNonce.toString())
+      .replace(/\$EVENT/g, EVENT);
+
+    let hasResolved = false;
 
     const window = new BrowserWindow({
       width: 320,
@@ -17,11 +21,16 @@ export function showPinPrompt(): Promise<string> {
     });
 
     window.on('closed', () => {
+      if (hasResolved) {
+        return;
+      }
       reject(new Error('ENCLAVE_TREZOR_CANCELED'));
     });
 
     ipcMain.once(EVENT, (_: IpcMessageEvent, pin: string) => {
       resolve(pin);
+      hasResolved = true;
+      window.close();
     });
 
     window.loadURL(`data:text/html;charset=UTF-8,${encodeURIComponent(html)}`);
