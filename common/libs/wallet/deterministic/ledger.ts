059404f37374e2cae2d6affb30f1ffd081276e57
hackyminer <hackyminer@gmail.com>
2018-08-17 06:10:14 +0900

    comments added

--- common/libs/wallet/deterministic/ledger.ts
+++ common/libs/wallet/deterministic/ledger.ts
@@ -56,10 +56,15 @@ export class LedgerWallet extends HardwareWallet {
       let v = result.v;
       if (t._chainId > 0) {
         // EIP155 support. check/recalc signature v value.
+        // Please see https://github.com/LedgerHQ/blue-app-eth/commit/8260268b0214810872dabd154b476f5bb859aac0
+        // currently, ledger returns only 1-byte truncated signatur_v
         const rv = parseInt(v, 16);
-        let cv = t._chainId * 2 + 35;
+        let cv = t._chainId * 2 + 35; // calculated signature v, without signature bit.
         /* tslint:disable no-bitwise */
         if (rv !== cv && (rv & cv) !== rv) {
+          // (rv !== cv) : for v is truncated byte case
+          // (rv & cv): make cv to truncated byte
+          // (rv & cv) !== rv: signature v bit needed
           cv += 1; // add signature v bit.
         }
         v = cv.toString(16);
