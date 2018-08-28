ece4c2784ecc90fd6df58dca69ce7c66898d678e
Connor Bryan <cchromium@gmail.com>
2018-08-28 15:27:49 -0500

    Adjustments to FourthSlide and HardwareWalletOptions

--- common/components/v2/OnboardingModal/OnboardingModal.tsx
+++ common/components/v2/OnboardingModal/OnboardingModal.tsx
@@ -21,124 +21,118 @@ function ProgressDots({ currentStep = 1, totalSteps = 4 }) {
   );
 }
 
-function Slide({ children }) {
-  return <section className="Slide">{children}</section>;
-}
-
 function HardwareWalletChoice({ image, text }) {
   return (
     <section className="HardwareWalletChoice">
-      <img className="HardwareWalletChoice-image" src={image} alt={text} />
-      <p className="HardwareWalletChoice-text">{text}</p>
+      <section className="HardwareWalletChoice-image">
+        <img src={image} alt={text} />
+      </section>
+      <section className="HardwareWalletChoice-text">
+        <p>{text}</p>
+      </section>
     </section>
   );
 }
 
 function FirstSlide() {
   return (
-    <Slide>
-      <section className="FirstSlide">
-        <section className="FirstSlide-content">
-          <h1>Welcome to MyCrypto.com</h1>
-          <p>
-            Please read the next few screens for your own safety. Your funds could be stolen if you
-            do not pay attention to these warnings.
-          </p>
-          <button className="Button">Next</button>
-        </section>
+    <section className="FirstSlide">
+      <section className="FirstSlide-content">
+        <h1>Welcome to MyCrypto.com</h1>
+        <p>
+          Please read the next few screens for your own safety. Your funds could be stolen if you do
+          not pay attention to these warnings.
+        </p>
+        <button className="Button">Next</button>
       </section>
-    </Slide>
+    </section>
   );
 }
 
 function SecondSlide() {
   return (
-    <Slide>
-      <section className="SecondSlide">
-        <section className="SecondSlide-content">
-          <section>
-            <h1>With Banks...</h1>
-            <p>
-              They control your account <br />
-              They own your info <br />
-              They add fees <br />
-              They tell you what you can do <br />
-            </p>
-            <button className="Button first">Next</button>
-          </section>
-          <section>
-            <h1>With MyCrypto...</h1>
-            <p>
-              You control your ”account” <br />
-              You own your info <br />
-              No fees are added <br />
-              You do whatever you want <br />
-            </p>
-            <button className="Button second">Next</button>
-          </section>
+    <section className="SecondSlide">
+      <section className="SecondSlide-content">
+        <section>
+          <h1>With Banks...</h1>
+          <p>
+            They control your account <br />
+            They own your info <br />
+            They add fees <br />
+            They tell you what you can do <br />
+          </p>
+          <button className="Button first">Next</button>
+        </section>
+        <section>
+          <h1>With MyCrypto...</h1>
+          <p>
+            You control your ”account” <br />
+            You own your info <br />
+            No fees are added <br />
+            You do whatever you want <br />
+          </p>
+          <button className="Button second">Next</button>
         </section>
       </section>
-    </Slide>
+    </section>
   );
 }
 
 function ThirdSlide() {
   return (
-    <Slide>
-      <section className="ThirdSlide">
-        <section className="ThirdSlide-content">
+    <section className="ThirdSlide">
+      <section className="ThirdSlide-content">
+        <section>
+          <h1>Please understand that we can't...</h1>
+          <ul>
+            <li>Access your funds for you</li>
+            <li>Recover, reset, or modify ANY of your information</li>
+            <li>Reverse, cancel, or refund transactions</li>
+            <li>Freeze accounts</li>
+          </ul>
+        </section>
+        <section>
           <section>
-            <h1>Please understand that we can't...</h1>
+            <h1>You're responsible for...</h1>
             <ul>
-              <li>Access your funds for you</li>
-              <li>Recover, reset, or modify ANY of your information</li>
-              <li>Reverse, cancel, or refund transactions</li>
-              <li>Freeze accounts</li>
+              <li>
+                Keeping your information safe. This includes:
+                <ul>
+                  <li>Private Keys/Mnemonic Phrases</li>
+                  <li>JSON files</li>
+                  <li>Hardware wallet PINs</li>
+                </ul>
+              </li>
+              <li>Making sure you're not on a phishing site</li>
+              <li>Reducing risk by using the MyCrypto downloadable app</li>
             </ul>
           </section>
-          <section>
-            <section>
-              <h1>You're responsible for...</h1>
-              <ul>
-                <li>
-                  Keeping your information safe. This includes:
-                  <ul>
-                    <li>Private Keys/Mnemonic Phrases</li>
-                    <li>JSON files</li>
-                    <li>Hardware wallet PINs</li>
-                  </ul>
-                </li>
-                <li>Making sure you're not on a phishing site</li>
-                <li>Reducing risk by using the MyCrypto downloadable app</li>
-              </ul>
-            </section>
-            <button className="Button">Next</button>
-          </section>
+          <button className="Button">Next</button>
         </section>
       </section>
-    </Slide>
+    </section>
   );
 }
 
 function FourthSlide() {
   return (
-    <Slide>
-      <section className="FourthSlide">
-        <section className="FourthSlide-content">
-          <h1>You're ready to get started!</h1>
-          <p>
-            Pro-Tip: in order for your funds to be the most secure, we recommend getting a hardware
-            wallet to use with MyCrypto. Here are a few options we love:
-          </p>
-          <section className="FourthSlide-content-wallets">
-            <HardwareWalletChoice image={trezor} text="Get a Trezor" />
-            <HardwareWalletChoice image={ledger} text="Get a Ledger" />
-          </section>
-          <p>Need more info before you dive in? See Support Center</p>
-          <button className="Button second">Get Started</button>
+    <section className="FourthSlide">
+      <section className="FourthSlide-content">
+        <h1 className="FourthSlide-content-heading">You're ready to get started!</h1>
+        <p className="FourthSlide-content-text">
+          Pro-Tip: in order for your funds to be the most secure, we recommend getting a hardware
+          wallet to use with MyCrypto. Here are a few options we love:
+        </p>
+        <section className="FourthSlide-content-wallets">
+          <HardwareWalletChoice image={trezor} text="Get a Trezor" />
+          <HardwareWalletChoice image={ledger} text="Get a Ledger" />
         </section>
+        <p className="FourthSlide-content-text">
+          Need more info before you dive in? See Support Center
+        </p>
+        <button className="Button">Get Started</button>
       </section>
-    </Slide>
+    </section>
   );
 }
 
