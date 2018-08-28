489cda1adc0d79cae2dd923374d1f4abb93923ac
Connor Bryan <cchromium@gmail.com>
2018-08-28 16:21:14 -0500

    More improvements

--- common/components/v2/OnboardingModal/OnboardingModal.tsx
+++ common/components/v2/OnboardingModal/OnboardingModal.tsx
@@ -40,8 +40,8 @@ function FirstSlide() {
       <section className="FirstSlide-content">
         <h1 className="FirstSlide-content-heading">Welcome to MyCrypto.com</h1>
         <p className="FirstSlide-content-text">
-          Please read the next few screens for your own safety. Your funds could be stolen if you do
-          not pay attention to these warnings.
+          Please read the next few screens for your own safety. <br />
+          Your funds could be stolen if you do not pay attention to these warnings.
         </p>
         <button className="Button">Next</button>
       </section>
