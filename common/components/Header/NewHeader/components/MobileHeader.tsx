4e09face5517c84dddc3585b693ddc45db892680
Connor Bryan <cchromium@gmail.com>
2018-10-06 20:58:08 -0500

    Initial formatting of mobile header

--- common/components/Header/NewHeader/components/MobileHeader.tsx
+++ common/components/Header/NewHeader/components/MobileHeader.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import classnames from 'classnames';
 
 import { AppState } from 'features/reducers';
 import { configSelectors } from 'features/config';
@@ -20,6 +21,7 @@ interface DispatchProps {
 type Props = StateProps & DispatchProps;
 
 interface State {
+  menuVisible: boolean;
   visibleDropdowns: {
     [dropdown: string]: boolean;
   };
@@ -27,6 +29,7 @@ interface State {
 
 class MobileHeader extends Component<Props> {
   public state: State = {
+    menuVisible: false,
     visibleDropdowns: {
       sendAndReceive: false,
       buyAndExchange: false,
@@ -35,8 +38,83 @@ class MobileHeader extends Component<Props> {
   };
 
   public render() {
-    return <section className="MobileHeader">Header</section>;
+    const { nodeLabel, toggleSidebar } = this.props;
+    const { menuVisible } = this.state;
+    const menuIcon = classnames('fa', {
+      'fa-bars': !menuVisible,
+      'fa-close': menuVisible
+    });
+
+    return (
+      <section className="MobileHeader">
+        <section className="MobileHeader-top">
+          <section className="MobileHeader-top-menu-button" onClick={this.toggleMenu}>
+            <i className={menuIcon} />
+          </section>
+          <section className="MobileHeader-top-logo">
+            <img src={logo} alt="Our logo" />
+          </section>
+          {/* Dummy <div /> for flex spacing */}
+          <div />
+        </section>
+        {menuVisible && (
+          <section className="MobileHeader-menu">
+            <ul className="MobileHeader-menu-top">
+              <li>
+                Send & Receive <i className="fa fa-caret-down" />
+              </li>
+              <li>
+                Buy & Exchange <i className="fa fa-caret-down" />
+              </li>
+              <li>
+                Tools <i className="fa fa-caret-down" />
+              </li>
+              <li>
+                <i className="fa fa-plus" /> Create Wallet
+              </li>
+            </ul>
+            <ul className="MobileHeader-menu-mid">
+              <li>
+                English <i className="fa fa-caret-down" />
+              </li>
+              <li onClick={toggleSidebar}>
+                {nodeLabel.network} ({nodeLabel.info}) <i className="fa fa-caret-down" />
+              </li>
+            </ul>
+            <ul className="MobileHeader-menu-bottom">
+              <li>
+                <a href="https://support.mycrypto.com/">
+                  Help & Support <i className="fa fa-caret-right" />
+                </a>
+              </li>
+              <li>
+                <a href="https://medium.com/@mycrypto">
+                  Latest News <i className="fa fa-caret-right" />
+                </a>
+              </li>
+            </ul>
+          </section>
+        )}
+      </section>
+    );
   }
+
+  private toggleMenu = () =>
+    this.setState((prevState: State) => ({
+      menuVisible: !prevState.menuVisible
+    }));
+
+  private toggleDropdown = (dropdown: string) =>
+    this.setState((prevState: State) => ({
+      visibleDropdowns: {
+        ...prevState.visibleDropdowns,
+        [dropdown]: !prevState.visibleDropdowns[dropdown]
+      }
+    }));
+
+  private toggleSendAndReceive = () => this.toggleDropdown('sendAndReceive');
+  private toggleBuyAndExchange = () => this.toggleDropdown('buyAndExchange');
+  private toggleTools = () => this.toggleDropdown('tools');
 }
 
 const mapStateToProps = (state: AppState) => ({
