9e4caff5eda517e9a738522d98052de84f397dd4
Connor Bryan <cchromium@gmail.com>
2018-10-06 19:43:59 -0500

    Add dropdowns

--- common/components/Header/NewHeader/NewHeader.tsx
+++ common/components/Header/NewHeader/NewHeader.tsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logo from 'assets/images/logo-mycrypto.svg';
 import './NewHeader.scss';
+
+const LINKSET = {
+  SEND_AND_RECEIVE: [
+    {
+      to: '/account',
+      title: 'Send Ether & Tokens'
+    },
+    {
+      to: '/account/request',
+      title: 'Request Payment'
+    },
+    {
+      to: '/account/info',
+      title: 'Wallet Info'
+    },
+    {
+      to: '/account/recent-txs',
+      title: 'View Recent Transactions'
+    },
+    {
+      to: '/account/address-book',
+      title: 'Address Book'
+    }
+  ],
+  BUY_AND_EXCHANGE: [
+    {
+      to: '/swap',
+      title: 'Swap ETH & Tokens'
+    }
+  ],
+  TOOLS: [
+    {
+      to: '/sign-and-verify-message',
+      title: 'Sign & Verify Message'
+    },
+    {
+      to: '/contracts',
+      title: 'Interact with Contracts'
+    },
+    {
+      to: '/tx-status',
+      title: 'Check Transaction Status'
+    },
+    {
+      to: '/pushTx',
+      title: 'Broadcast Transaction'
+    },
+    {
+      to: '/ens',
+      title: 'ENS Domains'
+    }
+  ]
+};
 
 class NewHeader extends React.Component {
   render() {
@@ -9,7 +63,7 @@ class NewHeader extends React.Component {
       <section className="NewHeader">
         <section className="NewHeader-top">
           <section className="NewHeader-top-left">
-            <ul>
+            <ul className="NewHeader-top-links">
               <li>Help & Support</li>
               <li>Latest News</li>
             </ul>
@@ -18,21 +72,46 @@ class NewHeader extends React.Component {
             <img src={logo} alt="Our logo" />
           </section>
           <section className="NewHeader-top-right">
-            <ul>
+            <ul className="NewHeader-top-links">
               <li>English</li>
               <li>Ethereum (Auto)</li>
             </ul>
           </section>
         </section>
         <section className="NewHeader-bottom">
-          <section className="NewHeader-bottom-links">
-            <ul>
-              <li>Send & Receive</li>
-              <li>Buy & Exchange</li>
-              <li>Tools</li>
-              <li>Create Wallet</li>
-            </ul>
-          </section>
+          <ul className="NewHeader-bottom-links">
+            <li className="NewHeader-bottom-links-item">
+              Send & Receive
+              <ul className="NewHeader-bottom-links-dropdown">
+                {LINKSET.SEND_AND_RECEIVE.map(item => (
+                  <li key={item.to}>
+                    <Link to={item.to}>{item.title}</Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+            <li className="NewHeader-bottom-links-item">
+              Buy & Exchange
+              <ul className="NewHeader-bottom-links-dropdown">
+                {LINKSET.BUY_AND_EXCHANGE.map(item => (
+                  <li key={item.to}>
+                    <Link to={item.to}>{item.title}</Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+            <li className="NewHeader-bottom-links-item">
+              Tools
+              <ul className="NewHeader-bottom-links-dropdown">
+                {LINKSET.TOOLS.map(item => (
+                  <li key={item.to}>
+                    <Link to={item.to}>{item.title}</Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+            <li className="NewHeader-bottom-links-item">Create Wallet</li>
+          </ul>
         </section>
       </section>
     );
