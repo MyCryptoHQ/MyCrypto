dfd28c46217c3d96b1617700f01beea21e4619a6
Connor Bryan <cchromium@gmail.com>
2018-11-13 15:26:09 -0600

    Disable fields when they are pending

--- common/features/transaction/sign/selectors.ts
+++ common/features/transaction/sign/selectors.ts
@@ -5,3 +5,4 @@ const getTransactionState = (state: AppState) => state.transaction;
 export const getSignState = (state: AppState) => getTransactionState(state).sign;
 export const getSignedTx = (state: AppState) => getSignState(state).local.signedTransaction;
 export const getWeb3Tx = (state: AppState) => getSignState(state).web3.transaction;
+export const getSignPending = (state: AppState) => getSignState(state).pending;
