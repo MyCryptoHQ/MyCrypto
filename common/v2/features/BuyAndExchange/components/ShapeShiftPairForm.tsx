fb705272719645e060b30caa9c42f05bae52c3e3
Connor Bryan <cchromium@gmail.com>
2018-11-28 15:00:12 -0600

    Add validation to PairForm

--- common/v2/features/BuyAndExchange/components/ShapeShiftPairForm.tsx
+++ common/v2/features/BuyAndExchange/components/ShapeShiftPairForm.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage, FormikErrors } from 'formik';
 
 import { MarketPairHash } from 'v2/services/types';
 import './ShapeShiftPairForm.scss';
+
+interface Values {
+  deposit: string;
+  depositAmount: string;
+  withdraw: string;
+  withdrawAmount: string;
+}
 
 interface Props {
   rates: MarketPairHash | null;
   options: string[];
   onSubmit(values: any, bag: any): void;
 }
+
+const validate = (values: Values, rates: MarketPairHash): FormikErrors<Values> => {
+  const { deposit, depositAmount, withdraw } = values;
+  const pair = `${withdraw}_${deposit}`.toUpperCase();
+  const { min, maxLimit } = rates[pair];
+  const amount = parseFloat(depositAmount);
+  const errors: FormikErrors<Values> = {};
+
+  // Deposit Amount
+  if (amount <= min) {
+    errors.depositAmount = `The minimum amount you can deposit is ${min}.`;
+  }
+
+  if (amount >= maxLimit) {
+    errors.depositAmount = `The maximum amount you can deposit is ${maxLimit}.`;
+  }
+
+  return errors;
+};
 
 const clearAmountFields = (_: React.ChangeEvent<any>, props: any) => {
   props.setFieldValue('depositAmount', '0.00');
@@ -30,7 +56,33 @@ const changeOtherAmountField = (
   const { rate } = rates[pair];
   const amount = (value * rate).toString();
 
+  props.setTouched({
+    depositAmount: true,
+    withdrawAmount: true
+  });
   props.setFieldValue(otherFieldName, amount);
+  props.handleChange(e);
+};
+
+const handleAssetSelect = (e: React.ChangeEvent<any>, props: any) => {
+  const { target: { name, value } } = e;
+  const previousValue = props.values[name];
+  const opposites: { [field: string]: string } = {
+    deposit: 'withdraw',
+    withdraw: 'deposit'
+  };
+  const opposite = opposites[name];
+  const otherAsset = props.values[opposite];
+
+  if (value === otherAsset) {
+    props.setFieldValue(opposite, previousValue);
+  }
+
+  clearAmountFields(e, props);
+  props.setTouched({
+    depositAmount: false,
+    withdrawAmount: false
+  });
   props.handleChange(e);
 };
 
@@ -43,6 +95,7 @@ export default function ShapeShiftPairForm({ rates, options, onSubmit }: Props) 
         withdraw: options[1],
         withdrawAmount: '0.00'
       }}
+      validate={values => validate(values, rates)}
       onSubmit={onSubmit}
       render={props => {
         return (
@@ -57,22 +110,12 @@ export default function ShapeShiftPairForm({ rates, options, onSubmit }: Props) 
                   changeOtherAmountField(e, props, rates, ['deposit', 'withdraw'], 'withdrawAmount')
                 }
               />
+              <ErrorMessage name="depositAmount" />
               <Field
                 component="select"
                 name="deposit"
-                onChange={(e: React.ChangeEvent<any>) => {
-                  const { target: { value: asset } } = e;
-                  const { deposit, withdraw } = props.values;
-
-                  if (asset === withdraw) {
-                    props.setFieldValue('withdraw', deposit);
-                  }
-
-                  clearAmountFields(e, props);
-                  props.handleChange(e);
-                }}
+                onChange={(e: React.ChangeEvent<any>) => handleAssetSelect(e, props)}
               >
-                <option value="">Select an asset</option>
                 {options.map(option => (
                   <option key={option} value={option}>
                     {option}
@@ -93,19 +136,8 @@ export default function ShapeShiftPairForm({ rates, options, onSubmit }: Props) 
               <Field
                 component="select"
                 name="withdraw"
-                onChange={(e: React.ChangeEvent<any>) => {
-                  const { target: { value: asset } } = e;
-                  const { deposit, withdraw } = props.values;
-
-                  if (asset === deposit) {
-                    props.setFieldValue('deposit', withdraw);
-                  }
-
-                  clearAmountFields(e, props);
-                  props.handleChange(e);
-                }}
+                onChange={(e: React.ChangeEvent<any>) => handleAssetSelect(e, props)}
               >
-                <option value="">Select an asset</option>
                 {options.map(option => (
                   <option key={option} value={option}>
                     {option}
@@ -114,6 +146,9 @@ export default function ShapeShiftPairForm({ rates, options, onSubmit }: Props) 
               </Field>
             </fieldset>
             <fieldset>
+              <button type="button" className="btn btn-secondary" onClick={() => props.resetForm()}>
+                Reset
+              </button>
               <button type="submit" className="btn btn-primary">
                 Continue
               </button>
