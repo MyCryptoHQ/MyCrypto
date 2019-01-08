d0a92cb954888028eca64f0c2b68621b3c771f7e
Connor Bryan <cchromium@gmail.com>
2019-01-08 17:52:14 -0600

    Pause

--- common/v2/features/CreateWallet/components/SelectNetworkPanel.tsx
+++ common/v2/features/CreateWallet/components/SelectNetworkPanel.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { ComboBox, Heading, Panel, Typography } from '@mycrypto/ui';
+
+import './SelectNetworkPanel.scss';
 
 export default function SelectNetworkPanel() {
-  return <section className="SelectNetworkPanel">Select Network</section>;
+  return (
+    <Panel className="SelectNetworkPanel">
+      <Heading className="SelectNetworkPanel-heading">Select Network</Heading>
+      <Typography>
+        Not sure what to choose? Leave displayed defaults below and just click next!
+      </Typography>
+      <label>Network</label>
+      <ComboBox value="Ethereum" items={new Set(['Ethereum'])} />
+      <label>Node</label>
+      <ComboBox value="Automatic" items={new Set(['Automatic'])} />
+    </Panel>
+  );
 }
