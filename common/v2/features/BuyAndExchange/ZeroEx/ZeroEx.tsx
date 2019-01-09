741786cbd3b055345e201e500f95103d2ba1d59d
blurpesec <hahn.michael.f@gmail.com>
2019-01-09 12:28:07 -0500

    Fixes csp error. Adds notification of unavailability

--- common/v2/features/BuyAndExchange/ZeroEx/ZeroEx.tsx
+++ common/v2/features/BuyAndExchange/ZeroEx/ZeroEx.tsx
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 
+import { isDesktop } from 'v2/utils';
 import { replaceZeroExContainer } from './helpers';
 import { ZEROEX_CONTAINER_ID, ZEROEX_RELAYER } from './constants';
 import './ZeroEx.scss';
 
 // Legacy
 import TabSection from 'containers/TabSection';
+import Warning from 'components/ui/Warning';
 
 export default class ZeroEx extends Component {
   public componentDidMount() {
-    this.renderZeroExInstant();
+    if (!isDesktop()) {
+      this.renderZeroExInstant();
+    }
   }
 
   public render() {
     return (
       <TabSection>
-        <section className="ZeroEx">
-          <section className="Tab-content-pane" id={ZEROEX_CONTAINER_ID} />
-        </section>
+        {isDesktop() ? (
+          <Warning>
+            Unfortunately, 0x Instant functionality is currently unavailable for Desktop App users.
+            This functionality will be available in a future update -- stay tuned!
+          </Warning>
+        ) : (
+          <section className="ZeroEx">
+            <section className="Tab-content-pane" id={ZEROEX_CONTAINER_ID} />
+          </section>
+        )}
       </TabSection>
     );
   }
--- common/v2/utils/environment.ts
+++ common/v2/utils/environment.ts
@@ -1,1 +1,2 @@
 export const isDevelopment = (): boolean => process.env.NODE_ENV !== 'production';
+export const isDesktop = (): boolean => !!process.env.BUILD_ELECTRON;
