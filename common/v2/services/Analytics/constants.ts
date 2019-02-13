f59c25d05ca89c84d963d35497643246119af351
Peter Zlodej <peter.zlodej@poviolabs.com>
2019-02-13 10:45:00 +0100

    Added analytics categories

--- common/v2/services/Analytics/constants.ts
+++ common/v2/services/Analytics/constants.ts
@@ -1,8 +1,15 @@
 // The root URL for Analytics API requests.
-export const ANALYTICS_API_URL = 'https://analytics.mycryptoapi.com/piwik.php';
+export const ANALYTICS_API_URL = 'https://proxy.mycryptoapi.com/analytics';
 
 // The ID of the analytic website.
 export const ANALYTICS_ID_SITE = 4;
 
 // Parameter required for tracking, must be set to 1.
 export const ANALYTICS_REC = 1;
+
+// Analytics event category names
+export enum ANALYTICS_CATEGORIES {
+  HEADER = 'Header',
+  FOOTER = 'Footer',
+  SIDEBAR = 'Sidebar'
+}
