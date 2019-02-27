e068886c316116547c6cf8d876b6095dcae89fa8
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2019-02-27 14:50:20 -0500

    Use react state to handle account changes

--- common/v2/features/DevTools/DevTools.tsx
+++ common/v2/features/DevTools/DevTools.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Formik } from 'formik';
 import { List, Address, Panel, Button, Input } from '@mycrypto/ui';
 import styled from 'styled-components';
 
-import './DevTools.scss';
 import AccountServiceBase from 'v2/services/Account/Account';
 import { account, extendedAccount } from 'v2/services/Account';
 import { truncate } from 'v2/libs';
+import console = require('console');
+
+// const AccountsContext = React.createContext({});
 
 const DevToolsWidget = styled.div`
   position: absolute;
@@ -30,62 +32,89 @@ const DevToolsInput = styled(Input)`
   font-size: 1em;
 `;
 
-export default function DevTools() {
-  const Account = new AccountServiceBase();
-  Account.init();
-  const accounts: extendedAccount[] = Account.readAccounts() || [];
+const Account = new AccountServiceBase();
 
-  const list = accounts.map((account: extendedAccount) => {
+export default class DevTools extends Component {
+  constructor(props) {
+    super(props);
+    Account.init();
+    const accounts: extendedAccount[] = Account.readAccounts() || [];
+    this.state = {
+      accounts: accounts
+    };
+  }
+  getAccounts = () => {
+    const accounts: extendedAccount[] = Account.readAccounts() || [];
+    this.setState({ accounts: accounts });
+  };
+
+  handleSubmit = () => {
+    this.getAccounts();
+  };
+
+  handleDelete = uuid => {
+    Account.deleteAccount(uuid);
+    this.getAccounts();
+  };
+
+  render() {
+    const { accounts } = this.state;
+    const list = accounts.map((account: extendedAccount) => {
+      return (
+        <AccountContainer>
+          <Address title={account.label} address={account.address} truncate={truncate} />
+          <DeleteButton onClick={() => this.handleDelete(account.uuid)} icon="exit" />
+        </AccountContainer>
+      );
+    });
     return (
-      <AccountContainer>
-        <Address title={account.label} address={account.address} truncate={truncate} />
-        <DeleteButton onClick={() => Account.deleteAccount(account.uuid)} icon="exit" />
-      </AccountContainer>
+      <DevToolsWidget>
+        <Panel>
+          <List group>{list}</List>
+          <div className="Settings-heading">Enter a new Account</div>
+          <Formik
+            initialValues={{
+              address: '0x80200997f095da94E404F7E0d581AAb1fFba9f7d',
+              label: 'test1',
+              network: 'ETH'
+            }}
+            onSubmit={(values: account, { setSubmitting }) => {
+              Account.createAccount(values);
+              this.handleSubmit();
+              setSubmitting(false);
+            }}
+          >
+            {({ values, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+              <form onSubmit={handleSubmit}>
+                Address:{' '}
+                <DevToolsInput
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values['address']}
+                />
+                <br />
+                Label:{' '}
+                <DevToolsInput
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values['label']}
+                />
+                <br />
+                Network:{' '}
+                <DevToolsInput
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values['network']}
+                />
+                <br />
+                <Button type="submit" disabled={isSubmitting}>
+                  Submit
+                </Button>
+              </form>
+            )}
+          </Formik>
+        </Panel>
+      </DevToolsWidget>
     );
-  });
-
-  return (
-    <DevToolsWidget>
-      <Panel>
-        <List group>{list}</List>
-        <div className="Settings-heading">Enter a new Account</div>
-        <Formik
-          initialValues={{
-            address: '0x80200997f095da94E404F7E0d581AAb1fFba9f7d',
-            label: 'test1',
-            network: 'ETH'
-          }}
-          onSubmit={(values: account, { setSubmitting }) => {
-            Account.createAccount(values);
-            setSubmitting(false);
-          }}
-        >
-          {({ values, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
-            <form onSubmit={handleSubmit}>
-              Address:{' '}
-              <DevToolsInput
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values['address']}
-              />
-              <br />
-              Label:{' '}
-              <DevToolsInput onChange={handleChange} onBlur={handleBlur} value={values['label']} />
-              <br />
-              Network:{' '}
-              <DevToolsInput
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values['network']}
-              />
-              <br />
-              <Button type="submit" disabled={isSubmitting}>
-                Submit
-              </Button>
-            </form>
-          )}
-        </Formik>
-      </Panel>
-    </DevToolsWidget>
-  );
+  }
 }
