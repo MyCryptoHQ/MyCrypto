f5e171698298f9219539add60c89353f25ba3a8b
Nick McCurdy <nick@nickmccurdy.com>
2019-03-06 22:53:55 -0500

    Create a test case per dependency

--- spec/package.json.spec.ts
+++ spec/package.json.spec.ts
@@ -4,17 +4,16 @@ interface Dependencies {
   [key: string]: string;
 }
 
+const dependencies = Object.entries({
+  ...(packageJSON.dependencies as Dependencies),
+  ...(packageJSON.devDependencies as Dependencies)
+});
+
 // from https://docs.npmjs.com/files/package.json#dependencies
 const nonExactPrefixes = /^(~|\^|>|>=|<|<=)/;
 
 describe('package.json', () => {
-  it.each(['dependencies', 'devDependencies'])(
-    '%s should not contain any non-exact versions',
-    property => {
-      const deps = Object.values(packageJSON[property] as Dependencies);
-      deps.forEach(depVersion => {
-        expect(depVersion).not.toMatch(nonExactPrefixes);
-      });
-    }
-  );
+  it.each(dependencies)('%s should have an exact version', (dep, depVersion) => {
+    expect(depVersion).not.toMatch(nonExactPrefixes);
+  });
 });
