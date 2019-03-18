dc3d3aa1c521818fb0da3c459f8ef79d1474b721
Peter Zlodej <peter.zlodej@poviolabs.com>
2019-03-18 11:58:36 +0100

    Updated Download App Panel Styles

--- common/v2/features/Home/components/DownloadAppPanel.tsx
+++ common/v2/features/Home/components/DownloadAppPanel.tsx
@@ -9,16 +9,12 @@ import champagneIcon from 'common/assets/images/icn-champagne-2.svg';
 export default function DownloadAppPanel() {
   return (
     <Panel basic={true} className="DownloadAppPanel">
-      <div className="DownloadAppPanel-callToAction">
-        <Typography className="DownloadAppPanel-callToAction-title">
-          {translate('HOME_DOWNLOAD_TITLE')}
-        </Typography>
-        <Typography className="DownloadAppPanel-callToAction-description">
-          {translate('HOME_DOWNLOAD_DESCRIPTION')}
-        </Typography>
-        <Button className="DownloadAppPanel-callToAction-button">Download for macOS</Button>
+      <div className="callToAction">
+        <Typography className="title">{translate('HOME_DOWNLOAD_TITLE')}</Typography>
+        <Typography className="description">{translate('HOME_DOWNLOAD_DESCRIPTION')}</Typography>
+        <Button className="button">Download for macOS</Button>
       </div>
-      <img className="DownloadAppPanel-image" src={champagneIcon} />
+      <img className="image" src={champagneIcon} />
     </Panel>
   );
 }
