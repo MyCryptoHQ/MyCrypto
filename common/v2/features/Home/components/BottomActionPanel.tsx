096e96a7a1970188cb322748051ab0d1e8aa8e0a
Amadej <amadej.pevec@gmail.com>
2019-03-18 12:43:28 +0100

    Use translations instead of hardcoded strings

--- common/v2/features/Home/components/BottomActionPanel.tsx
+++ common/v2/features/Home/components/BottomActionPanel.tsx
@@ -2,17 +2,14 @@ import React from 'react';
 import { Panel, Typography, Button } from '@mycrypto/ui';
 
 import './BottomActionPanel.scss';
+import translate from 'translations';
 
 export default function BottomActionPanel() {
   return (
     <Panel basic={true} className="BottomActionPanel">
-      <Typography className="BottomActionPanel-title">
-        Ready to start managing your funds safely and confidently?
-      </Typography>
-      <Button className="BottomActionPanel-button">Get Started</Button>
-      <Typography className="BottomActionPanel-link">
-        Have Questions? We're Here to Help!
-      </Typography>
+      <Typography className="BottomActionPanel-title">{translate('HOME_BOTTOM_TITLE')}</Typography>
+      <Button className="BottomActionPanel-button">{translate('HOME_BOTTOM_GET_STARTED')}</Button>
+      <Typography className="BottomActionPanel-link">{translate('HOME_BOTTOM_HELP')}</Typography>
     </Panel>
   );
 }
