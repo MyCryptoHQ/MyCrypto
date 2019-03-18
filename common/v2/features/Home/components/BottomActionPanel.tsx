6ecec2b3d380a13abce28863280f9dd7afa7af5c
Peter Zlodej <peter.zlodej@poviolabs.com>
2019-03-18 14:45:19 +0100

    Updated bottom action panel

--- common/v2/features/Home/components/BottomActionPanel.tsx
+++ common/v2/features/Home/components/BottomActionPanel.tsx
@@ -6,13 +6,11 @@ import './BottomActionPanel.scss';
 export default function BottomActionPanel() {
   return (
     <Panel basic={true} className="BottomActionPanel">
-      <Typography className="BottomActionPanel-title">
+      <Typography className="title">
         Ready to start managing your funds safely and confidently?
       </Typography>
-      <Button className="BottomActionPanel-button">Get Started</Button>
-      <Typography className="BottomActionPanel-link">
-        Have Questions? We're Here to Help!
-      </Typography>
+      <Button className="button">Get Started</Button>
+      <Typography className="link">Have Questions? We're Here to Help!</Typography>
     </Panel>
   );
 }
