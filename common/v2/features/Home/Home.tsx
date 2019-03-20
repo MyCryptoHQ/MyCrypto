1ff5a9723b93ec48fff020bf00ed4acfc2cfd1c7
Peter Zlodej <peter.zlodej@poviolabs.com>
2019-03-20 10:07:29 +0100

    Download button functionality in download panel

--- common/v2/features/Home/Home.tsx
+++ common/v2/features/Home/Home.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { OS } from 'v2/services/Github';
 import { Layout } from 'v2/features';
 import {
   GetStartedPanel,
@@ -10,34 +10,65 @@ import {
   BottomActionPanel,
   FeaturesPanel
 } from './components';
+import { getFeaturedOS } from 'v2/features/helpers';
+import { GithubService } from 'v2/services';
+import { GITHUB_RELEASE_NOTES_URL as DEFAULT_LINK } from 'v2/features/constants';
 import './Home.scss';
 
-export default function Home() {
-  return (
-    <Layout className="WhiteBackground" fluid={true}>
-      <section className="Home">
-        <section className="LimitedWidth">
-          <GetStartedPanel />
+const OSNames: { [key: string]: string } = {
+  [OS.WINDOWS]: 'Windows',
+  [OS.MAC]: 'macOS',
+  [OS.LINUX64]: 'Linux (64-bit)'
+};
+
+const featuredOS = getFeaturedOS();
+
+export default class Home extends Component {
+  state = {
+    appDownloadLink: DEFAULT_LINK,
+    OSName: OSNames[featuredOS]
+  };
+
+  componentDidMount = async () => {
+    try {
+      const releaseURLs = await GithubService.instance.getReleasesURLs();
+      const currentPlatformURL = releaseURLs[featuredOS] || DEFAULT_LINK;
+      this.setState({ appDownloadLink: currentPlatformURL });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  render() {
+    return (
+      <Layout className="WhiteBackground" fluid={true}>
+        <section className="Home">
+          <section className="LimitedWidth">
+            <GetStartedPanel />
+          </section>
+          <section className="Home-compatibleWallets">
+            <CompatibleWalletsPanel />
+          </section>
+          <section className="Home-featuresPanel">
+            <FeaturesPanel />
+          </section>
+          <section className="Home-downloadApp">
+            <DownloadAppPanel
+              downloadLink={this.state.appDownloadLink}
+              OSName={this.state.OSName}
+            />
+          </section>
+          <section className="Home-peaceOfMind">
+            <PeaceOfMindPanel />
+          </section>
+          <section className="Home-testimonials">
+            <TestimonialsPanel />
+          </section>
+          <section className="Home-bottomAction">
+            <BottomActionPanel />
+          </section>
         </section>
-        <section className="Home-compatibleWallets">
-          <CompatibleWalletsPanel />
-        </section>
-        <section className="Home-featuresPanel">
-          <FeaturesPanel />
-        </section>
-        <section className="Home-downloadApp">
-          <DownloadAppPanel />
-        </section>
-        <section className="Home-peaceOfMind">
-          <PeaceOfMindPanel />
-        </section>
-        <section className="Home-testimonials">
-          <TestimonialsPanel />
-        </section>
-        <section className="Home-bottomAction">
-          <BottomActionPanel />
-        </section>
-      </section>
-    </Layout>
-  );
+      </Layout>
+    );
+  }
 }
