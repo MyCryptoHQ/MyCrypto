81fb3dbf8b1194652173c40cbf5ed50960f44e7e
Peter Zlodej <peter.zlodej@poviolabs.com>
2019-03-21 12:44:37 +0100

    Move features panel style to styled components

--- common/v2/features/Home/components/FeaturesPanel.tsx
+++ common/v2/features/Home/components/FeaturesPanel.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Panel, Typography } from '@mycrypto/ui';
+import { Panel } from '@mycrypto/ui';
+import styled from 'styled-components';
 
 import './FeaturesPanel.scss';
 import translate from 'translations';
+import { COLORS, BREAK_POINTS } from 'v2/features/constants';
 
 import manageWalletsImage from 'common/assets/images/manage-your-wallets.svg';
 import viewDashboardImage from 'common/assets/images/view-dashboard.svg';
@@ -12,41 +14,199 @@ import manageWalletsImageMobile from 'common/assets/images/manage-your-wallets-m
 import viewDashboardImageMobile from 'common/assets/images/view-dashboard-mobile.svg';
 import yourCryptoImageMobile from 'common/assets/images/control-crypto-mobile.svg';
 
+const { SCREEN_SM, SCREEN_MD, SCREEN_LG, SCREEN_XL, SCREEN_XXL } = BREAK_POINTS;
+const { DARK_SLATE_BLUE } = COLORS;
+
+const MainPanel = styled(Panel)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 88px 148px;
+  max-width: ${SCREEN_XXL};
+
+  @media (max-width: ${SCREEN_SM}) {
+    padding: 42px 0;
+  }
+`;
+
+const TitleArea = styled.div`
+  text-align: center;
+
+  @media (max-width: ${SCREEN_SM}) {
+    margin-bottom: 20px;
+    padding: 0 12px;
+  }
+`;
+
+const Title = styled.p`
+  font-size: 35px;
+  font-weight: bold;
+  color: ${DARK_SLATE_BLUE};
+  line-height: 1.5;
+
+  @media (max-width: ${SCREEN_SM}) {
+    font-size: 23px;
+  }
+`;
+
+const Description = styled.p`
+  font-size: 30px;
+  line-height: 1.5;
+  margin-top: 8px;
+  font-weight: normal;
+  color: ${DARK_SLATE_BLUE};
+
+  @media (max-width: ${SCREEN_SM}) {
+    font-size: 16px;
+  }
+`;
+
+const FeaturesSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 32px;
+
+  @media (max-width: ${SCREEN_MD}) {
+    margin-top: 0;
+  }
+`;
+
+interface FeatureWrapperProps {
+  captionRight: boolean;
+}
+
+const FeatureWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  margin-bottom: 80px;
+
+  @media (min-width: ${SCREEN_LG}) {
+    flex-direction: ${(props: FeatureWrapperProps) => (props.captionRight ? 'row-reverse' : 'row')};
+  }
+
+  @media (max-width: ${SCREEN_LG}) {
+    flex-direction: column;
+    margin-bottom: 0;
+  }
+`;
+
+const FeatureCaptions = styled.div`
+  max-width: 500px;
+  width: 50%;
+  @media (max-width: ${SCREEN_LG}) {
+    width: 100%;
+    order: 1;
+  }
+  @media (max-width: ${SCREEN_SM}) {
+    padding: 0 12px;
+  }
+`;
+
+const FeatureTitle = styled.p`
+  font-size: 30px;
+  font-weight: bold;
+  color: ${DARK_SLATE_BLUE};
+  line-height: normal;
+
+  @media (min-width: ${SCREEN_XL}) {
+    font-size: 30px;
+  }
+  @media (max-width: ${SCREEN_LG}) {
+    margin-top: 80px;
+    text-align: center;
+  }
+  @media (max-width: ${SCREEN_SM}) {
+    margin-top: 30px;
+    font-size: 20px;
+    text-align: center;
+    width: 100%;
+  }
+`;
+
+const FeatureDescription = styled.p`
+  font-size: 21px;
+  line-height: 1.5;
+  margin-top: 8px;
+  font-weight: normal;
+  color: ${DARK_SLATE_BLUE};
+
+  @media (min-width: ${SCREEN_XL}) {
+    font-size: 21px;
+  }
+  @media (max-width: ${SCREEN_LG}) {
+    text-align: center;
+  }
+  @media (max-width: ${SCREEN_SM}) {
+    font-size: 16px;
+  }
+`;
+
+const FeatureImage = styled.img`
+  width: 70%;
+  max-height: 550px;
+
+  @media (max-width: ${SCREEN_LG}) {
+    width: 100%;
+    margin-top: 24px;
+    order: 2;
+  }
+
+  @media (max-width: ${SCREEN_SM}) {
+    display: none;
+  }
+`;
+
+const FeatureMobileImage = styled.img`
+  display: none;
+
+  @media (max-width: ${SCREEN_SM}) {
+    display: block;
+    order: 2;
+  }
+
+  @media (max-width: 420px) {
+    width: 100%;
+  }
+`;
+
 interface FeatureProps {
   name: React.ReactElement<any>;
   description: React.ReactElement<any>;
   image: string;
   mobileImage: string;
-  captionRight?: boolean;
+  captionRight: boolean;
 }
 
 const Feature: React.SFC<FeatureProps> = props => {
   const { name, description, image, mobileImage, captionRight } = props;
   return (
-    <div className={captionRight ? 'feature right' : 'feature'}>
-      <div className="captions">
-        <Typography className="title">{name}</Typography>
-        <Typography className="description">{description}</Typography>
-      </div>
-      <img className="image" src={image} />
-      <img className="mobileImage" src={mobileImage} />
-    </div>
+    <FeatureWrapper captionRight={captionRight}>
+      <FeatureCaptions>
+        <FeatureTitle>{name}</FeatureTitle>
+        <FeatureDescription>{description}</FeatureDescription>
+      </FeatureCaptions>
+      <FeatureImage src={image} />
+      <FeatureMobileImage src={mobileImage} />
+    </FeatureWrapper>
   );
 };
 
 export default function FeaturesPanel() {
   return (
-    <Panel basic={true} className="FeaturesPanel">
-      <div className="titleArea">
-        <Typography className="title">{translate('HOME_FEATURES_TITLE')}</Typography>
-        <Typography className="description">{translate('HOME_FEATURES_DESCRIPTION')}</Typography>
-      </div>
-      <div className="featuresSection">
+    <MainPanel basic={true}>
+      <TitleArea>
+        <Title>{translate('HOME_FEATURES_TITLE')}</Title>
+        <Description>{translate('HOME_FEATURES_DESCRIPTION')}</Description>
+      </TitleArea>
+      <FeaturesSection>
         <Feature
           name={translate('HOME_FEATURES_MANAGE_TITLE')}
           description={translate('HOME_FEATURES_MANAGE_DESCRIPTION')}
           image={manageWalletsImage}
           mobileImage={manageWalletsImageMobile}
+          captionRight={false}
         />
         <Feature
           name={translate('HOME_FEATURES_VIEW_TITLE')}
@@ -60,8 +220,9 @@ export default function FeaturesPanel() {
           description={translate('HOME_FEATURES_CONTROL_DESCRIPTION')}
           image={yourCryptoImage}
           mobileImage={yourCryptoImageMobile}
+          captionRight={false}
         />
-      </div>
-    </Panel>
+      </FeaturesSection>
+    </MainPanel>
   );
 }
