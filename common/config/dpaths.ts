8b04df1e07a559a534b6e5ae3945005ee4eb5b08
Suryansh Kumar <sahil9756@gmail.com>
2019-03-26 20:03:44 +0530

    Add EtherInc (ETI) Support

--- common/config/dpaths.ts
+++ common/config/dpaths.ts
@@ -163,6 +163,11 @@ export const THUNDERCORE_DEFAULT: DPath = {
   value: "m/44'/1001'/0'/0"
 };
 
+export const ETI_DEFAULT: DPath = {
+  label: 'Default (ETI)',
+  value: "m/44'/464'/0'/0"
+};
+
 export const DPaths: DPath[] = [
   ETH_DEFAULT,
   ETH_TREZOR,
@@ -195,7 +200,8 @@ export const DPaths: DPath[] = [
   REOSC_DEFAULT,
   ARTIS_SIGMA1,
   ARTIS_TAU1,
-  THUNDERCORE_DEFAULT
+  THUNDERCORE_DEFAULT,
+  ETI_DEFAULT
 ];
 
 // PATHS TO BE INCLUDED REGARDLESS OF WALLET FORMAT
