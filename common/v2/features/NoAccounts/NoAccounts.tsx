523612f9c4299b60954497d1dab5b1b9e84b1b8f
Azarielle <sharon.manrique2@hotmail.com>
2019-04-18 12:22:30 -0400

    styling of no-accounts web views complete

--- common/v2/features/NoAccounts/NoAccounts.tsx
+++ common/v2/features/NoAccounts/NoAccounts.tsx
@@ -3,26 +3,29 @@ import styled from 'styled-components';
 import { Layout } from 'v2/features';
 import sadWallet from 'common/assets/images/icn-sad-wallet.svg';
 import { Button } from '@mycrypto/ui';
+import translate from 'translations';
 
 const NoAccountsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
   width: 1245px;
   height: 505px;
   border-radius: 3px;
   box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.07);
   background-color: #ffffff;
-  justify-content: center, 
-  align-content:center, 
 `;
 
 const NoAccountsContent = styled.div`
-  display: flex;
+  margin-top: 45px;
+  width: 503px;
   flex-direction: column;
   align-items: center;
   padding: 18px 4px 26px 4px;
   text-align: center;
 `;
 const Header = styled.p`
-  font-size: 32px;
+  font-size: 24px;
   font-weight: bold;
   line-height: normal;
   margin-top: 0;
@@ -39,33 +42,55 @@ const Description = styled.p`
 `;
 
 const ImgIcon = styled.img`
-  width: 135px;
-  height: 135px;
+  width: 130px;
+  height: 99px;
   margin: 21px 0 28px 0;
 `;
 
 const PrimaryButton = styled(Button)`
-  width: 320px;
+  width: 230px;
   margin-bottom: 15px;
-  font-size: 18px;
+  font-size: 17px;
+  text-align: center;
+  white-space: nowrap;
 
   @media (min-width: 700px) {
-    width: 420px;
+    width: 230px;
   }
 `;
 
-const WhiteButton = styled(Button)`
-  width: 320px;
+const WhiteButtonFirst = styled(Button)`
+  width: 230px;
   margin-bottom: 15px;
   font-size: 17px;
+  white-space: nowrap;
+  text-align: center; 
+  padding-left: 1.25; 
 
   @media (min-width: 700px) {
-    width: 200px;
+    width: 230px;
 
     &:first-of-type {
-      margin-right: 20px;
-    }
+      margin-right: 20px; 
   }
+`;
+
+const WhiteButtonSecond = styled(Button)`
+  width: 230px;
+  margin-bottom: 15px;
+  font-size: 17px;
+  white-space: nowrap;
+  text-align: center; 
+  padding-left: 1.1rem; 
+
+  @media (min-width: 700px) {
+    width: 230px;
+
+`;
+
+const ButtonGroup = styled.div`
+  justify-content: center;
+  width: 503px;
 `;
 
 export default class NoAccounts extends Component {
@@ -73,14 +98,14 @@ export default class NoAccounts extends Component {
     return (
       <Layout centered={true}>
         <NoAccountsContainer>
-          <NoAccountsContent>
+          <NoAccountsContent className="no-accounts">
             <ImgIcon src={sadWallet} />
-            <Header>You dont have any accounts in your wallet.</Header>
-            <Description>
-              To access your funds add one of your existing accounts or create a new accounts now.
-            </Description>
-            <WhiteButton secondary={true}>Add Existing Account</WhiteButton>
-            <WhiteButton secondary={true}>Import MyCrypto Settings</WhiteButton>
+            <Header>{translate('NO_ACCOUNTS_HEADER')}</Header>
+            <Description>{translate('NO_ACCOUNTS_DESCRIPTION')}</Description>
+            <ButtonGroup>
+              <WhiteButtonFirst secondary={true}>Add Existing Account</WhiteButtonFirst>
+              <WhiteButtonSecond secondary={true}>Import MyCrypto Settings</WhiteButtonSecond>
+            </ButtonGroup>
             <PrimaryButton>Create New Account</PrimaryButton>
           </NoAccountsContent>
         </NoAccountsContainer>
