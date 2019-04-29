81a1b0de268b5dfbd7790e8d88b6846996684cfc
Azarielle <sharon.manrique2@hotmail.com>
2019-04-29 15:56:27 -0400

    refactored stateless AccountList class to functions

--- common/v2/features/Dashboard/components/AccountList.tsx
+++ common/v2/features/Dashboard/components/AccountList.tsx
@@ -1,5 +1,5 @@
 import { Address, Button, CollapsibleTable, Icon, Network, Typography } from '@mycrypto/ui';
-import React, { Component } from 'react';
+import React from 'react';
 import { Redirect } from 'react-router-dom';
 import styled from 'styled-components';
 import { truncate } from 'v2/libs';
@@ -7,10 +7,31 @@ import { ExtendedAccount } from 'v2/services';
 import './AccountList.scss';
 import DashboardPanel from './DashboardPanel';
 
-interface Props {
+type DeleteAccount = (uuid: string) => void;
+interface AccountListProps {
   accounts: ExtendedAccount[];
   className?: string;
-  deleteAccount(uuid: string): void;
+  deleteAccount: DeleteAccount;
+}
+
+export default function AccountList(props: AccountListProps) {
+  const { accounts, deleteAccount, className } = props;
+
+  const shouldRedirect = accounts === undefined || accounts === null || accounts.length === 0;
+  if (shouldRedirect) {
+    return <Redirect to="/no-accounts" />;
+  }
+
+  return (
+    <DashboardPanel
+      heading="Your Accounts"
+      action="Add Account"
+      actionLink="/dashboard/add-account"
+      className={`AccountList ${className}`}
+    >
+      <CollapsibleTable breakpoint={450} {...buildAccountTable(accounts, deleteAccount)} />
+    </DashboardPanel>
+  );
 }
 
 const DeleteButton = styled(Button)`
@@ -18,72 +39,47 @@ const DeleteButton = styled(Button)`
   margin-left: 1em;
 `;
 
-export default class AccountList extends Component<Props> {
-  public handleAccountDelete = (uuid: string) => () => {
-    const { deleteAccount } = this.props;
-    deleteAccount(uuid);
-  };
-
-  public renderRedirect() {
-    if (
-      this.props.accounts === undefined ||
-      this.props.accounts === null ||
-      this.props.accounts.length === 0
-    ) {
-      return <Redirect to="/no-accounts" />;
+function buildAccountTable(accounts: ExtendedAccount[], deleteAccount: DeleteAccount) {
+  return {
+    head: ['Favorite', 'Address', 'Network', 'Value', 'Delete'],
+    body: accounts.map(account => {
+      return [
+        <Icon key={0} icon="star" />,
+        <Address
+          key={1}
+          title={`${account.label}-(${account.accountType})`}
+          address={account.address}
+          truncate={truncate}
+        />,
+        <Network key={3} color="#a682ff">
+          {account.network}
+        </Network>,
+        <Typography key={4}>{account.value}</Typography>,
+        <DeleteButton
+          key={5}
+          onClick={handleAccountDelete(deleteAccount, account.uuid)}
+          icon="exit"
+        />
+      ];
+    }),
+    config: {
+      primaryColumn: 'Address',
+      sortableColumn: 'Address',
+      sortFunction: (a: any, b: any) => {
+        const aLabel = a.props.label;
+        const bLabel = b.props.label;
+        return aLabel === bLabel ? true : aLabel.localeCompare(bLabel);
+      },
+      hiddenHeadings: ['Favorite', 'Delete'],
+      iconColumns: ['Favorite', 'Delete']
     }
-  }
+  };
+}
 
-  public BuildAccountList() {
-    const { accounts } = this.props;
-    return {
-      head: ['Favorite', 'Address', 'Network', 'Value', 'Delete'],
-      body: accounts.map(account => {
-        return [
-          <Icon key={`account-icon${account.uuid}`} icon="star" />,
-          <Address
-            key={`account-key-${account.value}`}
-            title={`${account.label}- (${account.accountType})`}
-            address={account.address}
-            truncate={truncate}
-          />,
-          <Network key={`account-network-${account.network}`} color="#a682ff">
-            {account.network}
-          </Network>,
-          <Typography key={`account-value-${account.value}`}>{account.value}</Typography>,
-          <DeleteButton
-            key={`account-id-${account.uuid}`}
-            onClick={this.handleAccountDelete(account.uuid)}
-            icon="exit"
-          />
-        ];
-      }),
-      config: {
-        primaryColumn: 'Address',
-        sortableColumn: 'Address',
-        sortFunction: (a: any, b: any) => {
-          const aLabel = a.props.label;
-          const bLabel = b.props.label;
-          return aLabel === bLabel ? true : aLabel.localeCompare(bLabel);
-        },
-        hiddenHeadings: ['Favorite', 'Delete'],
-        iconColumns: ['Favorite', 'Delete']
-      }
-    };
-  }
-  public render() {
-    const { className } = this.props;
-
-    return (
-      <DashboardPanel
-        heading="Your Accounts"
-        action="Add Account"
-        actionLink="/dashboard/add-account"
-        className={`AccountList ${className}`}
-      >
-        <CollapsibleTable breakpoint={450} {...this.BuildAccountList()} />
-        {this.renderRedirect()}
-      </DashboardPanel>
-    );
-  }
+/**
+ * A higher order function that binds to an account uuid, which returns a handler that will
+ * delete the bound account onClick
+ */
+function handleAccountDelete(deleteAccount: DeleteAccount, uuid: string) {
+  return () => deleteAccount(uuid);
 }
