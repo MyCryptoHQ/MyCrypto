53e4e54b8519162e7386b1dc14e8970676047644
Azarielle <sharon.manrique2@hotmail.com>
2019-04-29 22:11:48 -0400

    clean up unused files in add-existing-account-views flow

--- common/v2/features/AddAccount/components/ConnectSafeTMiniPanel.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function ConnectSafeTMiniPanel() {
-  return <section className="ConnectSafeTMiniPanel">Connect Safe-T Mini</section>;
-}
--- common/v2/features/AddAccount/components/index.ts
+++ common/v2/features/AddAccount/components/index.ts
@@ -1,10 +1,3 @@
-export { default as ConnectLedgerPanel } from './ConnectLedgerPanel';
-export { default as ConnectMetaMaskPanel } from './ConnectMetaMaskPanel';
-export { default as ConnectParitySignerPanel } from './ConnectParitySignerPanel';
-export { default as ConnectSafeTMiniPanel } from './ConnectSafeTMiniPanel';
-export { default as ConnectTrezorPanel } from './ConnectTrezorPanel';
-export { default as SelectNetworkPanel } from './SelectNetworkPanel';
-export { default as SelectAddressPanel } from './SelectAddressPanel';
 export * from './DeterministicWallets';
 export * from './InsecureWalletWarning';
 export * from './Keystore';
