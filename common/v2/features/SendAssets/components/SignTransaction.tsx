3baa33bb6788c3a5457058740f9d2774de12a139
Azarielle <sharon.manrique2@hotmail.com>
2019-05-27 16:56:29 -0400

    check for metamask account changes

--- common/v2/features/SendAssets/components/SignTransaction.tsx
+++ common/v2/features/SendAssets/components/SignTransaction.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ISendState, ITxFields } from '../types';
 import { SignTransactionPrivateKey } from './SignTransactionWallets';
 import { DeepPartial } from 'shared/types/util';
-// import SignTransactionMetaMask from './SignTransactionWallets/Metamask';
+import SignTransactionMetaMask from './SignTransactionWallets/Metamask';
 
 interface Props {
   stateValues: ISendState;
@@ -12,25 +12,31 @@ interface Props {
   updateState(state: DeepPartial<ISendState>): void;
 }
 
-export default function SignTransaction({ transactionFields }: Props) {
-  const whichWallet = transactionFields.accountType;
+export default class SignTransaction extends Component<Props> {
+  public render() {
+    const { stateValues, transactionFields } = this.props;
+    const whichWallet = transactionFields.accountType;
 
-  switch (whichWallet) {
-    case 'privateKey':
-      return (
-        <div>
-          <SignTransactionPrivateKey />
-        </div>
-      );
-    // case 'web3':
-    //   return (
-    //     <div>
-    //       <SignTransactionMetaMask stateValues={transactionFields}/>
-    //     </div>
-    //   );
-    default:
-      return null;
-    // case 'Mnemonic':
-    //   return <div>Mnemonic</div>;
+    switch (whichWallet) {
+      case 'privateKey':
+        return (
+          <div>
+            <SignTransactionPrivateKey />
+          </div>
+        );
+      case 'web3':
+        return (
+          <div>
+            <SignTransactionMetaMask
+              stateValues={stateValues}
+              transactionFields={transactionFields}
+            />
+          </div>
+        );
+      default:
+        return null;
+      // case 'Mnemonic':
+      //   return <div>Mnemonic</div>;
+    }
   }
 }
--- common/v2/features/SendAssets/components/SignTransactionWallets/Metamask.tsx
+++ common/v2/features/SendAssets/components/SignTransactionWallets/Metamask.tsx
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
 import MetamaskSVG from 'common/assets/images/wallets/metamask-2.svg';
-import './MetaMask.scss';
 import * as ethers from 'ethers';
-import { AsyncSendable } from 'ethers/providers/web3-provider';
-import { ISendState } from '../../types';
+// tslint:disable-next-line: no-duplicate-imports
+import { utils } from 'ethers';
+import { AsyncSendable, Web3Provider } from 'ethers/providers/web3-provider';
+import React, { Component } from 'react';
+import { ISendState, ITxFields } from '../../types';
+import './MetaMask.scss';
 
-type AsyncSendableMetamask = AsyncSendable & { enable(): Promise<boolean> };
+type AsyncSendableMetamask = AsyncSendable & {
+  on(evt: string, cb: (params: any[]) => void): void;
+  enable(): Promise<boolean>;
+};
 
 declare global {
   interface Window {
@@ -14,16 +19,26 @@ declare global {
   }
 }
 
+// const networks = {
+//   ethereum: 1,
+//   ropsten: 3,
+//   rinkeby: 4,
+//   kovan: 42
+// };
+
 interface Props {
   stateValues: ISendState;
+  transactionFields: ITxFields;
 }
 
 interface MetaMaskUserState {
   account: string;
   network: string;
-  provider: boolean;
-  // provider2: Web3Provider | null;
+  metaMaskSigner: any;
+  metaMaskProvider: Web3Provider | null;
   metaMaskAccountMatches: boolean;
+  metaMaskNetworkMatches: boolean;
+  initAccountPolling: null;
 }
 
 const DEFAULT_NETWORK_FOR_FALLBACK = 'ropsten';
@@ -32,16 +47,25 @@ export default class SignTransactionMetaMask extends Component<Props> {
   public state: MetaMaskUserState = {
     account: '',
     network: '',
-    provider: false,
-    metaMaskAccountMatches: false
+    metaMaskProvider: null,
+    metaMaskSigner: false,
+    metaMaskAccountMatches: false,
+    metaMaskNetworkMatches: false,
+    initAccountPolling: null
   };
 
+  constructor(props: Props) {
+    super(props);
+    this.getMetaMaskProviderAndSigner = this.getMetaMaskProviderAndSigner.bind(this);
+  }
+
   public componentDidMount() {
-    this.getMetaMaskProvider();
+    this.getMetaMaskProviderAndSigner();
+    this.initMetMaskPolling();
   }
 
   public render() {
-    // const { stateValues: { transactionFields: { senderAddress } } } = this.props;
+    const { stateValues } = this.props;
     return (
       <div className="SignTransactionMetaMask-panel">
         <div className="SignTransactionMetaMask-title">Sign the Transaction with MetaMask</div>
@@ -52,6 +76,15 @@ export default class SignTransactionMetaMask extends Component<Props> {
           <img src={MetamaskSVG} />
         </div>
         <div className="SignTransactionMetaMask-input">
+          {this.state.metaMaskAccountMatches === false && (
+            <div className="SignTransactionMetaMask-wrong-address">
+              {' '}
+              Please switch the account in MetaMask to {
+                stateValues.transactionFields.senderAddress
+              }{' '}
+              <br /> in order to proceed{' '}
+            </div>
+          )}
           <div className="SignTransactionMetaMask-description">
             Because we never save, store, or transmit your secret, you need to sign each transaction
             in order to send it. MyCrypto puts YOU in control of your assets.
@@ -69,14 +102,31 @@ export default class SignTransactionMetaMask extends Component<Props> {
 
   // }
 
-  private checkAddressMatches(metaMaskAddress: string) {
-    // console.log(senderAddress, account);
-    if (metaMaskAddress === this.state.account) {
+  private initMetMaskPolling() {
+    setInterval(this.getMetaMaskProviderAndSigner, 2000);
+  }
+
+  private checkAddressMatches(metaMaskAddress: string, stateValues: ISendState) {
+    const desiredAddress = utils.getAddress(stateValues.transactionFields.senderAddress);
+
+    if (metaMaskAddress === desiredAddress) {
       return this.setState({ metaMaskAccountMatches: true });
+    } else {
+      return this.setState({ metaMaskAccountMatches: false });
     }
   }
 
-  private async getMetaMaskProvider() {
+  // private checkNetworkMatches(metaMaskNetwork: string, stateValues: ISendState) {
+  //   const desiredNetwork = stateValues.network;
+
+  //   if (metaMaskNetwork === desiredNetwork) {
+  //     return this.setState({ metaMaskNetworkMatches: true });
+  //   } else {
+  //     return this.setState({ metaMaskNetworkMatches: false });
+  //   }
+  // }
+
+  private async getMetaMaskProviderAndSigner() {
     if (window.ethereum) {
       await window.ethereum.enable();
       const metaMaskProvider = new ethers.providers.Web3Provider(window.ethereum);
@@ -84,16 +134,25 @@ export default class SignTransactionMetaMask extends Component<Props> {
       const metaMaskNetwork = await metaMaskProvider.getNetwork();
       const metaMaskAddress = await metaMaskSigner.getAddress();
 
+      const checksumAddress = utils.getAddress(metaMaskAddress);
+
       await this.setState({
-        account: metaMaskAddress,
+        account: checksumAddress,
         network: metaMaskNetwork.chainId,
-        provider: metaMaskSigner !== null ? true : false
+        metaMaskProvider,
+        metaMaskSigner
       });
-      this.checkAddressMatches(metaMaskAddress);
+
+      this.watchForAccountChanges(window.ethereum);
+      this.checkAddressMatches(checksumAddress, this.props.stateValues);
       console.log(this.state);
     } else {
       const defaultProvider = ethers.getDefaultProvider(DEFAULT_NETWORK_FOR_FALLBACK);
       this.setState({ provider: defaultProvider });
     }
+  }
+
+  private watchForAccountChanges(ethereum: NonNullable<Window['ethereum']>) {
+    ethereum.on('accountsChanged', this.getMetaMaskProviderAndSigner);
   }
 }
