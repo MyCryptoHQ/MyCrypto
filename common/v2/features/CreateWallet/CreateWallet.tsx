61f0068993aead6d4395b5965b4e9fb1e4be516b
Peter Zlodej <peter.zlodej@poviolabs.com>
2019-05-27 16:27:07 +0200

    Updated layout and styling of create wallet

--- common/v2/features/CreateWallet/CreateWallet.tsx
+++ common/v2/features/CreateWallet/CreateWallet.tsx
@@ -1,13 +1,61 @@
 import React from 'react';
 import { withRouter, Link, RouteComponentProps } from 'react-router-dom';
 import { Button, Typography } from '@mycrypto/ui';
+import styled from 'styled-components';
 
-import { ContentPanel } from 'v2/components';
+import { ExtendedContentPanel } from 'v2/components';
+import { translateRaw } from 'translations';
 import { Layout } from 'v2/features';
 import './CreateWallet.scss';
 
 // Legacy
 import newWalletIcon from 'common/assets/images/icn-new-wallet.svg';
+
+const DescriptionItem = styled(Typography)`
+  margin-top: 18px;
+  font-weight: normal;
+  font-size: 18px !important;
+
+  strong {
+    font-weight: 900;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 33px;
+  margin-bottom: 25px;
+`;
+
+const WalletImage = styled.img`
+  width: 152px;
+  height: 163px;
+`;
+
+const StyledButton = styled(Button)`
+  font-size: 18px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 28px;
+  &:focus,
+  &:hover {
+    embed {
+      filter: brightness(0) invert(1);
+    }
+  }
+`;
+
+const BottomActions = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 30px;
+  line-height: 1.5;
+`;
 
 export interface PanelProps {
   totalSteps: number;
@@ -19,26 +67,33 @@ export interface PanelProps {
 export function CreateWallet({ history }: RouteComponentProps<{}>) {
   return (
     <Layout centered={true}>
-      <ContentPanel
+      <ExtendedContentPanel
         onBack={() => history.push('/')}
-        heading="Create New Wallet"
-        description="Creating a mnemonic phrase - a list of 12 words -  will enable you to keep your funds safe and unlock your wallet."
-        className="SelectMethodPanel"
+        heading={translateRaw('CREATE_ACCOUNT_TITLE')}
       >
-        <div className="CreateWallet-content">
-          <img src={newWalletIcon} alt="New wallet" className="CreateWallet-content-icon" />
-          <Link to="/create-wallet/mnemonic">
-            <Button className="CreateWallet-content-button">Create Mnemonic Phrase</Button>
-          </Link>
-          <Typography>
-            Don’t want use a phrase?{' '}
-            <Link to="/create-wallet/keystore">Create wallet with keystore file.</Link>
-          </Typography>
-          <Typography>
-            Already have a wallet? <a href="#">Unlock it now.</a>
-          </Typography>
-        </div>
-      </ContentPanel>
+        <DescriptionItem>{translateRaw('CREATE_ACCOUNT_DESCRIPTION_1')}</DescriptionItem>
+        <DescriptionItem>{translateRaw('CREATE_ACCOUNT_DESCRIPTION_2')}</DescriptionItem>
+        <ImageWrapper>
+          <WalletImage src={newWalletIcon} alt="New wallet" />
+        </ImageWrapper>
+
+        <DescriptionItem>{translateRaw('CREATE_ACCOUNT_DESCRIPTION_3')}</DescriptionItem>
+        <Link to="/create-wallet/mnemonic">
+          <StyledButton>{translateRaw('CREATE_ACCOUNT_BUTTON')}</StyledButton>
+        </Link>
+        <BottomActions>
+          <div>
+            {translateRaw('CREATE_WALLET_WITH_KEYSTORE_BOTTOM_ACTION_1')}{' '}
+            <Link to="/create-wallet/keystore">
+              {translateRaw('CREATE_WALLET_WITH_KEYSTORE_BOTTOM_ACTION_2')}
+            </Link>
+          </div>
+          <div>
+            {translateRaw('CREATE_WALLET_UNLOCK_WALLET_BOTTOM_ACTION_1')}{' '}
+            <Link to="#">{translateRaw('CREATE_WALLET_UNLOCK_WALLET_BOTTOM_ACTION_2')}</Link>
+          </div>
+        </BottomActions>
+      </ExtendedContentPanel>
     </Layout>
   );
 }
