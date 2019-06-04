ab1a875a338eea584fde558c46f02ed14c502d9a
blurpesec <hahn.michael.f@gmail.com>
2019-06-04 08:19:51 -0400

    simplify validation syntax

--- common/v2/features/SendAssets/components/validators/validators.ts
+++ common/v2/features/SendAssets/components/validators/validators.ts
@@ -1,33 +1,25 @@
 import { isValidHex, gasPriceValidator, gasLimitValidator } from 'v2/libs/validators';
 
 export function validateGasPriceField(value: string): string | undefined {
-  let error;
   if (!gasPriceValidator(value)) {
-    error = 'Gas price must be a valid number.';
+    return 'Gas price must be a valid number.';
   }
-  return error;
 }
 
 export function validateGasLimitField(value: string): string | undefined {
-  let error;
   if (!gasLimitValidator(value)) {
-    error = 'Gas limit must be a valid number.';
+    return 'Gas limit must be a valid number.';
   }
-  return error;
 }
 
 export function validateDataField(value: string): string | undefined {
-  let error;
   if (!isValidHex(value)) {
-    error = 'Data field must contain valid hex.';
+    return 'Data field must contain valid hex.';
   }
-  return error;
 }
 
 export function validateNonceField(value: string): string | undefined {
-  let error;
   if (!(parseInt(value, 10) >= 0)) {
-    error = 'Nonce must be a valid number.';
+    return 'Nonce must be a valid number.';
   }
-  return error;
 }
