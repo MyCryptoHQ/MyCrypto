5c036dfc96aadf5dff937e65577994d6ecb54a77
Amadej Pevec <amadej.pevec@poviolabs.com>
2019-06-12 14:14:33 +0200

    Rewrite scss styles to styled component

--- common/v2/features/Drawer/screens/SelectLanguage.tsx
+++ common/v2/features/Drawer/screens/SelectLanguage.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import classnames from 'classnames';
+import styled from 'styled-components';
 
 import { AnalyticsService, ANALYTICS_CATEGORIES } from 'v2/services';
 import { languages } from 'config';
 import { translateRaw } from 'translations';
 import { AppState } from 'features/reducers';
 import { configMetaActions, configMetaSelectors } from 'features/config';
+
+const LanguagesList = styled.ul`
+  flex-grow: 1;
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  overflow: auto;
+`;
+
+interface LanguageProps {
+  isSelected: boolean;
+}
+
+const Language =
+  styled.li <
+  LanguageProps >
+  `
+  margin: 0;
+  padding: 15px 20px;
+  border-top: 1px solid #e9e9e9;
+  cursor: pointer;
+  transition: background 0.3s ease-in;
+
+  &:hover {
+    background: #f2f2f2;
+  }
+
+  ${props => props.isSelected && 'background: #f2f2f2;'};
+`;
 
 interface StateProps {
   languageSelection: ReturnType<typeof configMetaSelectors.getLanguageSelection>;
@@ -37,21 +66,17 @@ type Props = StateProps & DispatchProps;
 
 function LanguageSelect({ languageSelection, changeLanguage, onClose }: Props) {
   return (
-    <section className="SidebarScreen">
-      <ul className="SidebarScreen-list-full">
-        {Object.entries(languages).map(([code, language]) => (
-          <li
-            key={code}
-            className={classnames('SidebarScreen-language', {
-              'is-selected': languageSelection === code
-            })}
-            onClick={() => handleLanguageSelect(code, languageSelection, changeLanguage, onClose)}
-          >
-            {language}
-          </li>
-        ))}
-      </ul>
-    </section>
+    <LanguagesList>
+      {Object.entries(languages).map(([code, language]) => (
+        <Language
+          isSelected={languageSelection === code}
+          key={code}
+          onClick={() => handleLanguageSelect(code, languageSelection, changeLanguage, onClose)}
+        >
+          {language}
+        </Language>
+      ))}
+    </LanguagesList>
   );
 }
 
