58797dcaf19ac4280c533e4af9310a53f70bc398
Srecko Smodis <srecko.smodis@poviolabs.com>
2019-07-19 13:25:10 +0200

    Reorder linux build steps remove prettier (not esential) remove cache for Travis

--- travis-scripts/test-linux.sh
+++ travis-scripts/test-linux.sh
@@ -2,11 +2,12 @@
 
 set -ev # return value 1 (error) if any command fails, and display each command before its run
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  yarn prettier:diff
-  yarn test:coverage -- --maxWorkers=2
-  yarn report-coverage
+  # yarn prettier:diff - not sure if we need this for build
+  # move linting before tests as it finnishes earlier
   yarn tslint
   yarn tscheck
   yarn freezer
   yarn freezer:validate
+  yarn test:coverage -- --maxWorkers=2
+  yarn report-coverage
 fi
