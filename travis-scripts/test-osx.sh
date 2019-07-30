2294d6bcbd802aba60b67c6c02315c5ee121959f
Srecko Smodis <srecko.smodis@poviolabs.com>
2019-07-30 14:32:30 +0200

    Move test to OSX

--- travis-scripts/test-osx.sh
+++ travis-scripts/test-osx.sh
@@ -2,6 +2,10 @@
 
 set -ev # return value 1 (error) if any command fails, and display each command before its run
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  yarn tslint
+  yarn tscheck
+  yarn test:coverage -- --maxWorkers=2
+  yarn report-coverage
   yarn build:electron
   ls -la dist/electron-builds
 fi
