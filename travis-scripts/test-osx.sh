2235088bc49058cf49d324a8cca51c7fb76162c4
Srecko Smodis <srecko.smodis@poviolabs.com>
2019-07-31 09:10:53 +0200

    Removing freezer from script Travis builds skip that anyway

--- travis-scripts/test-osx.sh
+++ travis-scripts/test-osx.sh
@@ -4,8 +4,6 @@ set -ev # return value 1 (error) if any command fails, and display each command 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   yarn tslint
   yarn tscheck
-  yarn freezer
-  yarn freezer:validate
   yarn test:coverage -- --maxWorkers=2
   yarn report-coverage
   yarn build:electron
