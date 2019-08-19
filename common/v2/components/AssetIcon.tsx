c1330b66826a30d9725592c1f379c3f9de34d98f
blurpesec <hahn.michael.f@gmail.com>
2019-08-19 11:18:15 -0700

    swapped asset import to svg & versioned api

--- common/v2/components/AssetIcon.tsx
+++ common/v2/components/AssetIcon.tsx
@@ -5,10 +5,10 @@ import manifest from 'cryptocurrency-icons/manifest.json';
 import { IAsset, TSymbol } from 'v2/types';
 // Relies on https://github.com/atomiclabs/cryptocurrency-icons using fixed version number through CDN
 // @TODO: We should be using our own sprite served over a trusted CDN
-const baseURL = 'https://cdn.mycryptoapi.com/icons';
+const baseURL = 'https://cdn.mycryptoapi.com/v1/icons';
 
 function buildUrl(symbol: TSymbol) {
-  return `${baseURL}/${symbol.toLowerCase()}.png`;
+  return `${baseURL}/${symbol.toLowerCase()}.svg`;
 }
 
 function getIconUrl(symbol: TSymbol) {
