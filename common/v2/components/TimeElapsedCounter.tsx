d6153b3faf2bdb701adefcdff2c511f02485ae8a
blurpesec <hahn.michael.f@gmail.com>
2019-08-21 11:42:27 -0700

    fix up timeelapsed counter

--- common/v2/components/TimeElapsedCounter.tsx
+++ common/v2/components/TimeElapsedCounter.tsx
@@ -1,62 +1,57 @@
 import React, { useEffect, useState } from 'react';
 
-export interface Props {
+interface Props {
   timestamp: number;
   isSeconds?: boolean; // Used to convert s timestamps to ms.
 }
+
+interface TimeCalculations {
+  elapsedSeconds: number;
+  seconds: number;
+  minutes: number;
+  hours: number;
+  days: number;
+}
+
+const calculateTimeComponents = (elapsedTimeInSeconds: number): TimeCalculations => {
+  return {
+    elapsedSeconds: elapsedTimeInSeconds,
+    seconds: Math.floor(elapsedTimeInSeconds % 60),
+    minutes: Math.floor(elapsedTimeInSeconds / 60) % 60,
+    hours: Math.floor(elapsedTimeInSeconds / 3600) % 24,
+    days: Math.floor(elapsedTimeInSeconds / 86400)
+  };
+};
 
 const TimeElapsedCounter = ({ timestamp, isSeconds }: Props) => {
   const convertedTimestamp = isSeconds ? timestamp * 1000 : timestamp;
   const elapsed = Math.floor((Date.now() - convertedTimestamp) / 1000);
 
-  const [elapsedTimeSeconds, setElapsedTimeSeconds] = useState(Math.floor(elapsed % 60));
-  const [elapsedTimeMinutes, setElapsedTimeMinutes] = useState(Math.floor(elapsed / 60) % 60);
-  const [elapsedTimeHours, setElapsedTimeHours] = useState(Math.floor(elapsed / 3600) % 24);
-  const [elapsedTimeDays, setElapsedTimeDays] = useState(Math.floor(elapsed / 86400));
+  const timeComponents = calculateTimeComponents(elapsed);
+
+  const [elapsedTime, setElapsedTime] = useState(timeComponents);
 
   useEffect(() => {
-    /* TODO: Remove memory leak associated with not clearing this 
-    interval. Figure out how to do that in hooks. */
-    setInterval(() => {
-      setElapsedTimeSeconds(elapsedSeconds => {
-        const newElapsedTimeSeconds = elapsedSeconds + 1;
-        if (newElapsedTimeSeconds % 60 === 0) {
-          setElapsedTimeMinutes(elapsedMinutes => {
-            const newElapsedTimeMinutes = elapsedMinutes + 1;
-            if (newElapsedTimeMinutes % 60 === 0) {
-              setElapsedTimeHours(elapsedHours => {
-                const newElapsedTimeHours = elapsedHours + 1;
-                if (newElapsedTimeHours % 24 === 0) {
-                  setElapsedTimeDays(elapsedDays => {
-                    const newElapsedTimeDays = elapsedDays + 1;
-                    return newElapsedTimeDays;
-                  });
-                }
-                return newElapsedTimeHours % 24;
-              });
-            }
-            return newElapsedTimeMinutes % 60;
-          });
-        }
-        return newElapsedTimeSeconds % 60;
-      });
+    setTimeout(() => {
+      const newElapsedTimeSeconds = Math.floor((Date.now() - convertedTimestamp) / 1000);
+      setElapsedTime(calculateTimeComponents(newElapsedTimeSeconds));
     }, 1000);
-  }, []);
+  }, [elapsedTime]);
 
   return (
     <>
-      {elapsedTimeDays !== 0 &&
-        (elapsedTimeDays === 1 ? `${elapsedTimeDays} day ` : `${elapsedTimeDays} days `)}
-      {elapsedTimeHours !== 0 &&
-        (elapsedTimeHours === 1 ? `${elapsedTimeHours} hour ` : `${elapsedTimeHours} hours `)}
-      {elapsedTimeMinutes !== 0 &&
-        (elapsedTimeMinutes === 1
-          ? `${elapsedTimeMinutes} minute `
-          : `${elapsedTimeMinutes} minutes `)}
-      {elapsedTimeSeconds !== 0 &&
-        (elapsedTimeSeconds === 1
-          ? `${elapsedTimeSeconds} second `
-          : `${elapsedTimeSeconds} seconds `)}
+      {elapsedTime.days !== 0 &&
+        (elapsedTime.days === 1 ? `${elapsedTime.days} day ` : `${elapsedTime.days} days `)}
+      {elapsedTime.hours !== 0 &&
+        (elapsedTime.hours === 1 ? `${elapsedTime.hours} hour ` : `${elapsedTime.hours} hours `)}
+      {elapsedTime.minutes !== 0 &&
+        (elapsedTime.minutes === 1
+          ? `${elapsedTime.minutes} minute `
+          : `${elapsedTime.minutes} minutes `)}
+      {elapsedTime.seconds !== 0 &&
+        (elapsedTime.seconds === 1
+          ? `${elapsedTime.seconds} second `
+          : `${elapsedTime.seconds} seconds `)}
       {'ago'}
     </>
   );
