0da775682ba226424a89e9d82556b1a5ba120a37
Peter Zlodej <peter.zlodej@poviolabs.com>
2019-08-28 10:14:56 +0200

    Use styled components for token list

--- common/v2/features/Dashboard/components/TokenPanel/TokenList.tsx
+++ common/v2/features/Dashboard/components/TokenPanel/TokenList.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Typography } from '@mycrypto/ui';
+import styled from 'styled-components';
 
-import './TokenList.scss';
-
-// Legacy
 import moreIcon from 'common/assets/images/icn-more.svg';
 
 // Fake Data
@@ -17,24 +15,74 @@ const tokens = [
     image: 'https://placehold.it/30x30',
     name: 'OmiseGO',
     value: '$3,037.95'
+  },
+  {
+    image: 'https://placehold.it/30x30',
+    name: 'OmiseGO2',
+    value: '$3,037.95'
+  },
+  {
+    image: 'https://placehold.it/30x30',
+    name: 'OmiseGO3',
+    value: '$3,037.95'
+  },
+  {
+    image: 'https://placehold.it/30x30',
+    name: 'OmiseGO4',
+    value: '$3,037.95'
   }
 ];
 
+const TokenListWrapper = styled.div`
+  max-height: 300px;
+  overflow-y: auto;
+`;
+
+const Token = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 0;
+`;
+
+const Asset = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const AssetName = styled(Typography)`
+  margin: 0 0 0 15px !important;
+  font-weight: normal;
+  font-size: 14px;
+`;
+
+const TokenValueWrapper = styled.div`
+  margin: 0;
+  display: flex;
+  align-items: center;
+`;
+
+const TokenValue = styled(Typography)`
+  margin: 0 15px 0 0 !important;
+  font-weight: normal;
+  font-size: 16px;
+`;
+
 export function TokenList() {
   return (
-    <div className="TokenList-tokens">
+    <TokenListWrapper>
       {tokens.map(({ image, name, value }) => (
-        <div key={name} className="TokenList-tokens-token">
-          <div className="TokenList-tokens-token-asset">
+        <Token key={name}>
+          <Asset>
             <img src={image} alt={name} />
-            <Typography className="TokenList-tokens-token-asset-name">{name}</Typography>
-          </div>
-          <div className="TokenList-tokens-token-value">
-            <Typography className="TokenList-tokens-token-value-amount">{value}</Typography>
+            <AssetName>{name}</AssetName>
+          </Asset>
+          <TokenValueWrapper>
+            <TokenValue>{value}</TokenValue>
             <img src={moreIcon} alt="More" />
-          </div>
-        </div>
+          </TokenValueWrapper>
+        </Token>
       ))}
-    </div>
+    </TokenListWrapper>
   );
 }
