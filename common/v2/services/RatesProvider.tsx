ab706d64291c4a123155894b9b9813b12a033b52
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2019-09-05 11:31:19 +0300

    remove duplicate ticker values from rate polling

--- common/v2/services/RatesProvider.tsx
+++ common/v2/services/RatesProvider.tsx
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 
 import { StoreContext } from 'v2/services/Store';
 import { PollingService } from 'v2/workers';
-import { IRates, TTicker, StoreAsset } from 'v2/types';
+import { IRates, TTicker } from 'v2/types';
 
 interface State {
   rates: IRates;
@@ -17,22 +17,18 @@ const buildQueryUrl = (assets: TTicker[], currencies: TTicker[]) => `
   ${RATES_URL}/?fsyms=${assets.join(',')}&tsyms=${currencies.join(',')}
 `;
 
-// The cryptocompare api that our proxie uses fails gracefully and will return a conversion rate
-// even if some are tickers are invalid (e.g WETH, GoerliETH etc.)
-// @TODO: figure out how to handle the conversion more elegantly then `DEFAULT_FIAT_RATE`
-const getAssetTickers = (assets: StoreAsset[]): TTicker[] => assets.map(a => a.ticker as TTicker);
-
 export const RatesContext = createContext({} as State);
 
 export function RatesProvider({ children }: { children: React.ReactNode }) {
   const [rates, setRates] = useState({});
-  const { accounts, assets } = useContext(StoreContext);
+  const { accounts, assetTickers } = useContext(StoreContext);
 
   useEffect(() => {
-    const currentAssets = assets();
-    const currentTickers = getAssetTickers(currentAssets);
+    // The cryptocompare api that our proxie uses fails gracefully and will return a conversion rate
+    // even if some are tickers are invalid (e.g WETH, GoerliETH etc.)
+    const currentTickers = assetTickers();
     const worker = new PollingService(
-      buildQueryUrl(currentTickers, DEFAULT_FIAT_PAIRS),
+      buildQueryUrl(currentTickers, DEFAULT_FIAT_PAIRS), // @TODO: figure out how to handle the conversion more elegantly then `DEFAULT_FIAT_RATE`
       POLLING_INTERRVAL,
       (data: IRates) => setRates(data),
       err => console.debug('[RatesProvider]', err)
