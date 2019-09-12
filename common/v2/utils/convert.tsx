09b8532d1dd95fbec3c67ed8d4d0a85685cda6b4
Peter Zlodej <peter.zlodej@poviolabs.com>
2019-09-12 13:11:32 +0200

    Modify convertToFiat to support more decimals

--- common/v2/utils/convert.tsx
+++ common/v2/utils/convert.tsx
@@ -1,12 +1,11 @@
 import { bigNumberify, formatEther, BigNumber } from 'ethers/utils';
 
-// Rate is received as decimal numbers
-// While the balance is a BigNumber.js
-// To operate the conversion we multiple by 100 in order to do BigNumber math
-// Then we make sure to divide by 100 to receive the correct Float.
 export const convertToFiat = (balance: BigNumber, rate: number = 1): number => {
-  const rateDivisor = 100;
-  const rateBN = bigNumberify(rate * rateDivisor);
+  const splitRate = rate.toString().split('.');
+  const decimals = splitRate.length > 1 ? splitRate[1].length : 0;
+  const rateDivisor = Math.pow(10, decimals);
+  const rateBN = bigNumberify(Math.round(rate * rateDivisor));
+
   const convertedFloat = parseFloat(formatEther(balance.mul(rateBN)));
   return convertedFloat / rateDivisor;
 };
