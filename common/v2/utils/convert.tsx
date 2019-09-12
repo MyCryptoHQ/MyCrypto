8d8956ee014b4c22972109acc9be372362c9d35d
Frederik Bolding <frederik.bolding@gmail.com>
2019-09-12 11:34:01 +0200

    Fixed an underflow when calculating wallet balances

--- common/v2/utils/convert.tsx
+++ common/v2/utils/convert.tsx
@@ -1,11 +1,12 @@
 import { bigNumberify, formatEther, BigNumber } from 'ethers/utils';
 
-// Rate is received as decimal numbers
+// Rate is received as decimal numbers, sometimes with as many as 4 decimals
 // While the balance is a BigNumber.js
-// To operate the conversion we multiple by 100 in order to do BigNumber math
-// Then we make sure to divide by 100 to receive the correct Float.
+// To operate the conversion we multiple by 10000 (because we may have 4 decimals) in order to do BigNumber math
+// Then we make sure to divide by 10000 to receive the correct Float.
 export const convertToFiat = (balance: BigNumber, rate: number = 1): number => {
-  const rateDivisor = 100;
+  rate = parseFloat(rate.toFixed(4));
+  const rateDivisor = 10000;
   const rateBN = bigNumberify(rate * rateDivisor);
   const convertedFloat = parseFloat(formatEther(balance.mul(rateBN)));
   return convertedFloat / rateDivisor;
