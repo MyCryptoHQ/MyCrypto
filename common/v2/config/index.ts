c1d9a811d1bbc3890ddfc537c71fe6f01dce0899
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2019-09-18 16:55:45 +0200

    preserve file history during rename

--- common/v2/config/index.ts
+++ common/v2/config/index.ts
@@ -12,7 +12,7 @@ export {
 } from './constants';
 export { NODES_CONFIG } from './nodes';
 export { ROUTE_PATHS } from './routePaths';
-export { WALLET_INFO } from './walletInfo';
+export { WALLET_INFO } from './wallets';
 export { NETWORKS_CONFIG } from './networks';
 export { ETHSCAN_NETWORKS } from './ethScan';
 export { Fiats, AssetsData, ContractsData } from './cacheData';
