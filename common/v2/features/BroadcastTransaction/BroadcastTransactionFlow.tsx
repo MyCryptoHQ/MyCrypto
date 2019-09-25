67fe91590cb72a3f0d9387596a00696178663391
Peter Zlodej <peter.zlodej@poviolabs.com>
2019-09-25 10:59:31 +0200

    Replace old broadcast tx form, update routes

---
+++ common/v2/features/BroadcastTransaction/BroadcastTransactionFlow.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import EthTx from 'ethereumjs-tx';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+
+import { ExtendedContentPanel } from 'v2/components';
+import { BroadcastTx } from './components';
+import { ROUTE_PATHS } from 'v2/config';
+
+const BroadcastTransactionFlow = (props: RouteComponentProps<{}>) => {
+  const [step, setStep] = useState(0);
+  const [network, setNetwork] = useState('Ethereum');
+  const [signedTransaction, setSignedTransaction] = useState('');
+  const [transaction, setTransaction] = useState<EthTx | undefined>(undefined);
+  const steps = [BroadcastTx];
+
+  const goToNextStep = () => {
+    setStep(step + 1);
+  };
+
+  const goToPreviousStep = () => {
+    const { history } = props;
+    if (step === 0) {
+      history.push(ROUTE_PATHS.DASHBOARD.path);
+    } else {
+      setStep(step - 1);
+    }
+  };
+
+  const Step = steps[step];
+
+  return (
+    <ExtendedContentPanel
+      onBack={goToPreviousStep}
+      stepper={{ current: step + 1, total: steps.length }}
+      width="650px"
+    >
+      <Step
+        goToNextStep={goToNextStep}
+        selectNetwork={setNetwork}
+        network={network}
+        setTransaction={setTransaction}
+        transaction={transaction}
+        setSignedTransaction={setSignedTransaction}
+        signedTransaction={signedTransaction}
+      />
+    </ExtendedContentPanel>
+  );
+};
+
+export default withRouter(BroadcastTransactionFlow);
