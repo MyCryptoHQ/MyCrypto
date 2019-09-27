a5e822a5927c7a0f03889d32ef898186f6a5f788
Sonicrida <Sonicrida@live.com>
2019-09-26 17:06:25 -0700

    Fix settings tabs position

--- common/v2/features/Settings/components/index.ts
+++ common/v2/features/Settings/components/index.ts
@@ -2,3 +2,4 @@ export { default as AddressBookPanel } from './AddressBook';
 export { default as AddToAddressBook } from './AddToAddressBook';
 export { default as GeneralSettings } from './GeneralSettings';
 export { default as YourAccounts } from './YourAccounts';
+export { default as Tabs } from './Tabs';
