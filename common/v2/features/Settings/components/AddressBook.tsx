0cc7108f9e26d13862624a1d7671536f625fdaa3
Jonathan Chhabra <434238+Sonicrida@users.noreply.github.com>
2019-10-03 07:04:37 -0700

    Add deletion confirmation to address book

--- common/v2/features/Settings/components/AddressBook.tsx
+++ common/v2/features/Settings/components/AddressBook.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Icon, Copyable, Identicon, Button } from '@mycrypto/ui';
 
 import { DashboardPanel, CollapsibleTable, Typography, Network } from 'v2/components';
 import { ExtendedAddressBook } from 'v2/types';
 import { truncate } from 'v2/utils';
-import { BREAK_POINTS } from 'v2/theme';
+import { BREAK_POINTS, breakpointToNumber } from 'v2/theme';
 
 interface Props {
   addressBook: ExtendedAddressBook[];
@@ -53,22 +53,101 @@ const STypography = styled(Typography)`
   }
 `;
 
+const TableOverlay = styled.div`
+  max-width: 674px;
+  height: 67px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: #b5bfc7;
+  color: #fff;
+  padding: 1em;
+`;
+
+const OverlayText = styled(Typography)`
+  color: #fff;
+  flex-grow: 1;
+  text-overflow: hidden;
+  width: 50%;
+`;
+
+const OverlayButtons = styled.div`
+  align-self: flex-end;
+`;
+
+const OverlayDelete = styled(Button)`
+  font-size: 14px;
+  margin-left: 5px;
+`;
+
+const OverlayCancel = styled(Button)`
+  font-size: 14px;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  margin-left: 5px;
+`;
+
+export const screenIsMobileSized = (breakpoint: number): boolean =>
+  window.matchMedia(`(max-width: ${breakpoint}px)`).matches;
+
 export default function AddressBook({ addressBook, toggleFlipped, deleteAddressBooks }: Props) {
+  const [deletingIndex, setDeletingIndex] = useState();
+
+  const overlayRows = [deletingIndex];
+
   const addressBookTable = {
     head: ['Favorite', 'Label', 'Address', 'Network', 'Notes', 'Delete'],
-    body: addressBook.map(({ address, label, network, notes, uuid }: ExtendedAddressBook) => [
-      <Icon key={0} icon="star" />,
-      <Label key={1}>
-        <SIdenticon address={address} />
-        <STypography bold={true} value={label} />
-      </Label>,
-      <Copyable key={2} text={address} truncate={truncate} isCopyable={true} />,
-      <Network key={3} color="#a682ff">
-        {network}
-      </Network>,
-      <Typography key={4} value={notes} />,
-      <DeleteButton key={5} onClick={() => deleteAddressBooks(uuid)} icon="exit" />
-    ]),
+    overlay:
+      overlayRows && overlayRows[0] !== undefined ? (
+        <TableOverlay>
+          <OverlayText>
+            Are you sure you want to delete {addressBook[overlayRows[0]].label} address with
+            address: {truncate(addressBook[overlayRows[0]].address)}?
+          </OverlayText>
+          <OverlayButtons>
+            <OverlayDelete
+              onClick={() => {
+                deleteAddressBooks(addressBook[overlayRows[0]].uuid);
+                setDeletingIndex(undefined);
+              }}
+            >
+              Delete
+            </OverlayDelete>
+            <OverlayCancel secondary={true} onClick={() => setDeletingIndex(undefined)}>
+              Cancel
+            </OverlayCancel>
+          </OverlayButtons>
+        </TableOverlay>
+      ) : (
+        <></>
+      ),
+    overlayRows,
+    body: addressBook.map(
+      ({ address, label, network, notes, uuid }: ExtendedAddressBook, index) => [
+        <Icon key={0} icon="star" />,
+        <Label key={1}>
+          <SIdenticon address={address} />
+          <STypography bold={true} value={label} />
+        </Label>,
+        <Copyable key={2} text={address} truncate={truncate} isCopyable={true} />,
+        <Network key={3} color="#a682ff">
+          {network}
+        </Network>,
+        <Typography key={4} value={notes} />,
+        <DeleteButton
+          key={5}
+          onClick={() => {
+            if (screenIsMobileSized(breakpointToNumber(BREAK_POINTS.SCREEN_XS)) === false) {
+              setDeletingIndex(index);
+            }
+            if (screenIsMobileSized(breakpointToNumber(BREAK_POINTS.SCREEN_XS)) === true) {
+              deleteAddressBooks(uuid);
+            }
+          }}
+          icon="exit"
+        />
+      ]
+    ),
     config: {
       primaryColumn: 'Label',
       sortableColumn: 'Label',
