1bf8a51360a10acfa45764ea28e4f1f0cf24ee9c
Amadej Pevec <amadej.pevec@poviolabs.com>
2019-10-16 17:00:00 +0200

    Show loader when waiting for allowance transaction to be mined

---
+++ common/v2/features/SwapAssets/components/SetAllowance.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { StoreAccount } from 'v2/types';
+import { translate } from 'translations';
+import { WALLET_STEPS } from '../helpers';
+import { Spinner } from 'v2/components';
+
+const AllowanceWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Loader = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+  font-weight: bold;
+  font-size: 18px;
+`;
+
+const LoaderText = styled.div`
+  margin-left: 8px;
+`;
+
+interface Props {
+  account: StoreAccount;
+  isSettingAllowance: boolean;
+}
+
+export default function SetAllowance(props: Props) {
+  const { account, isSettingAllowance } = props;
+  const Component: any = account && WALLET_STEPS[account.wallet];
+
+  return (
+    <AllowanceWrapper>
+      <Component {...props} />
+      {isSettingAllowance && (
+        <Loader>
+          <Spinner size={'x2'} /> <LoaderText>{translate('SWAP_SETTING_ALLOWANCE')}</LoaderText>
+        </Loader>
+      )}
+    </AllowanceWrapper>
+  );
+}
--- common/v2/features/SwapAssets/components/index.ts
+++ common/v2/features/SwapAssets/components/index.ts
@@ -2,3 +2,4 @@ export { default as SwapAssets } from './SwapAssets';
 export { default as SelectAddress } from './SelectAddress';
 export { default as ConfirmSwap } from './ConfirmSwap';
 export { default as SwapTransactionReceipt } from './SwapTransactionReceipt';
+export { default as SetAllowance } from './SetAllowance';
