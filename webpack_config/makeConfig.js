04974a56436106e69247d58cc4e8a418e2c8aa69
Amadej Pevec <amadej.pevec@poviolabs.com>
2019-10-21 15:22:03 +0200

    Revert removed csp settings

--- webpack_config/makeConfig.js
+++ webpack_config/makeConfig.js
@@ -176,7 +176,7 @@ module.exports = function(opts = {}) {
         creator: config.twitter.creator
       },
       metaCsp: options.isProduction
-        ? "default-src 'none'; worker-src 'self' blob:; child-src 'self'; manifest-src 'self'; img-src 'self' data: https://cdn.mycryptoapi.com/; connect-src *; frame-src 'self' https://connect.trezor.io;"
+        ? "default-src 'none'; script-src 'self'; worker-src 'self' blob:; child-src 'self'; style-src 'self' 'unsafe-inline'; manifest-src 'self'; font-src 'self'; img-src 'self' data: https://cdn.mycryptoapi.com/; connect-src *; frame-src 'self' https://connect.trezor.io;"
         : ''
     }),
 
