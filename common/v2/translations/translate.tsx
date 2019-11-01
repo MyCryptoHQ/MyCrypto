61b64a08f857c45740e644d6b9060e09a026a8c1
blurpesec <hahn.michael.f@gmail.com>
2019-10-31 17:45:48 -0700

    remove translateRawV2

--- common/v2/translations/translate.tsx
+++ common/v2/translations/translate.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import TranslateMarkdown from 'v2/components/TranslateMarkdown';
-import { translateRawV2 } from './translateRaw';
+import { translateRaw } from './translateRaw';
 
 export type TranslatedText = React.ReactElement<any> | string;
 
@@ -8,7 +8,7 @@ export function translate(
   key: string,
   variables?: { [name: string]: string }
 ): React.ReactElement<any> {
-  return <TranslateMarkdown source={translateRawV2(key, undefined, variables)} />;
+  return <TranslateMarkdown source={translateRaw(key, variables)} />;
 }
 
 export default translate;
