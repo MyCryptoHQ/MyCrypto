28dd7497930b56c927f08a8fe32f53476c005dba
blurpesec <hahn.michael.f@gmail.com>
2019-10-31 18:09:08 -0700

    removed production code from root files

--- common/Root.tsx
+++ common/Root.tsx
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
 import { setConfig } from 'react-hot-loader';
 import { hot } from 'react-hot-loader/root';
-import { connect, Provider } from 'react-redux';
-import { Store } from 'redux';
 import { ThemeProvider } from 'styled-components';
-import { Theme } from 'config';
-import { QrSignerModal } from 'containers';
-import { configMetaSelectors, configSelectors } from 'features/config';
-import { AppState } from 'features/reducers';
-import { transactionMetaActions } from 'features/transaction';
-
 // v2
 import { GAU_THEME } from 'v2/theme';
 import { IS_DEV, IS_ELECTRON } from 'v2/utils';
@@ -19,60 +11,32 @@ import { DevTools } from 'v2/features';
 import AppProviders from './AppProviders';
 import { AppRouter } from './AppRouter';
 
-interface OwnProps {
-  store: Store<AppState>;
-}
-
-interface StateProps {
-  networkUnit: ReturnType<typeof configSelectors.getNetworkUnit>;
-  theme: ReturnType<typeof configMetaSelectors.getTheme>;
-}
-
-interface DispatchProps {
-  setUnitMeta: transactionMetaActions.TSetUnitMeta;
-}
-
-type Props = OwnProps & StateProps & DispatchProps;
-
-class RootClass extends Component<Props> {
+class RootClass extends Component {
   public componentDidMount() {
-    this.props.setUnitMeta(this.props.networkUnit);
     this.addBodyClasses();
-    this.updateTheme(this.props.theme);
   }
 
   public componentDidCatch(error: Error) {
     console.error(error);
   }
 
-  public componentDidUpdate(prevProps: Props) {
-    if (this.props.theme !== prevProps.theme) {
-      this.updateTheme(this.props.theme, prevProps.theme);
-    }
-  }
-
   public render() {
-    const { store } = this.props;
-
     return (
       <DevModeProvider>
         <ThemeProvider theme={GAU_THEME}>
-          <Provider store={store}>
-            <AppProviders>
-              <AppRouter />
-              <QrSignerModal />
-              <div id="ModalContainer" />
-              {IS_ELECTRON ? <NewAppReleaseModal /> : <></>}
-              {IS_DEV ? (
-                <>
-                  <DevToolsContainer />
-                  <DevModeToggle />
-                </>
-              ) : (
-                <></>
-              )}
-            </AppProviders>
-          </Provider>
+          <AppProviders>
+            <AppRouter />
+            <div id="ModalContainer" />
+            {IS_ELECTRON ? <NewAppReleaseModal /> : <></>}
+            {IS_DEV ? (
+              <>
+                <DevToolsContainer />
+                <DevModeToggle />
+              </>
+            ) : (
+              <></>
+            )}
+          </AppProviders>
         </ThemeProvider>
       </DevModeProvider>
     );
@@ -94,14 +58,6 @@ class RootClass extends Component<Props> {
     }
 
     document.body.className += ` ${classes.join(' ')}`;
-  }
-
-  private updateTheme(theme: Theme, oldTheme?: Theme) {
-    const root = document.documentElement;
-    if (oldTheme) {
-      root.classList.remove(`theme--${oldTheme}`);
-    }
-    root.classList.add(`theme--${theme}`);
   }
 }
 
@@ -128,19 +84,7 @@ const DevToolsContainer = () => {
   return isDevelopmentMode ? <DevTools /> : <></>;
 };
 
-const mapStateToProps = (state: AppState): StateProps => ({
-  networkUnit: configSelectors.getNetworkUnit(state),
-  theme: configMetaSelectors.getTheme(state)
-});
-
-const ConnectedRoot = connect(
-  mapStateToProps,
-  {
-    setUnitMeta: transactionMetaActions.setUnitMeta
-  }
-)(RootClass);
-
 // Silence RHL 'reconciliation failed' errors
 // https://github.com/gatsbyjs/gatsby/issues/7209#issuecomment-415807021
 setConfig({ logLevel: 'no-errors-please' });
-export default hot(ConnectedRoot);
+export default hot(RootClass);
