69027141b0b07d3c2f9befc37ff431d792a23970
Frederik Bolding <frederik.bolding@gmail.com>
2019-11-26 22:38:46 +0100

    Removed legacy send flow

--- common/containers/index.ts
+++ common/containers/index.ts
@@ -3,5 +3,4 @@ export { default as QrSignerModal } from './QrSignerModal';
 export { default as BroadcastTx } from './Tabs/BroadcastTx';
 export { default as Contracts } from './Tabs/Contracts';
 export { default as GenerateWallet } from './Tabs/GenerateWallet';
-export { default as SendTransaction } from './Tabs/SendTransaction';
 export { default as SupportPage } from './Tabs/SupportPage';
