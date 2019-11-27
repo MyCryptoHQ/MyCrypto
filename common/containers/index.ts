9e68c5a09eb18104a5e35816606c66d5e528fc74
Frederik Bolding <frederik.bolding@gmail.com>
2019-11-27 12:52:00 +0100

    Removed export causing issue

--- common/containers/index.ts
+++ common/containers/index.ts
@@ -1,4 +1,3 @@
 export { default as TabSection } from './TabSection';
 export { default as BroadcastTx } from './Tabs/BroadcastTx';
 export { default as GenerateWallet } from './Tabs/GenerateWallet';
-export { default as SupportPage } from './Tabs/SupportPage';
