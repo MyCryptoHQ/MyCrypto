e336f524a5c0b7d4f67711cc348bd33c38848d8a
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2019-11-27 11:59:06 +0100

    Remove unused component

--- common/v2/components/CopyableCodeBlock.tsx
+++ common/v2/components/CopyableCodeBlock.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button, Copyable } from '@mycrypto/ui';
+import { Copyable } from '@mycrypto/ui';
 
 interface Props {
   children?: React.ReactNode;
