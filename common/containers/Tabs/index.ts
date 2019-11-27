d390729e8ddeecaa993ed619e6c40f557e746883
Frederik Bolding <frederik.bolding@gmail.com>
2019-11-27 12:23:42 +0100

    Removed legacy send flow (#2875)

--- common/containers/Tabs/index.ts
+++ common/containers/Tabs/index.ts
@@ -1,7 +1,5 @@
 import { default as GenerateWallet } from './GenerateWallet';
-import { default as SendTransaction } from './SendTransaction';
 
 export default {
-  GenerateWallet,
-  SendTransaction
+  GenerateWallet
 };
