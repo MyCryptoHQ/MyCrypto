749800a7b332b13dae7e12fbb5268378584afa3a
Frederik Bolding <frederik.bolding@gmail.com>
2019-11-27 13:31:38 +0100

    Removed legacy misc containers (#2876)
    * Removed legacy misc containers
    
    * Removed export causing issue

--- common/containers/index.ts
+++ common/containers/index.ts
@@ -1,6 +1,3 @@
 export { default as TabSection } from './TabSection';
-export { default as QrSignerModal } from './QrSignerModal';
 export { default as BroadcastTx } from './Tabs/BroadcastTx';
-export { default as Contracts } from './Tabs/Contracts';
 export { default as GenerateWallet } from './Tabs/GenerateWallet';
-export { default as SupportPage } from './Tabs/SupportPage';
