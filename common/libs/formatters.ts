460de1b6258f268df18d695782620fdd869dfacb
Fernand Lime <fernand.lime@gmail.com>
2019-12-10 04:32:52 +0100

    Fix Electron not running

---
+++ common/libs/formatters.ts
@@ -0,0 +1,7 @@
+export function stripHexPrefix(value: string) {
+  return value.replace('0x', '');
+}
+
+export function stripHexPrefixAndLower(value: string): string {
+  return stripHexPrefix(value).toLowerCase();
+}
