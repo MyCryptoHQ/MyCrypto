3c19b69e47f24b1136823080afe124308962e980
Frederik Bolding <frederik.bolding@gmail.com>
2019-12-11 00:12:17 +0100

    Removed unused types

--- common/typescript/redux-test-utils.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'redux-test-utils' {
-  import { Store } from 'react-redux';
-
-  export function createMockStore(testState: any): Store<any>;
-  export function createMockDispatch(testAction: any): any;
-}
