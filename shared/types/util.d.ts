34e08883d262cd83ac905587cc126a24e9c32052
Klemen Kunstek <klemen.kunstek1@gmail.com>
2019-12-17 21:49:41 +0100

    Remove unused code from 'shared' and 'electron-app' folders (#2914)

--- shared/types/util.d.ts
+++
@@ -1,7 +0,0 @@
-export type DeepPartial<T> = {
-  [P in keyof T]?: T[P] extends (infer U)[]
-    ? DeepPartial<U>[]
-    : T[P] extends ReadonlyArray<infer U>
-    ? ReadonlyArray<DeepPartial<U>>
-    : DeepPartial<T[P]>;
-};
