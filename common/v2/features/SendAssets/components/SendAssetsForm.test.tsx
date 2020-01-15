01c1ed30830a8b3ca056ceaf8fe71bd8dfad4bd3
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-01-15 15:04:50 +0100

    Configure jest for use with react-testing-library

--- common/v2/features/SendAssets/components/SendAssetsForm.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-
-import { render } from 'test-utils';
-
-test('Says Hello', async () => {
-  const { getByRole } = render(<button>Hello</button>);
-  expect(getByRole('button')).toHaveTextContent('Hello');
-});
