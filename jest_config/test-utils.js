fdf8fecfce5813c72de2bb4c574d604c98e08683
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-01-15 15:04:50 +0100

    Configure jest for use with react-testing-library

--- jest_config/test-utils.js
+++ jest_config/test-utils.js
@@ -1,22 +1,34 @@
 // Setup react-testing-library
 // https://testing-library.com/docs/react-testing-library/setup#custom-render
-
 import { render } from '@testing-library/react'
 
+// Mock for features used by react-slider
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () { },
+      removeListener: function () { }
+    };
+  };
+
+window.requestAnimationFrame =
+  window.requestAnimationFrame ||
+  function (callback) {
+    setTimeout(callback, 0);
+  };
+
+// Create custom renderer to share setup between component tests.
 const AllTheProviders = ({ children }) => {
-  return (
-    <>
-      
-    </>
-  )
+  return (<></>)
 }
 
-const customRender = (ui, options) =>
-  render(ui, { wrapper: AllTheProviders, ...options })
+const customRender = (ui, options) => render(ui, { wrapper: AllTheProviders, ...options })
+
 
 // re-export everything
 export * from '@testing-library/react'
 
 // override render method
 // export { customRender as render }
-// export { render
