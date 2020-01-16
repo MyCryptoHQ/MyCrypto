05e0b8ce267699cfde56a6cf3e1013d34b969e75
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-01-16 12:59:50 +0100

    Revert "Remove aws-sdk"
    This reverts commit 00532a7d615b3eb00fa222529c03b2b9d668859e.

--- webpack_config/electron-main.development.js
+++ webpack_config/electron-main.development.js
@@ -127,17 +127,10 @@ module.exports = {
     })
   ],
 
-  externals: [
-    {
-      'node-hid': 'commonjs node-hid',
-      usb: 'commonjs usb'
-    },
-    {
-      // The following line removes the need to add the aws-sdk in the package.json
-      // https://medium.com/@sheepsteak/excluding-the-aws-sdk-from-a-serverless-package-8dcad2f31954
-      "aws-sdk": "commonjs aws-sdk"
-    }
-  ],
+  externals: {
+    'node-hid': 'commonjs node-hid',
+    usb: 'commonjs usb'
+  },
 
   node: {
     __dirname: false,
