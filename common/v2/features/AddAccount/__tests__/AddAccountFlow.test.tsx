d3a678fbd9a041aab927c7a3cee0439e3b153570
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-01-28 13:56:42 +0100

    useUpdateEffect to remove superficial re-render and test
    useUpdateEffect to remove superficial re-render and test

---
+++ common/v2/features/AddAccount/__tests__/AddAccountFlow.test.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { MemoryRouter, Route, Switch } from 'react-router';
+import { simpleRender, fireEvent } from 'test-utils';
+
+import { AccountContext, NetworkContext, INetworkContext } from 'v2/services/Store';
+import { translateRaw } from 'v2/translations';
+import { ROUTE_PATHS, WALLETS_CONFIG } from 'v2/config';
+import { WalletId } from 'v2/types';
+import AddAccountFlow, { isValidWalletId } from 'v2/features/AddAccount/AddAccountFlow';
+import { IAccountContext } from 'v2/services/Store/Account/AccountProvider';
+
+/* Test helpers */
+describe('isValidWalletId()', () => {
+  it('Determines if a string is a valid WalletId', () => {
+    expect(isValidWalletId('WEB3')).toBe(true);
+    expect(isValidWalletId('web3')).toBe(false);
+    expect(isValidWalletId(undefined)).toBe(false);
+  });
+});
+
+/* Test components */
+describe('AddAccountFlow', () => {
+  let history: any;
+  let location: any;
+
+  const component = (path?: string) => (
+    <MemoryRouter initialEntries={path ? [path] : undefined}>
+      <NetworkContext.Provider
+        value={
+          ({
+            networks: []
+          } as unknown) as INetworkContext
+        }
+      >
+        <AccountContext.Provider
+          value={
+            ({
+              getAccountByAddressAndNetworkName: jest.fn()
+            } as unknown) as IAccountContext
+          }
+        >
+          <Switch>
+            <Route
+              path="*"
+              render={props => {
+                history = props.history;
+                location = props.location;
+                return <AddAccountFlow {...props} />;
+              }}
+            />
+          </Switch>
+        </AccountContext.Provider>
+      </NetworkContext.Provider>
+    </MemoryRouter>
+  );
+
+  const renderComponent = (pathToLoad?: string) => {
+    return simpleRender(component(pathToLoad));
+  };
+
+  test('Can render the component', () => {
+    const { getByText } = renderComponent();
+    const selector = translateRaw('DECRYPT_ACCESS').trim();
+    expect(getByText(selector)).toBeInTheDocument();
+  });
+
+  test('Can select a wallet and adds the walletId to the path', () => {
+    const { getByText } = renderComponent();
+    const config = WALLETS_CONFIG[WalletId.LEDGER_NANO_S];
+    const expectedPath = `${ROUTE_PATHS.ADD_ACCOUNT.path}`;
+    fireEvent.click(getByText(translateRaw(config.lid).trim()));
+    expect(location.pathname).toEqual(`${expectedPath}/${config.id.toLowerCase()}`);
+    expect(history.action).toEqual('REPLACE');
+    expect(getByText('Select Network')).toBeInTheDocument(); // Expect to see the Network selection step
+  });
+});
--- common/v2/vendor/react-use/index.ts
+++ common/v2/vendor/react-use/index.ts
@@ -8,4 +8,5 @@ export { default as usePromise } from './usePromise';
 export { default as useThrottleFn } from './useThrottleFn';
 export { default as useEvent } from './useEvent';
 export { default as useEffectOnce } from './useEffectOnce';
+export { default as useUpdateEffect } from './useUpdateEffect';
 export { isClient } from './util';
