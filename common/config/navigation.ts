59d77e77328beea8b55a13db0f312dffd75a887e
Andrey Galkin <andrey@futoin.org>
2020-02-02 07:28:23 +0200

    CHANGED: to hide ENS menu items

--- common/config/navigation.ts
+++ common/config/navigation.ts
@@ -24,10 +24,12 @@ export const navigationLinks: NavigationLink[] = [
     name: 'NAV_CONTRACTS',
     to: '/contracts'
   },
+  /*
   {
     name: 'NAV_ENS',
     to: '/ens'
   },
+*/
   {
     name: 'NAV_SIGN',
     to: '/sign-and-verify-message'
