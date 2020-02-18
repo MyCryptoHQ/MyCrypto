7e9647d65f6ee6b3859da35bb1204555213e03dc
Frederik Bolding <frederik.bolding@gmail.com>
2020-02-18 18:29:41 +0100

    Made sure values are strings for isTransactionFeeHigh

--- common/v2/features/SendAssets/components/SendAssetsForm.tsx
+++ common/v2/features/SendAssets/components/SendAssetsForm.tsx
@@ -531,9 +531,9 @@ export default function SendAssetsForm({ txConfig, onComplete }: IStepComponentP
                   values.amount,
                   getAssetRate(baseAsset || undefined) || 0,
                   isERC20Tx(values.asset),
-                  values.gasLimitField,
+                  values.gasLimitField.toString(),
                   values.advancedTransaction
-                    ? values.gasPriceField
+                    ? values.gasPriceField.toString()
                     : values.gasPriceSlider.toString()
                 ) && <InlineMessage value={translate('HIGH_TRANSACTION_FEE')} />}
               </fieldset>
