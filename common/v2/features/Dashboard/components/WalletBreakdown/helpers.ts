6711fe3260199f7db093ea1de36ffb8fd2073640
blurpesec <hahn.michael.f@gmail.com>
2020-02-18 05:55:42 -0800

    remove nested conditionals

--- common/v2/features/Dashboard/components/WalletBreakdown/helpers.ts
+++ common/v2/features/Dashboard/components/WalletBreakdown/helpers.ts
@@ -5,15 +5,12 @@ export const calculateShownIndex = (
   // selectedIndex is the index of the asset balance element that is being hovered over.
   // chartBalancesLength is the number of slices in the WalletBreakdown pie chart.
   if (numberOfPieChartSlices > selectedIndex && selectedIndex !== -1) {
-    // This will show the relevant onHovered slice.
+    // This will "select" the relevant onHovered slice.
     return selectedIndex;
-  } else {
-    if (numberOfPieChartSlices <= selectedIndex) {
-      // This will show the last element in the chartBalances array, which is the combined "other tokens" slice.
-      return numberOfPieChartSlices - 1;
-    } else {
-      // Default to showing the largest slice in the pie chart.
-      return 0;
-    }
+  } else if (numberOfPieChartSlices <= selectedIndex) {
+    // This will "select" the last element in the chartBalances array, which is the combined "other tokens" slice.
+    return numberOfPieChartSlices - 1;
   }
+  // Default to "selecting" the largest slice in the pie chart.
+  return 0;
 };
