11cb88ac761af9efc8d3ba0045f27dee84674f47
Amadej Pevec <amadej.pevec@gmail.com>
2020-03-04 17:23:57 +0100

    Fix charts bug (#3084)
    * Fix showing slices in chart not existing in list
    
    * Bump recharts lib, prevent missing labels due to interrupted chart animation
    
    * Enable chart animation when balances order changes

--- common/v2/features/Dashboard/components/WalletBreakdown/BreakdownChart.tsx
+++ common/v2/features/Dashboard/components/WalletBreakdown/BreakdownChart.tsx
@@ -1,6 +1,7 @@
-import React, { Component, SetStateAction, Dispatch } from 'react';
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
 import { PieChart, Pie, Sector, Cell, PieLabelRenderProps } from 'recharts';
+import * as R from 'ramda';
 
 import { Balance } from './types';
 import { SMALLEST_CHART_SHARE_SUPPORTED } from './WalletBreakdownView';
@@ -17,7 +18,12 @@ const MainWrapper = styled.div`
 interface BreakdownChartProps {
   balances: Balance[];
   selectedAssetIndex: number;
-  setSelectedAssetIndex: Dispatch<SetStateAction<number>>;
+  isChartAnimating: boolean;
+  shouldAnimate: boolean;
+  handleMouseOver(index: number): void;
+  handleMouseLeave(index: number): void;
+  setIsChartAnimating(isAnimating: boolean): void;
+  setShouldAnimate(shouldAnimate: boolean): void;
 }
 interface CustomLabelProps extends PieLabelRenderProps {
   cx: number;
@@ -42,103 +48,115 @@ interface ActiveSectionProps {
   activeIndex: number;
 }
 
-export default class BreakdownChart extends Component<BreakdownChartProps> {
-  public isChartAnimating = false;
+// Make active section bigger than the rest
+const ActiveSection = (props: ActiveSectionProps) => {
+  const { cx, cy, innerRadius, outerRadius, startAngle, endAngle, fill } = props;
+  return (
+    <g>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius + 20}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+    </g>
+  );
+};
 
-  // Make active section bigger than the rest
-  public ActiveSection = (props: ActiveSectionProps) => {
-    const { cx, cy, innerRadius, outerRadius, startAngle, endAngle, fill } = props;
-    return (
-      <g>
-        <Sector
-          cx={cx}
-          cy={cy}
-          innerRadius={innerRadius}
-          outerRadius={outerRadius + 20}
-          startAngle={startAngle}
-          endAngle={endAngle}
-          fill={fill}
-        />
-      </g>
-    );
-  };
+const CustomLabel = (
+  labelProps: CustomLabelProps,
+  selectedAssetIndex: number,
+  handleMouseOver: BreakdownChartProps['handleMouseOver']
+): React.ReactElement<any> => {
+  const { cx, cy, midAngle, innerRadius, outerRadius, percent, ticker, index } = labelProps;
 
-  public CustomLabel = (labelProps: CustomLabelProps): React.ReactElement<any> => {
-    const { cx, cy, midAngle, innerRadius, outerRadius, percent, ticker, index } = labelProps;
-    const { selectedAssetIndex } = this.props;
+  const isActiveSection = selectedAssetIndex === index;
+  const centerOffset = isActiveSection ? 0.7 : 0.6;
 
-    const isActiveSection = selectedAssetIndex === index;
-    const centerOffset = isActiveSection ? 0.7 : 0.6;
+  // Position the label in the center of the pie section
+  const radian = Math.PI / 180;
+  const radius = (innerRadius + (outerRadius - innerRadius)) * centerOffset;
+  const x = percent < 1 ? cx + radius * Math.cos(-midAngle * radian) : cx;
+  const y = percent < 1 ? cy + radius * Math.sin(-midAngle * radian) : cy;
 
-    // Position the label in the center of the pie section
-    const radian = Math.PI / 180;
-    const radius = (innerRadius + (outerRadius - innerRadius)) * centerOffset;
-    const x = percent < 1 ? cx + radius * Math.cos(-midAngle * radian) : cx;
-    const y = percent < 1 ? cy + radius * Math.sin(-midAngle * radian) : cy;
+  // Don't show the label if percent is 3%
+  return percent > SMALLEST_CHART_SHARE_SUPPORTED ? (
+    <text
+      onMouseOver={() => handleMouseOver(index)}
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={'middle'}
+      dominantBaseline="central"
+    >
+      {ticker}
+    </text>
+  ) : (
+    <text />
+  );
+};
 
-    // Don't show the label if percent is 3%
-    return percent > SMALLEST_CHART_SHARE_SUPPORTED ? (
-      <text x={x} y={y} fill="white" textAnchor={'middle'} dominantBaseline="central">
-        {ticker}
-      </text>
-    ) : (
-      <text />
-    );
-  };
+// Generate colors for all sections of the pie
+const generateColors = (length: number) => {
+  const startColor = { r: 223, g: 215, b: 245 };
+  const endColor = { r: 166, g: 130, b: 255 };
 
-  // Generate colors for all sections of the pie
-  public generateColors = (length: number) => {
-    const startColor = { r: 223, g: 215, b: 245 };
-    const endColor = { r: 166, g: 130, b: 255 };
+  const colors = [];
+  const step = length > 1 ? 1 / (length - 1) : 0;
 
-    const colors = [];
-    const step = length > 1 ? 1 / (length - 1) : 0;
+  for (let i = 0; i < length; i++) {
+    const ratio = i * step;
+    const r = Math.ceil(startColor.r * ratio + endColor.r * (1 - ratio));
+    const g = Math.ceil(startColor.g * ratio + endColor.g * (1 - ratio));
+    const b = Math.ceil(startColor.b * ratio + endColor.b * (1 - ratio));
+    colors.push('#' + r.toString(16) + g.toString(16) + b.toString(16));
+  }
+  return colors;
+};
 
-    for (let i = 0; i < length; i++) {
-      const ratio = i * step;
-      const r = Math.ceil(startColor.r * ratio + endColor.r * (1 - ratio));
-      const g = Math.ceil(startColor.g * ratio + endColor.g * (1 - ratio));
-      const b = Math.ceil(startColor.b * ratio + endColor.b * (1 - ratio));
-      colors.push('#' + r.toString(16) + g.toString(16) + b.toString(16));
-    }
-    return colors;
-  };
+const BreakdownChart = React.memo(
+  ({
+    balances,
+    selectedAssetIndex,
+    handleMouseOver,
+    handleMouseLeave,
+    setIsChartAnimating,
+    shouldAnimate,
+    setShouldAnimate
+  }: BreakdownChartProps) => {
+    const COLORS = useMemo(() => generateColors(balances.length), [balances.length]);
 
-  public handleMouseEnter = (_: any, index: number) => {
-    // Ignore mouse enter while animating the chart
-    if (this.isChartAnimating) return;
-
-    this.props.setSelectedAssetIndex(index);
-  };
-
-  public handleMouseLeave = (_: any) => {
-    if (this.isChartAnimating) return;
-
-    this.props.setSelectedAssetIndex(-1);
-  };
-
-  public render() {
-    const { balances, selectedAssetIndex } = this.props;
-    const COLORS = this.generateColors(balances.length);
     return (
       <MainWrapper>
-        <PieChart width={400} height={350}>
+        <PieChart
+          width={400}
+          height={350}
+          onMouseLeave={() => handleMouseLeave(-1)}
+          onMouseEnter={() => handleMouseLeave(-1)}
+        >
           <Pie
+            isAnimationActive={shouldAnimate}
             activeIndex={selectedAssetIndex}
-            activeShape={this.ActiveSection}
+            activeShape={ActiveSection}
             data={balances}
             cx={200}
             cy={200}
             innerRadius={0}
             outerRadius={110}
-            label={p => this.CustomLabel(p as CustomLabelProps)}
+            label={p => CustomLabel(p as CustomLabelProps, selectedAssetIndex, handleMouseOver)}
             labelLine={false}
             dataKey="fiatValue"
-            onMouseEnter={this.handleMouseEnter}
-            onMouseLeave={this.handleMouseLeave}
+            onMouseEnter={(_: any, index: number) => handleMouseOver(index)}
+            onMouseLeave={() => handleMouseLeave(-1)}
             animationDuration={800}
-            onAnimationStart={() => (this.isChartAnimating = true)}
-            onAnimationEnd={() => (this.isChartAnimating = false)}
+            onAnimationStart={() => setIsChartAnimating(true)}
+            onAnimationEnd={() => {
+              setShouldAnimate(false);
+              setIsChartAnimating(false);
+            }}
           >
             {balances.map((entry, index) => (
               <Cell fill={COLORS[index]} stroke={COLORS[index]} key={entry.name} />
@@ -147,5 +165,8 @@ export default class BreakdownChart extends Component<BreakdownChartProps> {
         </PieChart>
       </MainWrapper>
     );
-  }
-}
+  },
+  (prev, next) => next.isChartAnimating || R.equals(prev, next)
+);
+
+export default BreakdownChart;
