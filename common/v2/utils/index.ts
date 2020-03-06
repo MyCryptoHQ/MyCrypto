3b64ab66429b5525dc80f0203c3f8a577b375e7a
blurpesec <hahn.michael.f@gmail.com>
2020-03-05 17:03:12 -0800

    added some defizap protocol filters

--- common/v2/utils/index.ts
+++ common/v2/utils/index.ts
@@ -50,3 +50,5 @@ export { withContext } from './withContext';
 export { getWeb3Config, isWeb3Wallet } from './web3';
 export { toArray } from './toArray';
 export { objToString } from './objToString';
+export * from './constants';
+export * from './assetFilters';
