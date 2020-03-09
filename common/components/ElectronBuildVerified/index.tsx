755e68f5a233a8f96b7cbdb9be6a3c72353d2db7
409H <409H@users.noreply.github.com>
2020-03-09 21:57:28 +0000

    Code changes from Maartens comments

--- common/components/ElectronBuildVerified/index.tsx
+++ common/components/ElectronBuildVerified/index.tsx
@@ -4,68 +4,76 @@ import { VERSION } from 'config';
 import './Banner.scss';
 
 interface BannerState {
-    displayBanner: boolean;
+  displayBanner: boolean;
 }
 
 interface BannerProps {
-    versionNow: string;
+  versionNow: string;
 }
 
-const STORAGE_NAME = "ElectronBuildVerified";
+const STORAGE_NAME = 'ElectronBuildVerified';
 
 export default class ElectronBuildVerified extends Component<BannerProps, BannerState> {
-    public state: State = {
-        displayBanner: this.isBannerPrompted()
-    };
+  public state: State = {
+    displayBanner: this.isBannerPrompted()
+  };
 
-    constructor(props: BannerProps) {
-        super(props);
+  constructor(props: BannerProps) {
+    super(props);
+  }
+
+  public render() {
+    const { displayBanner } = this.state;
+    if (displayBanner) {
+      return (
+        <div className="BannerContainer">
+          <input type="checkbox" onChange={e => this.handleCheck(e)} />{' '}
+          <span>
+            I have{' '}
+            <a href="https://support.mycrypto.com/staying-safe/verifying-authenticity-of-desktop-app">
+              verified that this build ({VERSION})
+            </a>{' '}
+            is signed by MyCrypto and I understand the risks of not verifying the build.
+          </span>
+        </div>
+      );
     }
 
-    public render() {
-        const { displayBanner } = this.state;
-        if(displayBanner) {
-            return(<div className="BannerContainer">
-                <input
-                    type="checkbox"
-                    onChange={(e) => this.handleCheck(e)}
-                />{' '}
-                <span>
-                    I have{' '}
-                    <a href="https://support.mycrypto.com/staying-safe/verifying-authenticity-of-desktop-app">verified that this build ({VERSION})</a>{' '}
-                    is signed by MyCrypto and I understand the risks of not verifying the build.
-                </span>
-            </div>)
+    return null;
+  }
+
+  componentWillMount() {
+    this.setState({ displayBanner: this.isBannerPrompted() });
+  }
+
+  private isBannerPrompted() {
+    let storageVerified;
+    if (localStorage.getItem(STORAGE_NAME) !== null) {
+      try {
+        storageVerified = localStorage.getItem(STORAGE_NAME);
+        const objStorage = JSON.parse(storageVerified);
+        if (objStorage.versionChecked === this.props.versionNow) {
+          return false;
         }
-
-        return null;
+      } catch {
+        //
+      }
     }
 
-    componentWillMount() {
-        this.setState({ displayBanner: this.isBannerPrompted() })
-    }
+    return true;
+  }
 
-    private isBannerPrompted() {
-        let storageVerified;
-        if(localStorage.getItem(STORAGE_NAME) !== null) {
-            try {
-                storageVerified = localStorage.getItem(STORAGE_NAME);
-                const objStorage = JSON.parse(storageVerified);
-                if(objStorage.versionChecked === this.props.versionNow) {
-                    return false;
-                }
-            } catch {
-                //
-            }
-        } 
-
-        return true;
+  private handleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      this.setState({ displayBanner: false });
+      localStorage.setItem(
+        STORAGE_NAME,
+        JSON.stringify({
+          versionChecked: this.props.versionNow,
+          ack: true,
+          ts: Math.floor(Date.now() / 1000)
+        })
+      );
     }
-
-    private handleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if(event.target.checked) {
-            this.setState({ displayBanner: false })
-            localStorage.setItem(STORAGE_NAME, JSON.stringify({"versionChecked": this.props.versionNow, "ack": true, "ts": Math.floor(Date.now() / 1000)}))
-        }
-    }
+  };
 }
