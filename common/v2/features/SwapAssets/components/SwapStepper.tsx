7592ec6075f4fe40c4c3b58173a7d328ebc8cebe
Frederik Bolding <frederik.bolding@gmail.com>
2020-03-10 19:53:02 +0100

    Implemented new SwapStepper design

--- common/v2/features/SwapAssets/components/SwapStepper.tsx
+++ common/v2/features/SwapAssets/components/SwapStepper.tsx
@@ -4,9 +4,10 @@ import VerticalStepper from 'v2/components/VerticalStepper';
 import { ISwapAsset } from '../types';
 import { SwapFromToDiagram } from 'v2/components/TransactionFlow/displays';
 
-import step1SVG from 'assets/images/icn-unlock-wallet.svg';
-import step2SVG from 'assets/images/icn-sent.svg';
+import step1SVG from 'assets/images/icn-send.svg';
+import step2SVG from 'assets/images/icn-receive.svg';
 import { translateRaw } from 'v2/translations';
+import { Typography } from 'v2/components';
 
 interface Props {
   currentStep: number;
@@ -21,30 +22,31 @@ export default function SwapStepper(props: Props) {
   const { currentStep, fromAsset, toAsset, fromAmount, toAmount, onClick } = props;
 
   const tokenStep = {
-    title: `Enable ${fromAsset.symbol}`,
+    title: translateRaw('APPROVE_SWAP'),
     icon: step1SVG,
     content: translateRaw('SWAP_STEP1_TEXT', { $token: fromAsset.symbol }),
-    buttonText: `Activate ${fromAsset.symbol}`,
+    buttonText: translateRaw('APPROVE_SWAP'),
     onClick
   };
 
   const transferStep = {
-    title: 'Complete Transaction',
+    title: translateRaw('COMPLETE_SWAP'),
     icon: step2SVG,
-    content: (
-      <>
-        <text>More copy here</text>
-        <SwapFromToDiagram
-          fromSymbol={fromAsset.symbol}
-          toSymbol={toAsset.symbol}
-          fromAmount={fromAmount}
-          toAmount={toAmount}
-        />
-      </>
-    ),
+    content: translateRaw('SWAP_STEP2_TEXT'),
     buttonText: translateRaw('CONFIRM_TRANSACTION'),
     onClick
   };
 
-  return <VerticalStepper currentStep={currentStep} steps={[tokenStep, transferStep]} />;
+  return (
+    <div>
+      <Typography>{translateRaw('SWAP_INTRO')}</Typography>
+      <SwapFromToDiagram
+        fromSymbol={fromAsset.symbol}
+        toSymbol={toAsset.symbol}
+        fromAmount={fromAmount}
+        toAmount={toAmount}
+      />
+      <VerticalStepper currentStep={currentStep} steps={[tokenStep, transferStep]} />
+    </div>
+  );
 }
