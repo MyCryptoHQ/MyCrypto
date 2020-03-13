b33481765c1901732c91b8a616d5cbebd044cd9d
Frederik Bolding <frederik.bolding@gmail.com>
2020-03-13 16:35:30 +0100

    Multi TX Receipt (#3109)
    * Initial MultiTxReceipt implementation
    
    * Cleaned up slightly
    
    * More clean up
    
    * Trigger CI

---
+++ common/v2/components/TransactionFlow/MultiTxReceipt.stories.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { fTxReceipts, fTxConfigs } from '@fixtures';
+import { ITxStatus, ITxType, TSymbol } from 'v2/types';
+import { noOp, bigify } from 'v2/utils';
+
+import MultiTxReceipt, { TxReceiptData } from './MultiTxReceipt';
+import { SwapDisplayData, ISwapAsset } from 'v2/features/SwapAssets/types';
+
+// Define props
+const timestamp = 1583266291;
+const resetFlow = noOp;
+const DAI: ISwapAsset = { name: 'DAI Stablecoin v2.0', symbol: 'DAI' as TSymbol };
+const ETH: ISwapAsset = { name: 'Ethereum', symbol: 'ETH' as TSymbol };
+const swapDisplay: SwapDisplayData = {
+  fromAsset: DAI,
+  toAsset: ETH,
+  fromAmount: bigify('10'),
+  toAmount: bigify('0.5')
+};
+const transactions: TxReceiptData[] = [
+  {
+    label: 'Approve',
+    config: fTxConfigs[0],
+    receipt: fTxReceipts[0],
+    status: ITxStatus.SUCCESS,
+    timestamp
+  },
+  {
+    label: 'Send Transaction',
+    config: fTxConfigs[1],
+    receipt: fTxReceipts[1],
+    status: ITxStatus.PENDING,
+    timestamp
+  }
+];
+
+export default { title: 'MultiTxReceipt' };
+
+export const swapTransactionReceipt = () => (
+  <div className="sb-container" style={{ maxWidth: '620px' }}>
+    <MultiTxReceipt
+      txType={ITxType.SWAP}
+      transactions={transactions}
+      resetFlow={resetFlow}
+      onComplete={resetFlow}
+      swapDisplay={swapDisplay}
+    />
+  </div>
+);
--- common/v2/features/SwapAssets/components/SwapTransactionReceipt.tsx
+++ common/v2/features/SwapAssets/components/SwapTransactionReceipt.tsx
@@ -3,7 +3,7 @@ import * as R from 'ramda';
 
 import { translateRaw } from 'v2/translations';
 import { AssetContext } from 'v2/services';
-import { TxReceipt } from 'v2/components/TransactionFlow';
+import { TxReceipt, MultiTxReceipt } from 'v2/components/TransactionFlow';
 import { StoreAccount, ITxType } from 'v2/types';
 
 import { SwapDisplayData, IAssetPair, TxEnveloppe } from '../types';
@@ -45,11 +45,28 @@ export default function SwapTransactionReceipt({
     };
   });
 
-  return (
+  const transactionsData = transactions.map((t, idx) => ({
+    label: t.label,
+    config: txConfigs[idx],
+    receipt: txReceipts[idx],
+    status: t.status,
+    timestamp: 0 //TODO
+  }));
+
+  return txReceipts.length === 1 ? (
     <TxReceipt
       txType={ITxType.SWAP}
       txReceipt={txReceipts[0]}
       txConfig={txConfigs[0]}
+      completeButtonText={translateRaw('SWAP_START_ANOTHER')}
+      resetFlow={onSuccess}
+      onComplete={onSuccess}
+      swapDisplay={swapDisplay}
+    />
+  ) : (
+    <MultiTxReceipt
+      txType={ITxType.SWAP}
+      transactions={transactionsData}
       completeButtonText={translateRaw('SWAP_START_ANOTHER')}
       resetFlow={onSuccess}
       onComplete={onSuccess}
