a156040e2756c3e02357d78a01dc792c0e4ef085
Frederik Bolding <frederik.bolding@gmail.com>
2020-03-17 11:50:58 +0100

    Added proper icons to MembershipPanel

--- common/v2/features/Dashboard/components/MembershipPanel.tsx
+++ common/v2/features/Dashboard/components/MembershipPanel.tsx
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
+import * as R from 'ramda';
 
 import { StoreContext } from 'v2/services';
 import { DashboardPanel, Typography, Button, Link } from 'v2/components';
 import { FONT_SIZE, COLORS, SPACING } from 'v2/theme';
 import { translateRaw } from 'v2/translations';
 import { ROUTE_PATHS } from 'v2/config';
+import defaultIcon from 'common/assets/images/membership/membership-none.svg';
+import { MEMBERSHIP_CONFIG } from 'v2/features/PurchaseMembership/config';
 
 const SDashboardPanel = styled(DashboardPanel)<{ isMyCryptoMember: boolean }>`
   display: flex;
@@ -55,15 +58,25 @@ const InvertedButton = styled(Button)`
   border: 2px solid ${COLORS.BLUE_GREEN};
 `;
 
+const Icon = styled.img<{ isMyCryptoMember: boolean }>`
+  ${props => !props.isMyCryptoMember && 'opacity: 0.25;'}
+`;
+
 type Props = RouteComponentProps<{}>;
 function MembershipPanel({ history }: Props) {
-  const { isMyCryptoMember } = useContext(StoreContext);
+  const { isMyCryptoMember, memberships } = useContext(StoreContext);
+
+  const allMemberships = R.uniq(R.flatten(Object.values(memberships)));
+  const membership =
+    allMemberships.length > 0 ? allMemberships[allMemberships.length - 1] : undefined;
+
+  const icon = membership ? MEMBERSHIP_CONFIG[membership].icon : defaultIcon;
 
   return (
     <SDashboardPanel isMyCryptoMember={isMyCryptoMember} padChildren={true}>
       <Wrapper>
         <ImageWrapper>
-          <img src="https://via.placeholder.com/100x100" />
+          <Icon isMyCryptoMember={isMyCryptoMember} src={icon} />
         </ImageWrapper>
         <TextWrapper isMyCryptoMember={isMyCryptoMember}>
           <Header as="div">{translateRaw('MEMBERSHIP')}</Header>
