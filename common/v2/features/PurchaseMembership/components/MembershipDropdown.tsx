33e933703f51e1fc8824d8e94920d96fac8ec6d8
Frederik Bolding <frederik.bolding@gmail.com>
2020-03-29 13:24:51 +0200

    Fixed some copy and styling issues

--- common/v2/features/PurchaseMembership/components/MembershipDropdown.tsx
+++ common/v2/features/PurchaseMembership/components/MembershipDropdown.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
+import { OptionComponentProps } from 'react-select';
 
 import { translateRaw } from 'v2/translations';
 import { Typography, Dropdown } from 'v2/components';
+import { COLORS, FONT_SIZE, SPACING } from 'v2/theme';
 import { MEMBERSHIP_CONFIG, IMembershipConfig } from '../config';
-import { OptionComponentProps } from 'react-select';
 
 const SContainer = styled('div')`
   display: flex;
   flex-direction: row;
   padding: 12px;
+`;
+
+const DiscountTypography = styled(Typography)`
+  color: ${COLORS.BLUE_BRIGHT};
+  font-size: ${FONT_SIZE.XS};
+  margin-left: ${SPACING.XS};
 `;
 
 interface Props {
@@ -36,9 +43,11 @@ function MembershipDropdown({ name, value, onSelect }: Props) {
 class MembershipOption extends React.PureComponent<OptionComponentProps> {
   public render() {
     const { option, onSelect } = this.props;
+    const value = (option.value as unknown) as IMembershipConfig;
     return (
       <SContainer onClick={() => onSelect && onSelect(option, null)}>
         <Typography value={option.label} />
+        <DiscountTypography value={value.discountNotice} />
       </SContainer>
     );
   }
