db2b1822eecf1fd6aa0f7e2525ac930822182b35
409H <409H@users.noreply.github.com>
2020-04-01 17:38:32 +0100

    Added logic to add margintop to fix css issue on ShapeShiftDisabled

--- common/components/ui/Warning.tsx
+++ common/components/ui/Warning.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './Warning.scss';
+import { isDesktop } from 'v2/utils';
 
 interface WarningProps {
   highlighted?: boolean;
@@ -8,9 +9,10 @@ interface WarningProps {
 
 const Warning: React.SFC<WarningProps> = ({ highlighted, children }) => {
   const className = `Warning ${highlighted ? 'highlighted' : ''}`;
+  const addMarginTop = isDesktop() ? 0 : "1em"; //Add extra margin to fix a small CSS issue of the warning being too close to the Banner
 
   return (
-    <section className={className}>
+    <section className={className} style={{'marginTop': addMarginTop}}>
       <section className="Warning-icon">
         <i className="fa fa-exclamation-triangle" />
       </section>
