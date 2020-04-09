9134e8a394883ee057da11b60d73cf892ac7a1e1
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-04-09 18:24:48 +0200

    Include link to add account in header

--- common/v2/features/Layout/Header/constants.ts
+++ common/v2/features/Layout/Header/constants.ts
@@ -28,6 +28,11 @@ export const links = [
         to: APP_ROUTES_OBJECT.SWAP.path,
         enabled: APP_ROUTES_OBJECT.SWAP.enabled,
         title: translateRaw('SWAP')
+      },
+      {
+        to: APP_ROUTES_OBJECT.ADD_ACCOUNT.path,
+        enabled: APP_ROUTES_OBJECT.ADD_ACCOUNT.enabled,
+        title: translateRaw('ADD_ACCOUNT')
       }
     ]
   },
