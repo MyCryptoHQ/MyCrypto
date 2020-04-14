e867a4f909fafeb417fd3a412832d4bee6f47aa3
Frederik Bolding <frederik.bolding@gmail.com>
2020-04-14 23:19:59 +0200

    Added missing private key signing

--- common/v2/components/SignTransactionWallets/PrivateKey.tsx
+++ common/v2/components/SignTransactionWallets/PrivateKey.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { stripHexPrefix } from 'ethjs-util';
-import { Button } from '@mycrypto/ui';
+import { ethers } from 'ethers';
 
-import { TogglablePassword, Input } from 'v2/components';
+import { TogglablePassword, Input, Button } from 'v2/components';
 import { isValidPrivKey, isValidEncryptedPrivKey } from 'v2/services/EthService';
 import { ISignComponentProps } from 'v2/types';
 import translate, { translateRaw } from 'v2/translations';
@@ -99,7 +99,7 @@ export default class SignTransactionPrivateKey extends Component<
             {translateRaw('SIGN_TX_EXPLANATION')}
           </div>
           <div className="SignTransactionPrivateKey-footer">
-            <Button className="SignTransactionPrivateKey-button">
+            <Button onClick={this.unlock} className="SignTransactionPrivateKey-button">
               {translateRaw('DEP_SIGNTX')}
             </Button>
             {WALLETS_CONFIG.PRIVATE_KEY.helpLink && (
@@ -144,5 +144,12 @@ export default class SignTransactionPrivateKey extends Component<
   private unlock = async (e: React.SyntheticEvent<HTMLElement>) => {
     e.preventDefault();
     e.stopPropagation();
+    const { rawTransaction } = this.props;
+    const { key } = this.state;
+
+    // TODO: Handle encrypted private keys
+    const signerWallet = new ethers.Wallet(key);
+    const rawSignedTransaction: any = await signerWallet.sign(rawTransaction);
+    this.props.onSuccess(rawSignedTransaction);
   };
 }
