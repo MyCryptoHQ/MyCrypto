fec0787a8ebfdd729ea9cec4283b68293afb5d8a
blurpesec <hahn.michael.f@gmail.com>
2020-04-20 11:16:34 -0700

    swapped generic to static asset

--- common/v2/components/AssetIcon.tsx
+++ common/v2/components/AssetIcon.tsx
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { RatesContext } from 'v2/services';
+import genericIcon from 'assets/generic.svg'
 
 // Relies on https://github.com/atomiclabs/cryptocurrency-icons using fixed version number through CDN
 // @TODO: We should be using our own sprite served over a trusted CDN
 const baseURL = 'https://mycryptoapi.com/api/v1/images';
-const genericURL = 'https://cdn.mycryptoapi.com/v1/icons/generic.svg';
 
 function buildUrl(uuid: string) {
   return `${baseURL}/${uuid}.png`;
@@ -15,7 +15,7 @@ function getIconUrl(uuid: string, assetIconsManifest: any) {
   const assetIconsManifestEntry =
     assetIconsManifest && assetIconsManifest[uuid] && assetIconsManifest[uuid].coinGeckoId;
   const curr = assetIconsManifest ? assetIconsManifestEntry : false;
-  return curr ? buildUrl(uuid) : genericURL;
+  return curr ? buildUrl(uuid) : genericIcon;
 }
 
 const SImg = styled('img')`
