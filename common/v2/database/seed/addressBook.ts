b203ce7da26871a0a40f991b7eb149b4f26465fc
blurpesec <hahn.michael.f@gmail.com>
2020-04-20 16:31:00 -0700

    add mycrypto donation addr to address book

--- common/v2/database/seed/addressBook.ts
+++ common/v2/database/seed/addressBook.ts
@@ -14,3 +14,12 @@ export const addressBook: { [key in string]: AddressBook } = {
     network: 'Ethereum'
   }
 };
+
+export const defaultAddressBook: { [key in string]: AddressBook } = {
+  'a1acf1f2-0380-5bd6-90c3-2b4a0974a6fe': {
+    label: 'MyCrypto Tip Jar',
+    address: '0x4bbeEB066eD09B7AEd07bF39EEe0460DFa261520',
+    notes: 'Toss us a coin!',
+    network: 'Ethereum'
+  }
+};
