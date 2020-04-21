fcba8faaa23eb2ffaba010d980931e9039060da1
HARRY DENLEY <HARRY.DENLEY@OUTLOOK.COM>
2020-04-21 18:37:48 +0100

    Removed commented code from 0x removal

--- common/v2/features/BuyAndExchange/ZeroEx/ZeroEx.tsx
+++ common/v2/features/BuyAndExchange/ZeroEx/ZeroEx.tsx
@@ -1,46 +1,11 @@
 import React, { Component } from 'react';
-
-// import { isDesktop } from 'v2/utils';
-// import { replaceZeroExContainer } from './helpers';
-// import {
-//   ZEROEX_CONTAINER_ID,
-//   ZEROEX_RELAYER,
-//   ZEROEX_FEE_ADDRESS,
-//   ZEROEX_FEE_PERCENTAGE
-// } from './constants';
 import './ZeroEx.scss';
 
 // Legacy
-// import TabSection from 'containers/TabSection';
 import ZeroExDisabled from './ZeroExDisabled';
 
 export default class ZeroEx extends Component {
-  public componentDidMount() {
-    // if (!isDesktop()) {
-    //   this.renderZeroExInstant();
-    // }
-  }
-
   public render() {
     return <ZeroExDisabled />;
   }
-
-  // private renderZeroExInstant() {
-  //   const { zeroExInstant } = window as any;
-
-  //   if (zeroExInstant) {
-  //     zeroExInstant.render(
-  //       {
-  //         orderSource: ZEROEX_RELAYER,
-  //         affiliateInfo: {
-  //           feeRecipient: ZEROEX_FEE_ADDRESS,
-  //           feePercentage: ZEROEX_FEE_PERCENTAGE
-  //         }
-  //       },
-  //       `#${ZEROEX_CONTAINER_ID}`
-  //     );
-
-  //     replaceZeroExContainer();
-  //   }
-  // }
 }
