8825a52124894ff9dc341333f7c089bc51b232c0
HARRY DENLEY <HARRY.DENLEY@OUTLOOK.COM>
2020-05-12 00:44:25 +0100

    Removed imports that are now gone

--- common/v2/features/BuyAndExchange/ShapeShift/index.ts
+++ common/v2/features/BuyAndExchange/ShapeShift/index.ts
@@ -1,4 +1,2 @@
 export { default as ShapeShift } from './ShapeShift';
-export { default as ShapeShiftAuthorization } from './ShapeShiftAuthorization';
-export { default as ShapeShiftSend } from './ShapeShiftSend';
 export { default as ShapeShiftDisabled } from './ShapeShiftDisabled';
--- common/v2/services/ShapeShift/index.ts
+++ common/v2/services/ShapeShift/index.ts
@@ -1,4 +1,3 @@
-export { default as ShapeShiftService } from './ShapeShift';
 export * from './constants';
 export * from './helpers';
 export * from './types';
