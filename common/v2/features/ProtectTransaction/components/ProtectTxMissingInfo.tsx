63d9e8745efcbadc38e52a9d94b5c613eea47176
Frederik Bolding <frederik.bolding@gmail.com>
2020-05-11 14:13:37 +0200

    Fixed review comments

--- common/v2/features/ProtectTransaction/components/ProtectTxMissingInfo.tsx
+++ common/v2/features/ProtectTransaction/components/ProtectTxMissingInfo.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
 import { SPACING } from 'v2/theme';
+import { translateRaw } from 'v2/translations';
 
 import infoSVG from 'assets/images/icn-info-blue.svg';
-import { translateRaw } from 'v2/translations';
 
 const Wrapper = styled.div`
   margin-top: ${SPACING.MD};
@@ -16,7 +16,7 @@ const Content = styled.p`
 export const ProtectTxMissingInfo: FC = () => {
   return (
     <Wrapper>
-      <img height={SPACING.LG} width={SPACING.LG} src={infoSVG} />
+      <img height="42px" width="42px" src={infoSVG} />
       <h4>{translateRaw('MISSING_INFORMATION')}</h4>
       <Content>{translateRaw('PTX_MISSING_INFORMATION_DESCRIPTION')}</Content>
     </Wrapper>
