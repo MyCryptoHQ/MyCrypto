62d7150a35ab7b6b155f90e79366b538173f911a
HARRY DENLEY <HARRY.DENLEY@OUTLOOK.COM>
2020-05-12 01:18:23 +0100

    Modified copy to point users to beta

--- common/v2/features/BuyAndExchange/ShapeShift/ShapeShiftDisabled.tsx
+++ common/v2/features/BuyAndExchange/ShapeShift/ShapeShiftDisabled.tsx
@@ -8,9 +8,14 @@ export default class ShapeShiftDisabled extends Component {
   public render() {
     return (
       <TabSection>
-          <Warning>
-            ShapeShift functionality is currently unavailable on the MyCrypto App.
-          </Warning>
+        <Warning>
+          ShapeShift functionality is currently unavailable on the MyCrypto App. <br />
+          To continue with your swap, please use the new and improved swap functionality on the
+          MyCrypto Beta! <br />
+          <a href="https://beta.mycrypto.com/swap" target="_blank" rel="noreferrer">
+            Continue Swap &raquo;
+          </a>
+        </Warning>
       </TabSection>
     );
   }
