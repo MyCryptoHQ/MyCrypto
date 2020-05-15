a27090adf3b9bc2bff1f65312fa384e4a47b7bc4
Matej Lubej <matej.lubej@poviolabs.com>
2020-05-15 13:25:41 +0200

    Purchace membership remove reexports

--- src/features/PurchaseMembership/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as MembershipPurchaseForm } from './MembershipPurchaseForm';
-export { default as ConfirmMembershipPurchase } from './ConfirmMembershipPurchase';
-export { default as ConfirmMembershipPurchaseMultiTx } from './ConfirmMembershipPurchaseMultiTx';
-export { default as MembershipPurchaseReceipt } from './MembershipPurchaseReceipt';
-export { default as MembershipPlanCard } from './MembershipPlanCard';
