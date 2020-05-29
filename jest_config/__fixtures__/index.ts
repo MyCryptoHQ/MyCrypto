d502aacc7e9062d7097a6b2574017957bcbb458c
blurpesec <hahn.michael.f@gmail.com>
2020-05-28 21:44:47 -0700

    added index exports and removed commented stuff

--- jest_config/__fixtures__/index.ts
+++ jest_config/__fixtures__/index.ts
@@ -6,8 +6,25 @@ export { fSettings } from './settings';
 export { default as fTxConfig } from './txConfig.json';
 export { default as fTxConfigs } from './txConfigs.json';
 export { default as fTxReceipt } from './txReceipt.json';
-export { default as fERC20TxReceipt } from './erc20TxReceipt.json';
 export { default as fTxReceipts } from './txReceipts.json';
 export { default as fTxReceiptProvider } from './txReceiptProvider.json';
 export { default as fTxParcels } from './txParcels';
 export { default as customNodeConfig } from './customNode';
+
+// Non-Web3 ERC20 Tx Items
+export { default as fERC20NonWeb3TxConfig } from './erc20NonWeb3TxConfig.json';
+export { default as fERC20NonWeb3TxReceipt } from './erc20NonWeb3TxReceipt.json';
+
+// Web3 ERC20 Tx Items
+export { default as fERC20Web3TxConfig } from './erc20Web3TxConfig.json';
+export { default as fERC20Web3TxReceipt } from './erc20Web3TxReceipt.json';
+
+// Non-Web3 ETH Tx Items
+export { default as fETHNonWeb3TxConfig } from './ethNonWeb3TxConfig.json';
+export { default as fETH20NonWeb3TxResponse } from './ethNonWeb3TxResponse.json';
+export { default as fETH20NonWeb3TxReceipt } from './ethNonWeb3TxReceipt.json';
+
+// Web3 ETH Tx Items
+export { default as fETHWeb3TxConfig } from './ethWeb3TxConfig.json';
+export { default as fETH20Web3TxResponse } from './ethWeb3TxResponse.json';
+export { default as fETH20Web3TxReceipt } from './ethWeb3TxReceipt.json';
