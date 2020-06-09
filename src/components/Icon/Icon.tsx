145071c32003ffa6e19426ab3f1c398080634ea4
Luit Hollander <MrLuit@users.noreply.github.com>
2020-06-09 19:19:58 +0200

    Small tweaks

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,40 +2,23 @@ import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
 import InlineSVG, { IProps as InlineSVGProps } from 'react-inlinesvg';
 
-import back from '@assets/icons/actions/back.svg';
-import expand from '@assets/icons/actions/expand.svg';
-
-import logoMyCrypto from '@assets/icons/brand/logo.svg';
-import logoMyCryptoText from '@assets/icons/brand/logo-text.svg';
-
-import coinmarketcap from '@assets/icons/social/coinmarketcap.svg';
-import facebook from '@assets/icons/social/facebook.svg';
-import github from '@assets/icons/social/github.svg';
-import reddit from '@assets/icons/social/reddit.svg';
-import slack from '@assets/icons/social/slack.svg';
-import telegram from '@assets/icons/social/telegram.svg';
-import twitter from '@assets/icons/social/twitter.svg';
-
-import website from '@assets/icons/website.svg';
-import whitepaper from '@assets/icons/whitepaper.svg';
-
 const icons = {
-  back,
-  expand,
+  back: require('@assets/icons/actions/back.svg'),
+  expand: require('@assets/icons/actions/expand.svg'),
 
-  'logo-mycrypto': logoMyCrypto,
-  'logo-mycrypto-text': logoMyCryptoText,
+  'logo-mycrypto': require('@assets/icons/brand/logo.svg'),
+  'logo-mycrypto-text': require('@assets/icons/brand/logo-text.svg'),
 
-  coinmarketcap,
-  facebook,
-  github,
-  reddit,
-  slack,
-  telegram,
-  twitter,
+  coinmarketcap: require('@assets/icons/social/coinmarketcap.svg'),
+  facebook: require('@assets/icons/social/facebook.svg'),
+  github: require('@assets/icons/social/github.svg'),
+  reddit: require('@assets/icons/social/reddit.svg'),
+  slack: require('@assets/icons/social/slack.svg'),
+  telegram: require('@assets/icons/social/telegram.svg'),
+  twitter: require('@assets/icons/social/twitter.svg'),
 
-  website,
-  whitepaper
+  website: require('@assets/icons/website.svg'),
+  whitepaper: require('@assets/icons/expand.svg')
 };
 
 const StyledInlineSVG = styled(InlineSVG)`
