2014216c3f41247ba912a5c2320f669228cb04b2
Frederik Bolding <frederik.bolding@gmail.com>
2020-07-02 11:19:44 +0200

    Move PTX report mock data to fixtures

--- src/features/ProtectTransaction/__tests__/ProtectTxReport.test.tsx
+++ src/features/ProtectTransaction/__tests__/ProtectTxReport.test.tsx
@@ -1,39 +1,12 @@
 import React from 'react';
 import { simpleRender } from 'test-utils';
-import find from 'lodash/find';
 
 import { translateRaw } from '@translations';
-import { TUuid, TAddress } from '@types';
-import { ETHUUID, noOp } from '@utils';
-import { assets } from '@database/seed/assets';
+import { noOp } from '@utils';
+import { unknownReport, scamReport, verifiedReport } from '@fixtures';
 
 import { ProtectTxReportUI } from '../components/ProtectTxReport';
-import { PTXReport, NansenReportType } from '../types';
-
-const asset = find(assets, { uuid: ETHUUID as TUuid })!;
-
-const unknownReport: PTXReport = {
-  asset,
-  address: '0x88F7B1E26c3A52CA3cD8aF4ba1b448391eb31d88' as TAddress,
-  labels: [],
-  status: NansenReportType.UNKNOWN,
-  balance: '0',
-  lastTransaction: { ticker: 'ETH', value: '1', timestamp: '1593632079' }
-};
-
-const scamReport: PTXReport = {
-  ...unknownReport,
-  address: '0x820C415a17Bf165a174e6B55232D956202d9470f' as TAddress,
-  labels: ['Scam'],
-  status: NansenReportType.MALICIOUS
-};
-
-const verifiedReport: PTXReport = {
-  ...unknownReport,
-  address: '0x4bbeEB066eD09B7AEd07bF39EEe0460DFa261520' as TAddress,
-  labels: ['MyCrypto: Donate'],
-  status: NansenReportType.WHITELISTED
-};
+import { PTXReport } from '../types';
 
 const renderComponent = (report: PTXReport) => {
   return simpleRender(<ProtectTxReportUI report={report} onHide={noOp} isWeb3={false} />);
