d9f0ec8def8ff6ce0e95ee2235e4b7260d6df88d
Frederik Bolding <frederik.bolding@gmail.com>
2020-07-15 18:29:32 +0200

    Add comment to feature flag utils

--- __tests__/featureflag-utils.js
+++ __tests__/featureflag-utils.js
@@ -1,10 +1,11 @@
 import { ClientFunction } from 'testcafe';
 
+// Accesses functions available on window object that change state in FeatureFlagProvider
 const setFeatureFlag = ClientFunction((key, value) => {
   window.setFeatureFlag(key, value);
 });
-const clearFeatureFlags = ClientFunction(() => {
-  window.clearFeatureFlags();
+const resetFeatureFlags = ClientFunction(() => {
+  window.resetFeatureFlags();
 });
 
-export { setFeatureFlag, clearFeatureFlags };
+export { setFeatureFlag, resetFeatureFlags };
