869912178481e48240679ff7f69bb5b5ffedb6b6
Frederik Bolding <frederik.bolding@gmail.com>
2020-07-22 14:46:36 +0200

    Fix issue with new version of MetaMask inpage provider

--- src/vendor/inpage-metamask.js
+++ src/vendor/inpage-metamask.js
@@ -1,5 +1,5 @@
 import LocalMessageDuplexStream from 'post-message-stream';
-import MetamaskInpageProvider from '@metamask/inpage-provider';
+import { initProvider } from '@metamask/inpage-provider';
 
 // Firefox Metamask Hack
 // Due to https://github.com/MetaMask/metamask-extension/issues/3133
@@ -12,20 +12,9 @@ import MetamaskInpageProvider from '@metamask/inpage-provider';
       target: 'contentscript'
     });
 
-    // compose the inpage provider
-    const inpageProvider = new MetamaskInpageProvider(metamaskStream);
-
-    // set a high max listener count to avoid unnecesary warnings
-    inpageProvider.setMaxListeners(100);
-
-    // Work around for web3@1.0 deleting the bound `sendAsync` but not the unbound
-    // `sendAsync` method on the prototype, causing `this` reference issues
-    const ethereum = new Proxy(inpageProvider, {
-      // straight up lie that we deleted the property so that it doesnt
-      // throw an error in strict mode
-      deleteProperty: () => true
+    // this will initialize the provider and set it as window.ethereum
+    initProvider({
+      connectionStream: metamaskStream
     });
-
-    window.ethereum = ethereum;
   }
 })();
