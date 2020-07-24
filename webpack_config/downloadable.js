b89e931d40dea46bd28c9ae682c8319ebe1ec4f8
blurpesec <hahn.michael.f@gmail.com>
2020-07-23 17:10:59 -0700

    add env var

--- webpack_config/downloadable.js
+++ webpack_config/downloadable.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const production = require('./production');
 const config = require('./config');
+
 module.exports = merge.smart(production, {
   output: {
     path: path.join(config.path.output, 'web'),
@@ -14,7 +15,7 @@ module.exports = merge.smart(production, {
       'BUILD_DOWNLOADABLE': 'true',
       'BUILD_HTML': 'true',
       'BUILD_ENV': process.env.BUILD_ENV,
-      'NODE_ENV': process.env.NODE_ENV
+      'NODE_ENV': process.env.NODE_ENV || 'production'
     })
   ]
 });
