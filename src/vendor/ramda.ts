4f0eaffa998bf40517ab1dda1918e2fe6741f526
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-07-29 15:45:14 +0200

    Simplify ramda imports

---
+++ src/vendor/ramda.ts
@@ -0,0 +1,9 @@
+/**
+ *  Explicit list of helper functions in order to reduce bundle size
+ */
+
+export { default as sortBy } from 'ramda/src/sortBy';
+export { default as compose } from 'ramda/src/compose';
+export { default as toLower } from 'ramda/src/toLower';
+export { default as prop } from 'ramda/src/prop';
+export { default as map } from 'ramda/src/map';
