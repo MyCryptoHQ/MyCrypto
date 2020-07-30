3bc66e80b858d16281de47e7b9f7bbf41adf26e9
blurpesec <hahn.michael.f@gmail.com>
2020-07-30 14:00:54 -0700

    added comment explanation

--- src/types/web3Permissions.ts
+++ src/types/web3Permissions.ts
@@ -1,5 +1,10 @@
 import { TAddress } from '@types';
 
+/*
+  These types are used for Web3 interactions under Metamask's permissions system
+  identified here: https://github.com/MetaMask/rpc-cap
+  Web3 api methods are usually injected by a browser extension or mobile app browser
+*/
 export interface Web3RequestPermissionsResult {
   '@context': string[];
   invoker: string;
