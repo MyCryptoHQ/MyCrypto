9ecbcfc1a54bb7cbf8ac623be24a7e2ffd871d59
Frederik Bolding <frederik.bolding@gmail.com>
2020-07-31 10:32:01 +0200

    Add PrivateKey mock and finished test

--- src/features/SendAssets/__tests__/SignTransactionPrivateKey.test.tsx
+++ src/features/SendAssets/__tests__/SignTransactionPrivateKey.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { simpleRender } from 'test-utils';
+import { simpleRender, fireEvent, waitFor } from 'test-utils';
 
 import { fTxConfig } from '@fixtures';
 import { WalletId } from '@types';
@@ -26,14 +26,28 @@ const getHeader = (wallet: WalletId) => {
   });
 };
 
+const mockSign = jest.fn().mockImplementation(() => Promise.resolve('txhash'));
+jest.mock('ethers', () => {
+  return {
+    Wallet: jest.fn().mockImplementation(() => ({ sign: mockSign }))
+  };
+});
+
 describe('SignTransaction', () => {
   afterEach(() => {
     jest.resetAllMocks();
   });
 
-  test('Can render Private Key signing', () => {
-    const { getByText } = getComponent();
+  test('Can render Private Key signing', async () => {
+    const { getByText, container } = getComponent();
     const selector = getHeader(WalletId.PRIVATE_KEY);
     expect(getByText(selector)).toBeInTheDocument();
+
+    const input = container.querySelector('input');
+    fireEvent.click(input!);
+    fireEvent.change(input!, { target: { value: 'privkey' } });
+    fireEvent.click(getByText(translateRaw('DEP_SIGNTX')));
+
+    await waitFor(() => expect(defaultProps.onComplete).toBeCalledWith('txhash'));
   });
 });
