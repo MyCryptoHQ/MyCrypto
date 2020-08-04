b5f13ebe3ff0bb0e8b9d5dd1fe54d980dfaeeec6
blurpesec <hahn.michael.f@gmail.com>
2020-08-03 17:25:32 -0700

    rename keystore file

--- __tests__/addaccount-page.po.js
+++ __tests__/addaccount-page.po.js
@@ -59,7 +59,7 @@ export default class AddAccountPage extends BasePage {
       .typeText(Selector('div[data-testid="selector"]').find('input'), FIXTURE_ETHEREUM)
       .click(Selector('div[data-testid="selector"]').find('span').withText(FIXTURE_ETHEREUM))
       .click(Selector('button').withText(getTransValueByKey('ACTION_6')))
-      .setFilesToUpload(Selector(`div`).withText(getTransValueByKey('ADD_RADIO_2_SHORT')).find('input'), './TEST_KEYSTORE_FILE.json')
+      .setFilesToUpload(Selector(`div`).withText(getTransValueByKey('ADD_RADIO_2_SHORT')).find('input'), './testKeystoreFile.json')
 
     // Inpute keystore password
     await t.typeText(Selector(`input`).withAttribute('placeholder', getTransValueByKey('INPUT_PASSWORD_LABEL')), FIXTURE_TEST_KEYSTORE_FILE_PASSWORD)
