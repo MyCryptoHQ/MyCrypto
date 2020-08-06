a84c19a7d647460eee593fa3c33c02253571fb69
Frederik Bolding <frederik.bolding@gmail.com>
2020-08-06 23:56:36 +0200

    Flipped boolean return values on validators

--- src/features/SendAssets/components/validators/validators.ts
+++ src/features/SendAssets/components/validators/validators.ts
@@ -12,34 +12,34 @@ import { isValidPositiveOrZeroInteger } from '@services/EthService/validators';
 export function validateGasPriceField(): TestOptions {
   return {
     message: translateRaw('ERROR_10'),
-    test: (value) => !gasPriceValidator(value)
+    test: (value) => gasPriceValidator(value)
   };
 }
 
 export function validateGasLimitField(): TestOptions {
   return {
     message: translateRaw('ERROR_8'),
-    test: (value) => !gasLimitValidator(value)
+    test: (value) => gasLimitValidator(value)
   };
 }
 
 export function validateDataField(): TestOptions {
   return {
     message: translateRaw('ERROR_9'),
-    test: (value) => !isValidHex(value) || !isHexPrefixed(value)
+    test: (value) => isValidHex(value) && isHexPrefixed(value)
   };
 }
 
 export function validateNonceField(): TestOptions {
   return {
     message: translateRaw('ERROR_11'),
-    test: (value) => !isValidPositiveOrZeroInteger(value)
+    test: (value) => isValidPositiveOrZeroInteger(value)
   };
 }
 
 export function validateAmountField(): TestOptions {
   return {
     message: translateRaw('ERROR_0'),
-    test: (value) => !isValidPositiveNumber(value)
+    test: (value) => isValidPositiveNumber(value)
   };
 }
