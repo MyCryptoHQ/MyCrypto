4d4efad035c0015b878e087731d46a416a5b8421
Frederik Bolding <frederik.bolding@gmail.com>
2020-08-06 11:06:33 +0200

    Add test for WEB3_SIGN

--- src/features/SendAssets/__tests__/SendAssets.reducer.test.tsx
+++ src/features/SendAssets/__tests__/SendAssets.reducer.test.tsx
@@ -1,4 +1,6 @@
-import { IFormikFields } from '@types';
+import { bigNumberify } from 'ethers/utils';
+
+import { IFormikFields, ITxStatus } from '@types';
 import { fAssets } from '@../jest_config/__fixtures__/assets';
 import { fAccount, fNetwork } from '@fixtures';
 import { getDefaultEstimates } from '@services';
@@ -33,23 +35,64 @@ describe('SendAssetsReducer', () => {
         prevState
       );
       const txConfig = newState.txConfig!;
-      expect(txConfig.senderAccount.address).toEqual(form.account.address);
-      expect(txConfig.receiverAddress).toEqual(form.address.value);
-      expect(txConfig.amount).toEqual(form.amount);
-      expect(txConfig.asset.uuid).toEqual(form.asset.uuid);
-      expect(txConfig.network.id).toEqual(form.network.id);
-      expect(txConfig.baseAsset.uuid).toEqual(fNetwork.baseAsset);
-      expect(txConfig.nonce).toEqual(form.nonceField);
+      expect(txConfig.senderAccount.address).toBe(form.account.address);
+      expect(txConfig.receiverAddress).toBe(form.address.value);
+      expect(txConfig.amount).toBe(form.amount);
+      expect(txConfig.asset.uuid).toBe(form.asset.uuid);
+      expect(txConfig.network.id).toBe(form.network.id);
+      expect(txConfig.baseAsset.uuid).toBe(fNetwork.baseAsset);
+      expect(txConfig.nonce).toBe(form.nonceField);
 
-      expect(newState.signedTx).toEqual(prevState.signedTx);
-      expect(newState.txReceipt).toEqual(prevState.txReceipt);
+      expect(newState.signedTx).toBe(prevState.signedTx);
+      expect(newState.txReceipt).toBe(prevState.txReceipt);
     });
   });
   describe('SIGN', () => {
     // TODO
   });
   describe('WEB3_SIGN', () => {
-    // TODO
+    it('it updates the txReceipt with values from txConfig', () => {
+      const prevState = {
+        txReceipt: undefined,
+        txConfig: {
+          asset: fAssets[0],
+          baseAsset: fAssets[0],
+          amount: '1',
+          gasPrice: '80',
+          gasLimit: '21000',
+          nonce: '10',
+          data: '0x',
+          rawTransaction: {
+            value: '1',
+            to: fAccount.address
+          },
+          from: fAccount.address,
+          senderAccount: fAccount
+        },
+        signedTx: undefined
+      };
+      const payload = '0x12345678';
+      const newState = dispatch({
+        type: sendAssetsReducer.actionTypes.WEB3_SIGN,
+        payload
+      })(prevState);
+      const txConfig = prevState.txConfig;
+      const txReceipt = newState.txReceipt!;
+      expect(txReceipt.hash).toBe(payload);
+      expect(txReceipt.amount).toBe(txConfig.amount);
+      expect(txReceipt.asset.uuid).toBe(txConfig.asset.uuid);
+      expect(txReceipt.baseAsset.uuid).toBe(txConfig.baseAsset.uuid);
+      expect(txReceipt.data).toBe(txConfig.data);
+      expect(txReceipt.status).toBe(ITxStatus.PENDING);
+      expect(txReceipt.to).toBe(fAccount.address);
+      expect(txReceipt.from).toBe(fAccount.address);
+      expect(txReceipt.gasLimit).toEqual(bigNumberify(txConfig.gasLimit));
+      expect(txReceipt.gasPrice).toEqual(bigNumberify(txConfig.gasPrice));
+      expect(txReceipt.value).toEqual(bigNumberify(txConfig.rawTransaction.value));
+
+      expect(newState.signedTx).toBe(prevState.signedTx);
+      expect(newState.txConfig).toBe(prevState.txConfig);
+    });
   });
   describe('SEND', () => {
     it('sets send to true and otherwise keeps the state as is', () => {
@@ -57,11 +100,11 @@ describe('SendAssetsReducer', () => {
       const newState = dispatch({ type: sendAssetsReducer.actionTypes.SEND, payload: undefined })(
         prevState
       );
-      expect(newState.send).toEqual(true);
+      expect(newState.send).toBe(true);
 
-      expect(newState.signedTx).toEqual(prevState.signedTx);
-      expect(newState.txReceipt).toEqual(prevState.txReceipt);
-      expect(newState.txConfig).toEqual(prevState.txConfig);
+      expect(newState.signedTx).toBe(prevState.signedTx);
+      expect(newState.txReceipt).toBe(prevState.txReceipt);
+      expect(newState.txConfig).toBe(prevState.txConfig);
     });
   });
   describe('AFTER_SEND', () => {
