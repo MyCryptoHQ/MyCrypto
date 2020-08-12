cfd046575f96771fa0be1e2d2220a03c3767f0b6
Frederik Bolding <frederik.bolding@gmail.com>
2020-08-12 17:30:45 +0200

    Enable feature flag for E2E test

--- __tests__/txstatus.test.js
+++ __tests__/txstatus.test.js
@@ -3,6 +3,7 @@ import { getByText, getAllByText } from '@testing-library/testcafe';
 
 import { PAGES, FIXTURE_SEND_AMOUNT, FIXTURES_CONST } from './fixtures';
 import { getTransValueByKey } from './translation-utils';
+import { setFeatureFlag, resetFeatureFlags } from './featureflag-utils';
 
 import TxStatusPage from './txstatus-page.po';
 
@@ -11,6 +12,7 @@ const txStatusPage = new TxStatusPage();
 fixture('TX Status').page(PAGES.TX_STATUS);
 
 test('Can fetch status of a successful TX', async (t) => {
+  await setFeatureFlag('TX_STATUS', true);
   await txStatusPage.waitPageLoaded();
 
   const title = getByText(getTransValueByKey('TX_STATUS'));
@@ -23,4 +25,6 @@ test('Can fetch status of a successful TX', async (t) => {
   await t.expect(Selector('button').withText(getTransValueByKey('FETCH')).exists).eql(false);
   await t.expect(getAllByText(FIXTURE_SEND_AMOUNT, { exact: false })).ok();
   await t.expect(getAllByText(getTransValueByKey('SUCCESS'))).ok();
+
+  await resetFeatureFlags();
 });
