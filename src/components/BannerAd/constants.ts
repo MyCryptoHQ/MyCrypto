c6cc453f37693f18c923f929864ef1a528bfc3d0
H <409H@users.noreply.github.com>
2020-08-12 11:23:40 +0100

    More BannerAds (#3423)

--- src/components/BannerAd/constants.ts
+++ src/components/BannerAd/constants.ts
@@ -3,6 +3,12 @@ import ledgerDesktop from '@assets/images/banners/Ledger_Desktop.png';
 import ledgerMobile from '@assets/images/banners/Ledger_Mobile.png';
 import trezorDesktop from '@assets/images/banners/Trezor_Desktop.png';
 import trezorMobile from '@assets/images/banners/Trezor_Mobile.png';
+import coinbaseDesktop from '@assets/images/banners/Coinbase_Desktop.png';
+import coinbaseMobile from '@assets/images/banners/Coinbase_Mobile.png';
+import quiknodeDesktop from '@assets/images/banners/QuikNode_Desktop.png';
+import quiknodeMobile from '@assets/images/banners/QuikNode_Mobile.png';
+import unstoppableDesktop from '@assets/images/banners/UnstoppableDomains_Desktop.png';
+import unstoppableMobile from '@assets/images/banners/UnstoppableDomains_Mobile.png';
 
 export const ads = [
   {
@@ -16,5 +22,23 @@ export const ads = [
     srcDesktop: ledgerDesktop,
     srcMobile: ledgerMobile,
     url: EXT_URLS.LEDGER_REFERRAL.url
+  },
+  {
+    name: 'Coinbase',
+    srcDesktop: coinbaseDesktop,
+    srcMobile: coinbaseMobile,
+    url: EXT_URLS.COINBASE_REFERRAL.url
+  },
+  {
+    name: 'Quiknode',
+    srcDesktop: quiknodeDesktop,
+    srcMobile: quiknodeMobile,
+    url: EXT_URLS.QUIKNODE_REFERRAL.url
+  },
+  {
+    name: 'UnstoppableDomains',
+    srcDesktop: unstoppableDesktop,
+    srcMobile: unstoppableMobile,
+    url: EXT_URLS.UNSTOPPABLEDOMAINS_REFERRAL.url
   }
 ];
