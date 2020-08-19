b93ddcdc238a03786fc22e3e9dbc9035684dd0d6
Frederik Bolding <frederik.bolding@gmail.com>
2020-08-19 13:04:48 +0200

    Add mock and expand TxStatus test

--- src/features/TxStatus/TxStatus.test.tsx
+++ src/features/TxStatus/TxStatus.test.tsx
@@ -1,21 +1,53 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router';
-import { simpleRender } from 'test-utils';
+import { simpleRender, waitFor } from 'test-utils';
 
-import { NetworkContext } from '@services';
+import {
+  NetworkContext,
+  StoreContext,
+  AssetContext,
+  AddressBookContext,
+  SettingsContext,
+  RatesContext
+} from '@services';
 import { translateRaw } from '@translations';
+import { fAccount, fNetwork, fAssets } from '@fixtures';
 
 import TxStatus from './TxStatus';
 
-const TX_HASH = '0x949b7cbfcc4d7a82c83221de4f9e153ce5a161e10722216dae3bcb6eadd9b34c';
+const TX_HASH = '0x6a705a2943f19079dd712fa0b2ae1f7b036454ca6df881afc9e17573ee6ede8a ';
+
+jest.mock('ethers/providers', () => {
+  const { mockFactory } = require('./__mocks__/txstatus');
+  return mockFactory();
+});
 
 /* Test components */
 describe('TxStatus', () => {
   const component = (path?: string) => (
     <MemoryRouter initialEntries={path ? [path] : undefined}>
-      <NetworkContext.Provider value={{ getNetworkById: jest.fn(), networks: [] } as any}>
-        <TxStatus />
-      </NetworkContext.Provider>
+      <StoreContext.Provider value={{ accounts: [fAccount] } as any}>
+        <AssetContext.Provider value={{ assets: fAssets } as any}>
+          <NetworkContext.Provider
+            value={
+              {
+                getNetworkById: jest.fn(),
+                networks: [fNetwork]
+              } as any
+            }
+          >
+            <AddressBookContext.Provider
+              value={{ getContactByAddressAndNetworkId: jest.fn() } as any}
+            >
+              <SettingsContext.Provider value={{ settings: { fiatCurrency: 'USD' } } as any}>
+                <RatesContext.Provider value={{ getAssetRate: jest.fn() } as any}>
+                  <TxStatus />
+                </RatesContext.Provider>
+              </SettingsContext.Provider>
+            </AddressBookContext.Provider>
+          </NetworkContext.Provider>
+        </AssetContext.Provider>
+      </StoreContext.Provider>
     </MemoryRouter>
   );
 
@@ -29,10 +61,10 @@ describe('TxStatus', () => {
     expect(getByText(selector)).toBeInTheDocument();
   });
 
-  test('Can get hash from query string', () => {
-    const { getByText, getByDisplayValue } = renderComponent(`/tx-status?hash=${TX_HASH}`);
+  test('When information given in query string handles fetching correctly', async () => {
+    const { getByText } = renderComponent(`/tx-status?hash=${TX_HASH}&network=Ropsten`);
     const selector = translateRaw('TX_STATUS');
     expect(getByText(selector)).toBeInTheDocument();
-    expect(getByDisplayValue(TX_HASH)).toBeInTheDocument();
+    await waitFor(() => expect(getByText(fAssets[1].ticker, { exact: false })).toBeInTheDocument());
   });
 });
