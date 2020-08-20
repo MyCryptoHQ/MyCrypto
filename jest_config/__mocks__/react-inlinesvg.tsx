a2f30bb021bbcc54e9b664aa54393dd8fb35e3f0
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-08-20 17:59:30 +0200

    Mock react-inlinesvg for jest

---
+++ jest_config/__mocks__/react-inlinesvg.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * We use react-inlingsvg in the app and rely on `fileMock` to stub svg file requests in jest
+ * Because of the stub, the dependency throughs an `isomorphic-fetch` error requesting an `absolute url`.
+ * Mock the component returned by the dependency to silence the error.
+ * ref. https://github.com/gilbarbara/react-inlinesvg/issues/140
+ */
+
+const svgMock = ({ src }) => <svg id={src} />;
+
+export default svgMock;
