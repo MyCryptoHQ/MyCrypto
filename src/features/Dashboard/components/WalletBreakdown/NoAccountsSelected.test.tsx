11646e3ae5358ea2bc60b769e1cae62c68ac04d2
Guillaume Roux <guillaumeroux123@gmail.com>
2020-08-20 18:50:37 +0200

    Fix missing translation keys on NoAccounts (#3447)

---
+++ src/features/Dashboard/components/WalletBreakdown/NoAccountsSelected.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { simpleRender } from 'test-utils';
+
+import { translateRaw } from '@translations';
+import { default as NoAccountsSelected } from './NoAccountsSelected';
+
+describe('NoAccountsSelected', () => {
+  test('Translations are rendered', () => {
+    const { getByText } = simpleRender(<NoAccountsSelected />);
+
+    const headerText = getByText(translateRaw('NO_ACCOUNTS_SELECTED_HEADER'));
+    const descriptionText = getByText(translateRaw('NO_ACCOUNTS_SELECTED_DESCRIPTION'));
+
+    expect(headerText).toBeInTheDocument();
+    expect(headerText).not.toEqual('NO_ACCOUNTS_SELECTED_HEADER');
+
+    expect(descriptionText).toBeInTheDocument();
+    expect(descriptionText).not.toEqual('NO_ACCOUNTS_SELECTED_DESCRIPTION');
+  });
+});
