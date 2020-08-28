f7270969cf1e5e8bc99f1e0fac01b147e92753d7
Frederik Bolding <frederik.bolding@gmail.com>
2020-08-28 14:06:22 +0200

    Add test for deriveTxType

---
+++ src/services/TxHistory/helpers.spec.ts
@@ -0,0 +1,36 @@
+import { fAccounts, fTxReceipt } from '@fixtures';
+
+import { deriveTxType } from './helpers';
+import { ITxHistoryType } from '@features/Dashboard/types';
+
+describe('deriveTxType', () => {
+  it('derives OUTBOUND tx correctly', () => {
+    const result = deriveTxType(fAccounts, fTxReceipt);
+    expect(result).toBe(ITxHistoryType.OUTBOUND);
+  });
+  it('derives TRANSFER tx correctly', () => {
+    const result = deriveTxType(fAccounts, {
+      ...fTxReceipt,
+      receiverAddress: fTxReceipt.from,
+      from: fTxReceipt.from,
+      to: fTxReceipt.from
+    });
+    expect(result).toBe(ITxHistoryType.TRANSFER);
+  });
+  it('derives INBOUND tx correctly', () => {
+    const result = deriveTxType(fAccounts, {
+      ...fTxReceipt,
+      receiverAddress: fTxReceipt.from,
+      from: fTxReceipt.to,
+      to: fTxReceipt.from
+    });
+    expect(result).toBe(ITxHistoryType.INBOUND);
+  });
+  it('default to passed txType', () => {
+    const result = deriveTxType(fAccounts, {
+      ...fTxReceipt,
+      from: fTxReceipt.to
+    });
+    expect(result).toBe(ITxHistoryType.STANDARD);
+  });
+});
