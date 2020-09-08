2d2140291ab19c236b759f5fbd7ed64e670cbe01
Frederik Bolding <frederik.bolding@gmail.com>
2020-09-08 18:53:09 +0200

    Refactor ToastProvider and test (#3497)

--- src/features/index.ts
+++ src/features/index.ts
@@ -3,7 +3,7 @@ export { DevToolsManager } from './DevTools';
 export { ScreenLockLocking, ScreenLockProvider, ScreenLockContext } from './ScreenLock';
 export { default as PageNotFound } from './PageNotFound';
 export { NotificationsProvider } from './NotificationsPanel';
-export { ToastProvider } from './Toasts';
+export { useToasts } from './Toasts';
 export { ErrorProvider, ErrorContext } from './ErrorHandling';
 export { TokenMigration } from './TokenMigration';
 export { Dashboard } from './Dashboard';
