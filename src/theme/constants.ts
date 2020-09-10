7fe927c2eb85df34c9400088cda999246a8f1eed
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-09-10 12:16:04 +0200

    Fix (optional) support for Python 3+ (#3501)
    * Fix support for Python 3
    
    * Update parse-eth-tokens to remove node-gyp dependency
    
    * Update build requirements
    
    * Add node-gyp as devDependency
    
    * Update CI script
    
    * Fix SCSS warning

--- src/theme/constants.ts
+++ src/theme/constants.ts
@@ -1,12 +1,14 @@
 import { light } from '@mycrypto/ui';
 
-// Direct require to customise the webpack default scss loader
-export const GAU_COLORS = require('sass-extract-loader?{"plugins": ["sass-extract-js"]}!./colors.scss');
-
 // Combine the themes in a single object to be consummed by SC ThemeProvider
 export const GAU_THEME = Object.assign({}, light, {
   GAU: {
-    COLORS: GAU_COLORS
+    COLORS: {
+      borderColor: 'rgb(232, 234, 237)',
+      brightSkyBlue: 'rgb(30, 184, 231)',
+      cloudyBlue: 'rgb(181, 191, 199)',
+      dividerColor: 'rgb(229, 236, 243)'
+    }
   }
 });
 
