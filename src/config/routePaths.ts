5c8ce20ff47b88f385d90fe244b099320209bb5d
blurpesec <hahn.michael.f@gmail.com>
2020-09-28 15:40:31 -0700

    fix e2e tests

--- src/config/routePaths.ts
+++ src/config/routePaths.ts
@@ -155,6 +155,11 @@ const PATHS: IRoutePath[] = [
     name: 'ANT_TOKEN_MIGRATION',
     title: 'ANT Token Migration',
     path: '/migrate/ant'
+  },
+  {
+    name: 'GOLEM_TOKEN_MIGRATION',
+    title: 'GOLEM Token Migration',
+    path: '/migrate/golem'
   }
 ];
 
