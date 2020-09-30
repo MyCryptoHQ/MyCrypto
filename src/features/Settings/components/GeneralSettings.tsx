3a87bf5a8fd57138f310be724d9c05c024a806ca
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-09-30 16:13:00 +0200

    Remove commented code

--- src/features/Settings/components/GeneralSettings.tsx
+++ src/features/Settings/components/GeneralSettings.tsx
@@ -140,14 +140,6 @@ const GeneralSettings: FC<SettingsProps> = ({ globalSettings, updateGlobalSettin
           </Link>
         </SettingsControl>
       </SettingsField>
-      {/* Hidding Paper wallet section according to CH4412
-        <SettingsField>
-          <SettingsLabel>{translate('SETTINGS_PAPER_LABEL')}</SettingsLabel>
-          <SettingsControl>
-            <SettingsButton secondary={true}>{translate('SETTINGS_DOWNLOAD_LABEL')}</SettingsButton>
-            <SettingsButton secondary={true}>{translate('SETTINGS_PRINT_LABEL')}</SettingsButton>
-          </SettingsControl>
-        </SettingsField> */}
       <SettingsField>
         <SettingsLabel>
           {translate('SETTINGS_INACTIVITY_LABEL')}{' '}
