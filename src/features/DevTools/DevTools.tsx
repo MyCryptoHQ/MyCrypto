a7dfcf06a0b153a213e298bf0cd54af0284155c2
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-10-26 23:09:22 +0100

    Add candy toggle

--- src/features/DevTools/DevTools.tsx
+++ src/features/DevTools/DevTools.tsx
@@ -22,9 +22,20 @@ const SCheckbox = styled(Checkbox)`
   margin-bottom: 0;
 `;
 
-const SDevToolsToggle = styled.button`
-  position: absolute;
-  width: 120px;
+const SToggle = styled.button`
+  position: fixed;
+  bottom: 1.25em;
+  left: 1.25em;
+
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 0;
+  font-size: 1.5em;
+  /* From smooth shadow-generator https://s.muz.li/MWNjYTY2Yjk4 */
+  box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.02), 0 6.7px 5.3px rgba(0, 0, 0, 0.028),
+    0 12.5px 10px rgba(0, 0, 0, 0.035), 0 22.3px 17.9px rgba(0, 0, 0, 0.042),
+    0 41.8px 33.4px rgba(0, 0, 0, 0.05), 0 100px 80px rgba(0, 0, 0, 0.07);
 `;
 
 const Wrapper = styled.div<{ isActive: boolean }>`
@@ -33,19 +44,19 @@ const Wrapper = styled.div<{ isActive: boolean }>`
   left: 0;
   z-index: 1000;
   max-width: 450px;
+  box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.02),
+    0 6.7px 5.3px rgba(0, 0, 0, 0.028),
+    0 12.5px 10px rgba(0, 0, 0, 0.035),
+    0 22.3px 17.9px rgba(0, 0, 0, 0.042),
+    0 41.8px 33.4px rgba(0, 0, 0, 0.05),
+    0 100px 80px rgba(0, 0, 0, 0.07);
 
   ${({ isActive }) => isActive && `height: 100vh;`}
 
   @media (max-width: ${BREAK_POINTS.SCREEN_SM}) {
     max-width: 100vw;
   }
-  box-shadow:
-    0 2.8px 2.2px rgba(0, 0, 0, 0.02),
-    0 6.7px 5.3px rgba(0, 0, 0, 0.028),
-    0 12.5px 10px rgba(0, 0, 0, 0.035),
-    0 22.3px 17.9px rgba(0, 0, 0, 0.042),
-    0 41.8px 33.4px rgba(0, 0, 0, 0.05),
-    0 100px 80px rgba(0, 0, 0, 0.07);
+
 `;
 
 const DBTools = () => {
@@ -94,24 +105,19 @@ const FeatureFlags = () => {
   );
 };
 
-const DevToolsToggle = () => {
-  const { isActive, toggleDevTools } = useDevTools();
-  return (
-    <SDevToolsToggle onClick={toggleDevTools}>
-      {isActive ? 'DevMode On' : 'DevMode Off'}
-    </SDevToolsToggle>
-  );
-};
-
 const DevToolsManager = () => {
-  const { isActive } = useDevTools();
+  const { isActive, toggleDevTools } = useDevTools();
   const greeting = useSelector(getGreeting);
 
   if (IS_PROD) return <></>;
 
   return (
     <Wrapper isActive={isActive}>
-      <DevToolsToggle />
+      <SToggle onClick={toggleDevTools}>
+        <span role="img" aria-label="lollipop">
+          🍭
+        </span>
+      </SToggle>
       {isActive && (
         <Panel style={{ marginBottom: 0, paddingTop: 50, width: '400px', height: '100%' }}>
           {/* Toggle feature availability */}
