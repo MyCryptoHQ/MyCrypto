ae6df93d5fb7dedaf1f657e0b42becce54b7b67e
Frederik Bolding <frederik.bolding@gmail.com>
2020-10-29 12:23:23 +0100

    Add test

--- jest_config/__fixtures__/account.ts
+++ jest_config/__fixtures__/account.ts
@@ -30,7 +30,7 @@ export const fAccounts: StoreAccount[] = [
         name: 'REPv1',
         networkId: 'Ethereum',
         type: 'erc20',
-        ticker: 'ETH' as TTicker,
+        ticker: 'REPv1' as TTicker,
         decimal: 18,
         contractAddress: '0x1985365e9f78359a9B6AD760e32412f4a445E862',
         isCustom: false,
