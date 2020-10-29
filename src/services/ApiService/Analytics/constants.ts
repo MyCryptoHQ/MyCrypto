4bff112ebd2cf4d386d1101d663000b2f6dc4f63
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-10-29 14:28:41 +0100

    Add analytics proxy url

--- src/services/ApiService/Analytics/constants.ts
+++ src/services/ApiService/Analytics/constants.ts
@@ -1,8 +1,6 @@
 import { IS_DEV } from '@utils';
 // The root URL for Analytics API requests.
-export const ANALYTICS_API_URL = IS_DEV
-  ? 'http://localhost:5555'
-  : 'https://analytics.proxy.mycryptoapi.com/';
+export const ANALYTICS_API_URL = IS_DEV ? 'http://localhost:5555' : 'https://stats.mycryptoapi.com';
 
 // WriteKey for Segment API
 export const ANALYTICS_WRITE_KEY = process.env.SEGMENT_WRITE_KEY || '';
