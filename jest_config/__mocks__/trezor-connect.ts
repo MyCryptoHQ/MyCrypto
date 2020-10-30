69573660fc83aa1e43e8342c10d467bf51bda613
Frederik Bolding <frederik.bolding@gmail.com>
2020-10-30 17:09:45 +0100

    Remove trezor-connect mock

--- jest_config/__mocks__/trezor-connect.ts
+++
@@ -1,7 +0,0 @@
-import TrezorConnect from 'trezor-connect/lib/env/browser';
-
-/**
- * For whatever reason Jest does not work with `trezor-connect` without mocking and exporting the browser bundle as
- * default.
- */
-export default TrezorConnect;
