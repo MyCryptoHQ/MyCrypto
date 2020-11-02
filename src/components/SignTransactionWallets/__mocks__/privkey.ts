1ca0c9a9cd17d158ff98a74121e45cccfcbe3f34
Frederik Bolding <frederik.bolding@gmail.com>
2020-11-02 11:19:03 +0100

    Delete unused mocks

--- src/components/SignTransactionWallets/__mocks__/privkey.ts
+++
@@ -1,7 +0,0 @@
-export const mockFactory = (signResult: string) => {
-  return {
-    Wallet: jest.fn().mockImplementation(() => ({
-      sign: jest.fn().mockImplementation(() => Promise.resolve(signResult))
-    }))
-  };
-};
