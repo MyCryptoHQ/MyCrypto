ac2fbb3e1cee4c6e9c3d2f7c7cfd0af1ec7853ff
H <409H@users.noreply.github.com>
2020-11-11 12:47:54 +0000

    Added PoolTogether referral banner (#3602)

--- src/components/BannerAd/constants.ts
+++ src/components/BannerAd/constants.ts
@@ -2,6 +2,8 @@ import coinbaseDesktop from '@assets/images/banners/Coinbase_Desktop.png';
 import coinbaseMobile from '@assets/images/banners/Coinbase_Mobile.png';
 import ledgerDesktop from '@assets/images/banners/Ledger_Desktop.png';
 import ledgerMobile from '@assets/images/banners/Ledger_Mobile.png';
+import PooltogetherDesktop from '@assets/images/banners/Pooltogether_Desktop.png';
+import PooltogetherMobile from '@assets/images/banners/Pooltogether_Mobile.png';
 import quiknodeDesktop from '@assets/images/banners/QuikNode_Desktop.png';
 import quiknodeMobile from '@assets/images/banners/QuikNode_Mobile.png';
 import trezorDesktop from '@assets/images/banners/Trezor_Desktop.png';
@@ -40,5 +42,11 @@ export const ads = [
     srcDesktop: unstoppableDesktop,
     srcMobile: unstoppableMobile,
     url: EXT_URLS.UNSTOPPABLEDOMAINS_REFERRAL.url
+  },
+  {
+    name: 'PoolTogether',
+    srcDesktop: PooltogetherDesktop,
+    srcMobile: PooltogetherMobile,
+    url: EXT_URLS.POOLTOGETHER_REFERRAL.url
   }
 ];
