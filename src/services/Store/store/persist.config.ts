e860ba0f422ea2e76ae09c6fea1f756a093955c0
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-11-17 19:30:11 +0100

    Replace DataService with redux-persist

--- src/services/Store/store/persist.config.ts
+++ src/services/Store/store/persist.config.ts
@@ -2,17 +2,19 @@ import { Reducer } from '@reduxjs/toolkit';
 import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+import { DataStore } from '@types';
+
 export const REDUX_PERSIST_ACTION_TYPES = [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER];
 
 const APP_PERSIST_CONFIG = {
   key: 'Storage',
-  keyPrefix: 'MYC:',
+  keyPrefix: 'MYC_',
   storage,
   blacklist: [],
-  throttle: 3000
+  throttle: 1000
   // serialize    -> (s) => marshallState(s)
   // deserialize  -> (s) => deMarshallState(s.legacay)
 };
 
-export const createPersistReducer = <T extends Reducer>(reducer: T) =>
+export const createPersistReducer = (reducer: Reducer<DataStore>) =>
   persistReducer(APP_PERSIST_CONFIG, reducer);
