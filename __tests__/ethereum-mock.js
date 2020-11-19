3fd7c180435a9555fbcc2aab638166bb994f6341
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-11-19 15:54:32 +0100

    Use predicate in waitForProperty

--- __tests__/ethereum-mock.js
+++ __tests__/ethereum-mock.js
@@ -1,21 +1,19 @@
 import { ClientFunction, t } from 'testcafe';
 
 import { FIXTURES_CONST } from './fixtures';
+import { waitForProperty } from './helpers';
 
 const NETWORKS = {
   5: 'https://goerli.mycryptoapi.com'
 };
 
-export const _setupEthereumMock = ClientFunction((privateKey, chainId, rpcUrl) => {
+const _setupEthereumMock = ClientFunction((privateKey, chainId, rpcUrl) => {
   window.ethereum.initialize(privateKey, chainId, rpcUrl);
 });
 
 export const setupEthereumMock = async (privateKey, chainId) => {
   await t
-    // Make sure to call the client function in order to assert against its return value:
-    // e.g "You passed a ClientFunction object to 't.expect()'.
-    // If you want to check the function's return value, use parentheses to call the function: fnName()."
-    .expect(ClientFunction(() => window.ethereum)())
+    .expect(waitForProperty((w) => w.ethereum.initialize))
     .ok({ timeout: FIXTURES_CONST.TIMEOUT });
   const rpcUrl = NETWORKS[chainId];
   await _setupEthereumMock(privateKey, chainId, rpcUrl);
--- __tests__/helpers.js
+++ __tests__/helpers.js
@@ -1,11 +1,20 @@
 import { ClientFunction } from 'testcafe';
 
-export const waitForProperty = ClientFunction((windowKey, timeout) => {
-  return new Promise(function (resolve, reject) {
+/**
+ * Testcafe is run in node, while ClientFunctions are run in the DOM.
+ * Custom window properties are not immediately available in ClientFunction.
+ * https://stackoverflow.com/a/56704217
+ */
+export const waitForProperty = ClientFunction((predicate, timeout) => {
+  return new Promise((resolve, reject) => {
     let intervalId = null;
     let timeoutId = null;
-    let checkCondition = function () {
-      return window[windowKey];
+
+    const checkCondition = function () {
+      if (typeof predicate === 'string') return window[predicate];
+      else {
+        return predicate(window);
+      }
     };
 
     timeoutId = setTimeout(function () {
