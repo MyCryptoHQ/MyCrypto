ef34fef6deb1b398521780c5312fbf8fc412a677
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-11-20 15:24:44 +0100

    Create serialize, deserialize transforms

--- src/services/Store/store/persist.config.ts
+++ src/services/Store/store/persist.config.ts
@@ -1,20 +1,70 @@
 import { Reducer } from '@reduxjs/toolkit';
-import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+import {
+  createTransform,
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE
+} from 'redux-persist';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import storage from 'redux-persist/lib/storage';
+import { ValuesType } from 'utility-types';
 
-import { DataStore } from '@types';
+import { DataStore, LocalStorage, LSKeys } from '@types';
 
 export const REDUX_PERSIST_ACTION_TYPES = [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER];
+
+// type ArrayToObj = <K extends string | number>(
+//   k: K
+// ) => <V extends any[]>(arr: V) => Record<K, ValuesType<V>>;
+const arrayToObj = (key) => (arr) => arr.reduce((acc, curr) => ({ ...acc, [curr[key]]: curr }), {});
+
+const serialize = (slice: ValuesType<DataStore>, key: string) => {
+  switch (key) {
+    case LSKeys.ACCOUNTS:
+    case LSKeys.ADDRESS_BOOK:
+    case LSKeys.CONTRACTS:
+    case LSKeys.ASSETS:
+    case LSKeys.NOTIFICATIONS:
+    case LSKeys.USER_ACTIONS:
+      return arrayToObj('uuid')(slice);
+    case LSKeys.NETWORKS:
+      return arrayToObj('id')(slice);
+    case LSKeys.SETTINGS:
+    case LSKeys.PASSWORD:
+    default:
+      return slice;
+  }
+};
+const deserialize = (slice: ValuesType<LocalStorage>, key: string) => {
+  switch (key) {
+    case LSKeys.ACCOUNTS:
+    case LSKeys.ADDRESS_BOOK:
+    case LSKeys.NETWORKS:
+    case LSKeys.CONTRACTS:
+    case LSKeys.ASSETS:
+    case LSKeys.NOTIFICATIONS:
+    case LSKeys.USER_ACTIONS:
+      return Object.values(slice);
+    case LSKeys.SETTINGS:
+    case LSKeys.PASSWORD:
+    default:
+      return slice;
+  }
+};
+
+const transform = createTransform(serialize, deserialize);
 
 const APP_PERSIST_CONFIG = {
   key: 'Storage',
   keyPrefix: 'MYC_',
   storage,
   blacklist: [],
-  stateReconciler: autoMergeLevel2
-  // serialize    -> (s) => marshallState(s)
-  // deserialize  -> (s) => deMarshallState(s.legacay)
+  stateReconciler: autoMergeLevel2,
+  transforms: [transform]
 };
 
 export const createPersistReducer = (reducer: Reducer<DataStore>) =>
