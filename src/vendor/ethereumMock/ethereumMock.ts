3ddfa4df07d8fadcf53c3f30da432e553900e9cb
Frederik Bolding <frederik.bolding@gmail.com>
2020-11-24 15:48:46 +0100

    Improve error handling in ethereum mock + Pass unknown methods to RPC

--- src/vendor/ethereumMock/ethereumMock.ts
+++ src/vendor/ethereumMock/ethereumMock.ts
@@ -1,13 +1,15 @@
 import { ethers, Wallet } from 'ethers';
+import { JsonRpcProvider } from 'ethers/providers';
 
 export const ethereumMock = () => {
   let wallet: Wallet;
+  let provider: JsonRpcProvider;
   let chainId: number;
 
   // CONFIG
   const initialize = (privKey: string, _chainId: number, jsonRpcUrl: string) => {
     chainId = _chainId;
-    const provider = new ethers.providers.JsonRpcProvider(jsonRpcUrl, chainId);
+    provider = new ethers.providers.JsonRpcProvider(jsonRpcUrl, chainId);
     wallet = new Wallet(privKey, provider);
   };
 
@@ -33,6 +35,10 @@ export const ethereumMock = () => {
     switch (method) {
       case 'eth_accounts':
         return [wallet.address];
+      case 'wallet_getPermissions':
+      case 'wallet_requestPermissions':
+        // Don't support for now
+        return undefined;
       case 'net_version':
         return chainId.toString();
       case 'eth_sendTransaction': {
@@ -43,11 +49,9 @@ export const ethereumMock = () => {
         });
         return result.hash;
       }
-      case 'eth_getTransactionCount': {
-        return '0x0';
-      }
       default:
-        return undefined;
+        // Default to sending to JSON RPC
+        return await provider.send(method, params);
     }
   };
 
@@ -55,7 +59,11 @@ export const ethereumMock = () => {
     { method, params, id }: { method: string; params: any; id: number },
     callback: (error: any, result: any) => void
   ) => {
-    getResult(method, params).then((result) => callback(null, wrapResult({ id, result })));
+    if (!provider || !wallet) {
+      callback('Mock not initialized', null);
+    } else {
+      getResult(method, params).then((result) => callback(null, wrapResult({ id, result })));
+    }
   };
 
   return { initialize, enable, sendAsync, on, removeAllListeners };
