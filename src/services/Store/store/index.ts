f79d3918061bfb835c2a7b48170c4b12d32e7062
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-11-24 21:56:43 +0100

    Refactor account.slice to legacy structure

--- src/services/Store/store/index.ts
+++ src/services/Store/store/index.ts
@@ -6,4 +6,5 @@ export { initialState as initialLegacyState } from './legacy.reducer';
 export { useSelector, default as useAppState } from './useAppState';
 export { useDispatch };
 export { createNotification, updateNotification } from './notification.slice';
+export { createAccount, destroyAccount, updateAccount, updateAccounts } from './account.slice';
 export { APP_PERSIST_CONFIG } from './persist.config';
