9f98b7a48e6904b343881f6965e569a2a73a5edb
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-11-26 12:16:15 +0100

    Add timeout to role

--- __tests__/roles.js
+++ __tests__/roles.js
@@ -2,7 +2,7 @@ import { getByText } from '@testing-library/testcafe';
 import { ClientFunction, Role } from 'testcafe';
 
 import { setupEthereumMock } from './ethereum-mock';
-import { ENV, PAGES } from './fixtures';
+import { ENV, FIXTURES_CONST, PAGES } from './fixtures';
 import { getTransValueByKey } from './translation-utils';
 
 const getCurrentLocation = ClientFunction(() => window.location.href);
@@ -14,7 +14,7 @@ export const web3Account = Role(
     await t
       .click(getByText(getTransValueByKey('ADD_WEB3_DEFAULT')))
       .expect(getCurrentLocation())
-      .eql(PAGES.DASHBOARD);
+      .eql(PAGES.DASHBOARD, { timeout: FIXTURES_CONST.TIMEOUT });
   },
   { preserveUrl: true }
 );
