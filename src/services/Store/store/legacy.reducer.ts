bbe9f56673dd3a41d063fc52d253dc3ec54a7276
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2020-12-15 17:28:32 +0100

    Use combine reducers

--- src/services/Store/store/legacy.reducer.ts
+++ src/services/Store/store/legacy.reducer.ts
@@ -1,53 +1,17 @@
-import { PayloadAction, Reducer } from '@reduxjs/toolkit';
+import { combineReducers, PayloadAction, Reducer } from '@reduxjs/toolkit';
 
-import { DataStore, LSKeys } from '@types';
+import { DataStore } from '@types';
 
-import accountSlice, {
-  createAccount,
-  createAccounts,
-  destroyAccount,
-  updateAccount,
-  updateAccountAssets,
-  updateAccounts
-} from './account.slice';
-import assetSlice, {
-  addAssetsFromAPI,
-  createAsset,
-  createAssets,
-  destroyAsset,
-  fetchAssets,
-  updateAsset,
-  updateAssets
-} from './asset.slice';
-import contactSlice, { createContact, destroyContact, updateContact } from './contact.slice';
-import contractSlice, { createContract, destroyContract } from './contract.slice';
+import accountSlice from './account.slice';
+import assetSlice from './asset.slice';
+import contactSlice from './contact.slice';
+import contractSlice from './contract.slice';
 import { initialLegacyState } from './legacy.initialState';
-import networkSlice, {
-  createNetwork,
-  createNetworks,
-  deleteNode,
-  destroyNetwork,
-  updateNetwork,
-  updateNetworks
-} from './network.slice';
-import notificationSlice, { createNotification, updateNotification } from './notification.slice';
-import passwordSlice, { setPassword } from './password.slice';
-import settingsSlice, {
-  addExcludedAsset,
-  addFavorite,
-  addFavorites,
-  removeExcludedAsset,
-  resetFavoritesTo,
-  setFiat,
-  setInactivityTimer,
-  setLanguage,
-  setRates
-} from './settings.slice';
-import userActionSlice, {
-  createUserAction,
-  destroyUserAction,
-  updateUserAction
-} from './userAction.slice';
+import networkSlice from './network.slice';
+import notificationSlice from './notification.slice';
+import passwordSlice from './password.slice';
+import settingsSlice from './settings.slice';
+import userActionSlice from './userAction.slice';
 
 export enum ActionT {
   RESET = 'RESET'
@@ -57,6 +21,18 @@ export enum ActionT {
 export function init(initialState: DataStore) {
   return initialState;
 }
+const databaseReducer = combineReducers({
+  version: () => initialLegacyState.version,
+  [accountSlice.name]: accountSlice.reducer,
+  [assetSlice.name]: assetSlice.reducer,
+  [contactSlice.name]: contactSlice.reducer,
+  [contractSlice.name]: contractSlice.reducer,
+  [networkSlice.name]: networkSlice.reducer,
+  [notificationSlice.name]: notificationSlice.reducer,
+  [settingsSlice.name]: settingsSlice.reducer,
+  [userActionSlice.name]: userActionSlice.reducer,
+  [passwordSlice.name]: passwordSlice.reducer
+});
 
 const legacyReducer: Reducer<DataStore, PayloadAction<any>> = (
   state = initialLegacyState,
@@ -68,104 +44,10 @@ const legacyReducer: Reducer<DataStore, PayloadAction<any>> = (
       const { data } = payload;
       return init(data as DataStore);
     }
-
-    /**
-     * Delegate notification handling to appropriate slice.
-     * We place it in legacy reducer instead of combine reducer to respect
-     * legacy state shape.
-     * @todo: Redux. Place in individual slice once reducer migration begins.
-     */
-    case addFavorite.type:
-    case addFavorites.type:
-    case resetFavoritesTo.type:
-    case setLanguage.type:
-    case setFiat.type:
-    case addExcludedAsset.type:
-    case removeExcludedAsset.type:
-    case setRates.type:
-    case setInactivityTimer.type: {
-      return {
-        ...state,
-        [LSKeys.SETTINGS]: settingsSlice.reducer(state.settings, action)
-      };
-    }
-    case createNetwork.type:
-    case createNetworks.type:
-    case updateNetwork.type:
-    case updateNetworks.type:
-    case destroyNetwork.type:
-    case deleteNode.type: {
-      return {
-        ...state,
-        [LSKeys.NETWORKS]: networkSlice.reducer(state.networks, action)
-      };
-    }
-    case createAccount.type:
-    case createAccounts.type:
-    case updateAccount.type:
-    case updateAccounts.type:
-    case destroyAccount.type:
-    case updateAccountAssets.type: {
-      return {
-        ...state,
-        [LSKeys.ACCOUNTS]: accountSlice.reducer(state.accounts, action)
-      };
-    }
-    case createAsset.type:
-    case createAssets.type:
-    case updateAsset.type:
-    case updateAssets.type:
-    case destroyAsset.type:
-    case addAssetsFromAPI.type:
-    case fetchAssets.type: {
-      return {
-        ...state,
-        [LSKeys.ASSETS]: assetSlice.reducer(state.assets, action)
-      };
-    }
-
-    case createContact.type:
-    case updateContact.type:
-    case destroyContact.type: {
-      return {
-        ...state,
-        [LSKeys.ADDRESS_BOOK]: contactSlice.reducer(state.addressBook, action)
-      };
-    }
-
-    case createUserAction.type:
-    case updateUserAction.type:
-    case destroyUserAction.type: {
-      return {
-        ...state,
-        [LSKeys.USER_ACTIONS]: userActionSlice.reducer(state.userActions, action)
-      };
-    }
-
-    case createContract.type:
-    case destroyContract.type: {
-      return {
-        ...state,
-        [LSKeys.CONTRACTS]: contractSlice.reducer(state.contracts, action)
-      };
-    }
-
-    case createNotification.type:
-    case updateNotification.type: {
-      return {
-        ...state,
-        [LSKeys.NOTIFICATIONS]: notificationSlice.reducer(state.notifications, action)
-      };
-    }
-    case setPassword.type: {
-      return {
-        ...state,
-        [LSKeys.PASSWORD]: passwordSlice.reducer(state.password, action)
-      };
-    }
     default: {
-      return state;
+      return databaseReducer(state, action);
     }
   }
 };
+
 export default legacyReducer;
