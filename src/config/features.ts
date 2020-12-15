f5016f9bf084b42b7f976304e1e495891d187e2c
Guillaume Roux <guillaumeroux123@gmail.com>
2020-12-15 17:48:31 +0100

    switch back feature flags

--- src/config/features.ts
+++ src/config/features.ts
@@ -15,8 +15,8 @@ export const FEATURE_FLAGS = {
   TX_HISTORY: 'core',
   MYC_MEMBERSHIP: 'core',
   DOWNLOAD_DESKTOP_APP: 'core',
-  OLD_NAVIGATION: false, //@todo: toggle back before merging
-  NEW_NAVIGATION: true, //@todo: toggle back before merging
+  OLD_NAVIGATION: !IS_DEV,
+  NEW_NAVIGATION: IS_DEV,
 
   /* Manage Assets */
   ADD_ACCOUNT: 'core',
