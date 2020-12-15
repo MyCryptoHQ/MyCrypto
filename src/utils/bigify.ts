f59a61388b72be12192806b928776f7a7a067a26
Frederik Bolding <frederik.bolding@gmail.com>
2020-12-15 15:54:31 +0100

    Support EthScan BN in bigify + simplify token fetching function

--- src/utils/bigify.ts
+++ src/utils/bigify.ts
@@ -1,10 +1,13 @@
+import { BigNumber as EthScanBN } from '@ethersproject/bignumber';
 import BigNumber from 'bignumber.js';
 import BN from 'bn.js';
 import { BigNumber as BigNumberish } from 'ethers/utils';
 
-export const bigify = (v: BigNumber.Value | BigNumber | BigNumberish): BigNumber => {
+export const bigify = (v: BigNumber.Value | BigNumber | BigNumberish | EthScanBN): BigNumber => {
   if (BigNumberish.isBigNumber(v)) {
     return new BigNumber(v.toString());
+  } else if (typeof v === 'object' && '_hex' in v) {
+    return new BigNumber(v._hex);
   } else {
     return new BigNumber(v);
   }
