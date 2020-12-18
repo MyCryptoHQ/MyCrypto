3bcdc8edd40c7ae3ef00be0b2bfd61380ccb000c
Frederik Bolding <frederik.bolding@gmail.com>
2020-12-18 11:31:41 +0100

    Fix selector circular dependency issue

--- src/services/Store/store/selectors.ts
+++ src/services/Store/store/selectors.ts
@@ -1,6 +1,5 @@
 import { DataStore } from '@types';
 
-import persistanceSlice from './persistance.slice';
 import { AppState } from './root.reducer';
 
-export const getAppState = (s: AppState): DataStore => s[persistanceSlice.name as 'database'];
+export const getAppState = (s: AppState): DataStore => s.database;
