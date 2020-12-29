4cadc5b228a80988246af9833736fb8c1244d316
Frederik Bolding <frederik.bolding@gmail.com>
2020-12-29 02:26:55 +0100

    Improve formatting, styling and typing

--- src/features/PortfolioValueGraph/PortfolioValueGraph.tsx
+++ src/features/PortfolioValueGraph/PortfolioValueGraph.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 
 import { Heading } from '@mycrypto/ui';
+import BigNumber from 'bignumber.js';
 import { formatEther } from 'ethers/utils';
 import { flatten } from 'ramda';
 import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis } from 'recharts';
@@ -9,7 +10,7 @@ import styled from 'styled-components';
 import { DashboardPanel } from '@components';
 import { StoreContext } from '@services';
 import { BREAK_POINTS, SPACING } from '@theme';
-import { bigify, fromWei, mapAsync } from '@utils';
+import { bigify, formatDate, mapAsync } from '@utils';
 
 const DashboardWrapper = styled.div`
   width: 100%;
@@ -31,11 +32,13 @@ const StyledLayout = styled.div`
   }
 `;
 
-const buildQueryURL = (asset, fiat, from, to) =>
+const buildQueryURL = (asset: string, fiat: string, from: number, to: number) =>
   `https://api.coingecko.com/api/v3/coins/${asset}/market_chart/range?vs_currency=${fiat}&from=${from}&to=${to}`;
 
+type PortfolioValueState = { timestamp: number; value: string }[];
+
 export default function PortfolioValueGraph() {
-  const [data, setData] = useState(undefined);
+  const [data, setData] = useState<PortfolioValueState | undefined>(undefined);
   const { assets } = useContext(StoreContext);
 
   useEffect(() => {
@@ -44,17 +47,16 @@ export default function PortfolioValueGraph() {
       if (!asset.mappings || !asset.mappings.coinGeckoId) {
         return [];
       }
-      console.log(formatEther(asset.balance));
       const url = buildQueryURL(asset.mappings!.coinGeckoId, 'eur', 1577491200, 1609169111);
       const result = await fetch(url).then((res) => res.json());
-      const prices = result.prices;
+      const prices = result.prices as [number, string][];
       return prices.map(([timestamp, value]) => ({
         value: bigify(formatEther(asset.balance)).multipliedBy(bigify(value)),
         timestamp
       }));
     })
       .then((result) =>
-        flatten(result).reduce((acc, value) => {
+        flatten(result).reduce<{ timestamp: number; value: BigNumber }[]>((acc, value) => {
           const existingIdx = acc.findIndex((a) => a.timestamp === value.timestamp);
           if (existingIdx === -1) {
             acc.push(value);
@@ -69,8 +71,6 @@ export default function PortfolioValueGraph() {
       .then(setData);
   }, []);
 
-  console.log(data);
-
   return (
     <StyledLayout>
       <DashboardWrapper>
@@ -79,11 +79,26 @@ export default function PortfolioValueGraph() {
         </DashboardSubHeader>
         <DashboardPanel heading="Portfolio Performance">
           {data ? (
-            <ResponsiveContainer width="95%" height={400}>
+            <ResponsiveContainer width="100%" height={400}>
               <LineChart data={data}>
-                <XAxis dataKey="timestamp" />
-                <Tooltip />
-                <Line type="monotone" dataKey="value" stroke="#8884d8" activeDot={{ r: 8 }} />
+                <XAxis
+                  dataKey="timestamp"
+                  interval={30}
+                  tickFormatter={(time) =>
+                    new Date(time).toLocaleString('default', { month: 'long' })
+                  }
+                />
+                <Tooltip
+                  labelFormatter={(time) => formatDate(parseInt(time.toString()) / 1000)}
+                  formatter={(value) => `${parseFloat(value.toString()).toFixed(4)} EUR`}
+                />
+                <Line
+                  type="monotone"
+                  dataKey="value"
+                  stroke="#8884d8"
+                  dot={false}
+                  activeDot={{ r: 8 }}
+                />
               </LineChart>
             </ResponsiveContainer>
           ) : (
