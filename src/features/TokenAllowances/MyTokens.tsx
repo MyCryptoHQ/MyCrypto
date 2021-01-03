2b1809e624623c9da86d97930c13b67a39703ee7
HARRY DENLEY <HARRY.DENLEY@OUTLOOK.COM>
2021-01-03 20:50:10 +0000

    Added contract to name mapping and filtered duplicate spender events to most recent

--- src/features/TokenAllowances/MyTokens.tsx
+++ src/features/TokenAllowances/MyTokens.tsx
@@ -7,7 +7,8 @@ import { ProviderHandler } from '@services';
 import { BREAK_POINTS, breakpointToNumber } from '@theme';
 import { translateRaw } from '@translations';
 import { Network } from '@types';
-import { isSameAddress } from '@utils';
+import { hexWeiToString } from '@utils';
+import { Contracts } from '@database';
 
 import { IFormattedLogEntry, MyTokensProps } from './types';
 
@@ -48,15 +49,12 @@ export default function MyTokens({ address, assets, network }: MyTokensProps) {
         logs.map((log) => {
           const spenderAddress = `0x` + log.topics[2].substr(log.topics[2].length - 40);
 
-          if (isSameAddress(spenderAddress, contractAddress)) {
-            return false;
-          }
-
           const formattedLog = {
             tokenContract: contractAddress,
             decimals: decimal,
             spender: spenderAddress,
-            allowance: log.data
+            allowance: log.data,
+            blockNumber: log.blockNumber
           };
 
           setTokenAllowances((tokenAllowances: IFormattedLogEntry) => [
@@ -69,32 +67,48 @@ export default function MyTokens({ address, assets, network }: MyTokensProps) {
   };
 
   const formatAllowance = (allowance: string, decimals: number) => {
-    // @todo - format this to be human readable (ie: 1000...00000/1e18)
-    return `${allowance}/${decimals}`;
+    // @todo - get the totalSupply of the contract also
+    if (allowance === [`0x`, `f`.repeat(64)].join("")) {
+      return `Unlimited`;
+    }
+
+    return `${allowance}/${decimals} - ${(hexWeiToString(allowance))}`;
   };
 
   const formatAddress = (address: string) => {
     const addr = `0x` + address.substr(address.length - 40);
-    //@todo - do some address to label lookup here!
+
+    const namedContract = Contracts.Ethereum.filter((contract) => contract.address.toLowerCase() === address.toLowerCase())
+    if (namedContract.length > 0) {
+      //@todo - make this link out to the block explorer?
+      return namedContract[0].name
+    }
+
     return addr;
   };
 
   const formatAllowanceRecords = (contractAddress: string) => {
+
     const contractSpecificEvents = tokenAllowances.filter(
       (ev: IFormattedLogEntry) => ev.tokenContract === contractAddress
     );
 
     if (contractSpecificEvents.length === 0) {
-      return <NoSpenders>No address is approved to spend this token</NoSpenders>;
+      return <NoSpenders>No third-party address is approved to spend this token</NoSpenders>;
     }
+
+    // Sort by blocknumber desc
+    contractSpecificEvents.sort((a: IFormattedLogEntry, b: IFormattedLogEntry) => a.blockNumber < b.blockNumber ? 1 : -1)
+    // Return only the most recent log entry per 1 spender address
+    const uniqueSpenders = [...new Map(contractSpecificEvents.map((logEvent: IFormattedLogEntry) => [logEvent.spender, logEvent])).values()];
 
     return (
       <ul>
-        {contractSpecificEvents.map((ev: IFormattedLogEntry, index: number) => {
+        {uniqueSpenders.map((ev: IFormattedLogEntry, index: number) => {
           const { spender, allowance, decimals } = ev;
           return (
             <li key={index}>
-              {formatAddress(spender)} can spend {formatAllowance(allowance, decimals)}
+              {formatAddress(spender)} can spend {formatAllowance(allowance, decimals)} tokens
             </li>
           );
         })}
