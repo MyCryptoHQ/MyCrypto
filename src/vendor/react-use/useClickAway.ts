bc50b7e257718935049dd64037cd6f33af66974a
Guillaume Roux <guillaumeroux123@gmail.com>
2021-01-07 11:07:29 +0100

    add useClickAway hook

---
+++ src/vendor/react-use/useClickAway.ts
@@ -0,0 +1,32 @@
+import { RefObject, useEffect, useRef } from 'react';
+
+import { off, on } from './util';
+
+const defaultEvents = ['mousedown', 'touchstart'];
+
+const useClickAway = <E extends Event = Event>(
+  ref: RefObject<HTMLElement | null>,
+  onClickAway: (event: E) => void,
+  events: string[] = defaultEvents
+) => {
+  const savedCallback = useRef(onClickAway);
+  useEffect(() => {
+    savedCallback.current = onClickAway;
+  }, [onClickAway]);
+  useEffect(() => {
+    const handler = (event: E) => {
+      const { current: el } = ref;
+      el && !el.contains(event.target as Node) && savedCallback.current(event);
+    };
+    for (const eventName of events) {
+      on(document, eventName, handler);
+    }
+    return () => {
+      for (const eventName of events) {
+        off(document, eventName, handler);
+      }
+    };
+  }, [events, ref]);
+};
+
+export default useClickAway;
