656c0affd714d925d25bfb1217881bae94ccc780
Guillaume Roux <guillaumeroux123@gmail.com>
2021-01-07 17:08:07 +0100

    replace old navigation by the new one

--- src/config/features.ts
+++ src/config/features.ts
@@ -15,8 +15,8 @@ export const FEATURE_FLAGS = {
   TX_HISTORY: 'core',
   MYC_MEMBERSHIP: 'core',
   DOWNLOAD_DESKTOP_APP: 'core',
-  OLD_NAVIGATION: !IS_DEV,
-  NEW_NAVIGATION: IS_DEV,
+  OLD_NAVIGATION: false,
+  NEW_NAVIGATION: true,
 
   /* Manage Assets */
   ADD_ACCOUNT: 'core',
