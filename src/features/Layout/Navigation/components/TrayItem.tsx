87dfa7434a09877203a2178de8558c4cc3ec71ef
Guillaume Roux <guillaumeroux123@gmail.com>
2021-01-07 17:11:42 +0100

    add missing tools links

--- src/features/Layout/Navigation/components/TrayItem.tsx
+++ src/features/Layout/Navigation/components/TrayItem.tsx
@@ -9,7 +9,11 @@ import { IRouteLink } from '@types';
 export const TrayItem = ({ item, current }: { item: IRouteLink; current: boolean }) => (
   <Link style={{ width: '100%', margin: '8px 0' }} to={item.to}>
     <Box variant="rowAlign">
-      <Icon color={COLORS.BLUE_DARK_SLATE} type={item.icon} width="24px" />
+      {item.icon ? (
+        <Icon color={COLORS.BLUE_DARK_SLATE} type={item.icon} width="24px" />
+      ) : (
+        <Box width="24px"> </Box>
+      )}
       <Text ml="15px" color="BLUE_DARK_SLATE" fontWeight={current ? 'bold' : 'normal'}>
         {item.title}
       </Text>
