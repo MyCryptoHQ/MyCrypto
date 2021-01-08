994edea3a11c45e0dd2cce466259ebba930eb559
Guillaume Roux <guillaumeroux123@gmail.com>
2021-01-08 18:23:25 +0100

    change account table right icons

--- src/components/WalletUnlock/DeterministicAccountTable.tsx
+++ src/components/WalletUnlock/DeterministicAccountTable.tsx
@@ -372,7 +372,7 @@ const DeterministicTable = ({
             onClick={generateFreshAddress}
             disabled={!isComplete || freshAddressIndex >= DEFAULT_GAP_TO_SCAN_FOR}
           >
-            <Icon type="add" color="none" />
+            <Icon type="add" color="none" width="32px" />
 
             {!isComplete ? (
               <Tooltip tooltip={<Trans id="DETERMINISTIC_WAIT_FOR_SCAN" />}>
