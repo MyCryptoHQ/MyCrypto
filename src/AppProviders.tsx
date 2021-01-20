2ce8e9335634990312706b18267cbc610e145689
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-01-20 17:25:14 +0100

    Respect FeatureFlag in trackInit

--- src/AppProviders.tsx
+++ src/AppProviders.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 
 import { ErrorProvider } from '@features';
-import { DevToolsProvider, FeatureFlagProvider, RatesProvider, StoreProvider } from '@services';
+import { DevToolsProvider, RatesProvider, StoreProvider } from '@services';
 import { DataProvider } from '@services/Store';
 
 function AppProviders({ children }: { children: JSX.Element[] | JSX.Element | null }) {
   return (
-    <FeatureFlagProvider>
-      <DevToolsProvider>
-        <ErrorProvider>
-          <DataProvider>
-            {/* StoreProvider relies on the others Providers */}
-            <StoreProvider>
-              {/* RatesProvider relies on the Store */}
-              <RatesProvider>{children}</RatesProvider>
-            </StoreProvider>
-          </DataProvider>
-        </ErrorProvider>
-      </DevToolsProvider>
-    </FeatureFlagProvider>
+    <DevToolsProvider>
+      <ErrorProvider>
+        <DataProvider>
+          {/* StoreProvider relies on the others Providers */}
+          <StoreProvider>
+            {/* RatesProvider relies on the Store */}
+            <RatesProvider>{children}</RatesProvider>
+          </StoreProvider>
+        </DataProvider>
+      </ErrorProvider>
+    </DevToolsProvider>
   );
 }
 
