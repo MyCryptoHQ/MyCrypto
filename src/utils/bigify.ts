05a5a8f178b952146dff360b7a05d340997c38da
Frederik Bolding <frederik.bolding@gmail.com>
2021-01-21 13:27:18 +0100

    Use hex string for bigify

--- src/utils/bigify.ts
+++ src/utils/bigify.ts
@@ -8,8 +8,10 @@ export type Bigish = BigNumber;
 
 export const bigify = (v: BigNumber.Value | BigNumber | BigNumberish | bigint | BN): BigNumber => {
   BigNumber.config({ DECIMAL_PLACES: DEFAULT_ASSET_DECIMAL, EXPONENTIAL_AT: 1e9 });
-  if (BigNumberish.isBigNumber(v) || BN.isBN(v)) {
-    return new BigNumber(v.toString());
+  if (BigNumberish.isBigNumber(v)) {
+    return new BigNumber(v.toHexString());
+  } else if (BN.isBN(v)) {
+    return new BigNumber(v.toString('hex'));
   } else if (typeof v === 'object' && '_hex' in v) {
     return new BigNumber(v._hex);
   } else if (typeof v === 'bigint') {
