893e7a4b6f7b8bbf870cadacd618b6e57c4839b6
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-01-25 23:13:01 +0100

    Cleanup useAnalytics test

--- src/services/Analytics/useAnalytics.test.tsx
+++ src/services/Analytics/useAnalytics.test.tsx
@@ -1,36 +1,21 @@
 import { act, renderHook } from '@testing-library/react-hooks';
 import { actionWithPayload, mockUseDispatch, ProvidersWrapper } from 'test-utils';
 
-import { useFeatureFlags } from '@services';
-import { useEffectOnce } from '@vendor';
-
 import { default as useAnalytics } from './useAnalytics';
 
-const renderUseAnalytics = (isActive: boolean = true) => {
-  return renderHook(
-    () => {
-      const { setFeatureFlag } = useFeatureFlags();
-      useEffectOnce(() => {
-        if (!isActive) {
-          setFeatureFlag('ANALYTICS', false);
-        }
-      });
-
-      return useAnalytics();
-    },
-    { wrapper: ProvidersWrapper }
-  );
+const renderUseAnalytics = () => {
+  return renderHook(useAnalytics, { wrapper: ProvidersWrapper });
 };
 
 describe('useAnalytics', () => {
-  it('track(): dispatchs action when feature is active', () => {
+  it('track(): dispatchs action', () => {
     const mockDispatch = mockUseDispatch();
     const params = { name: 'Add Account' };
     const { result } = renderUseAnalytics();
     act(() => result.current.track(params));
     expect(mockDispatch).toHaveBeenCalledWith(actionWithPayload(params));
   });
-  it('trackPage(): dispatchs action when feature is active', () => {
+  it('trackPage(): dispatchs action', () => {
     const mockDispatch = mockUseDispatch();
     const params = { name: 'Knowledge base', title: 'dummy title' };
     const { result } = renderUseAnalytics();
