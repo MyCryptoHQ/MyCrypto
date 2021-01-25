41a9085825c08ce5c20b925ffa726f05c703606b
Frederik Bolding <frederik.bolding@gmail.com>
2021-01-25 22:03:28 +0100

    Delete extra network data + fix test

--- src/database/data/contracts/index.ts
+++ src/database/data/contracts/index.ts
@@ -2,12 +2,10 @@ import { Contract } from '@types';
 
 import ARTIS_SIGMA1 from './artis_sigma1.json';
 import ARTIS_TAU1 from './artis_tau1.json';
-import ESN from './esn.json';
 import ETC from './etc.json';
 import Ethereum from './eth.json';
 import EXP from './exp.json';
 import Goerli from './goerli.json';
-import PIRL from './pirl.json';
 import Rinkeby from './rinkeby.json';
 import Ropsten from './ropsten.json';
 import RSK from './rsk.json';
@@ -27,8 +25,6 @@ export const Contracts: Contracts = {
   Goerli,
   RSK,
   UBQ,
-  ESN,
   ARTIS_SIGMA1,
-  ARTIS_TAU1,
-  PIRL
+  ARTIS_TAU1
 };
