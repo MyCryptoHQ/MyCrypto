8b9a45210cfdb7978b9066bd3e000fc8b5ea88bc
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-01-27 00:27:04 +0100

    Check object existence

--- src/components/AccountList.tsx
+++ src/components/AccountList.tsx
@@ -530,7 +530,7 @@ const BuildAccountTable = (
           </LabelWithWallet>
         </Label>,
         <EthAddress key={index} address={account.address} truncate={true} isCopyable={copyable} />,
-        <Network key={index} color={account.network.color || COLORS.LIGHT_PURPLE}>
+        <Network key={index} color={account?.network?.color || COLORS.LIGHT_PURPLE}>
           {account.networkId}
         </Network>,
         isScanning ? (
