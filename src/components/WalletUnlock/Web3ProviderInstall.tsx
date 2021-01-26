70e537c3dc9fe287c52c4439b3b994cd5e6dd01c
blurpesec <hahn.michael.f@gmail.com>
2021-01-26 10:42:29 -0800

    delete scss for web3providerinstall

--- src/components/WalletUnlock/Web3ProviderInstall.tsx
+++ src/components/WalletUnlock/Web3ProviderInstall.tsx
@@ -1,93 +1,119 @@
 import React from 'react';
 
 import { Typography } from '@mycrypto/ui';
+import styled from 'styled-components';
 
 import AppStoreBadgeIMG from '@assets/images/mobile/app-store-badge.png';
 import GooglePlayBadgeIMG from '@assets/images/mobile/google-play-badge.png';
-import { Box, Heading, NewTabLink } from '@components';
+import { Box, Button, Heading, NewTabLink } from '@components';
 import { DOWNLOAD_MYCRYPTO_LINK, WALLETS_CONFIG } from '@config';
+import { BREAK_POINTS, SPACING } from '@theme';
 import translate from '@translations';
 import { useScreenSize } from '@utils';
-import './Web3ProviderInstall.scss';
+
+const Web3ImgContainer = styled.div`
+  margin: 2em;
+  padding-top: 2em;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+
+  @media (max-width: ${BREAK_POINTS.SCREEN_SM}) {
+    margin: 0.5em;
+    padding-top: 0.5em;
+  }
+`;
+
+const DownloadOption = styled(NewTabLink)`
+  margin-top: ${SPACING.BASE};
+`;
+
+const Footer = styled.div`
+  text-align: center;
+`;
+
+const SImgContainer = styled.div`
+  height: 150px;
+  width: 150px;
+`;
 
 function InstallTrunk() {
   const providers = [WALLETS_CONFIG.TRUST, WALLETS_CONFIG.COINBASE];
   return (
-    <div className="Web3-options">
+    <Box variant="rowCenter" mt={SPACING.BASE}>
       {providers.map((provider) => (
-        <div key={provider.id} className="Provider-container">
+        <Box key={provider.id} variant="columnCenter" margin={SPACING.SM}>
           <NewTabLink href={provider.install ? provider.install.getItLink : undefined}>
-            <div className="Provider-img">
+            <SImgContainer>
               <img src={provider.icon} />
-            </div>
+            </SImgContainer>
           </NewTabLink>
 
           <Typography>{provider.name}</Typography>
           {provider.install && provider.install.appStore && (
-            <NewTabLink className="download-option" href={provider.install.appStore}>
+            <DownloadOption href={provider.install.appStore}>
               <img src={AppStoreBadgeIMG} />
-            </NewTabLink>
+            </DownloadOption>
           )}
 
           {provider.install && provider.install.googlePlay && (
-            <NewTabLink className="download-option" href={provider.install.googlePlay}>
+            <DownloadOption href={provider.install.googlePlay}>
               <img src={GooglePlayBadgeIMG} />
-            </NewTabLink>
+            </DownloadOption>
           )}
-        </div>
+        </Box>
       ))}
-    </div>
+    </Box>
   );
 }
 
-function InstallMetaMask() {
+const InstallMetaMask = () => {
   const provider = WALLETS_CONFIG.METAMASK;
   return (
     <>
       <div>
-        <div className="Panel-content-img">
-          <div className="Provider-img">
+        <Web3ImgContainer>
+          <SImgContainer>
             <img src={provider.icon} />
-          </div>
-        </div>
+          </SImgContainer>
+        </Web3ImgContainer>
         <a
           href={provider.install ? provider.install.getItLink : undefined}
           target="_blank"
           rel="noreferrer"
         >
-          <button className="btn btn-primary btn-lg btn-block">
-            {translate('PROVIDER_DOWNLOAD', { $provider: provider.name })}
-          </button>
+          <Button>{translate('PROVIDER_DOWNLOAD', { $provider: provider.name })}</Button>
         </a>
       </div>
-      <div className="Provider-footer">
+      <Footer>
         {translate('ADD_ACCOUNT_WEB3_INSTALL_FOOTER')} <br />
         <NewTabLink
           content={translate('ADD_ACCOUNT_WEB3_INSTALL_FOOTER_LINK')}
           href={DOWNLOAD_MYCRYPTO_LINK}
         />
-      </div>
+      </Footer>
     </>
   );
-}
+};
 
-function Web3ProviderInstall() {
+const Web3ProviderInstall = () => {
   const { isMobile } = useScreenSize();
-  return (
-    <Box p="2.5em">
-      <Heading fontSize="32px" textAlign="center" fontWeight="bold">
-        {translate('ADD_ACCOUNT_WEB3_INSTALL_TITLE', {
-          $walletId: isMobile ? 'Web3 Provider' : 'Metamask'
-        })}
-      </Heading>
-      <div className="Panel-description">
-        {translate(
-          isMobile ? 'ADD_ACCOUNT_WEB3_INSTALL_MOBILE_DESC' : 'ADD_ACCOUNT_WEB3_INSTALL_DESC'
-        )}
-      </div>
-      <div className="Panel-content">{isMobile ? <InstallTrunk /> : <InstallMetaMask />}</div>
-    </Box>
-  );
-}
+  return <Web3ProviderInstallUI isMobile={isMobile} />;
+};
 
+export const Web3ProviderInstallUI = ({ isMobile }: { isMobile: boolean }) => (
+  <Box p="2.5em">
+    <Heading fontSize="32px" textAlign="center" fontWeight="bold">
+      {translate('ADD_ACCOUNT_WEB3_INSTALL_TITLE', {
+        $walletId: isMobile ? 'Web3 Provider' : 'Metamask'
+      })}
+    </Heading>
+    <div>
+      {translate(
+        isMobile ? 'ADD_ACCOUNT_WEB3_INSTALL_MOBILE_DESC' : 'ADD_ACCOUNT_WEB3_INSTALL_DESC'
+      )}
+    </div>
+    <div>{isMobile ? <InstallTrunk /> : <InstallMetaMask />}</div>
+  </Box>
+);
 export default Web3ProviderInstall;
