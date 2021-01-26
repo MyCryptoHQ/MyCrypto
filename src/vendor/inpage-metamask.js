c11ac2e136320c3c625a07f33db2ad9b7985e94c
Frederik Bolding <frederik.bolding@gmail.com>
2021-01-26 17:22:47 +0100

    Add debug code

--- src/vendor/inpage-metamask.js
+++ src/vendor/inpage-metamask.js
@@ -5,6 +5,32 @@ import LocalMessageDuplexStream from 'post-message-stream';
 // Due to https://github.com/MetaMask/metamask-extension/issues/3133
 
 (() => {
+  //debugger;
+  var consolere = {
+    channel: 'try-1dbb-acac-eb23',
+    api: '//console.re/connector.js',
+    ready: function (c) {
+      var d = document,
+        s = d.createElement('script'),
+        l;
+      s.src = this.api;
+      s.id = 'consolerescript';
+      s.onreadystatechange = s.onload = function () {
+        if (!l) {
+          c();
+        }
+        l = true;
+      };
+      d.getElementsByTagName('head')[0].appendChild(s);
+    }
+  };
+
+  consolere.ready(() => {
+    // eslint-disable-next-line no-global-assign
+    console = console.re;
+    console.re.log('remote log test');
+  });
+
   if (!window.ethereum && !window.web3 && navigator.userAgent.includes('Firefox')) {
     // setup background connection
     const metamaskStream = new LocalMessageDuplexStream({
