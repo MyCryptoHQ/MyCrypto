c38fe8d3f26d77feaf3a0fdcc7d586426ade33a7
Frederik Bolding <frederik.bolding@gmail.com>
2021-01-28 16:06:20 +0100

    Use actual links for Busy Bottoms (#3788)

--- src/components/BusyBottom/BusyBottom.tsx
+++ src/components/BusyBottom/BusyBottom.tsx
@@ -1,38 +1,38 @@
 import React from 'react';
 
-import { useHistory } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 
-import { Box, Icon, Link } from '@components';
+import { Box, Icon, NewTabLink } from '@components';
+import { Link } from '@components/NewTypography';
 import { translateRaw } from '@translations';
-import { BusyBottomConfig, TURL } from '@types';
-import { openLink } from '@utils';
+import { BusyBottomConfig } from '@types';
 
 import { configs } from './constants';
 
 export const BusyBottom = ({ type }: { type: BusyBottomConfig }) => {
-  const history = useHistory();
-
   return (
     <Box mt={4}>
       <Box style={{ textAlign: 'center' }} mb={2}>
         {translateRaw('BUSY_BOTTOM_HEADER')}
       </Box>
       <Box variant="rowAlign" justifyContent="center">
-        {configs[type].map(({ copy, copyVariables, link, external }, index) => (
-          <>
-            <Box
-              key={link}
-              onClick={() => (external ? openLink(link as TURL) : history.push(link!))}
-            >
-              <Link>{translateRaw(copy, copyVariables)}</Link>
-            </Box>
-            {index < configs[type].length - 1 && (
-              <Box paddingX="15px" paddingY={1} variant="rowAlign">
-                <Icon type="separator" />
-              </Box>
-            )}
-          </>
-        ))}
+        {configs[type].map(({ copy, copyVariables, link, external }, index) => {
+          const linkContent = <Link>{translateRaw(copy, copyVariables)}</Link>;
+          return (
+            <>
+              {external ? (
+                <NewTabLink href={link}>{linkContent}</NewTabLink>
+              ) : (
+                <RouterLink to={link!}>{linkContent}</RouterLink>
+              )}
+              {index < configs[type].length - 1 && (
+                <Box paddingX="15px" paddingY={1} variant="rowAlign">
+                  <Icon type="separator" />
+                </Box>
+              )}
+            </>
+          );
+        })}
       </Box>
     </Box>
   );
