ae5dc96fde2ad983a8661c357e613a70032a41e4
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-02-02 19:50:25 +0100

    Remove HelpLink

--- src/components/LinkApp.tsx
+++ src/components/LinkApp.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom';
-<<<<<<< HEAD
 import styled from 'styled-components';
 import {
   color,
@@ -50,21 +49,11 @@ interface LinkProps {
   readonly href: string;
   readonly isExternal?: boolean;
   readonly variant?: 'inlineLink' | 'defaultLink';
-=======
-
-import { Text, TextProps } from '@components';
-
-interface LinkProps {
-  isExternal: boolean;
-  variant?: 'inlineLink' | 'defaultLink';
-  href: string;
->>>>>>> c5a320e93 (Create LinkApp)
   onClick?(): void;
 }
 
 type LinkAppProps = LinkProps & Omit<RouterLinkProps, 'to'>;
 
-<<<<<<< HEAD
 const LinkApp: React.FC<LinkAppProps & TextProps> = ({
   href,
   isExternal = false,
@@ -72,23 +61,12 @@ const LinkApp: React.FC<LinkAppProps & TextProps> = ({
   onClick,
   ...props
 }) => {
-=======
-const LinkApp = ({
-  isExternal = false,
-  href,
-  variant = 'defaultLink',
-  children,
-  onClick,
-  ...props
-}: LinkAppProps & TextProps & { children: React.ReactNode }) => {
->>>>>>> c5a320e93 (Create LinkApp)
   const handleClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     if (!onClick) return;
     event.preventDefault();
     onClick();
   };
 
-<<<<<<< HEAD
   return isExternal ? (
     <SLink
       href={href}
@@ -101,25 +79,6 @@ const LinkApp = ({
   ) : (
     <SRouterLink to={href} variant={variant} onClick={handleClick} {...props} />
   );
-=======
-  if (isExternal) {
-    return (
-      <a href={href} target="_blank" rel="noreferrer" onClick={handleClick}>
-        <Text as="span" variant={variant}>
-          {children}
-        </Text>
-      </a>
-    );
-  } else {
-    return (
-      <RouterLink to={href} onClick={handleClick} {...props}>
-        <Text as="span" variant={variant}>
-          {children}
-        </Text>
-      </RouterLink>
-    );
-  }
->>>>>>> c5a320e93 (Create LinkApp)
 };
 
 export default LinkApp;
