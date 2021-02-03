80d297d025e0a47ce97238e1aee67137ae6b2f62
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-02-03 19:05:06 +0100

    Use styled-system for LinkApp

--- src/components/LinkApp.tsx
+++ src/components/LinkApp.tsx
@@ -1,8 +1,49 @@
 import React from 'react';
 
 import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom';
+import styled from 'styled-components';
+import {
+  color,
+  colorStyle,
+  fontStyle,
+  layout,
+  lineHeight,
+  size,
+  space,
+  textStyle,
+  typography
+} from 'styled-system';
 
-import { Text, TextProps } from '@components';
+import { TextProps } from '@components';
+import { textVariants } from '@theme';
+
+const SLink = styled.a<TextProps & HTMLAnchorElement>`
+  ${textVariants}
+  ${space}
+  ${fontStyle}
+  ${color}
+  ${size}
+  ${colorStyle}
+  ${textStyle}
+  ${lineHeight}
+  ${typography}
+  ${layout}
+  ${({ textTransform }) => textTransform && { 'text-transform': textTransform }}
+`;
+
+const SRouterLink = styled(RouterLink)<TextProps & RouterLinkProps>`
+  ${textVariants}
+  ${space}
+  ${fontStyle}
+  ${color}
+  ${size}
+  ${colorStyle}
+  ${textStyle}
+  ${lineHeight}
+  ${typography}
+  ${layout}
+  ${({ textTransform }) => textTransform && { 'text-transform': textTransform }}
+`;
 
 interface LinkProps {
   readonly href: string;
@@ -13,14 +54,13 @@ interface LinkProps {
 
 type LinkAppProps = LinkProps & Omit<RouterLinkProps, 'to'>;
 
-const LinkApp = ({
+const LinkApp: React.FC<LinkAppProps & TextProps> = ({
   href,
   isExternal = false,
   variant = 'defaultLink',
-  children,
   onClick,
   ...props
-}: LinkAppProps & TextProps & { children: React.ReactNode }) => {
+}) => {
   const handleClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     if (!onClick) return;
     event.preventDefault();
@@ -29,20 +69,17 @@ const LinkApp = ({
 
   if (isExternal) {
     return (
-      <a href={href} target="_blank" rel="noreferrer" onClick={handleClick}>
-        <Text as="span" variant={variant}>
-          {children}
-        </Text>
-      </a>
+      <SLink
+        href={href}
+        variant={variant}
+        target="_blank"
+        rel="noreferrer"
+        onClick={handleClick}
+        {...props}
+      />
     );
   } else {
-    return (
-      <RouterLink to={href} onClick={handleClick} {...props}>
-        <Text as="span" variant={variant}>
-          {children}
-        </Text>
-      </RouterLink>
-    );
+    return <SRouterLink to={href} variant={variant} onClick={handleClick} {...props} />;
   }
 };
 
