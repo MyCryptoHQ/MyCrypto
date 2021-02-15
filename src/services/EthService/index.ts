78d7d86d658c152cf1363297158eddcfe13cee81
Frederik Bolding <frederik.bolding@gmail.com>
2021-02-15 14:43:09 +0100

    Unnest folder

--- src/services/EthService/index.ts
+++ src/services/EthService/index.ts
@@ -9,7 +9,7 @@ export {
   AntMigrator,
   GolemV2Migration
 } from './contracts';
-export { Web3Node, isWeb3Node, setupWeb3Node } from './nodes';
+export { Web3Node, isWeb3Node, setupWeb3Node } from './web3';
 export {
   isValidPath,
   isValidETHAddress,
