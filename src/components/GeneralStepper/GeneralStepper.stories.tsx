9433a0204d122bea4efaaac2e67706b0f1872232
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-02-22 18:20:12 +0100

    Activate GeneralStepper

--- src/components/GeneralStepper/GeneralStepper.stories.tsx
+++ src/components/GeneralStepper/GeneralStepper.stories.tsx
@@ -1,56 +1,52 @@
-// import React from 'react';
+import React from 'react';
 
-// import { MemoryRouter, Route, Switch } from 'react-router-dom';
+import { GeneralStepper as GeneralStepperComponent, StepperProps } from './GeneralStepper';
 
-// import { GeneralStepper, StepperProps } from './GeneralStepper';
+const ExampleButtonComponent = ({ onComplete, onCompleteText }: any) => (
+  <>
+    <button onClick={() => onComplete(onCompleteText)}>Click Me</button>
+  </>
+);
 
-// const ExampleButtonComponent = ({ onComplete, onCompleteText }: any) => (
-//   <>
-//     <button onClick={() => onComplete(onCompleteText)}>Click Me</button>
-//   </>
-// );
+const ExampleButtonFinalComponent = ({
+  onComplete,
+  onCompleteText,
+  resetFlow,
+  completeButtonText
+}: any) => (
+  <>
+    <button onClick={() => onComplete(onCompleteText)}>Click Me</button>
+    <button onClick={() => resetFlow()}>{completeButtonText}</button>
+  </>
+);
 
-// const ExampleButtonFinalComponent = ({
-//   onComplete,
-//   onCompleteText,
-//   resetFlow,
-//   completeButtonText
-// }: any) => (
-//   <>
-//     <button onClick={() => onComplete(onCompleteText)}>Click Me</button>
-//     <button onClick={() => resetFlow()}>{completeButtonText}</button>
-//   </>
-// );
+const functionTest = (_: string, cb: any) => {
+  cb();
+};
 
-// const functionTest = (_: string, cb: any) => {
-//   cb();
-// };
+export default {
+  title: 'Molecules/GeneralStepper',
+  component: GeneralStepperComponent
+};
 
-// const props: StepperProps = {
-//   steps: [
-//     {
-//       label: 'Test Component 1',
-//       component: ExampleButtonComponent,
-//       props: { onCompleteText: 'Finished this1' },
-//       actions: (payload: any, cb: any) => functionTest(payload, cb)
-//     },
-//     {
-//       label: 'Test Component 2',
-//       component: ExampleButtonFinalComponent,
-//       props: { onCompleteText: 'Finished this2' }
-//     }
-//   ],
-//   defaultBackPath: '/dashboard',
-//   defaultBackPathLabel: 'Dashboard',
-//   completeBtnText: 'Finished'
-// };
+const Template = (args: StepperProps) => <GeneralStepperComponent {...args} />;
 
-// export default {
-//   title: 'Features/GeneralStepper',
-//   component: GeneralStepper
-// };
-
-// const Template = (args: StepperProps) => <GeneralStepper {...args} />;
-
-// export const DefaultState = Template.bind({});
-// DefaultState.args = props;
+export const GeneralStepper = Template.bind({});
+GeneralStepper.args = {
+  steps: [
+    {
+      label: 'Test Component 1',
+      component: ExampleButtonComponent,
+      props: { onCompleteText: 'Finished this1' },
+      actions: (payload: any, cb: any) => functionTest(payload, cb)
+    },
+    {
+      label: 'Test Component 2',
+      component: ExampleButtonFinalComponent,
+      props: { onCompleteText: 'Finished this2' }
+    }
+  ],
+  defaultBackPath: '/dashboard',
+  defaultBackPathLabel: 'Dashboard',
+  completeBtnText: 'Finished'
+};
