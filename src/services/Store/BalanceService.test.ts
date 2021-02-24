72758789b7870bb9a8b8746b7fd19ee25a55037b
blurpesec <hahn.michael.f@gmail.com>
2021-02-24 14:16:27 -0800

    added test for getSingleTokenBalanceForAddresses

--- src/services/Store/BalanceService.test.ts
+++ src/services/Store/BalanceService.test.ts
@@ -9,6 +9,7 @@ import { bigify } from '@utils';
 import {
   BalanceMap,
   getBaseAssetBalancesForAddresses,
+  getSingleTokenBalanceForAddresses,
   getTokenBalancesForAddresses
 } from './BalanceService';
 
@@ -21,9 +22,13 @@ jest.mock('@mycrypto/eth-scan', () => ({
     '0xfE5443FaC29fA621cFc33D41D1927fd0f5E0bB7c': {
       '0xad6d458402f60fd3bd25163575031acdce07538d': BigInt('1000000000000000000')
     }
+  })),
+  getTokenBalances: jest.fn().mockImplementation(async () => ({
+    '0xfE5443FaC29fA621cFc33D41D1927fd0f5E0bB7c': BigInt('1000000000000000000')
   }))
 }));
 ProviderHandler.prototype.getRawTokenBalance = jest.fn().mockResolvedValue('150');
+ProviderHandler.prototype.getTokenBalance = jest.fn().mockResolvedValue('150');
 ProviderHandler.prototype.getBalance = jest.fn().mockResolvedValue('150');
 ProviderHandler.fetchProvider = jest.fn().mockResolvedValue({} as FallbackProvider);
 
@@ -76,6 +81,30 @@ describe('getTokenBalancesForAddresses', () => {
         [fRopDAI.contractAddress as string]: bigify('150')
       }
     } as BalanceMap<BalanceMap<BigNumber>>;
+    expect(balances).toStrictEqual(expected);
+  });
+});
+
+describe('getSingleTokenBalanceForAddresses', () => {
+  it('gets result for getSingleTokenBalanceForAddresses with one address on an ethscan network', async () => {
+    const balances = await getSingleTokenBalanceForAddresses(fRopDAI, fNetworks[1], [
+      '0xfE5443FaC29fA621cFc33D41D1927fd0f5E0bB7c' as TAddress
+    ]);
+    const expected = {
+      '0xfE5443FaC29fA621cFc33D41D1927fd0f5E0bB7c': bigify('1000000000000000000')
+    } as BalanceMap<BigNumber>;
+    expect(balances).toStrictEqual(expected);
+  });
+
+  it('gets result for getSingleTokenBalanceForAddresses with one address on a non-ethscan network', async () => {
+    const balances = await getSingleTokenBalanceForAddresses(
+      { ...fRopDAI, networkId: RopstenNoEthScanNetworkId },
+      { ...fNetworks[1], id: RopstenNoEthScanNetworkId as NetworkId },
+      ['0xfE5443FaC29fA621cFc33D41D1927fd0f5E0bB7c' as TAddress]
+    );
+    const expected = {
+      '0xfE5443FaC29fA621cFc33D41D1927fd0f5E0bB7c': bigify('150')
+    } as BalanceMap<BigNumber>;
     expect(balances).toStrictEqual(expected);
   });
 });
