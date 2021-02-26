532291c1315bfc3a92e2ae68b0a3c445a09d103b
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-02-26 15:18:29 +0100

    Remove deprecated resetAppDb

--- src/services/Store/DataManager/DataProvider.tsx
+++ src/services/Store/DataManager/DataProvider.tsx
@@ -1,28 +1,17 @@
 import React, { createContext } from 'react';
 
-import { appReset, getAppState, initialLegacyState, useDispatch, useSelector } from '@store';
+import { getAppState, useSelector } from '@store';
 import { DataStore } from '@types';
 
-export interface DataCacheManager extends DataStore {
-  resetAppDb(): void;
-}
-
-export type IDataContext = DataCacheManager;
+export type IDataContext = DataStore;
 
 export const DataContext = createContext({} as IDataContext);
 
 export const DataProvider: React.FC = ({ children }) => {
-  const dispatch = useDispatch();
   const legacyState = useSelector(getAppState);
 
-  // @todo: Redux create action for reset once legacy.reducer is replaced.
-  const resetAppDb = (newDb = initialLegacyState) => {
-    dispatch(appReset(newDb));
-  };
-
   const stateContext: IDataContext = {
-    ...legacyState,
-    resetAppDb
+    ...legacyState
   };
 
   return <DataContext.Provider value={stateContext}>{children}</DataContext.Provider>;
