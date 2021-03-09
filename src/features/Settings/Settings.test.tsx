d753aa26f18b9d6edcc100fe6a9061c748b45831
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-03-09 18:31:48 +0100

    Remove DataContext from Settings and AddorEditNetworkNodes tests

--- src/features/Settings/Settings.test.tsx
+++ src/features/Settings/Settings.test.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
 import { Context as ResponsiveContext } from 'react-responsive';
-import { MemoryRouter } from 'react-router-dom';
-import { simpleRender } from 'test-utils';
+import { mockAppState, simpleRender } from 'test-utils';
 
 import { fAccounts, fAssets, fContacts, fNetworks, fSettings } from '@fixtures';
-import { DataContext, IDataContext, StoreProvider } from '@services';
+import { StoreProvider } from '@services';
 import { translateRaw } from '@translations';
 
 import Settings from './Settings';
@@ -13,59 +12,49 @@ import Settings from './Settings';
 function getComponent() {
   return simpleRender(
     <ResponsiveContext.Provider value={{ width: 900 }}>
-      <MemoryRouter>
-        <DataContext.Provider
-          value={
-            ({
-              addressBook: fContacts,
-              accounts: fAccounts,
-              assets: fAssets,
-              contracts: [],
-              networks: fNetworks,
-              userActions: [],
-              rates: {},
-              settings: fSettings
-            } as unknown) as IDataContext
-          }
-        >
-          <StoreProvider>
-            <Settings />
-          </StoreProvider>
-        </DataContext.Provider>
-      </MemoryRouter>
-    </ResponsiveContext.Provider>
+      <StoreProvider>
+        <Settings />
+      </StoreProvider>
+    </ResponsiveContext.Provider>,
+    {
+      initialState: mockAppState({
+        addressBook: fContacts,
+        accounts: fAccounts,
+        assets: fAssets,
+        contracts: [],
+        networks: fNetworks,
+        userActions: [],
+        rates: {},
+        settings: fSettings
+      })
+    }
   );
 }
 
 describe('Settings', () => {
   it('renders', async () => {
     const { getAllByText } = getComponent();
-
     getAllByText('Accounts').forEach((n) => expect(n).toBeInTheDocument());
   });
 
   it('has accounts', async () => {
     const { getAllByText } = getComponent();
-
     getAllByText(fAccounts[0].label).forEach((n) => expect(n).toBeInTheDocument());
   });
 
   it('has networks', async () => {
     const { getAllByText } = getComponent();
-
     getAllByText(fNetworks[0].name).forEach((n) => expect(n).toBeInTheDocument());
   });
 
   it('has general settings', async () => {
     const { getByText } = getComponent();
-
     expect(getByText(translateRaw('SETTINGS_GENERAL_LABEL'))).toBeDefined();
     expect(getByText(translateRaw('SETTINGS_FIAT_SELECTION_LABEL'))).toBeDefined();
   });
 
   it('has danger zone', async () => {
     const { getByText } = getComponent();
-
     expect(getByText(translateRaw('SETTINGS_DANGER_ZONE'))).toBeDefined();
   });
 });
