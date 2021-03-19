b0223a65c22dab9e32c15fb6debe39d104aef439
blurpesec <hahn.michael.f@gmail.com>
2021-03-19 14:28:51 -0700

    fix sort test

--- src/components/WalletUnlock/components/helpers.test.ts
+++ src/components/WalletUnlock/components/helpers.test.ts
@@ -81,25 +81,25 @@ describe('sortAccounts', () => {
       {
         address: '0xB7CBC4e1979E3FC37Bb603E837b0af5547ABCDEF',
         pathItem: { path: "m/44'/60'/0'/3", baseDPath: baseDPath, index: 3 },
-        balance: '1336254000000000',
+        balance: bigify('1336254000000000'),
         isSelected: true
       },
       {
         address: '0x4d1F9d958AFa2e96dab3f3Ce7162b87daEABCDEF',
         pathItem: { path: "m/44'/60'/0'/0", baseDPath: baseDPath, index: 0 },
-        balance: '0',
+        balance: bigify('0'),
         isSelected: false
       },
       {
         address: '0x8d84C8E14Ee8B741Aa9F6764985437905eABCDEF',
         pathItem: { path: "m/44'/60'/0'/1", baseDPath: baseDPath, index: 1 },
-        balance: '0',
+        balance: bigify('0'),
         isSelected: false
       },
       {
         address: '0x04725bF6D10046C9b6C7F6f60A52c966efABCDEF',
         pathItem: { path: "m/44'/60'/0'/2", baseDPath: baseDPath, index: 2 },
-        balance: '0',
+        balance: bigify('0'),
         isSelected: false
       }
     ];
@@ -112,7 +112,7 @@ describe('sortAccounts', () => {
       {
         address: '0xB7CBC4e1979E3FC37Bb603E837b0af5547ABCDEF',
         pathItem: { path: "m/44'/60'/0'/3", baseDPath: baseDPath, index: 3 },
-        balance: '1336254000000000',
+        balance: bigify('1336254000000000'),
         isSelected: true
       }
     ];
