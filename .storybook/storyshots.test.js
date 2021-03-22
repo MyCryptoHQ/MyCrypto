ac377fa39a21c90e8a9b85a527e205530d8b2d18
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-03-22 18:56:09 +0100

    Stub date localisation

--- .storybook/storyshots.test.js
+++ .storybook/storyshots.test.js
@@ -1,3 +1,18 @@
 import initStoryshots from '@storybook/addon-storyshots';
 
-initStoryshots();
+describe('', () => {
+  // Our localhosts have a different locale than CI. Since the snapshots
+  // contain localized dates (eg. TxReceipt), we stub the call and neutralize
+  // localization.
+  beforeAll(() => {
+    jest.spyOn(global.Date.prototype, 'toLocaleString').mockImplementation(function (args) {
+      return new Date(this.valueOf()).toISOString();
+    });
+  });
+
+  initStoryshots();
+
+  afterAll(() => {
+    Date.toLocaleString.mockRestore();
+  });
+});
