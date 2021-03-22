abdeb7312a4b31c659030c3136312e70574c4394
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-03-22 15:35:35 +0100

    Setup storyshots

--- src/features/PurchaseMembership/helpers.test.ts
+++ src/features/PurchaseMembership/helpers.test.ts
@@ -1,33 +1,11 @@
 import { fAssets } from '@../jest_config/__fixtures__/assets';
 
-import { DAIUUID, DEFAULT_NETWORK, XDAI_NETWORK, XDAIUUID } from '@config';
+import { DAIUUID, XDAIUUID } from '@config';
 import { fAccounts, fNetworks } from '@fixtures';
 
 import { IMembershipId, MEMBERSHIP_CONFIG } from './config';
-import { createApproveTx, createPurchaseTx, getMembershipContracts } from './helpers';
+import { createApproveTx, createPurchaseTx } from './helpers';
 import { MembershipSimpleTxFormFull } from './types';
-
-describe('getMembershipContracts()', () => {
-  it('can getMembershipContracts for the xdai network', () => {
-    const expected = [
-      '0xcB3BB4CCe15b492E7fdD7cb9a3835C034714207A',
-      '0xf97f516Cc0700a4Ce9Ee64D488F744f631e1525d',
-      '0xEB24302c4c78963e1b348b274aa9cC6fcbe80527'
-    ];
-    expect(getMembershipContracts(XDAI_NETWORK)).toStrictEqual(expected);
-  });
-
-  it('can getMembershipContracts for the ethereum network', () => {
-    const expected = [
-      '0x6cA105D2AF7095B1BCEeb6A2113D168ddDCD57cf',
-      '0xfe58C642A3F703e7Dc1060B3eE02ED4619046125',
-      '0x7a84f1074B5929cBB7bd08Fb450CF9Fb22bf5329',
-      '0xee2B7864d8bc731389562F820148e372F57571D8',
-      '0x098D8b363933D742476DDd594c4A5a5F1a62326a'
-    ];
-    expect(getMembershipContracts(DEFAULT_NETWORK)).toStrictEqual(expected);
-  });
-});
 
 describe('createApproveTx()', () => {
   it('can create valid approveTx from membership tx params created from the purchase membership form', () => {
