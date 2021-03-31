dd1761f99eb05d027dc34d86c0b0aabd6271f9a3
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-03-31 20:10:25 +0200

    Remove unused class

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -61,7 +61,7 @@ export const AppRoutes = () => {
         <DefaultHomeHandler>
           <Suspense
             fallback={
-              <Layout className="full-height">
+              <Layout>
                 <AppLoading />
               </Layout>
             }
