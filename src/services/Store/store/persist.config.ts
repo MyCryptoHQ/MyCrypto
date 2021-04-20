38ed3b6d3ae16fdcea0961445081d68936fdb2c3
Guillaume Roux <guillaumeroux123@gmail.com>
2021-04-20 16:44:29 +0200

    handle existing rates in settings

--- src/services/Store/store/persist.config.ts
+++ src/services/Store/store/persist.config.ts
@@ -158,7 +158,7 @@ export const migrations = {
     return {
       ...state,
       // @ts-expect-error rates are present in settings on data to be migrated, want to move it at root of persistence layer
-      rates: state.settings.rates && state.settings.rates,
+      rates: state.rates ? state.rates : state.settings.rates ? state.settings.rates : [],
       trackedAssets: state.trackedAssets ? state.trackedAssets : [],
       settings: dissoc('rates', state.settings)
     };
