a1f02884b0972d5cd6ff205e3c0807d02ad5b76a
Frederik Bolding <frederik.bolding@gmail.com>
2021-04-29 12:51:45 +0200

    Add hash and public key to request

---
+++ src/utils/signerProvider.ts
@@ -0,0 +1,23 @@
+import { hexlify } from '@ethersproject/bytes';
+import { sign, utils } from 'noble-ed25519';
+import WebsocketProvider from 'web3-providers-ws';
+
+import { stripHexPrefix } from '@utils';
+
+export const createSignerProvider = (privateKey: string, publicKey: string) => {
+  // @ts-expect-error This is a valid constructor, not sure why it's failing
+  const ws = new WebsocketProvider('ws://localhost:8000', { headers: { publicKey } });
+
+  const customProvider = {
+    ...ws,
+    send: async (payload: any, callback: any) => {
+      const encoded = new TextEncoder().encode(JSON.stringify(payload));
+      const hash = stripHexPrefix(hexlify(await utils.sha512(encoded)));
+      const sig = await sign(hash, privateKey);
+      const newPayload = { ...payload, hash, sig };
+      ws.send(newPayload, callback);
+    }
+  };
+
+  return customProvider;
+};
