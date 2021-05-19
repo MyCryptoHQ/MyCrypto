bc1f0868ff6512ef3775871c76040a178fa29087
Frederik Bolding <frederik.bolding@gmail.com>
2021-05-19 16:50:01 +0200

    Remove API key

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
     hardhat: {
       chainId: 1,
       forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/TPLd_8YqDbqK1_ciMrFF2URz6_mjbifb`
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`
       },
       accounts: {
         mnemonic: 'test test test test test test test test test test test ball'
