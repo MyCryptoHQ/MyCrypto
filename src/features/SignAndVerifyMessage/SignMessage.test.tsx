31243651e19513d110a89422aa8b496d42c58b6b
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-06-01 13:03:28 +0200

    Test SignMessage can reach form

--- src/features/SignAndVerifyMessage/SignMessage.test.tsx
+++ src/features/SignAndVerifyMessage/SignMessage.test.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
-import { mockStore, simpleRender } from 'test-utils';
+import { fireEvent, mockStore, simpleRender, waitFor } from 'test-utils';
 
+import { fNetworks } from '@fixtures';
 import { WalletId } from '@types';
 import { noOp } from '@utils';
 
+// eslint-disable-next-line jest/no-mocks-import
+import { mockWindow } from '../../../jest_config/__mocks__/web3';
 import SignMessage from './SignMessage';
 import slice from './signMessage.slice';
+
+jest.mock('@services/WalletService/walletService', () => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires, jest/no-mocks-import
+  const { Web3Wallet } = require('../../services/WalletService/non-deterministic');
+  return {
+    WalletFactory: {
+      ['WEB3']: {
+        init: async () => [new Web3Wallet('0xE8E0F5417B272f2a1C24419bd2cF6B3F584c6b9A', 'Ropsten')]
+      }
+    }
+  };
+});
 
 const defaultProps: React.ComponentProps<typeof SignMessage> = {
   setShowSubtitle: noOp
@@ -14,18 +29,34 @@ const defaultProps: React.ComponentProps<typeof SignMessage> = {
 
 const renderComponent = (state = {}, props = defaultProps) => {
   return simpleRender(<SignMessage {...props} />, {
-    initialState: mockStore({ storeSlice: { [slice.name]: state } })
+    initialState: mockStore({
+      storeSlice: { [slice.name]: state },
+      dataStoreState: { networks: fNetworks }
+    })
   });
 };
 
 describe('SignMessage', () => {
+  beforeEach(() => {
+    // Mock window.ethereum
+    const customWindow = window as CustomWindow;
+    mockWindow(customWindow);
+  });
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   test('it renders WalletList by default', async () => {
     const { getByText } = renderComponent();
     expect(getByText(/Web3/)).toBeDefined();
   });
 
-  test('it renders form provided wallet', async () => {
-    const { getByText } = renderComponent({ walletId: WalletId.WEB3 });
-    expect(getByText(/Web3/)).toBeDefined();
+  test('it displays form after connect', async () => {
+    const { getByText, getByRole } = renderComponent({ walletId: WalletId.WEB3 });
+
+    fireEvent.click(getByRole('button', { name: /Connect via Web3 Provider/i }));
+
+    expect(getByText(/unlocking/i)).toBeDefined();
+    await waitFor(() => expect(getByText(/Sign Message/i)).toBeDefined());
   });
 });
