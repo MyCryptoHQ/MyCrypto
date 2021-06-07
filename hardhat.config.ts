1cb3b298d1a0b80950b37f066cbc9f2686644840
Frederik Bolding <frederik.bolding@gmail.com>
2021-06-07 15:35:07 +0200

    Simplify import

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,6 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomiclabs/hardhat-ethers';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config();
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
   networks: {
