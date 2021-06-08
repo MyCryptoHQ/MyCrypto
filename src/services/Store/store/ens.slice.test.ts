88ecb29e02c6f0faf1e611db22a503c780c6aac8
Frederik Bolding <frederik.bolding@gmail.com>
2021-06-08 13:54:50 +0200

    Add tests

---
+++ src/services/Store/store/ens.slice.test.ts
@@ -0,0 +1,55 @@
+import { call } from 'redux-saga-test-plan/matchers';
+import { throwError } from 'redux-saga-test-plan/providers';
+import { expectSaga, mockAppState } from 'test-utils';
+
+import { fAccounts, fEnsApiResponse, fEnsRecords } from '@fixtures';
+import { ENSService } from '@services/ApiService/Ens';
+
+import slice, { ensSaga, fetchENS, initialState } from './ens.slice';
+
+const reducer = slice.reducer;
+const { fetchError, setRecords } = slice.actions;
+
+jest.mock('apollo-boost', () => ({
+  query: jest.fn().mockImplementation(() => Promise.resolve(fEnsApiResponse))
+}));
+
+describe('EnsSlice', () => {
+  it('has an initial state', () => {
+    const actual = reducer(undefined, { type: null });
+    const expected = initialState;
+    expect(actual).toEqual(expected);
+  });
+
+  it('setRecords(): sets record array', () => {
+    const actual = reducer(initialState, setRecords(fEnsRecords));
+    const expected = { ...initialState, fetched: true, records: fEnsRecords };
+    expect(actual).toEqual(expected);
+  });
+
+  it('fetchError(): sets an error', () => {
+    const actual = reducer(initialState, fetchError());
+    const expected = { ...initialState, error: true };
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('ensSaga()', () => {
+  it('fetches ens', () => {
+    return expectSaga(ensSaga)
+      .withState(mockAppState({ accounts: fAccounts }))
+      .put(setRecords(fEnsRecords))
+      .dispatch(fetchENS())
+      .silentRun();
+  });
+
+  it('can sets error if the call fails', () => {
+    const error = new Error('error');
+    return expectSaga(ensSaga)
+      .withState(mockAppState({ accounts: fAccounts }))
+      .provide([[call.fn(ENSService.fetchOwnershipRecords), throwError(error)]])
+      .put(fetchError())
+      .dispatch(fetchENS())
+      .silentRun();
+  });
+});
