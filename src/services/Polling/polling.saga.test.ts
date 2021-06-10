6c36f5c5a8022c434906c1fdf9d9289d0812ed78
Guillaume Roux <guillaumeroux123@gmail.com>
2021-06-10 15:38:41 +0200

    start improving polling tests

--- src/services/Polling/polling.saga.test.ts
+++ src/services/Polling/polling.saga.test.ts
@@ -1,5 +1,5 @@
 import { createAction } from '@reduxjs/toolkit';
-import { put } from 'redux-saga/effects';
+import { call, put } from 'redux-saga/effects';
 import { expectSaga } from 'test-utils';
 
 import { IPollingPayload, default as pollingSagaWatcher } from './polling.saga';
@@ -41,5 +41,32 @@ describe('PollingSaga', () => {
         expect(effects.call).toHaveLength(4);
         expect(effects.put).toBeUndefined();
       });
+  });
+
+  it('pollingSaga(): Retries on failure', () => {
+    function* failingSaga() {
+      yield call(() => new Promise());
+    }
+
+    const pollStart = createAction('fake/start');
+    const pollStop = createAction('fake/stop');
+
+    const failingPollingParams: IPollingPayload = {
+      startAction: pollStart,
+      stopAction: pollStop,
+      params: {
+        interval: 10000,
+        retryOnFailure: true,
+        retries: 2,
+        retryAfter: 200
+      },
+      saga: failingSaga
+    };
+    return expectSaga(pollingSagaWatcher, pollingParams)
+      .call(failingPollingParams.saga)
+      .delay(failingPollingParams.params.retryAfter!)
+      .call(failingPollingParams.saga)
+      .delay(failingPollingParams.params.retryAfter!)
+      .silentRun();
   });
 });
