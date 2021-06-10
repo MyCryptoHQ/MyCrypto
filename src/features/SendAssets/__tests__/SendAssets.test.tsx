f64caedc7282e0d0c71aef25a9fea0ef923df658
Frederik Bolding <frederik.bolding@gmail.com>
2021-06-10 14:57:43 +0200

    Add test

--- src/features/SendAssets/__tests__/SendAssets.test.tsx
+++ src/features/SendAssets/__tests__/SendAssets.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import { APP_STATE, mockAppState, simpleRender } from 'test-utils';
+import { APP_STATE, fireEvent, mockAppState, simpleRender, waitFor } from 'test-utils';
 
 import SendAssets from '@features/SendAssets/SendAssets';
 import { fAccounts, fAssets } from '@fixtures';
 import { StoreContext } from '@services/Store';
+import { translateRaw } from '@translations';
 
 // SendFlow makes RPC calls to get nonce and gas.
 jest.mock('@vendor', () => {
@@ -24,9 +25,8 @@ describe('SendAssetsFlow', () => {
       <StoreContext.Provider
         value={
           ({
-            userAssets: [],
-            accounts: fAccounts,
-            getAccount: jest.fn()
+            userAssets: fAssets,
+            accounts: fAccounts
           } as unknown) as any
         }
       >
@@ -47,5 +47,17 @@ describe('SendAssetsFlow', () => {
     const selector = 'Send Assets';
     expect(getByText(selector)).toBeInTheDocument();
     expect(getByText(fAccounts[0].label)).toBeInTheDocument();
+  });
+
+  test('Can render too many decimals error.', async () => {
+    const { getByText, container } = renderComponent();
+    const amount = container.querySelector('input[name="amount"]')!;
+    fireEvent.change(amount, {
+      target: { value: '0.00000000000000000001' }
+    });
+    fireEvent.blur(amount);
+    await waitFor(() =>
+      expect(getByText(translateRaw('TOO_MANY_DECIMALS', { $decimals: '18' }))).toBeInTheDocument()
+    );
   });
 });
