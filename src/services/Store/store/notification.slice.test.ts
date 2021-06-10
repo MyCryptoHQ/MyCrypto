105f73c3b43142bb21f850c053d16c6f6cba6b46
Frederik Bolding <frederik.bolding@gmail.com>
2021-06-10 13:40:10 +0200

    Add tests

--- src/services/Store/store/notification.slice.test.ts
+++ src/services/Store/store/notification.slice.test.ts
@@ -1,9 +1,15 @@
-import { mockAppState } from 'test-utils';
+import { expectSaga, mockAppState } from 'test-utils';
 
-import { fNotifications } from '@fixtures';
+import { NotificationTemplates } from '@features/NotificationsPanel';
+import { fAccount, fNotifications } from '@fixtures';
+import { TUuid } from '@types';
 
 import {
   createNotification,
+  dismissNotification,
+  dismissNotificationWorker,
+  displayNotification,
+  displayNotificationWorker,
   initialState,
   selectNotifications,
   default as slice,
@@ -12,7 +18,18 @@ import {
 
 const reducer = slice.reducer;
 
-describe('Notification', () => {
+jest.mock('uuid/v4', () => jest.fn().mockImplementation(() => 'foo'));
+
+beforeAll(() => {
+  jest.useFakeTimers('modern');
+  jest.setSystemTime(new Date(2021, 6, 10));
+});
+
+afterAll(() => {
+  jest.useRealTimers();
+});
+
+describe('NotificationSlice', () => {
   it('has an initialState', () => {
     const actual = reducer(undefined, { type: null });
     const expected = initialState;
@@ -36,5 +53,77 @@ describe('Notification', () => {
     const actual = selectNotifications(mockAppState());
     const expected = initialState;
     expect(actual).toEqual(expected);
+  });
+
+  describe('displayNotificationWorker', () => {
+    it('creates notification if it doesnt exist', () => {
+      const notification = {
+        templateName: NotificationTemplates.walletAdded,
+        templateData: { address: fAccount.address }
+      };
+      return expectSaga(displayNotificationWorker, displayNotification(notification))
+        .withState(mockAppState({ notifications: [] }))
+        .put(
+          createNotification({
+            uuid: 'foo' as TUuid,
+            template: notification.templateName,
+            templateData: notification.templateData,
+            dateDisplayed: new Date(),
+            dismissed: false,
+            dateDismissed: undefined
+          })
+        )
+        .silentRun();
+    });
+
+    it('updates notification if it does exist', () => {
+      const notification = {
+        templateName: NotificationTemplates.onboardingPleaseUnderstand,
+        templateData: { address: fAccount.address }
+      };
+      const notificationState = {
+        uuid: 'foo' as TUuid,
+        template: notification.templateName,
+        templateData: notification.templateData,
+        dateDisplayed: new Date(),
+        dismissed: true,
+        dateDismissed: undefined
+      };
+      return expectSaga(displayNotificationWorker, displayNotification(notification))
+        .withState(mockAppState({ notifications: [notificationState] }))
+        .put(updateNotification(notificationState))
+        .silentRun();
+    });
+
+    it('dismisses old notifications', () => {
+      const notification = {
+        templateName: NotificationTemplates.walletAdded,
+        templateData: { address: fAccount.address }
+      };
+      const notificationState = {
+        uuid: 'foo' as TUuid,
+        template: notification.templateName,
+        templateData: notification.templateData,
+        dateDisplayed: new Date(),
+        dismissed: false,
+        dateDismissed: undefined
+      };
+      return expectSaga(displayNotificationWorker, displayNotification(notification))
+        .withState(mockAppState({ notifications: [notificationState] }))
+        .put(dismissNotification(notificationState))
+        .silentRun();
+    });
+  });
+
+  describe('dismissNotificationWorker', () => {
+    it('puts updateNotification with dismissed values', () => {
+      const notification = { ...fNotifications[0], dismissed: false };
+      const notifications = [notification, fNotifications[1]];
+
+      return expectSaga(dismissNotificationWorker, dismissNotification(notification))
+        .withState(mockAppState({ notifications }))
+        .put(updateNotification({ ...notification, dismissed: true, dateDismissed: new Date() }))
+        .silentRun();
+    });
   });
 });
