ce1c190096d1ec5cf31144667cc2f80099ec7caf
Frederik Bolding <frederik.bolding@gmail.com>
2021-06-28 14:40:11 +0200

    Test legacy Trezor/Ledger components

--- src/components/WalletUnlock/LedgerNano.test.tsx
+++ src/components/WalletUnlock/LedgerNano.test.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-import { simpleRender } from 'test-utils';
+import { fireEvent, simpleRender, waitFor } from 'test-utils';
 
 import { translateRaw } from '@translations';
 import { FormData } from '@types';
+import { truncate } from '@utils';
 
 import { LedgerNanoSDecrypt } from './LedgerNano';
+
+jest.mock('@ledgerhq/hw-transport-u2f');
+
+jest.mock('@mycrypto/eth-scan', () => ({
+  ...jest.requireActual('@mycrypto/eth-scan'),
+  getEtherBalances: jest.fn().mockResolvedValue({})
+}));
 
 const defaultProps = {
   formData: ({ network: 'Ethereum' } as unknown) as FormData,
@@ -17,17 +25,34 @@ const getComponent = () => {
 };
 
 describe('LedgerNano', () => {
+  // @ts-expect-error Bad mock please ignore
+  delete window.location;
+  // @ts-expect-error Bad mock please ignore
+  window.location = Object.assign(new URL('https://example.org'), {
+    ancestorOrigins: '',
+    assign: jest.fn(),
+    reload: jest.fn(),
+    replace: jest.fn()
+  });
+
   it('renders', () => {
-    // @ts-expect-error Bad mock please ignore
-    delete window.location;
-    // @ts-expect-error Bad mock please ignore
-    window.location = Object.assign(new URL('https://example.org'), {
-      ancestorOrigins: '',
-      assign: jest.fn(),
-      reload: jest.fn(),
-      replace: jest.fn()
-    });
     const { getByText } = getComponent();
     expect(getByText(translateRaw('UNLOCK_WALLET'), { exact: false })).toBeInTheDocument();
+  });
+
+  it('fetches addresses from Ledger and displays them', async () => {
+    const { getByText } = getComponent();
+    expect(getByText(translateRaw('UNLOCK_WALLET'), { exact: false })).toBeInTheDocument();
+    const button = getByText(translateRaw('ADD_LEDGER_SCAN'), { exact: false });
+
+    fireEvent.click(button);
+
+    await waitFor(() =>
+      expect(getByText(translateRaw('DECRYPT_PROMPT_SELECT_ADDRESS'))).toBeInTheDocument()
+    );
+
+    await waitFor(() =>
+      expect(getByText(truncate('0x31497F490293CF5a4540b81c9F59910F62519b63'))).toBeInTheDocument()
+    );
   });
 });
--- src/components/WalletUnlock/Trezor.test.tsx
+++ src/components/WalletUnlock/Trezor.test.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
-import { simpleRender } from 'test-utils';
+import { fireEvent, simpleRender, waitFor } from 'test-utils';
 
 import { translateRaw } from '@translations';
 import { FormData } from '@types';
+import { truncate } from '@utils';
 
 import { TrezorDecrypt } from './Trezor';
+
+jest.mock('@mycrypto/eth-scan', () => ({
+  ...jest.requireActual('@mycrypto/eth-scan'),
+  getEtherBalances: jest.fn().mockResolvedValue({})
+}));
 
 const defaultProps: React.ComponentProps<typeof TrezorDecrypt> = {
   formData: ({ network: 'Ethereum' } as unknown) as FormData,
@@ -20,5 +26,21 @@ describe('Trezor', () => {
   it('renders', () => {
     const { getByText } = getComponent();
     expect(getByText(translateRaw('UNLOCK_WALLET'), { exact: false })).toBeInTheDocument();
+  });
+
+  it('fetches addresses from Trezor and displays them', async () => {
+    const { getByText } = getComponent();
+    expect(getByText(translateRaw('UNLOCK_WALLET'), { exact: false })).toBeInTheDocument();
+    const button = getByText(translateRaw('ADD_TREZOR_SCAN'), { exact: false });
+
+    fireEvent.click(button);
+
+    await waitFor(() =>
+      expect(getByText(translateRaw('DECRYPT_PROMPT_SELECT_ADDRESS'))).toBeInTheDocument()
+    );
+
+    await waitFor(() =>
+      expect(getByText(truncate('0xc6D5a3c98EC9073B54FA0969957Bd582e8D874bf'))).toBeInTheDocument()
+    );
   });
 });
