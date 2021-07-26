7f69669fd91c687f5868b218276757b66c5fbc46
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-07-26 12:05:09 +0200

    Mock network calls

--- src/helpers/erc20.spec.ts
+++ src/helpers/erc20.spec.ts
@@ -1,3 +1,5 @@
+import nock from 'nock';
+
 import { repTokenMigrationConfig } from '@features/RepTokenMigration/config';
 import { fAccount, fDerivedApprovalTx, fNetworks, fRopDAI } from '@fixtures';
 import { TAddress } from '@types';
@@ -25,7 +27,34 @@ describe('formatApproveTx', () => {
 });
 
 describe('getTokenInformation', () => {
+  nock.disableNetConnect();
+
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
   it('fetches token information for ERC-20 tokens', async () => {
+    nock(/.*/)
+      .post(/.*/)
+      .reply(200, () => ({
+        id: 1,
+        jsonrpc: '2.0',
+        result: '0xc4db43'
+      }))
+      .post(/.*/)
+      .reply(200, () => ({
+        id: 1,
+        jsonrpc: '2.0',
+        result:
+          '0x000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000034441490000000000000000000000000000000000000000000000000000000000'
+      }))
+      .post(/.*/)
+      .reply(200, () => ({
+        id: 1,
+        jsonrpc: '2.0',
+        result: '0x0000000000000000000000000000000000000000000000000000000000000012'
+      }));
+
     await expect(
       getTokenInformation(fNetworks[0], '0x6b175474e89094c44da98b954eedeac495271d0f' as TAddress)
     ).resolves.toStrictEqual({
