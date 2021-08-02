4a36fedaba7f8d3321cd099c54dd8954c219e623
Frederik Bolding <frederik.bolding@gmail.com>
2021-08-02 13:33:08 +0200

    Use memoized Identicon for performance reasons

--- src/components/TranslateMarkdown.tsx
+++ src/components/TranslateMarkdown.tsx
@@ -8,16 +8,19 @@ interface Props {
   source: string;
 }
 
+//@ts-expect-error bad typing on props
+const Link = (props) => <LinkApp isExternal={true} {...props} />;
+
+const disallowed = ['html'];
+
+const components = {
+  a: Link,
+  p: Fragment // Remove <p> added by react-markdown.
+};
+
 const TranslateMarkdown = ({ source }: Props) => {
   return (
-    <Markdown
-      disallowedElements={['html']}
-      components={{
-        //@ts-expect-error bad typing on props
-        a: (props) => <LinkApp isExternal={true} {...props} />,
-        p: Fragment // Remove <p> added by react-markdown.
-      }}
-    >
+    <Markdown disallowedElements={disallowed} components={components}>
       {source}
     </Markdown>
   );
