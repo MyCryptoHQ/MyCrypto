919decd97fd3a89920968fe53c2bbb7e23e4e49a
Frederik Bolding <frederik.bolding@gmail.com>
2021-08-05 14:15:43 +0200

    Add EIP 1559 feature flag

--- src/helpers/eip1559.ts
+++ src/helpers/eip1559.ts
@@ -3,6 +3,20 @@ import { Network, StoreAccount, WalletId } from '@types';
 // @todo Figure out if anything else needs to be disabled?
 const DISABLED_WALLETS = [WalletId.TREZOR, WalletId.TREZOR_NEW, WalletId.WALLETCONNECT];
 
+const featureFlagName = 'MYC_EIP1559';
+
+// @todo DELETE THIS HACK
+export const getEIP1559FeatureFlag = () => {
+  const ls = localStorage.getItem(featureFlagName);
+  return ls !== 'false';
+};
+
+export const setEIP1559FeatureFlag = (value: boolean) => {
+  localStorage.setItem(featureFlagName, value.toString());
+};
+
 export const isEIP1559Supported = (network: Network, account: StoreAccount) => {
-  return network.supportsEIP1559 && !DISABLED_WALLETS.includes(account.wallet);
+  return (
+    getEIP1559FeatureFlag() && network.supportsEIP1559 && !DISABLED_WALLETS.includes(account.wallet)
+  );
 };
