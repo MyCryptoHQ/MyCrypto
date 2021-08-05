c76eac0b5c3c768bf6bd7695ebd4d1edf02c9fd4
Frederik Bolding <frederik.bolding@gmail.com>
2021-08-05 14:23:52 +0200

    Enable only Ledger

--- src/helpers/eip1559.ts
+++ src/helpers/eip1559.ts
@@ -1,7 +1,7 @@
 import { Network, StoreAccount, WalletId } from '@types';
 
 // @todo Figure out if anything else needs to be disabled?
-const DISABLED_WALLETS = [WalletId.TREZOR, WalletId.TREZOR_NEW, WalletId.WALLETCONNECT];
+const ENABLED_WALLETS = [WalletId.LEDGER_NANO_S, WalletId.LEDGER_NANO_S_NEW];
 
 const featureFlagName = 'MYC_EIP1559';
 
@@ -17,6 +17,6 @@ export const setEIP1559FeatureFlag = (value: boolean) => {
 
 export const isEIP1559Supported = (network: Network, account: StoreAccount) => {
   return (
-    getEIP1559FeatureFlag() && network.supportsEIP1559 && !DISABLED_WALLETS.includes(account.wallet)
+    getEIP1559FeatureFlag() && network.supportsEIP1559 && ENABLED_WALLETS.includes(account.wallet)
   );
 };
