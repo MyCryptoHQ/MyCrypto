c3983c1a52797e7a8d48bd21cd24b5b628a0a7b0
Frederik Bolding <frederik.bolding@gmail.com>
2021-08-09 15:34:53 +0200

    Remove some unused components and css

--- src/components/WalletUnlock/index.ts
+++ src/components/WalletUnlock/index.ts
@@ -3,7 +3,6 @@ export * from './Trezor';
 export * from './ViewOnly';
 export { default as Web3ProviderDecrypt } from './Web3Provider';
 export { default as Web3ProviderInstall } from './Web3ProviderInstall';
-export { default as InsecureWalletWarning } from './InsecureWalletWarning';
 export { default as WalletConnectDecrypt } from './WalletConnect';
 export { default as LedgerDecrypt } from './Ledger';
 export { default as TrezorUnlock } from './NewTrezor';
