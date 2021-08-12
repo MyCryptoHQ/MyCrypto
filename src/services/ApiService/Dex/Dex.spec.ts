b1ad55cef8cad802ff6f79c2e30f714864421f1b
Frederik Bolding <frederik.bolding@gmail.com>
2021-08-12 12:27:30 +0200

    Add test

--- src/services/ApiService/Dex/Dex.spec.ts
+++ src/services/ApiService/Dex/Dex.spec.ts
@@ -2,7 +2,17 @@ import mockAxios from 'jest-mock-axios';
 
 import { ETHUUID } from '@config';
 import { fAccount, fAssets, fNetwork, fNetworks, fRopDAI, fSwapQuote } from '@fixtures';
-import { ITxData, ITxGasPrice, ITxToAddress, ITxType, ITxValue, TTicker, TUuid } from '@types';
+import { fetchUniversalGasPriceEstimate } from '@services/ApiService/Gas';
+import {
+  ITxData,
+  ITxGasPrice,
+  ITxToAddress,
+  ITxType,
+  ITxValue,
+  TTicker,
+  TUuid,
+  WalletId
+} from '@types';
 
 import { DexService } from '.';
 import { formatTradeTx } from './Dex';
@@ -46,6 +56,47 @@ describe('SwapFlow', () => {
         gasPrice: '0x23db1d8400',
         to: '0xdef1c0ded9bec7f1a1670819833240f027b25eff',
         txType: 'SWAP',
+        value: '0x0',
+        metadata: { receivingAsset: ETHUUID }
+      });
+    });
+
+    it('returns the expected two transactions for a multi tx swap using eip1559', async () => {
+      (fetchUniversalGasPriceEstimate as jest.MockedFunction<
+        typeof fetchUniversalGasPriceEstimate
+      >).mockResolvedValueOnce({ maxFeePerGas: '100', maxPriorityFeePerGas: '10' });
+      const promise = DexService.instance.getOrderDetailsFrom(
+        { ...fNetwork, supportsEIP1559: true },
+        { ...fAccount, wallet: WalletId.LEDGER_NANO_S_NEW },
+        fRopDAI,
+        fAssets[0],
+        '1'
+      );
+      mockAxios.mockResponse({
+        data: { ...fSwapQuote }
+      });
+      const result = await promise;
+      expect(result.approvalTx).toStrictEqual({
+        chainId: fNetwork.chainId,
+        data:
+          '0x095ea7b3000000000000000000000000def1c0ded9bec7f1a1670819833240f027b25eff0000000000000000000000000000000000000000000000000de0b6b3a7640000',
+        maxFeePerGas: '0x174876e800',
+        maxPriorityFeePerGas: '0x2540be400',
+        from: fAccount.address,
+        to: '0x6b175474e89094c44da98b954eedeac495271d0f',
+        txType: 'APPROVAL',
+        type: 2,
+        value: '0x0'
+      });
+      expect(result.tradeTx).toStrictEqual({
+        chainId: fNetwork.chainId,
+        data:
+          '0xd9627aa400000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000000000000000000000000000000002429108b8f331000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000020000000000000000000000006b175474e89094c44da98b954eedeac495271d0f000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee869584cd0000000000000000000000001000000000000000000000000000000000000011000000000000000000000000000000000000000000000096596a1ef6601a8b3a',
+        maxFeePerGas: '0x174876e800',
+        maxPriorityFeePerGas: '0x2540be400',
+        to: '0xdef1c0ded9bec7f1a1670819833240f027b25eff',
+        txType: 'SWAP',
+        type: 2,
         value: '0x0',
         metadata: { receivingAsset: ETHUUID }
       });
