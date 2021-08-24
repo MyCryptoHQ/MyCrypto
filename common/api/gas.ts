bb866e889bf1267f06ad7404a05464164bde9ff4
wtzb <dev@wietzeb.nl>
2021-08-24 18:02:49 +0300

    Update MAX_GAS_FAST

--- common/api/gas.ts
+++ common/api/gas.ts
@@ -2,7 +2,7 @@ import { Omit } from 'react-redux';
 
 import { checkHttpStatus, parseJSON } from './utils';
 
-const MAX_GAS_FAST = 1000;
+const MAX_GAS_FAST = 1500;
 
 interface RawGasEstimates {
   safeLow: number;
