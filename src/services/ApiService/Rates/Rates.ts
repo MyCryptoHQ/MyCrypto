740b44822707ac117f409a672bd2c66e1e2ed9cf
Frederik Bolding <frederik.bolding@gmail.com>
2021-09-05 19:36:19 +0200

    Fix CoinGecko API url (#4112)

--- src/services/ApiService/Rates/Rates.ts
+++ src/services/ApiService/Rates/Rates.ts
@@ -1,11 +1,11 @@
 import { IRates } from '@types';
 
 import { default as ApiService } from '../ApiService';
-import { COINGECKO_RATES_API } from './constants';
+import { COINGECKO_BASE_URL } from './constants';
 
 export const RatesService = () => {
   const service = ApiService.generateInstance({
-    baseURL: COINGECKO_RATES_API,
+    baseURL: COINGECKO_BASE_URL,
     timeout: 20000
   });
 
@@ -14,7 +14,7 @@ export const RatesService = () => {
     currencies: string[]
   ): Promise<IRates> => {
     return service
-      .get('/', {
+      .get('/simple/price', {
         params: { ids: coinGeckoIds.join(','), vs_currencies: currencies.join(',') }
       })
       .then((res) => res.data)
