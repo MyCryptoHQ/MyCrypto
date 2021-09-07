57a0f5196ac878f4e562462e55192fcdcc4e46df
Frederik Bolding <frederik.bolding@gmail.com>
2021-09-07 15:04:44 +0200

    Fix typing

--- src/components/WalletUnlock/Ledger.test.tsx
+++ src/components/WalletUnlock/Ledger.test.tsx
@@ -34,7 +34,6 @@ const defaultProps = {
 };
 
 const getComponent = () => {
-  // @ts-expect-error Not mocking out all of store
   const { store } = createStore(mockAppState({ networks: APP_STATE.networks }));
   return simpleRender(
     <Provider store={store}>
