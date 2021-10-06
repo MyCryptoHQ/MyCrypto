526020d4e9abb20963388aa9964821df1d389446
Frederik Bolding <frederik.bolding@gmail.com>
2021-10-06 11:44:56 +0200

    Use function instead of class for service

--- src/services/ApiService/OpenSea/OpenSea.ts
+++ src/services/ApiService/OpenSea/OpenSea.ts
@@ -1,5 +1,3 @@
-import { AxiosInstance } from 'axios';
-
 import { OPENSEA_API, OPENSEA_IMAGE_PROXY_API } from '@config';
 import { ApiService } from '@services/ApiService';
 
@@ -9,13 +7,18 @@ const NFT_LIMIT_PER_QUERY = 50; // Max allowed by OpenSea
 const NFT_LIMIT_MAX = 1000; // To prevent overly spamming the API - @todo Discuss
 const COLLECTION_LIMIT = 300; // @todo Discuss
 
-export default abstract class OpenSeaService {
+export const OpenSeaService = () => {
+  const service = ApiService.generateInstance({
+    baseURL: OPENSEA_API,
+    timeout: 15000
+  });
+
   // @todo Make prettier
-  public static fetchAllAssets = async (owner: string): Promise<OpenSeaNFT[]> => {
+  const fetchAllAssets = async (owner: string): Promise<OpenSeaNFT[]> => {
     let page = 0;
     let results = [] as OpenSeaNFT[];
     while (results.length < NFT_LIMIT_MAX) {
-      const currentPage = await OpenSeaService.fetchAssets(owner, page);
+      const currentPage = await fetchAssets(owner, page);
       if (currentPage) {
         results = results.concat(currentPage);
       }
@@ -28,10 +31,10 @@ export default abstract class OpenSeaService {
     return results;
   };
 
-  public static fetchAssets = async (owner: string, page: number): Promise<OpenSeaNFT[] | null> => {
+  const fetchAssets = async (owner: string, page: number): Promise<OpenSeaNFT[] | null> => {
     try {
       const offset = page * NFT_LIMIT_PER_QUERY;
-      const { data } = await OpenSeaService.service.get('v1/assets', {
+      const { data } = await service.get('v1/assets', {
         params: { owner, offset, limit: NFT_LIMIT_PER_QUERY }
       });
       return data.assets;
@@ -41,11 +44,9 @@ export default abstract class OpenSeaService {
     }
   };
 
-  public static fetchCollections = async (
-    asset_owner: string
-  ): Promise<OpenSeaCollection[] | null> => {
+  const fetchCollections = async (asset_owner: string): Promise<OpenSeaCollection[] | null> => {
     try {
-      const { data } = await OpenSeaService.service.get('v1/collections', {
+      const { data } = await service.get('v1/collections', {
         params: { asset_owner, limit: COLLECTION_LIMIT }
       });
       return data;
@@ -55,9 +56,9 @@ export default abstract class OpenSeaService {
     }
   };
 
-  public static proxyAssets = async (assets: OpenSeaNFT[]): Promise<boolean> => {
+  const proxyAssets = async (assets: OpenSeaNFT[]): Promise<boolean> => {
     try {
-      const result = await OpenSeaService.service.post(
+      const result = await service.post(
         '',
         {
           assetURLs: assets
@@ -74,8 +75,7 @@ export default abstract class OpenSeaService {
     }
   };
 
-  private static service: AxiosInstance = ApiService.generateInstance({
-    baseURL: OPENSEA_API,
-    timeout: 15000
-  });
-}
+  return { fetchAllAssets, fetchAssets, fetchCollections, proxyAssets };
+};
+
+export default OpenSeaService();
