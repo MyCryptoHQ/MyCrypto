fac3864024b14ed6ceaaa04ec6e8016826b1f732
Frederik Bolding <frederik.bolding@gmail.com>
2021-10-06 15:03:22 +0200

    Pending state improvements (#4157)
    * Add state for pending tx without resubmit possibility
    
    * Fix tests

--- src/components/TransactionFlow/TxPendingState.test.tsx
+++ src/components/TransactionFlow/TxPendingState.test.tsx
@@ -4,13 +4,26 @@ import { simpleRender, waitFor } from 'test-utils';
 
 import { Fiats } from '@config';
 import { fNetwork, fTxConfigEIP1559, fTxReceiptEIP1559 } from '@fixtures';
+import { fetchEIP1559PriceEstimates } from '@services/ApiService/Gas';
 import { translateRaw } from '@translations';
 import { ITxStatus, WalletId } from '@types';
+import { bigify } from '@utils';
 
 import { TxPendingState } from './TxPendingState';
 
+jest.mock('@services/ApiService/Gas', () => ({
+  ...jest.requireActual('@services/ApiService/Gas'),
+  fetchGasPriceEstimates: jest.fn().mockResolvedValue({ fast: 50 }),
+  fetchEIP1559PriceEstimates: jest.fn().mockResolvedValue({
+    maxFeePerGas: '200000000000',
+    maxPriorityFeePerGas: '3000000000',
+    baseFee: '100000000000'
+  }),
+  getGasEstimate: jest.fn().mockResolvedValue(21000)
+}));
+
 const defaultProps: ComponentProps<typeof TxPendingState> = {
-  network: fNetwork,
+  network: { ...fNetwork, supportsEIP1559: true },
   baseAssetRate: 250,
   txConfig: fTxConfigEIP1559,
   txReceipt: fTxReceiptEIP1559,
@@ -34,7 +47,9 @@ describe('TxPendingState', () => {
 
   it('shows pending state', async () => {
     const { getByText } = getComponent(defaultProps);
-    expect(getByText(translateRaw('TRANSACTION_PENDING_DESCRIPTION'))).toBeDefined();
+    await waitFor(() =>
+      expect(getByText(translateRaw('TRANSACTION_PENDING_DESCRIPTION'))).toBeDefined()
+    );
     await waitFor(() =>
       expect(defaultProps.setLabel).toHaveBeenCalledWith(translateRaw('TRANSACTION_PENDING_HEADER'))
     );
@@ -43,7 +58,27 @@ describe('TxPendingState', () => {
   it('shows crowded state after 20 sec', async () => {
     const { getByText } = getComponent(defaultProps);
     jest.runOnlyPendingTimers();
-    expect(getByText(translateRaw('TRANSACTION_CROWDED_DESCRIPTION'))).toBeDefined();
+    await waitFor(() =>
+      expect(getByText(translateRaw('TRANSACTION_CROWDED_DESCRIPTION'))).toBeDefined()
+    );
+    await waitFor(() =>
+      expect(defaultProps.setLabel).toHaveBeenCalledWith(translateRaw('TRANSACTION_CROWDED_HEADER'))
+    );
+  });
+
+  it('shows crowded non submit state if gas price has not increased enough', async () => {
+    (fetchEIP1559PriceEstimates as jest.MockedFunction<
+      typeof fetchEIP1559PriceEstimates
+    >).mockResolvedValueOnce({
+      maxFeePerGas: bigify(fTxConfigEIP1559.maxFeePerGas),
+      maxPriorityFeePerGas: bigify(fTxConfigEIP1559.maxPriorityFeePerGas),
+      baseFee: bigify('1000000000')
+    });
+    const { getByText } = getComponent(defaultProps);
+    jest.runOnlyPendingTimers();
+    await waitFor(() =>
+      expect(getByText(translateRaw('TRANSACTION_CROWDED_NO_RESUBMIT_DESCRIPTION'))).toBeDefined()
+    );
     await waitFor(() =>
       expect(defaultProps.setLabel).toHaveBeenCalledWith(translateRaw('TRANSACTION_CROWDED_HEADER'))
     );
@@ -55,7 +90,9 @@ describe('TxPendingState', () => {
       txReceipt: { ...defaultProps.txReceipt, status: ITxStatus.SUCCESS }
     });
     jest.runOnlyPendingTimers();
-    expect(getByText(translateRaw('TRANSACTION_SUCCESS_DESCRIPTION'))).toBeDefined();
+    await waitFor(() =>
+      expect(getByText(translateRaw('TRANSACTION_SUCCESS_DESCRIPTION'))).toBeDefined()
+    );
     await waitFor(() =>
       expect(defaultProps.setLabel).toHaveBeenCalledWith(translateRaw('TRANSACTION_SUCCESS_HEADER'))
     );
@@ -64,7 +101,9 @@ describe('TxPendingState', () => {
   it('shows web3 notice', async () => {
     const { getByText } = getComponent(defaultProps);
     jest.runOnlyPendingTimers();
-    expect(getByText('Please use your Web3 Provider directly', { exact: false })).toBeDefined();
+    await waitFor(() =>
+      expect(getByText('Please use your Web3 Provider directly', { exact: false })).toBeDefined()
+    );
   });
 
   it('shows MetaMask notice', async () => {
@@ -76,7 +115,7 @@ describe('TxPendingState', () => {
       }
     });
     jest.runOnlyPendingTimers();
-    expect(getByText(translateRaw('PENDING_METAMASK_NOTICE'))).toBeDefined();
+    await waitFor(() => expect(getByText(translateRaw('PENDING_METAMASK_NOTICE'))).toBeDefined());
   });
 
   it('shows resend for HW', async () => {
@@ -88,6 +127,6 @@ describe('TxPendingState', () => {
       }
     });
     jest.runOnlyPendingTimers();
-    expect(getByText(translateRaw('RESEND_TRANSACTION'))).toBeDefined();
+    await waitFor(() => expect(getByText(translateRaw('RESEND_TRANSACTION'))).toBeDefined());
   });
 });
