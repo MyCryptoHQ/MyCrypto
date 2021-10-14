4665d0298d3ee71262124ef1dc5a801607d73869
Frederik Bolding <frederik.bolding@gmail.com>
2021-10-14 22:40:09 +0200

    Add hashing of NFT proxy urls (#4164)
    * Add hashing of proxy urls
    
    * Add tests

--- src/utils/fileExtensions.ts
+++ src/utils/fileExtensions.ts
@@ -6,8 +6,13 @@ export enum MediaType {
   Audio
 }
 
+export const getFileExtension = (url: string) => {
+  const split = url.split('.');
+  return split.length > 1 ? split.pop() : undefined;
+};
+
 export const detectMediaType = (url: string) => {
-  const fileExtension = url.split('.').pop();
+  const fileExtension = getFileExtension(url);
   if (fileExtension && SUPPORTED_VIDEO_EXTENSIONS.includes(fileExtension)) {
     return MediaType.Video;
   } else if (fileExtension && SUPPORTED_AUDIO_EXTENSIONS.includes(fileExtension)) {
