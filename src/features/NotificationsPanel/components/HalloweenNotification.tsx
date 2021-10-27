5d3ce9ba24a6f2cd4961a6d59b609dec1e4bfa92
Frederik Bolding <frederik.bolding@gmail.com>
2021-10-27 17:14:51 +0200

    Move some logic to a separate component

--- src/features/NotificationsPanel/components/HalloweenNotification.tsx
+++ src/features/NotificationsPanel/components/HalloweenNotification.tsx
@@ -6,7 +6,24 @@ import { PoapClaimService } from '@services/ApiService/PoapClaim';
 import { claimPromo, getAnalyticsUserID, getPromoPoap, useDispatch, useSelector } from '@store';
 import translate, { translateRaw } from '@translations';
 
-export const HalloweenNotification = () => {
+export const HalloweenNotification = () => (
+  <Box variant="rowAlign">
+    <Box mr="4">
+      <img src={poapImage} />
+    </Box>
+    <Box mr="2">
+      <Heading color="WARNING_ORANGE" fontWeight="bold" fontSize="24px" lineHeight="32px" mt="0">
+        {translateRaw('HALLOWEEN_POAP_NOTIFICATION_HEADER')}
+      </Heading>
+      <Body color="WARNING_ORANGE">{translate('HALLOWEEN_POAP_NOTIFICATION_BODY')}</Body>
+    </Box>
+    <Box variant="rowCenter" style={{ flexGrow: 1 }}>
+      <NotificationContent />
+    </Box>
+  </Box>
+);
+
+const NotificationContent = () => {
   const dispatch = useDispatch();
   const key = 'halloween2021';
   const analyticsId = useSelector(getAnalyticsUserID);
@@ -31,42 +48,31 @@ export const HalloweenNotification = () => {
 
   const isClaimed = promo?.claimed;
 
-  return (
-    <Box variant="rowAlign">
-      <Box mr="4">
-        <img src={poapImage} />
+  if (isError) {
+    return (
+      <Box variant="columnCenter">
+        <Text as="div" color="white" fontSize="2">
+          {translateRaw('HALLOWEEN_POAP_ERROR_HEADER')}
+        </Text>
+        <Text as="div" color="white">
+          {translate('HALLOWEEN_POAP_ERROR')}
+        </Text>
       </Box>
-      <Box mr="2">
-        <Heading color="WARNING_ORANGE" fontWeight="bold" fontSize="24px" lineHeight="32px" mt="0">
-          {translateRaw('HALLOWEEN_POAP_NOTIFICATION_HEADER')}
-        </Heading>
-        <Body color="WARNING_ORANGE">{translate('HALLOWEEN_POAP_NOTIFICATION_BODY')}</Body>
-      </Box>
-      <Box variant="rowCenter" style={{ flexGrow: 1 }}>
-        {isError ? (
-          <Box variant="columnCenter">
-            <Text as="div" color="white" fontSize="2">
-              {translateRaw('HALLOWEEN_POAP_ERROR_HEADER')}
-            </Text>
-            <Text as="div" color="white">
-              {translate('HALLOWEEN_POAP_ERROR')}
-            </Text>
-          </Box>
-        ) : !isClaimed ? (
-          <Button onClick={handleClaim} loading={isClaiming}>
-            {translateRaw('CLAIM_NOW')}
-          </Button>
-        ) : (
-          <Box variant="columnCenter">
-            <Text as="div" color="white" fontSize="2">
-              {translateRaw('POAP_CLAIM_APPROVED')}
-            </Text>
-            <LinkApp mt="2" isExternal href={promo!.claim!}>
-              {translateRaw('ACCESS_POAP_LINK')}
-            </LinkApp>
-          </Box>
-        )}
-      </Box>
+    );
+  }
+
+  return !isClaimed ? (
+    <Button onClick={handleClaim} loading={isClaiming}>
+      {translateRaw('CLAIM_NOW')}
+    </Button>
+  ) : (
+    <Box variant="columnCenter">
+      <Text as="div" color="white" fontSize="2">
+        {translateRaw('POAP_CLAIM_APPROVED')}
+      </Text>
+      <LinkApp mt="2" isExternal href={promo!.claim!}>
+        {translateRaw('ACCESS_POAP_LINK')}
+      </LinkApp>
     </Box>
   );
 };
