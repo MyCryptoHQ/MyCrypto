dd50d5a62412dd241e7d35b3eb39349156ae1066
Frederik Bolding <frederik.bolding@gmail.com>
2021-11-02 12:28:06 +0100

    Add missing icon fill (#4186)

--- src/features/Ens/EnsDashboard.tsx
+++ src/features/Ens/EnsDashboard.tsx
@@ -45,7 +45,7 @@ export default function EnsDashboard() {
             <Box variant="rowAlign">
               <LinkApp href={ROUTE_PATHS.SETTINGS.path} mr={SPACING.BASE} variant="opacityLink">
                 <Box variant="rowAlign">
-                  <Icon type="edit" width="1em" />
+                  <Icon type="edit" width="1em" color="BLUE_SKY" />
                   <Text ml={SPACING.XS} mb={0}>
                     {translateRaw('EDIT')}
                   </Text>
