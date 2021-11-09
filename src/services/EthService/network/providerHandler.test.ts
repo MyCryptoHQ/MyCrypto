8a0030e44306ec6b65cd9e283b2dfa53f6e29384
Frederik Bolding <frederik.bolding@gmail.com>
2021-11-09 01:11:13 +0100

    Add test

--- src/services/EthService/network/providerHandler.test.ts
+++ src/services/EthService/network/providerHandler.test.ts
@@ -48,4 +48,61 @@ describe('ProviderHandler', () => {
       ).resolves.toBeUndefined();
     });
   });
+
+  describe('isValidEIP1271Signature', () => {
+    nock.disableNetConnect();
+
+    afterEach(() => {
+      nock.cleanAll();
+    });
+
+    it('checks whether a signature is valid using EIP1271', async () => {
+      const provider = new ProviderHandler(fNetworks[0]);
+
+      nock(/.*/)
+        .post(/.*/)
+        .reply(200, () => ({
+          id: 1,
+          jsonrpc: '2.0',
+          result: '0x1626ba7e00000000000000000000000000000000000000000000000000000000'
+        }));
+
+      await expect(
+        provider.isValidEIP1271Signature({
+          address: '0x14987eb8d2553B3dfBe7B72AC5e062392E044644',
+          msg: 'testing with argent',
+          sig:
+            '0x0fe68c13c9f7cf89c565bce2f4930bd5aa0adf406c4edf0205a794e10c085ee36293abb9469f243d6438cada470f843c3f5bb1d9778af8465bbb7221da8e28de1b',
+          version: '2'
+        })
+      ).resolves.toBe(true);
+    });
+
+    it('returns false in case of errors', async () => {
+      const provider = new ProviderHandler(fNetworks[0]);
+
+      nock(/.*/)
+        .post(/.*/)
+        .reply(200, () => ({
+          id: 1,
+          jsonrpc: '2.0',
+          error: {
+            code: 3,
+            message: 'execution reverted: TM: Invalid signer',
+            data:
+              '0x08c379a000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000012544d3a20496e76616c6964207369676e65720000000000000000000000000000'
+          }
+        }));
+
+      await expect(
+        provider.isValidEIP1271Signature({
+          address: '0x14987eb8d2553B3dfBe7B72AC5e062392E044644',
+          msg: 'bla',
+          sig:
+            '0x0fe68c13c9f7cf89c565bce2f4930bd5aa0adf406c4edf0205a794e10c085ee36293abb9469f243d6438cada470f843c3f5bb1d9778af8465bbb7221da8e28de1b',
+          version: '2'
+        })
+      ).resolves.toBe(false);
+    });
+  });
 });
