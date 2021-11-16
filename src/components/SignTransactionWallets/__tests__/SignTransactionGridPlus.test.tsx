10a81d6eecbaae9af531ab94fa16ccdf42ed27e7
Frederik Bolding <frederik.bolding@gmail.com>
2021-11-16 13:41:58 +0100

    Add tests

---
+++ src/components/SignTransactionWallets/__tests__/SignTransactionGridPlus.test.tsx
@@ -0,0 +1,54 @@
+import { ComponentProps } from 'react';
+
+import { APP_STATE, mockAppState, simpleRender, waitFor } from 'test-utils';
+
+import SignTransaction from '@features/SendAssets/components/SignTransaction';
+import { fTxConfig } from '@fixtures';
+import { WalletId } from '@types';
+
+import { getHeader } from './helper';
+
+const defaultProps: ComponentProps<typeof SignTransaction> = {
+  txConfig: {
+    ...fTxConfig,
+    senderAccount: { ...fTxConfig.senderAccount, wallet: WalletId.GRIDPLUS }
+  },
+  onComplete: jest.fn()
+};
+
+const getComponent = () => {
+  return simpleRender(<SignTransaction {...defaultProps} />, {
+    initialState: mockAppState({
+      networks: APP_STATE.networks,
+      connections: {
+        wallets: {
+          [WalletId.GRIDPLUS]: {
+            wallet: WalletId.GRIDPLUS,
+            data: { deviceID: 'foo', password: 'bar' }
+          }
+        }
+      }
+    })
+  });
+};
+
+describe('SignTransactionWallets: GridPlus', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('Can handle GridPlus signing', async () => {
+    const { getByText } = getComponent();
+    const selector = getHeader(WalletId.GRIDPLUS);
+    expect(getByText(selector)).toBeInTheDocument();
+
+    await waitFor(() =>
+      expect(defaultProps.onComplete).toHaveBeenCalledWith(
+        '0xf8693284ee6b280082520894909f74ffdc223586d0d30e78016e707b6f5a45e2865af3107a40008029a0827cfaac70de301d4ced4695979dc7684fb014613b4055eb405d7330e2f6af5ea02a75d8e8afdd32f9097e6c9518bbc3e4748c193ecfcd089a95ec6e7d9674604b'
+      )
+    );
+  });
+});
