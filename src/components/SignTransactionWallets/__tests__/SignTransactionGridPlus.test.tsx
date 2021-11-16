2f82367995ecfd7511433afc8b2a630b9050b3e5
Frederik Bolding <frederik.bolding@gmail.com>
2021-11-16 14:56:44 +0100

    Try to fix tests on CI

--- src/components/SignTransactionWallets/__tests__/SignTransactionGridPlus.test.tsx
+++ src/components/SignTransactionWallets/__tests__/SignTransactionGridPlus.test.tsx
@@ -46,10 +46,12 @@ describe('SignTransactionWallets: GridPlus', () => {
     const selector = getHeader(WalletId.GRIDPLUS);
     expect(getByText(selector)).toBeInTheDocument();
 
-    await waitFor(() =>
-      expect(defaultProps.onComplete).toHaveBeenCalledWith(
-        '0xf8693284ee6b280082520894909f74ffdc223586d0d30e78016e707b6f5a45e2865af3107a40008029a0827cfaac70de301d4ced4695979dc7684fb014613b4055eb405d7330e2f6af5ea02a75d8e8afdd32f9097e6c9518bbc3e4748c193ecfcd089a95ec6e7d9674604b'
-      )
+    await waitFor(
+      () =>
+        expect(defaultProps.onComplete).toHaveBeenCalledWith(
+          '0xf8693284ee6b280082520894909f74ffdc223586d0d30e78016e707b6f5a45e2865af3107a40008029a0827cfaac70de301d4ced4695979dc7684fb014613b4055eb405d7330e2f6af5ea02a75d8e8afdd32f9097e6c9518bbc3e4748c193ecfcd089a95ec6e7d9674604b'
+        ),
+      { timeout: 60000 }
     );
   });
 });
