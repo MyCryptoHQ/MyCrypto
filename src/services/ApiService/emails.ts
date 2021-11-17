079aa7c93a848ab43322d1c406294e889fcf9d3c
blurpesec <hahn.michael.f@gmail.com>
2021-11-16 18:04:49 -0800

    removed unnecessary data from mailchimp call

--- src/services/ApiService/emails.ts
+++ src/services/ApiService/emails.ts
@@ -3,9 +3,7 @@ import axios from 'axios';
 const URL = 'https://proxy.mycryptoapi.com/mc';
 
 export function subscribeToMailingList(email: string) {
-  const url = `${URL}`;
-
-  return axios.post(url, {
+  return axios.post(URL, {
     email_address: email,
     status: 'subscribed',
     tags: ['app.mycrypto.com']
